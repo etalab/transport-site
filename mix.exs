70af2134456ed860c5f135a57b9a2601921ac5dc
l-vincent-l <lara.vincent@gmail.com>
2017-09-17 14:00:21 +0200

    Add Datagouvfr API

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Transport.Mixfile do
   def application do
     [
       mod: {Transport.Application, []},
-      extra_applications: [:logger, :runtime_tools, :oauth2]
+      extra_applications: [:logger, :runtime_tools, :oauth2, :httpoison]
     ]
   end
 
@@ -37,6 +37,7 @@ defmodule Transport.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
+      {:httpoison, "~> 0.13"},
       {:cowboy, "~> 1.0"},
       {:earmark, "~> 1.2"},
       {:oauth2, "~> 0.9"},
