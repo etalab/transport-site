ca05c474b4a0ba8940af315ed4debf3414ca0914
l-vincent-l <lara.vincent@gmail.com>
2017-09-19 16:45:28 +0200

    Add font-awesome

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const extractSass       = new ExtractTextPlugin({ filename: '../css/app.css', allChunks: true })
 
 module.exports = {
-    entry: ['./javascripts/app.js', './stylesheets/app.scss'],
+    entry: ['./javascripts/app.js', './stylesheets/app.scss', 'font-awesome/scss/font-awesome.scss'],
     output: {
         path: resolve('../priv/static/js'),
         filename: 'app.js'
@@ -38,7 +38,7 @@ module.exports = {
                 }
             }
         }, {
-            test: /\.scss$/,
+            test: /\.s?css$/,
             use: extractSass.extract({
                 use: [{
                     loader: 'css-loader',
@@ -62,6 +62,19 @@ module.exports = {
                 'url-loader?limit=10000',
                 'img-loader'
             ]
-        }]
+        },
+            // the url-loader uses DataUrls.
+            // the file-loader emits files.
+        {
+            test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            // Limiting the size of the woff fonts breaks font-awesome ONLY for the extract text plugin
+            // loader: "url?limit=10000"
+            use: 'url-loader'
+        },
+        {
+            test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+            use: 'file-loader'
+        }
+        ]
     }
 }
