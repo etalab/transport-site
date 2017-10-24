c4637f589a813e1dd88b286a50c8ed5335556125
l-vincent-l <lara.vincent@gmail.com>
2017-10-24 18:30:01 +0200

    Add celery compatibility

--- lib/transport_web/controllers/user_controller.ex
+++ lib/transport_web/controllers/user_controller.ex
@@ -1,6 +1,7 @@
 defmodule TransportWeb.UserController do
   use TransportWeb, :controller
   alias Transport.Datagouvfr.Client.{Organizations, User, Datasets}
+  alias Transport.DataValidator.Server
   alias TransportWeb.ErrorView
 
   def organizations(%Plug.Conn{} = conn, _) do
@@ -40,7 +41,8 @@ defmodule TransportWeb.UserController do
     conn
     |> Datasets.put(slug, {:add_tag, "GTFS"})
     |> case do
-      {:ok, _} ->
+      {:ok, dataset} ->
+        Server.validate_data(List.first(dataset["resources"])["url"])
         conn
         |> render("add_badge_dataset.html")
       {:error, _} ->
