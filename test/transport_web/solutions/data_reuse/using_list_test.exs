058f21ced6f0df90113a0e8db59cd94064944963
l-vincent-l <lara.vincent@gmail.com>
2018-02-09 15:26:25 +0100

    Fix UsingListTest

--- test/transport_web/solutions/data_reuse/using_list_test.exs
+++ test/transport_web/solutions/data_reuse/using_list_test.exs
@@ -20,10 +20,10 @@ defmodule TransportWeb.Solution.DataReuse.UsingListTest do
     %_{} = ReusableData.create_dataset %{
       download_uri: "https://link.to/angers.zip",
       license: "odc-odbl",
-      title: "Angers GTFS",
+      title: "Horaires et arrêts du réseau IRIGO - format GTFS",
       anomalies: [],
       coordinates: [-0.5630548425091684,47.47654241641714],
-      slug: "angers-gtfs",
+      slug: "horaires-et-arrets-du-reseau-irigo-format-gtfs",
       validations: %{"errors" => [], "warnings" => [], "notices" => []}
     }
 
@@ -43,8 +43,8 @@ defmodule TransportWeb.Solution.DataReuse.UsingListTest do
     assert visible_page_text() =~ "Jeux de données valides disponibles"
 
     # I can click on a dataset and see its details
-    click({:link_text, "Angers GTFS"})
-    assert visible_page_text() =~ "Angers GTFS"
+    click({:link_text, "Horaires et arrêts du réseau IRIGO - format GTFS"})
+    assert visible_page_text() =~ "IRIGO"
 
     # I can see or read somewhere that the dataset is valid
     assert visible_page_text() =~ "Valide"
