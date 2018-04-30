44df7c5e7149761fea39fdd286fe42e10adfe249
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2018-04-30 21:41:23 +0200

    Validate datasets in parallel

---
+++ test/transport/data_validation/events/dataset_created_test.exs
@@ -0,0 +1,6 @@
+defmodule Transport.DataValidation.Events.DatasetCreatedTest do
+  use ExUnit.Case, async: true
+  alias Transport.DataValidation.Events.DatasetCreated
+
+  doctest DatasetCreated
+end
