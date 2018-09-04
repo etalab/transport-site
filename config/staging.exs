1d7dfcb5f09e44600752b4f2e9c6be9a5018ce93
Tristram Gräbener <tristramg@gmail.com>
2018-09-04 13:42:39 +0200

    Remove heroku configurations

--- config/staging.exs
+++ config/staging.exs
@@ -3,7 +3,7 @@ alias Transport.Datagouvfr.Authentication
 
 config :transport, TransportWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "transport-site-staging.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "http://prochainement-transport.cleverapps.io", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
