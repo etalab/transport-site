4d1b46046ab858852cfce589d2d166c19345ebb1
Vincent <lara.vincent@gmail.com>
2018-10-17 17:13:34 +0200

    Fix tests

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,12 +1,12 @@
-const { resolve }          = require('path')
-const webpack              = require('webpack')
-const CopyWebpackPlugin    = require('copy-webpack-plugin')
-const ExtractTextPlugin    = require('extract-text-webpack-plugin')
-const extractImages        = new CopyWebpackPlugin([{ from: 'images', to: '../images' }])
-const extractSass          = new ExtractTextPlugin({ filename: '../css/app.css', allChunks: true })
-const fetchPolyfill        = new webpack.ProvidePlugin({ fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch' })
-const promisePolyfill      = new webpack.ProvidePlugin({ Promise: 'core-js/es6/promise' })
-const processEnv           = new webpack.DefinePlugin({ 'process.env': { 'DATAGOUVFR_SITE': JSON.stringify(process.env.DATAGOUVFR_SITE) } })
+const { resolve } = require('path')
+const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const extractImages = new CopyWebpackPlugin([{ from: 'images', to: '../images' }])
+const extractSass = new ExtractTextPlugin({ filename: '../css/app.css', allChunks: true })
+const fetchPolyfill = new webpack.ProvidePlugin({ fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch' })
+const promisePolyfill = new webpack.ProvidePlugin({ Promise: 'core-js/es6/promise' })
+const processEnv = new webpack.DefinePlugin({ 'process.env': { 'DATAGOUVFR_SITE': JSON.stringify(process.env.DATAGOUVFR_SITE) } })
 
 module.exports = {
     entry: [
@@ -37,74 +37,74 @@ module.exports = {
     ],
     devtool: 'source-map',
     module: {
-        rules: [{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
-        {
-            test: /\.(js|scss)$/,
-            exclude: [/node_modules/],
-            enforce: 'pre',
-            loader: 'import-glob-loader'
-        }, {
-            test: /\.tag$/,
-            exclude: /node_modules/,
-            enforce: 'pre',
-            loader: 'riot-tag-loader',
-            query: {
-                type: 'es6'
-            }
-        }, {
-            test: /\.tag$/,
-            exclude: [/node_modules/],
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015-riot']
+        rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(js|scss)$/,
+                exclude: [/node_modules/],
+                enforce: 'pre',
+                loader: 'import-glob-loader'
+            }, {
+                test: /\.tag$/,
+                exclude: /node_modules/,
+                enforce: 'pre',
+                loader: 'riot-tag-loader',
+                query: {
+                    type: 'es6'
                 }
-            }
-        }, {
-            test: /\.js$/,
-            exclude: [/node_modules/],
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['es2015']
-                }
-            }
-        }, {
-            test: /\.scss$/,
-            exclude: [/node_modules/],
-            use: extractSass.extract({
-                use: [{
-                    loader: 'css-loader',
+            }, {
+                test: /\.tag$/,
+                exclude: [/node_modules/],
+                use: {
+                    loader: 'babel-loader',
                     options: {
-                        sourceMap: true
+                        presets: ['es2015-riot']
                     }
-                }, {
-                    loader: 'sass-loader',
+                }
+            }, {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                use: {
+                    loader: 'babel-loader',
                     options: {
-                        sourceMap: true,
-                        outputStyle: 'compact'
+                        presets: ['es2015']
+                    }
+                }
+            }, {
+                test: /\.scss$/,
+                exclude: [/node_modules/],
+                use: extractSass.extract({
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            outputStyle: 'compact'
+                        }
+                    }]
+                })
+            }, {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                exclude: [/font-awesome/],
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '../images/'
                     }
                 }]
-            })
-        }, {
-            test: /\.(jpe?g|png|gif|svg)$/,
-            exclude: [/font-awesome/],
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: '../images/'
-                }
+            }, {
+                test: /\.(eot|ttf|otf|woff|woff2|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '../fonts/'
+                    }
+                }]
             }]
-        }, {
-            test: /\.(eot|ttf|otf|woff|woff2|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [{
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: '../fonts/'
-                }
-            }]
-        }]
     }
 }
