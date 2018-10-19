3f4949898891a33260b225ff92c502bfaacbe792
Tristram Gräbener <tristramg@gmail.com>
2018-10-19 12:38:54 +0200

    Remove old way to validate data

--- test/transport/data_validation/commands/validate_dataset_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule Transport.DataValidation.Commands.ValidateDatasetTest do
-  use ExUnit.Case, async: true
-  alias Transport.DataValidation.Aggregates.Dataset
-  alias Transport.DataValidation.Commands.ValidateDataset
-
-  doctest ValidateDataset
-end
--- test/transport/data_validation/events/dataset_updated_test.exs
+++
@@ -1,6 +0,0 @@
-defmodule Transport.DataValidation.Events.DatasetUpdatedTest do
-  use ExUnit.Case, async: true
-  alias Transport.DataValidation.Events.DatasetUpdated
-
-  doctest DatasetUpdated
-end
