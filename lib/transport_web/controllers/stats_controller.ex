4ac767425ba6d44d31e385b09f7b690415500152
Vincent <lara.vincent@gmail.com>
2018-10-30 16:04:12 +0100

    Add stats on stats!

--- lib/transport_web/controllers/stats_controller.ex
+++ lib/transport_web/controllers/stats_controller.ex
@@ -1,7 +1,33 @@
 defmodule TransportWeb.StatsController do
+  alias Transport.ReusableData
   use TransportWeb, :controller
 
+  defp has_dataset?(aom), do: !Enum.empty?(Map.get(aom, "datasets", []))
+  defp is_completed?(region), do: Map.get(Map.get(region, "properties", %{}), "completed", false)
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    aoms = Mongo.aggregate(
+      :mongo,
+      "aoms",
+      [
+        ReusableData.Dataset.aoms_lookup,
+      ],
+      pool: DBConnection.Poolboy
+    )
+    regions = Mongo.aggregate(
+      :mongo,
+      "regions",
+      [
+        ReusableData.Dataset.regions_lookup,
+      ],
+      pool: DBConnection.Poolboy
+    )
+    render(conn, "index.html",
+     nb_datasets: Enum.count(ReusableData.list_datasets),
+     nb_aoms: Enum.count(aoms),
+     nb_aoms_with_data: aoms |> Enum.filter(&has_dataset?/1) |> Enum.count,
+     nb_regions: Enum.count(regions),
+     nb_regions_completed: regions |> Enum.filter(&is_completed?/1) |> Enum.count
+    )
   end
 end
