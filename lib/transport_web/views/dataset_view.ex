ca5a33946228af4b6938e53efd99d3f311733c9f
Vincent <lara.vincent@gmail.com>
2018-12-03 16:31:06 +0100

    Pas de pagination si une seule page

--- lib/transport_web/views/dataset_view.ex
+++ lib/transport_web/views/dataset_view.ex
@@ -1,6 +1,6 @@
 defmodule TransportWeb.DatasetView do
   use TransportWeb, :view
-  import Scrivener.HTML
+  import TransportWeb.PaginationHelpers
 
   def render_sidebar_from_type(dataset), do: render_panel_from_type(dataset, "sidebar")
   def render_description_from_type(dataset), do: render_panel_from_type(dataset, "description")
