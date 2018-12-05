72810dbcd2e6437f970daf480794ead146575e6a
Vincent <lara.vincent@gmail.com>
2018-12-05 13:01:56 +0100

    Show debug errors in dev

--- config/dev.exs
+++ config/dev.exs
@@ -8,8 +8,7 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :transport, TransportWeb.Endpoint,
   http: [port: 5000],
-  debug_errors: false,
-  catch_errors: true,
+  debug_errors: true,
   code_reloader: true,
   check_origin: false,
   watchers: [
