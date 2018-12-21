d8f75c5f8a33ffd705334b8b7e417e2cca10bc6b
Vincent <lara.vincent@gmail.com>
2018-12-21 10:00:50 +0100

    Ajout argument --all à la commande

--- lib/mix/tasks/transport/simple_validation.ex
+++ lib/mix/tasks/transport/simple_validation.ex
@@ -5,13 +5,15 @@ defmodule Mix.Tasks.Transport.SimpleValidation do
 
   use Mix.Task
   alias Transport.{Repo, Resource}
+  import Ecto.Query
 
-  def run(_) do
+  def run(args) do
     Mix.Task.run("app.start", [])
 
     Resource
+    |> preload(:dataset)
     |> Repo.all()
-    |> Enum.filter(&Resource.needs_validation/1)
+    |> Enum.filter(&(List.first(args) == "--all" or Resource.needs_validation(&1)))
     |> Enum.each(&Resource.validate_and_save/1)
   end
 end
