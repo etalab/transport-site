56e7b4728773955650502fc3863985c5c534b888
Vincent <lara.vincent@gmail.com>
2018-12-27 10:24:31 +0100

    Ajout d'un test avec une région vide

--- test/transport/dataset_test.exs
+++ test/transport/dataset_test.exs
@@ -1,6 +1,19 @@
 defmodule Transport.DatasetTest do
   use ExUnit.Case, async: true
+  use TransportWeb.DatabaseCase, cleanup: [:datasets]
   alias Transport.Dataset
 
   doctest Dataset
+
+  @valid_changeset %{
+        datagouv_id: "5bc493d08b4c416c84a69500",
+        slug: "offre-de-transport-du-reseau-de-laval-agglomeration-gtfs",
+        resources: [%{validation: %{}}]
+  }
+
+  test "Region can be blank" do
+    changeset = Dataset.changeset(%Dataset{}, @valid_changeset)
+    assert changeset.valid?
+  end
+
 end
