3c25d7f0e25617c644f042cd918ce170997c69ee
Tristram Gräbener <tristramg@gmail.com>
2019-01-21 17:43:00 +0100

    handle multiple related objects

--- lib/transport_web/views/resource_view.ex
+++ lib/transport_web/views/resource_view.ex
@@ -1,4 +1,11 @@
 defmodule TransportWeb.ResourceView do
   use TransportWeb, :view
   import TransportWeb.PaginationHelpers
+
+  def format_related_objects(nil), do: ""
+  def format_related_objects(related_objects) do
+    related_objects
+    |> Enum.map(fn %{"id" => id, "name" => name} -> "#{name} (#{id})" end)
+    |> Enum.join(", ")
+  end
 end
