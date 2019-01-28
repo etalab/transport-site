8ad58beabc508b0376cea0d7667816c2b6e55292
Vincent <lara.vincent@gmail.com>
2019-01-28 14:13:58 +0100

    Fix /stats

--- lib/transport_web/controllers/dataset_controller.ex
+++ lib/transport_web/controllers/dataset_controller.ex
@@ -53,7 +53,6 @@ defmodule TransportWeb.DatasetController do
     select = [:id, :description, :licence, :logo, :spatial, :title, :slug]
 
     params
-    |> IO.inspect
     |> Dataset.list_datasets(select)
     |> Repo.paginate(page: config.page_number)
   end
