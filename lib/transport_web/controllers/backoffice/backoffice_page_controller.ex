5d164c7ccc65c8b057a91fb79a55f82acd38bfbf
Tristram Gräbener <tristramg@gmail.com>
2019-01-30 17:02:02 +0100

    Backoffice: factorize a bit the assigns

--- lib/transport_web/controllers/backoffice/backoffice_page_controller.ex
+++ lib/transport_web/controllers/backoffice/backoffice_page_controller.ex
@@ -10,33 +10,21 @@ defmodule TransportWeb.Backoffice.BackofficePageController do
   ## Controller functions
 
   def index(%Plug.Conn{} = conn, %{"q" => q} = params) when q != "" do
+    conn = assign(conn, :q, q)
     config = make_pagination_config(params)
-    datasets =
-      q
-      |> Dataset.search_datasets
-      |> preload([:region, :aom, :resources])
-      |> Repo.paginate(page: config.page_number)
-
-    conn
-    |> assign(:regions, region_names())
-    |> assign(:datasets, datasets)
-    |> assign(:q, q)
-    |> assign(:dataset_types, dataset_types())
-    |> render("index.html")
+    q
+    |> Dataset.search_datasets
+    |> preload([:region, :aom, :resources])
+    |> Repo.paginate(page: config.page_number)
+    |> render_index(conn)
   end
 
   def index(%Plug.Conn{} = conn, params) do
     config = make_pagination_config(params)
-    datasets =
-      Dataset
-      |> preload([:region, :aom, :resources])
-      |> Repo.paginate(page: config.page_number)
-
-    conn
-    |> assign(:regions, region_names())
-    |> assign(:datasets, datasets)
-    |> assign(:dataset_types, dataset_types())
-    |> render("index.html")
+    Dataset
+    |> preload([:region, :aom, :resources])
+    |> Repo.paginate(page: config.page_number)
+    |> render_index(conn)
   end
 
   ## Private functions
@@ -51,5 +39,13 @@ defmodule TransportWeb.Backoffice.BackofficePageController do
     @dataset_types
     |> Enum.map(fn t -> {Gettext.dgettext(TransportWeb.Gettext, "backoffice", t), t} end)
     |> Enum.to_list
+  end
+
+  defp render_index(datasets, conn) do
+    conn
+    |> assign(:regions, region_names())
+    |> assign(:datasets, datasets)
+    |> assign(:dataset_types, dataset_types())
+    |> render("index.html")
   end
 end
