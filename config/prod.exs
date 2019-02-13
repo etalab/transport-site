2d2008140b0bfaf08f59e9d3c1efaa8b5468f2fd
Tristram Gräbener <tristramg@gmail.com>
2019-02-13 16:38:10 +0100

    Add sentry support

--- config/prod.exs
+++ config/prod.exs
@@ -20,3 +20,13 @@ config :transport, Transport.Repo,
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :sentry,
+  dsn: System.get_env("SENTRY_DSN"),
+  environment_name: :prod,
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!,
+  tags: %{
+    env: "production"
+  },
+  included_environments: [:prod]
