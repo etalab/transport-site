a63377eb65af5b1c550c137ec35df28994933f5e
Tristram Gräbener <tristramg@gmail.com>
2019-02-19 16:07:32 +0100

    Add a helper module to extract the last part of an url

---
+++ lib/transport/helpers.ex
@@ -0,0 +1,27 @@
+
+defmodule Transport.Helpers do
+  @moduledoc """
+  Helper functions that are used accross the whole project
+  """
+
+
+  @doc """
+  Gets the filename part of an url
+
+  ## Examples
+
+  iex> Helpers.filename_from_url("https://example.com/gtfs.zip")
+  "gtfs.zip"
+
+  iex> Helpers.filename_from_url("https://example.com/foo/bar/baz/bobette/")
+  "bobette"
+  """
+  def filename_from_url(url) do
+    url
+    |> URI.parse()
+    |> Map.get(:path)
+    |> String.trim_trailing("/")
+    |> String.split("/")
+    |> List.last()
+  end
+end
