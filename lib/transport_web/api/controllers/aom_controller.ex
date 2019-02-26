774092181f62e76db055892ca22ee1bf8f4cf4ca
Vincent <lara.vincent@gmail.com>
2019-02-26 12:01:03 +0100

    Add openapi specs

--- lib/transport_web/api/controllers/aom_controller.ex
+++ lib/transport_web/api/controllers/aom_controller.ex
@@ -4,8 +4,29 @@ defmodule TransportWeb.API.AomController do
   alias Transport.{AOM, Commune, Repo}
   import Ecto.Query
   alias Geo.JSON
+  alias OpenApiSpex.Operation
+  alias TransportWeb.API.Schemas.{AOMResponse, GeoJSONResponse}
 
   @aom_fields [:nom, :insee_commune_principale, :departement, :forme_juridique, :siren]
+
+  @spec open_api_operation(any) :: Operation.t
+  def open_api_operation(action), do: apply(__MODULE__, :"#{action}_operation", [])
+
+  def by_coordinates_operation do
+    %Operation{
+      tags: ["aom"],
+      summary: "Show AOM by coordinates",
+      description: "Show covered regions",
+      operationId: "API.AOMController.by_coordinates",
+      parameters: [
+        Operation.parameter(:lon, :query, :float, "Longitude"),
+        Operation.parameter(:lat, :query, :float, "Latitude")
+      ],
+      responses: %{
+        200 => Operation.response("AOM", "application/json", AOMResponse)
+      }
+    }
+  end
 
   def by_coordinates(conn, %{"lon" => lon, "lat" => lat}) do
     with {longitude, _} <- Float.parse(lon),
@@ -17,6 +38,21 @@ defmodule TransportWeb.API.AomController do
     end
   end
   def by_coordinates(conn, _), do: invalid_parameters(conn)
+
+  def by_insee_operation do
+    %Operation{
+      tags: ["insee"],
+      summary: "Show AOM by INSEE",
+      description: "Show covered regions",
+      operationId: "API.AOMController.by_coordinates",
+      parameters: [
+        Operation.parameter(:insee, :query, :string, "INSEE")
+      ],
+      responses: %{
+        200 => Operation.response("AOM", "application/json", AOMResponse)
+      }
+    }
+  end
 
   def by_insee(conn, %{"insee" => insee}) do
     query = from c in Commune,
@@ -44,13 +80,26 @@ defmodule TransportWeb.API.AomController do
   def query_by_coordinates(conn, lon, lat) do
     query = from a in AOM,
               select: map(a, @aom_fields),
-              where: fragment("st_contains(geom, st_point(?, ?))", ^lon, ^lat)
+              where: fragment("st_contains(geom, st_setsrid(st_point(?, ?), 4326))", ^lon, ^lat)
 
     render(conn, data: Repo.one(query))
   end
 
   def invalid_parameters(conn) do
     render(conn, data: %{"error" => "The parameters lon and lat are mandatory and must be floats"})
+  end
+
+  def geojson_operation do
+    %Operation{
+      tags: ["geojson"],
+      summary: "Show geojson of AOM",
+      description: "Show covered regions",
+      operationId: "API.AOMController.by_coordinates",
+      parameters: [],
+      responses: %{
+        200 => Operation.response("GeoJSON", "application/json", GeoJSONResponse)
+      }
+    }
   end
 
   def geojson(conn, _params) do
