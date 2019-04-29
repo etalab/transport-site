bdd155e8a33e45614c3e1c3d417a55226b873a29
Tristram Gräbener <tristramg@gmail.com>
2019-04-29 14:42:50 +0200

    Specialize a validation issue

--- lib/transport_web/views/resource_view.ex
+++ lib/transport_web/views/resource_view.ex
@@ -12,4 +12,11 @@ defmodule TransportWeb.ResourceView do
 
   def issue_type([]), do: nil
   def issue_type([h|_]), do: h["issue_type"]
+
+  def template(issues) do
+    case issue_type(issues.entries) do
+      "DuplicateStops" -> "_duplicate_stops_issue.html"
+      _ -> "_generic_issue.html"
+    end
+  end
 end
