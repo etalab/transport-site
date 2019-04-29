fa7484899effeed3a720c4096e592dd6aed124f0
Tristram Gräbener <tristramg@gmail.com>
2019-04-29 18:05:39 +0200

    Validation: make credo happy

--- lib/transport_web/views/resource_view.ex
+++ lib/transport_web/views/resource_view.ex
@@ -13,18 +13,18 @@ defmodule TransportWeb.ResourceView do
   def issue_type([h|_]), do: h["issue_type"]
 
   def template(issues) do
-    case issue_type(issues.entries) do
-      "DuplicateStops" -> "_duplicate_stops_issue.html"
-      "ExtraFile" -> "_extra_file_issue.html"
-      "MissingFile" -> "_missing_file_issue.html"
-      "NullDuration" -> "_speed_issue.html"
-      "ExcessiveSpeed" -> "_speed_issue.html"
-      "NegativeTravelTime" -> "_speed_issue.html"
-      "Slow" -> "_speed_issue.html"
-      "UnusedStop" -> "_unused_stop_issue.html"
-      "InvalidCoordinates" -> "_coordinates_issue.html"
-      "MissingCoordinates" -> "_coordinates_issue.html"
-      _ -> "_generic_issue.html"
-    end
+    %{
+      "DuplicateStops" => "_duplicate_stops_issue.html",
+      "ExtraFile" => "_extra_file_issue.html",
+      "MissingFile" => "_missing_file_issue.html",
+      "NullDuration" => "_speed_issue.html",
+      "ExcessiveSpeed" => "_speed_issue.html",
+      "NegativeTravelTime" => "_speed_issue.html",
+      "Slow" => "_speed_issue.html",
+      "UnusedStop" => "_unused_stop_issue.html",
+      "InvalidCoordinates" => "_coordinates_issue.html",
+      "MissingCoordinates" => "_coordinates_issue.html",
+    }
+    |> Map.get(issue_type(issues.entries), "_generic_issue.html")
   end
 end
