2d51d78171a2838f6d015d8e012dd4860e65226c
Vincent <lara.vincent@gmail.com>
2019-05-27 10:41:14 +0200

    Fix tasks

--- mix.exs
+++ mix.exs
@@ -5,11 +5,19 @@ defmodule Transport.MixProject do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases(),
     ]
   end
 
   defp deps do
     []
+  end
+
+  defp aliases do
+    [
+     test: ["ecto.create --quiet", "ecto.migrate", "test"],
+     "phx.migrate_phx.server": ["ecto.migrate", "phx.server"]
+    ]
   end
 end
