99979b2905dd91e0929bf146cd886b98e3738c31
Vincent <lara.vincent@gmail.com>
2019-09-27 11:10:06 +0200

    Add style to validate button

--- apps/transport/lib/transport_web/live/validate_dataset_view.ex
+++ apps/transport/lib/transport_web/live/validate_dataset_view.ex
@@ -4,51 +4,40 @@ defmodule TransportWeb.Live.ValidateDatasetView do
     import TransportWeb.Gettext, only: [dgettext: 2]
     require Logger
 
-    def render(%{step: :validated} = assigns) do
-        Logger.debug "validated"
-        ~L"""
-        <%= dgettext("backoffice_dataset", "Validated") %>
-        """
-    end
-    def render(%{step: :error} = assigns) do
-        ~L"""
-        <%= dgettext("backoffice_dataset", "Error") %>
-        """
-    end
-    def render(%{step: :validating} = assigns) do
-        ~L"""
-        <button class="button" disabled>
-        <%= dgettext("backoffice_dataset", "Validating") %>
-        </button>
-        """
-    end
+    @button_disabled [:validated, :error, :validating]
+
     def render(assigns) do
         ~L"""
-        <button class="button" phx-click="validate_dataset">
-        <%= dgettext("backoffice_dataset", "Validate") %>
+        <button class="<%= @button_class %>" phx-click="validate_dataset" <%= @button_disabled%>>
+        <%= @button_text %>
         </button>
         """
     end
 
     def mount(%{dataset_id: dataset_id, locale: locale}, socket) do
         Gettext.put_locale(locale)
-        {:ok, assign(socket, dataset_id: dataset_id)}
+        new_socket =
+          socket
+          |> assign(dataset_id: dataset_id)
+          |> assign_step(:first)
+
+        {:ok, new_socket}
     end
 
     def handle_event("validate_dataset", _value, socket) do
         send(self(), {:validate, socket.assigns.dataset_id})
 
-        {:noreply, assign(socket, step: :validating)}
+        {:noreply, assign_step(socket, :validating)}
     end
 
     def handle_info({:validate, dataset_id}, socket) do
         new_socket =
           case Dataset.validate(dataset_id) do
             {:ok, _} ->
-                assign(socket, step: :validated)
+                assign_step(socket, :validated)
             {:error, error} ->
                 Logger.error error
-                assign(socket, step: :error)
+                assign_step(socket, :error)
           end
 
         Process.send_after(self(), :display_form, 30_000)
@@ -56,6 +45,41 @@ defmodule TransportWeb.Live.ValidateDatasetView do
     end
 
     def handle_info(:display_form, socket) do
-        {:noreply, assign(socket, step: :display_form)}
+        {:noreply, assign_step(socket, :display_form)}
+    end
+
+    defp assign_step(socket, step) do
+        assign(
+            socket,
+            [
+                button_text: button_texts(step),
+                button_class: button_classes(step),
+                button_disabled: if step in @button_disabled do "disabled" else "" end
+            ]
+        )
+    end
+
+    defp button_texts(step) do
+        Map.get(
+            %{
+                validated: dgettext("backoffice_dataset", "Validated"),
+                error: dgettext("backoffice_dataset", "Error"),
+                validating: dgettext("backoffice_dataset", "Validating"),
+            },
+            step,
+            dgettext("backoffice_dataset", "Validate")
+        )
+    end
+
+    defp button_classes(step) do
+        Map.get(
+            %{
+                validated: "button success",
+                error: "button secondary",
+                validating: "button-outlined secondary",
+            },
+            step,
+            "button"
+        )
     end
 end
