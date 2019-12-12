5e4c7dcf2d2929222f7838caca38199df4026fe5
antoine-de <antoine.desbordes@gmail.com>
2019-12-12 16:46:12 +0100

    simplify restore_db.sh
    now we don't need to be logged as postgres, and the DB is not deleted
    and recreated

--- restore_db.sh
+++ restore_db.sh
@@ -1,35 +1,24 @@
 #!/bin/bash
 
-#Usage ./restore_db.sh db_name user_name absolute_path_to_backup
-#Use it as a postgres user
+# Usage ./restore_db.sh <db_name> <host> <user_name> <password> <absolute_path_to_backup>
+# or the ./restore_db.sh <absolute_path_to_backup> if the default options are ok for you
+# the latest production backup can be fetched on "transport-site-postgresql" in clevercloud
 
 if test "$#" -eq 1; then
     DB_NAME="transport_repo"
     USER_NAME="postgres"
     BACKUP_PATH=$1
-    OPTIONS="-h database -U postgres -d postgres"
-    export PGPASSWORD="example"
-elif test "$#" -eq 3; then
+    HOST="localhost"
+    export PGPASSWORD="postgres"
+elif test "$#" -eq 5; then
     DB_NAME=$1
-    USER_NAME=$2
-    BACKUP_PATH=$3
-    OPTIONS=""
+    HOST=$2
+    USER_NAME=$3
+    export PGPASSWORD=$4
+    BACKUP_PATH=$5
 else
-    echo "Usage : Usage ./restore_db.sh db_name user_name absolute_path_to_backup local_user"
+    echo "Usage : Usage ./restore_db.sh <db_name> <host> <user_name> <password> <absolute_path_to_backup>"
     exit 1
 fi
 
-
-psql $OPTIONS -c "DROP DATABASE IF EXISTS $DB_NAME"
-
-psql $OPTIONS -c "CREATE DATABASE $DB_NAME"
-
-pg_restore $OPTIONS -d $DB_NAME --format=c --no-owner $BACKUP_PATH
-
-psql $OPTIONS -c "ALTER DATABASE $DB_NAME OWNER TO $USER_NAME"
-
-for tbl in `psql $OPTIONS -qAt -c "select tablename from pg_tables where schemaname = 'public';" -d $DB_NAME` ; do  psql $OPTIONS -c "alter table \"$tbl\" owner to $USER_NAME" -d $DB_NAME ; done
-
-for tbl in `psql $OPTIONS -qAt -c "select sequence_name from information_schema.sequences where sequence_schema = 'public';" -d $DB_NAME` ; do  psql $OPTIONS -c "alter sequence \"$tbl\" owner to $USER_NAME" -d $DB_NAME ; done
-
-for tbl in `psql $OPTIONS -qAt -c "select table_name from information_schema.views where table_schema = 'public';" -d $DB_NAME` ; do  psql $OPTIONS -c "alter view \"$tbl\" owner to $USER_NAME" -d $DB_NAME ; done
+pg_restore -h $HOST -U $USER_NAME -d $DB_NAME --format=c --no-owner --clean $BACKUP_PATH
