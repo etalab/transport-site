de90d71c6b59db7a5b465332f02990b4aafa20e7
Tristram Gräbener <tristramg@gmail.com>
2019-12-18 14:13:41 +0100

    JS: fix new linter suggestions

--- apps/transport/client/javascripts/app.js
+++ apps/transport/client/javascripts/app.js
@@ -1,5 +1,5 @@
 import { Socket } from 'phoenix'
 import LiveSocket from 'phoenix_live_view'
 
-let liveSocket = new LiveSocket('/live', Socket)
+const liveSocket = new LiveSocket('/live', Socket)
 liveSocket.connect()
--- apps/transport/client/webpack.config.js
+++ apps/transport/client/webpack.config.js
@@ -7,14 +7,14 @@ const extractImages = new CopyWebpackPlugin([{ from: 'images', to: '../images' }
 const extractSass = new MiniCssExtractPlugin({ filename: '../css/app.css', allChunks: true })
 const fetchPolyfill = new webpack.ProvidePlugin({ fetch: 'exports-loader?self.fetch!whatwg-fetch/dist/fetch.umd' })
 const promisePolyfill = new webpack.ProvidePlugin({ Promise: 'core-js/es/promise' })
-const processEnv = new webpack.DefinePlugin({ 'process.env': { 'DATAGOUVFR_SITE': JSON.stringify(process.env.DATAGOUVFR_SITE) } })
+const processEnv = new webpack.DefinePlugin({ 'process.env': { DATAGOUVFR_SITE: JSON.stringify(process.env.DATAGOUVFR_SITE) } })
 
 module.exports = {
     mode: devMode ? 'development' : 'production',
     entry: {
-        'app': './javascripts/app.js',
-        'map': './javascripts/map.js',
-        'scss': './stylesheets/app.scss'
+        app: './javascripts/app.js',
+        map: './javascripts/map.js',
+        scss: './stylesheets/app.scss'
     },
     output: {
         path: resolve('../priv/static/js'),
