08dd84a14bbb2f453972ac772125ff08079c73ed
antoine-de <antoine.desbordes@gmail.com>
2020-01-23 18:40:59 +0100

    add tests

--- apps/transport/test/transport_web/controllers/backoffice_controller_test.exs
+++ apps/transport/test/transport_web/controllers/backoffice_controller_test.exs
@@ -15,6 +15,14 @@ defmodule TransportWeb.BackofficeControllerTest do
     "action" => "new"
   }
 
+  @dataset_with_zones_url "https://next.data.gouv.fr/fr/datasets/test-jeux-de-donnees-associe-a-plusieurs-villes/"
+  @dataset_with_zones %{
+    "url" => @dataset_with_zones_url,
+    "spatial" => "Grenoble",
+    "type" => "public-transit",
+    "action" => "new"
+  }
+
   @tag :external
   test "Add a dataset with a region and AOM", %{conn: conn} do
     conn =
@@ -102,6 +110,89 @@ defmodule TransportWeb.BackofficeControllerTest do
     assert redirected_to(conn, 302) == backoffice_page_path(conn, :index)
     assert Resource |> Repo.all() |> length() == 1
     assert get_flash(conn, :info) =~ "ajouté"
+  end
+
+  @tag :external
+  test "Add a dataset linked to cities", %{conn: conn} do
+    conn =
+      use_cassette "session/create-2" do
+        conn
+        |> init_test_session(redirect_path: "/datasets")
+        |> get(session_path(conn, :create, %{"code" => "secret"}))
+      end
+
+    dataset =
+      @dataset_with_zones_url
+      |> Map.put("region_id", nil)
+      |> Map.put("insee_commune_principale", nil)
+      |> Map.put("associated_territory_name", "pouet")
+      |> Map.put("use_datagouv_zones", "true")
+
+    conn =
+      use_cassette "dataset/dataset-with-multiple-cities.json" do
+        post(conn, backoffice_dataset_path(conn, :post), dataset)
+      end
+
+    assert redirected_to(conn, 302) == backoffice_page_path(conn, :index)
+    assert Resource |> Repo.all() |> length() == 1
+    assert get_flash(conn, :info) =~ "ajouté"
+  end
+
+  test "Add a dataset linked to cities and to the country", %{conn: conn} do
+    conn =
+      use_cassette "session/create-2" do
+        conn
+        |> init_test_session(redirect_path: "/datasets")
+        |> get(session_path(conn, :create, %{"code" => "secret"}))
+      end
+
+    dataset =
+      @dataset_with_zones
+      |> Map.put("region_id", nil)
+      |> Map.put("insee_commune_principale", nil)
+      |> Map.put("associated_territory_name", "pouet")
+      |> Map.put("use_datagouv_zones", "true")
+      |> Map.put("national_dataset", "true")
+
+    conn =
+      use_cassette "dataset/dataset-with-multiple-cities.json" do
+        post(conn, backoffice_dataset_path(conn, :post), dataset)
+      end
+
+    # It should not be possible to link a dataset to either
+    # a list of cities and to the whole country
+    assert redirected_to(conn, 302) == backoffice_page_path(conn, :index)
+    assert Resource |> Repo.all() |> length() == 0
+    flash = get_flash(conn, :error)
+
+    assert flash =~
+             "Vous devez remplir soit une région soit une AOM soit utiliser les zones data.gouv"
+  end
+
+  test "Add a dataset linked to a region and to the country", %{conn: conn} do
+    conn =
+      use_cassette "session/create-2" do
+        conn
+        |> init_test_session(redirect_path: "/datasets")
+        |> get(session_path(conn, :create, %{"code" => "secret"}))
+      end
+
+    dataset =
+      @dataset
+      |> Map.put("region_id", 1)
+      |> Map.put("insee_commune_principale", nil)
+      |> Map.put("national_dataset", "true")
+
+    conn =
+      use_cassette "dataset/tag.json-1" do
+        post(conn, backoffice_dataset_path(conn, :post), dataset)
+      end
+
+    # It should not be possible to link a dataset to either a region and to the whole country
+    assert redirected_to(conn, 302) == backoffice_page_path(conn, :index)
+    assert Resource |> Repo.all() |> length() == 0
+    flash = get_flash(conn, :error)
+    assert flash =~ "Un jeu de données ne pas pas être à la fois régional et national"
   end
 
   @tag :external
