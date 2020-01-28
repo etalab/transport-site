3ff17922fe2b32cb861b2ed06bbcd162d887152c
Tristram Gräbener <tristramg@gmail.com>
2020-01-28 11:42:12 +0100

    Dialyzer: ignore the error 'Missing Mix.something'

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,5 @@
+[
+  {":0:unknown_function Function Mix.env/0 does not exist."},
+  {":0:unknown_function Function Mix.shell/0 does not exist."},
+  {":0:unknown_function Function Mix.Task.run/2 does not exist."}
+]
