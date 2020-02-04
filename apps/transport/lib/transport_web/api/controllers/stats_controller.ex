17ade0c8d5a2a2c41b9930917c3c01f35b790383
Francis <fchabouis@gmail.com>
2020-02-04 15:54:34 +0100

    fix alias order

--- apps/transport/lib/transport_web/api/controllers/stats_controller.ex
+++ apps/transport/lib/transport_web/api/controllers/stats_controller.ex
@@ -1,6 +1,6 @@
 defmodule TransportWeb.API.StatsController do
   use TransportWeb, :controller
-  alias DB.{AOM, Dataset, Region, Repo, DatasetGeographicView}
+  alias DB.{AOM, Dataset, DatasetGeographicView, Region, Repo}
   import Ecto.Query
   alias Geo.JSON
   alias OpenApiSpex.Operation
