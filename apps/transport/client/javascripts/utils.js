d10d79232733ab3ac783eb1f09e815952de59f3a
Francis <fchabouis@gmail.com>
2020-02-11 14:39:21 +0100

    eslint on utils.js

--- apps/transport/client/javascripts/utils.js
+++ apps/transport/client/javascripts/utils.js
@@ -2,23 +2,23 @@ var addSeeMore = function (maxHeight, querySelector, seeMoreText, seeLessText) {
     document.querySelectorAll(querySelector).forEach(
         function (div) {
             div.style.maxHeight = maxHeight
-            div.style.overflow = "hidden"
+            div.style.overflow = 'hidden'
             if (div.scrollHeight > div.clientHeight) {
                 var parent = div.parentElement
-                var displayMore = document.createElement("div")
+                var displayMore = document.createElement('div')
                 parent.appendChild(displayMore)
-                displayMore.className = ".displayMore"
-                var link_displayMore = document.createElement("a")
-                displayMore.appendChild(link_displayMore)
-                link_displayMore.innerHTML = seeMoreText
-                displayMore.addEventListener("click",
+                displayMore.className = '.displayMore'
+                var linkDisplayMore = document.createElement('a')
+                displayMore.appendChild(linkDisplayMore)
+                linkDisplayMore.innerHTML = seeMoreText
+                displayMore.addEventListener('click',
                     function () {
-                        if (div.style.maxHeight != "100%") {
-                            div.style.maxHeight = "100%";
-                            link_displayMore.innerHTML = seeLessText
+                        if (div.style.maxHeight !== '100%') {
+                            div.style.maxHeight = '100%'
+                            linkDisplayMore.innerHTML = seeLessText
                         } else {
                             div.style.maxHeight = maxHeight
-                            link_displayMore.innerHTML = seeMoreText
+                            linkDisplayMore.innerHTML = seeMoreText
                         }
                     }
                 )
