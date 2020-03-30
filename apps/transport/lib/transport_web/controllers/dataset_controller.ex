etalab/transport-site

commit 4fb8ada3e74a68486ecb77fde1149ff83fad33bc

Dialyzer/refactor main controllers

apps/transport/lib/transport_web/controllers/dataset_controller.ex
@@ -8,8 +8,10 @@ defmodule TransportWeb.DatasetController do
   import Phoenix.HTML.Link
   require Logger
 
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(%Plug.Conn{} = conn, params), do: list_datasets(conn, params)
 
+  @spec list_datasets(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def list_datasets(%Plug.Conn{} = conn, %{} = params) do
     conn
     |> assign(:datasets, get_datasets(params))
@@ -50,64 +52,41 @@ defmodule TransportWeb.DatasetController do
     end
   end
 
+  @spec by_aom(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def by_aom(%Plug.Conn{} = conn, %{"aom" => id} = params) do
     error_msg = dgettext("errors", "AOM %{id} does not exist", id: id)
-
-    try do
-      AOM
-      |> where([a], a.id == ^id)
-      |> Repo.exists?()
-      |> case do
-        false ->
-          error_page(conn, error_msg)
-
-        true ->
-          list_datasets(conn, params)
-      end
-    rescue
-      Ecto.Query.CastError -> error_page(conn, error_msg)
-    end
+    by_territory(conn, AOM |> where([a], a.id == ^id), params, error_msg)
   end
 
+  @spec by_region(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def by_region(%Plug.Conn{} = conn, %{"region" => id} = params) do
     error_msg = dgettext("errors", "Region %{id} does not exist", id: id)
-
-    try do
-      Region
-      |> where([r], r.id == ^id)
-      |> Repo.exists?()
-      |> case do
-        false ->
-          error_page(conn, error_msg)
-
-        true ->
-          list_datasets(conn, params)
-      end
-    rescue
-      Ecto.Query.CastError -> error_page(conn, error_msg)
-    end
+    by_territory(conn, Region |> where([r], r.id == ^id), params, error_msg)
   end
 
+  @spec by_commune_insee(Plug.Conn.t(), map) :: Plug.Conn.t()
   def by_commune_insee(%Plug.Conn{} = conn, %{"insee_commune" => insee} = params) do
-    Commune
-    |> where([c], c.insee == ^insee)
-    |> Repo.exists?()
-    |> case do
-      false ->
-        message =
-          dgettext(
-            "errors",
-            "Impossible to find a city with the insee code %{insee}",
-            insee: insee
-          )
+    error_msg =
+      dgettext(
+        "errors",
+        "Impossible to find a city with the insee code %{insee}",
+        insee: insee
+      )
 
-        error_page(conn, message)
-
-      true ->
-        list_datasets(conn, params)
-    end
+    by_territory(conn, Commune |> where([c], c.insee == ^insee), params, error_msg)
   end
 
+  defp by_territory(conn, territory, params, error_msg) do
+    if Repo.exists?(territory) do
+      error_page(conn, error_msg)
+    else
+      list_datasets(conn, params)
+    end
+  rescue
+    Ecto.Query.CastError -> error_page(conn, error_msg)
+  end
+
+  @spec error_page(Plug.Conn.t(), binary()) :: Plug.Conn.t()
   defp error_page(conn, msg) do
     conn
     |> put_status(:not_found)
@@ -116,6 +95,7 @@ defmodule TransportWeb.DatasetController do
     |> render("404.html")
   end
 
+  @spec get_datasets(map()) :: Scrivener.Page.t()
   defp get_datasets(params) do
     config = make_pagination_config(params)
 
@@ -125,8 +105,10 @@ defmodule TransportWeb.DatasetController do
     |> Repo.paginate(page: config.page_number)
   end
 
+  @spec clean_datasets_query(map()) :: Ecto.Query.t()
   defp clean_datasets_query(params), do: params |> Dataset.list_datasets() |> exclude(:preload)
 
+  @spec get_regions(map()) :: [Region.t()]
   defp get_regions(%{"tags" => _tags}) do
     # for tags, we do not filter the datasets since it causes a non valid sql query
     sub =
@@ -160,6 +142,7 @@ defmodule TransportWeb.DatasetController do
     |> Repo.all()
   end
 
+  @spec get_types(map()) :: [%{type: binary(), msg: binary()}]
   defp get_types(params) do
     params
     |> clean_datasets_query()
@@ -172,9 +155,7 @@ defmodule TransportWeb.DatasetController do
     |> Enum.reject(fn t -> is_nil(t.msg) end)
   end
 
-  defp redirect_to_slug_or_404(conn, %Dataset{} = dataset) do
-    redirect(conn, to: dataset_path(conn, :details, dataset.slug))
-  end
+  @spec redirect_to_slug_or_404(Plug.Conn.t(), number() | binary()) :: Plug.Conn.t()
 
   defp redirect_to_slug_or_404(conn, nil) do
     conn
@@ -191,6 +172,7 @@ defmodule TransportWeb.DatasetController do
     redirect_to_slug_or_404(conn, Repo.get_by(Dataset, datagouv_id: slug_or_id))
   end
 
+  @spec put_special_message(Plug.Conn.t(), map()) :: Plug.Conn.t()
   defp put_special_message(conn, %{"filter" => "has_realtime", "page" => page})
        when page != 1,
        do: conn
@@ -214,56 +196,41 @@ defmodule TransportWeb.DatasetController do
 
   defp put_special_message(conn, _params), do: conn
 
-  defp put_empty_message(%Plug.Conn{:assigns => %{:datasets => %{:entries => []}}} = conn, %{
-         "aom" => id
-       }) do
-    name =
-      case Repo.get(AOM, id) do
-        nil -> id
-        a -> a.nom
-      end
-
-    message = dgettext("page-shortlist", "AOM %{name} has not yet published any datasets", name: name)
-
-    conn
-    |> assign(:empty_message, raw(message))
+  @spec get_name(Ecto.Queryable.t(), binary()) :: binary()
+  defp get_name(territory, id) do
+    territory
+    |> Repo.get(id)
+    |> case do
+      nil -> id
+      t -> t.nom
+    end
   end
 
-  defp put_empty_message(%Plug.Conn{:assigns => %{:datasets => %{:entries => []}}} = conn, %{
-         "region" => id
-       }) do
-    name =
-      case Repo.get(Region, id) do
-        nil -> id
-        a -> a.nom
-      end
-
-    message = dgettext("page-shortlist", "There is no data for region %{name}", name: name)
-
-    conn
-    |> assign(:empty_message, raw(message))
+  @spec empty_message_by_territory(map()) :: binary()
+  defp empty_message_by_territory(%{"aom" => id}) do
+    dgettext("page-shortlist", "AOM %{name} has not yet published any datasets", name: get_name(AOM, id))
   end
 
-  defp put_empty_message(%Plug.Conn{:assigns => %{:datasets => %{:entries => []}}} = conn, %{
-         "insee_commune" => insee
-       }) do
+  defp empty_message_by_territory(%{"region" => id}) do
+    dgettext("page-shortlist", "There is no data for region %{name}", name: get_name(Region, id))
+  end
+
+  defp empty_message_by_territory(%{"insee_commune" => insee}) do
     name =
       case Repo.get_by(Commune, insee: insee) do
         nil -> insee
         a -> a.nom
       end
 
-    message = dgettext("page-shortlist", "There is no data for city %{name}", name: name)
-
-    conn
-    |> assign(:empty_message, raw(message))
+    dgettext("page-shortlist", "There is no data for city %{name}", name: name)
   end
 
-  defp put_empty_message(%Plug.Conn{:assigns => %{:datasets => %{:entries => []}}} = conn, _params) do
-    message = dgettext("page-shortlist", "No dataset found")
+  defp empty_message_by_territory(_params), do: dgettext("page-shortlist", "No dataset found")
 
-    conn
-    |> assign(:empty_message, raw(message))
+  @spec put_empty_message(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  defp put_empty_message(%Plug.Conn{:assigns => %{:datasets => %{:entries => []}}} = conn, params) do
+    message = empty_message_by_territory(params)
+    assign(conn, :empty_message, raw(message))
   end
 
   defp put_empty_message(conn, _params), do: conn

apps/transport/lib/transport_web/controllers/aoms.ex
@@ -1,6 +1,6 @@
 defmodule TransportWeb.AOMSController do
   use TransportWeb, :controller
-  alias DB.{AOM, Commune, Repo, Resource}
+  alias DB.{AOM, Commune, Dataset, Repo, Resource}
   import Ecto.Query
 
   @csvheaders [
@@ -37,6 +37,7 @@ defmodule TransportWeb.AOMSController do
     }
   end
 
+  @spec csv(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def csv(conn, _params) do
     conn
     |> put_resp_content_type("text/csv")
@@ -44,6 +45,7 @@ defmodule TransportWeb.AOMSController do
     |> send_resp(200, csv_content())
   end
 
+  @spec aoms :: [map()]
   defp aoms do
     AOM
     |> preload([:datasets, :region, :parent_dataset])
@@ -54,25 +56,29 @@ defmodule TransportWeb.AOMSController do
     |> Enum.map(&prepare_aom/1)
   end
 
+  @spec self_published(AOM.t()) :: boolean
   defp self_published(aom) do
     !(aom.datasets
       |> Enum.filter(fn d -> d.type == "public-transit" end)
       |> Enum.empty?())
   end
 
+  @spec valid_dataset?(Dataset.t()) :: boolean()
   defp valid_dataset?(dataset), do: Enum.any?(dataset.resources, fn r -> !Resource.is_outdated?(r) end)
 
-  defp up_to_date?([]), do: nil
+  @spec up_to_date?([Dataset.t()]) :: boolean()
+  defp up_to_date?([]), do: false
 
   defp up_to_date?(datasets) do
     datasets
     |> Enum.filter(fn d -> d.type == "public-transit" end)
     |> case do
-      [] -> nil
+      [] -> false
       transit_datasets -> Enum.any?(transit_datasets, &valid_dataset?/1)
     end
   end
 
+  @spec csv_content() :: binary()
   defp csv_content do
     aoms()
     |> CSV.encode(headers: @csvheaders)

apps/transport/lib/transport_web/controllers/atom_controller.ex
@@ -3,6 +3,7 @@ defmodule TransportWeb.AtomController do
   alias DB.{Repo, Resource}
   import Ecto.Query
 
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     resources =
       Resource

apps/transport/lib/transport_web/controllers/blog_controller.ex
@@ -1,6 +1,7 @@
 defmodule TransportWeb.BlogController do
   use TransportWeb, :controller
 
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     articles =
       "????_??_??_*"
@@ -12,6 +13,7 @@ defmodule TransportWeb.BlogController do
     render(conn, "index.html", articles: articles)
   end
 
+  @spec page(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def page(conn, %{"page" => page}) do
     filename = make_path(page)
 
@@ -20,6 +22,7 @@ defmodule TransportWeb.BlogController do
     |> render("article.html")
   end
 
+  @spec read_file(binary()) :: map()
   defp read_file(path) do
     {header, title, image_path, _} =
       path
@@ -52,9 +55,11 @@ defmodule TransportWeb.BlogController do
     }
   end
 
-  def compute_image_path(path) when is_nil(path), do: nil
-  def compute_image_path(path), do: ~r/\((?<path>.*)\)/ |> Regex.run(path, capture: :all_names) |> List.first()
+  @spec compute_image_path(binary()) :: binary()
+  defp compute_image_path(path) when is_nil(path), do: nil
+  defp compute_image_path(path), do: ~r/\((?<path>.*)\)/ |> Regex.run(path, capture: :all_names) |> List.first()
 
+  @spec get_header_title_image(binary(), tuple()) :: tuple()
   defp get_header_title_image(l, {nil, nil, nil, _}), do: {[l], nil, nil, false}
   defp get_header_title_image("# " <> title, {h, nil, _, _}), do: {h, title, nil, false}
   defp get_header_title_image("![" <> image, {h, t, nil, _}), do: {h, t, image, false}
@@ -62,5 +67,6 @@ defmodule TransportWeb.BlogController do
   defp get_header_title_image(_l, {h, t, i, _}) when not is_nil(i) and not is_nil(t), do: {h, t, i, true}
   defp get_header_title_image(_l, {h, t, i, _}), do: {h, t, i, false}
 
+  @spec make_path(binary()) :: binary()
   defp make_path(filename), do: Path.join([:code.priv_dir(:transport), "blog", filename <> ".md"])
 end

apps/transport/lib/transport_web/controllers/contact_controller.ex
@@ -3,6 +3,7 @@ defmodule TransportWeb.ContactController do
   alias Mailjet.Client
   require Logger
 
+  @spec send_mail(Plug.Conn.t(), map()) :: {:error, any} | Plug.Conn.t()
   def send_mail(conn, %{"email" => email, "topic" => topic, "demande" => demande} = params) do
     case Client.send_mail("PAN, Formulaire Contact", "contact@transport.beta.gouv.fr", email, topic, demande, false) do
       {:ok, _} ->
@@ -17,5 +18,11 @@ defmodule TransportWeb.ContactController do
     end
   end
 
-  def send_mail(_, params), do: Logger.error("Bad parameters for sending email #{params}")
+  def send_mail(conn, params) do
+    Logger.error("Bad parameters for sending email #{params}")
+
+    conn
+    |> put_flash(:error, gettext("There has been an error, try again later"))
+    |> redirect(to: params["redirect_path"] || page_path(conn, :index))
+  end
 end

commit dd71f870d345f3d85b606521ac4adc5e35525535

add back redirect_to_slug_or_404 implementation
commit 4fb8ada3e74a68486ecb77fde1149ff83f broke the dataset access by
slug.

apps/transport/lib/transport_web/controllers/dataset_controller.ex
@@ -151,21 +151,26 @@ defmodule TransportWeb.DatasetController do
     %{all: Map.get(result, true, 0) + Map.get(result, false, 0), true: Map.get(result, true, 0)}
   end
 
-  @spec redirect_to_slug_or_404(Plug.Conn.t(), number() | binary()) :: Plug.Conn.t()
 
-  defp redirect_to_slug_or_404(conn, nil) do
+  @spec redirect_to_slug_or_404(Plug.Conn.t(), number() | binary()) :: Plug.Conn.t()
+  defp redirect_to_slug_or_404(conn, slug_or_id) when is_integer(slug_or_id) do
+    redirect_to_dataset(conn, Repo.get_by(Dataset, id: slug_or_id))
+  end
+
+  defp redirect_to_slug_or_404(conn, slug_or_id) do
+    redirect_to_dataset(conn, Repo.get_by(Dataset, datagouv_id: slug_or_id))
+  end
+
+  @spec redirect_to_dataset(Plug.Conn.t(), %Dataset{} | nil) :: Plug.Conn.t()
+  defp redirect_to_dataset(conn, nil) do
     conn
     |> put_status(:not_found)
     |> put_view(ErrorView)
     |> render("404.html")
   end
 
-  defp redirect_to_slug_or_404(conn, slug_or_id) when is_integer(slug_or_id) do
-    redirect_to_slug_or_404(conn, Repo.get_by(Dataset, id: slug_or_id))
-  end
-
-  defp redirect_to_slug_or_404(conn, slug_or_id) do
-    redirect_to_slug_or_404(conn, Repo.get_by(Dataset, datagouv_id: slug_or_id))
+  defp redirect_to_dataset(conn, %Dataset{} = dataset) do
+    redirect(conn, to: dataset_path(conn, :details, dataset.slug))
   end
 
   @spec get_name(Ecto.Queryable.t(), binary()) :: binary()