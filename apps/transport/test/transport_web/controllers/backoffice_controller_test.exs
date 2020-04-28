148aa335a80f5c165b6750696e166a3931ed881b
antoine-de <antoine.desbordes@gmail.com>
2020-04-28 15:30:11 +0200

    add a test

--- apps/transport/test/transport_web/controllers/backoffice_controller_test.exs
+++ apps/transport/test/transport_web/controllers/backoffice_controller_test.exs
@@ -170,6 +170,33 @@ defmodule TransportWeb.BackofficeControllerTest do
   end
 
   @tag :external
+  test "Add a dataset linked to an AO and with an empty territory name", %{conn: conn} do
+    conn =
+      use_cassette "session/create-2" do
+        conn
+        |> init_test_session(redirect_path: "/datasets")
+        |> get(session_path(conn, :create, %{"code" => "secret"}))
+      end
+
+    dataset =
+      @dataset_with_zones
+      |> Map.put("region_id", nil)
+      |> Map.put("associated_territory_name", "")
+      |> Map.put("national_dataset", "true")
+
+    conn =
+      use_cassette "dataset/dataset-with-multiple-cities.json" do
+        post(conn, backoffice_dataset_path(conn, :post), dataset)
+      end
+
+    # It should be possible to link a dataset to an AOM if the territory name
+    # is empty (but not null since it comes from a form)
+    assert redirected_to(conn, 302) == backoffice_page_path(conn, :index)
+    assert Resource |> Repo.all() |> length() == 1
+    assert get_flash(conn, :info) =~ "ajouté"
+  end
+
+  @tag :external
   test "Add a dataset linked to a region and to the country", %{conn: conn} do
     conn =
       use_cassette "session/create-2" do
