5db2887bb72c0ca2e07c9704a688036cca78bc67
antoine-de <antoine.desbordes@gmail.com>
2020-05-12 16:29:59 +0200

    fix nil error on dataset without any csv
    fixes https://sentry.data.gouv.fr/etalab/site/issues/1880127/

--- apps/transport/lib/transport_web/views/dataset_view.ex
+++ apps/transport/lib/transport_web/views/dataset_view.ex
@@ -234,10 +234,11 @@ defmodule TransportWeb.DatasetView do
     end
   end
 
-  @spec show_data_viz(map) :: boolean
-  def show_data_viz(%{type: type} = dataset) when type == "carsharing-areas" or type == "private-parking" do
-    List.first(other_resources(dataset)).format == "csv"
+  @spec get_resource_to_display(%Dataset{}) :: Resource.t() | nil
+  def get_resource_to_display(%Dataset{type: type, resources: resources})
+      when type == "carsharing-areas" or type == "private-parking" do
+    resources |> Enum.find(fn r -> r.format == "csv" end)
   end
 
-  def show_data_viz(%{}), do: false
+  def get_resource_to_display(%Dataset{}), do: nil
 end
