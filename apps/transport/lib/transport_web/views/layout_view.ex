6c681bd68e5f19318ddbdfbdafc490b08adf24f9
antoine-de <antoine.desbordes@gmail.com>
2020-06-25 14:12:12 +0200

    protect the app.html from errors
    when this page is rendered without passing though the :browser pipeline
    it was crashing

--- apps/transport/lib/transport_web/views/layout_view.ex
+++ apps/transport/lib/transport_web/views/layout_view.ex
@@ -6,4 +6,10 @@ defmodule TransportWeb.LayoutView do
   def current_path(conn) do
     Controller.current_path(conn)
   end
+
+  def has_flash(conn) do
+    # it's not nice to depend on some internal state, but it does not seems to have a better way
+    # to check if the `put_flash` function has been called, and it's important for error pages
+    not is_nil(conn.private[:phoenix_flash])
+  end
 end
