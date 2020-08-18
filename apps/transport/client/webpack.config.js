db09b70b2a49df18519f5f0884218aebe1726760
Francis <fchabouis@gmail.com>
2020-08-18 14:23:15 +0200

    update copy-webpack-plugin version to 6.0.0
    for security reasons

--- apps/transport/client/webpack.config.js
+++ apps/transport/client/webpack.config.js
@@ -3,7 +3,7 @@ const devMode = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const extractImages = new CopyWebpackPlugin([{ from: 'images', to: '../images' }])
+const extractImages = new CopyWebpackPlugin({ patterns: [{ from: 'images', to: '../images' }] })
 const extractSass = new MiniCssExtractPlugin({ filename: '../css/app.css', allChunks: true })
 const fetchPolyfill = new webpack.ProvidePlugin({ fetch: 'exports-loader?self.fetch!whatwg-fetch/dist/fetch.umd' })
 const promisePolyfill = new webpack.ProvidePlugin({ Promise: 'core-js/es/promise' })
@@ -46,57 +46,57 @@ module.exports = {
     devtool: 'source-map',
     module: {
         rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            {
-                test: /\.(js|scss)$/,
-                exclude: [/node_modules/],
-                enforce: 'pre',
-                loader: 'import-glob-loader'
-            }, {
-                test: /\.js$/,
-                exclude: [/node_modules/],
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
+        {
+            test: /\.(js|scss)$/,
+            exclude: [/node_modules/],
+            enforce: 'pre',
+            loader: 'import-glob-loader'
+        }, {
+            test: /\.js$/,
+            exclude: [/node_modules/],
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
                 }
-            }, {
-                test: /\.scss$/,
-                exclude: [/node_modules/],
-                use:
-                    [
-                        MiniCssExtractPlugin.loader,
-                        {
-                            loader: 'css-loader',
-                            options: {
-                                sourceMap: true
-                            }
-                        }, {
-                            loader: 'sass-loader',
-                            options: {
-                                sourceMap: true
-                            }
+            }
+        }, {
+            test: /\.scss$/,
+            exclude: [/node_modules/],
+            use:
+                [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
                         }
-                    ]
-            }, {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                exclude: [/font-awesome/],
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: '../images/'
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
                     }
-                }]
-            }, {
-                test: /\.(eot|ttf|otf|woff|woff2|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: '../fonts/'
-                    }
-                }]
+                ]
+        }, {
+            test: /\.(jpe?g|png|gif|svg)$/,
+            exclude: [/font-awesome/],
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: '../images/'
+                }
             }]
+        }, {
+            test: /\.(eot|ttf|otf|woff|woff2|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: '../fonts/'
+                }
+            }]
+        }]
     }
 }
