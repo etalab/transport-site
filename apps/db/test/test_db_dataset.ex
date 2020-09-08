53617a14f4a5d0e42d111ccee7694c4f054b56c1
Francis <fchabouis@gmail.com>
2020-09-08 16:25:30 +0200

    add a test on aom foreign key constraints

---
+++ apps/db/test/test_db_dataset.ex
@@ -0,0 +1,19 @@
+defmodule TransportWeb.BackofficeControllerTest do
+  use TransportWeb.DatabaseCase, cleanup: [:datasets]
+  alias DB.Repo
+
+  test "delete_parent_dataset" do
+    parent_dataset = Repo.insert!(%Dataset{})
+    linked_aom = Repo.insert!(%AOM{parent_dataset_id: parent_dataset.id, nom: "Jolie AOM"})
+
+    # linked_aom is supposed to have a parent_dataset id
+    assert not is_nil(linked_aom.parent_dataset_id)
+
+    # it should be possible to delete a dataset even if it is an AOM's parent dataset
+    assert {:ok, _} = Repo.delete(parent_dataset)
+
+    # after parent deletion, the aom should have a nil parent_dataset
+    linked_aom = Repo.get!(AOM, linked_aom.id)
+    assert is_nil(linked_aom.parent_dataset_id)
+  end
+end
