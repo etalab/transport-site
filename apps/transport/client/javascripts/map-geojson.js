2d63eb1bc0aac4caa66424c08b7f9b74dd88fae6
Francis <fchabouis@gmail.com>
2020-10-14 12:28:59 +0200

    change mapbox map at least temporarily
    https://blog.mapbox.com/deprecating-studio-classic-styles-d8892ac38cb4

--- apps/transport/client/javascripts/map-geojson.js
+++ apps/transport/client/javascripts/map-geojson.js
@@ -5,11 +5,10 @@ import L from 'leaflet'
  * @type {Object}
  */
 const Mapbox = {
-    url: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-    accessToken: 'pk.eyJ1IjoibC12aW5jZW50LWwiLCJhIjoiY2pzMWtlNG90MXA5cTQ5dGYwNDRyMDRvayJ9.RhYAa9O0Qla5zhJAb9iwJA',
+    url: 'https://api.mapbox.com/styles/v1/istopopoki/ckg98kpoc010h19qusi9kxcct/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
+    accessToken: 'pk.eyJ1IjoiaXN0b3BvcG9raSIsImEiOiJjaW12eWw2ZHMwMGFxdzVtMWZ5NHcwOHJ4In0.VvZvyvK0UaxbFiAtak7aVw',
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors <a href="https://spdx.org/licenses/ODbL-1.0.html">ODbL</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.light'
+    maxZoom: 20
 }
 
 function initilizeMap (id) {
@@ -18,7 +17,6 @@ function initilizeMap (id) {
         accessToken: Mapbox.accessToken,
         attribution: Mapbox.attribution,
         maxZoom: Mapbox.maxZoom,
-        id: Mapbox.id
     }).addTo(map)
 
     const markersfg = L.featureGroup().addTo(map)
