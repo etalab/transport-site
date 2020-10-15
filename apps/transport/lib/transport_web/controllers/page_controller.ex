2123d881a23121c53ec6ad798ffd94194cf92b48
Francis <fchabouis@gmail.com>
2020-10-15 14:01:37 +0200

    update rt public transit as well

--- apps/transport/lib/transport_web/controllers/page_controller.ex
+++ apps/transport/lib/transport_web/controllers/page_controller.ex
@@ -13,7 +13,7 @@ defmodule TransportWeb.PageController do
     |> assign(:count_coach, Dataset.count_coach())
     |> assign(:count_aoms_with_dataset, count_aoms_with_dataset())
     |> assign(:count_regions_completed, count_regions_completed())
-    |> assign(:count_has_realtime, Dataset.count_has_realtime())
+    |> assign(:count_public_transport_has_realtime, Dataset.count_public_transport_has_realtime())
     |> assign(:percent_population, percent_population())
     |> assign(:reusers, CSVDocuments.reusers())
     |> render("index.html")
