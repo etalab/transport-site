519797278821aecc56391e6d8bc1aba8f8392491
antoine-de <antoine.desbordes@gmail.com>
2020-11-03 17:33:18 +0100

    add test
    a plug an old test that was not run anymore...

--- apps/transport/lib/transport_web/views/dataset_view.ex
+++ apps/transport/lib/transport_web/views/dataset_view.ex
@@ -270,11 +270,11 @@ defmodule TransportWeb.DatasetView do
   Builds a licence.
   ## Examples
       iex> %Dataset{licence: "fr-lo"}
-      ...> |> Dataset.licence
-      "Open Licence"
+      ...> |> TransportWeb.DatasetView.licence
+      "fr-lo"
       iex> %Dataset{licence: "Libertarian"}
-      ...> |> Dataset.licence
-      "Not specified"
+      ...> |> TransportWeb.DatasetView.licence
+      "notspecified"
   """
   @spec licence(%Dataset{}) :: String.t()
   def licence(%Dataset{licence: licence}) do
@@ -286,6 +286,32 @@ defmodule TransportWeb.DatasetView do
     end
   end
 
+  @doc """
+  Returns the resources that need to be displayed on a map
+  ## Examples
+      iex> %Dataset{type: "carsharing-areas", resources: [
+      ...>   %DB.Resource{id: 1, format: "csv", last_update: "20201103T102030Z"},
+      ...>   %DB.Resource{id: 2, format: "csv", last_update: "20201105T102030Z"},
+      ...> ]}
+      ...> |> TransportWeb.DatasetView.get_resource_to_display
+      %DB.Resource{id: 2, format: "csv", last_update: "20201105T102030Z"}
+      iex> %Dataset{type: "public-transit", resources: [
+      ...>   %DB.Resource{id: 1, format: "gtfs", last_update: "20201103T102030Z"},
+      ...>   %DB.Resource{id: 2, format: "gtfs", last_update: "20201105T102030Z"},
+      ...> ]}
+      ...> |> TransportWeb.DatasetView.get_resource_to_display
+      nil
+      iex> %Dataset{type: "bike-sharing", resources: [
+      ...>   %DB.Resource{id: 1, url: "http://pouet.com/gbfs.json", last_update: "20201103T102030Z"},
+      ...>   %DB.Resource{id: 2, url: "http://pouet.com/gbfs.json", last_update: "20201105T102030Z"},
+      ...>   %DB.Resource{id: 3, url: "http://pouet.com/other_url.json", last_update: "20201107T102030Z"},
+      ...> ]}
+      ...> |> TransportWeb.DatasetView.get_resource_to_display
+      %DB.Resource{id: 2, url: "http://pouet.com/gbfs.json", last_update: "20201105T102030Z"}
+      iex> %Dataset{type: "carsharing-areas", resources: []}
+      ...> |> TransportWeb.DatasetView.get_resource_to_display
+      nil
+  """
   @spec get_resource_to_display(%Dataset{}) :: Resource.t() | nil
   def get_resource_to_display(%Dataset{type: type, resources: resources})
       when type == "carsharing-areas" or type == "private-parking" or type == "charging-stations" do
