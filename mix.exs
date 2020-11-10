f5b4023b9f053f6f938167f4614741318275fcb6
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-11-10 14:26:40 +0100

    Configure excoveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Transport.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
+      test_coverage: [tool: ExCoveralls],
       dialyzer: [plt_add_deps: :transitive, plt_add_apps: [:mix]]
     ]
   end
