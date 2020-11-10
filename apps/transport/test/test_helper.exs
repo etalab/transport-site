6901f142012c5328132ece718066bcaf36e3d0c0
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-11-10 14:21:55 +0100

    Fix format

--- apps/transport/test/test_helper.exs
+++ apps/transport/test/test_helper.exs
@@ -2,7 +2,7 @@
 Application.ensure_all_started(:hound)
 
 # Exclude all external tests from running (unless RUN_ALL is provided)
-run_all = (System.get_env("RUN_ALL") == "1")
+run_all = System.get_env("RUN_ALL") == "1"
 excludes = if run_all, do: [], else: [:integration, :solution, :external]
 ExUnit.configure(exclude: excludes ++ [:pending])
 
