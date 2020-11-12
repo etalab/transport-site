75c481fe26c68c8ffbbd9335f3a479717d750bfe
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-11-12 14:23:00 +0100

    Fix missing moduledoc

--- apps/transport/lib/support/pretty_json_encoder.ex
+++ apps/transport/lib/support/pretty_json_encoder.ex
@@ -1,5 +1,9 @@
 defmodule TransportWeb.PrettyJSONEncoder do
-  # see https://stackoverflow.com/a/38278387/20302
+  @moduledoc """
+  An encoder that you can use to pretty-print the API JSON output
+  for easier debugging. See https://stackoverflow.com/a/38278387/20302
+  """
+
   def encode_to_iodata!(data) do
     Jason.encode_to_iodata!(data, pretty: true)
   end
