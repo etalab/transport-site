3870c67dfdc5a9367438a1fc2d5929209ca2f837
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-11-16 17:08:42 +0100

    Verify we have a full url

--- apps/gbfs/test/gbfs/controllers/index_controller_test.exs
+++ apps/gbfs/test/gbfs/controllers/index_controller_test.exs
@@ -2,18 +2,20 @@ defmodule GBFS.IndexControllerTest do
   use TransportWeb.ConnCase, async: true
 
   describe "GET /" do
-    test "returns properly formatted hrefs", %{conn: conn} do
+    test "returns correct absolute urls", %{conn: conn} do
       first_href = conn
         |> get("/gbfs")
         |> json_response(200)
         |> Enum.at(0)
         |> get_in(["gbfs", "_links", "gbfs.json", "href"])
-        |> URI.parse
-        |> Map.get(:path)
 
-      # NOTE: this order is deterministic & established via the code
+      port = Application.get_env(:transport, TransportWeb.Endpoint) |> Keyword.get(:http) |> Keyword.get(:port)
+
+      # NOTE: the order of "networks" is deterministic & established via the code,
+      # which means we can fix data for the test
       # see bottom of GBFS.Router for the static definition.
-      assert first_href == "/gbfs/vcub/gbfs.json"
+      # we're looking both to ensure we have a full url here, and that the path is as expected
+      assert first_href == "http://127.0.0.1:#{port}/gbfs/vcub/gbfs.json"
     end
   end
 end
