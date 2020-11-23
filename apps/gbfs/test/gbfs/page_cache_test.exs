0129f49c9cdc1608bb75cda1e27059fee054afaf
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-11-23 09:42:44 +0100

    Add pending test to cover non-200 return case
    This helped me uncover #1379, which should be fixed first before finishing this one.

--- apps/gbfs/test/gbfs/page_cache_test.exs
+++ apps/gbfs/test/gbfs/page_cache_test.exs
@@ -1,5 +1,6 @@
 defmodule GBFS.PageCacheTest do
   use GBFS.ConnCase, async: true
+  use GBFS.ExternalCase
   import ExUnit.CaptureLog
   require Logger
 
@@ -31,5 +32,25 @@ defmodule GBFS.PageCacheTest do
 
     logs = issue_verified_query_and_capture_logs(conn, url)
     assert logs =~ "Cache miss for key"
+  end
+
+  # TODO: fix #1379, and then later decide what to do with caching for non-200 returns.
+  # Maybe we should cache them still, but with a shorter expiry time
+  @tag :pending
+  test "cache non-200 response with a different TTL?", %{conn: conn} do
+    url = "/gbfs/vcub/station_status.json"
+
+    use_cassette :stub, url: "~r/opendata.bordeaux-metropole.fr/", status_code: 500 do
+      logs =
+        capture_log(fn ->
+          response =
+            conn
+            |> get(url)
+
+          # see https://github.com/etalab/transport-site/issues/1379, this won't pass for now
+          # we are currently returning 200 where we should not
+          assert response.status == 500
+        end)
+    end
   end
 end
