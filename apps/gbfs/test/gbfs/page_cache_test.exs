8c8c7e2b9303af12cc5c1b7ea7c2d36eb2c2f9ed
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-11-23 15:31:52 +0100

    Improve integration tests
    I have removed all logs capture which were a bit crappy, and instead added mock calls to ensure that calls are made, or not made. This uses "mock" because ExVCR does not support count checking apparently.

--- apps/gbfs/test/gbfs/page_cache_test.exs
+++ apps/gbfs/test/gbfs/page_cache_test.exs
@@ -3,63 +3,63 @@ defmodule GBFS.PageCacheTest do
   # We set async to false to avoid that.
   use GBFS.ConnCase, async: false
   use GBFS.ExternalCase
-  import ExUnit.CaptureLog
-  require Logger
+  import Mock
 
-  def issue_verified_query_and_capture_logs(conn, url) do
-    capture_log(fn ->
-      response =
-        conn
-        |> get(url)
+  def run_query(conn, url) do
+    response =
+      conn
+      |> get(url)
 
-      assert response.status == 200
+    assert response.status == 200
+    assert Jason.decode!(response.resp_body) |> Map.has_key?("data")
 
-      # NOTE: not using json_response directly because it currently does not catch bogus duplicate "charset"
-      assert response |> get_resp_header("content-type") == ["application/json; charset=utf-8"]
-    end)
+    # NOTE: not using json_response directly because it currently does not catch bogus duplicate "charset"
+    assert response |> get_resp_header("content-type") == ["application/json; charset=utf-8"]
   end
 
+  @tag :focus
   test "caches HTTP 200 output", %{conn: conn} do
     # simulate a clean start, otherwise other tests will have filled the cache
     Cachex.clear(:gbfs)
 
-    url = "/gbfs"
+    url = "/gbfs/vlille/station_information.json"
+
+    mock = fn _, _, _ -> {:ok, %HTTPoison.Response{body: "{}", status_code: 200}} end
+
+    # first call must result in call to third party
+    with_mock HTTPoison, get: mock do
+      run_query(conn, url)
+      assert_called_exactly(HTTPoison.get(:_, :_, :_), 1)
+    end
+
+    # a cache entry must have been created, with proper expiry time
     cache_key = PageCache.build_cache_key(url)
-
-    logs = issue_verified_query_and_capture_logs(conn, url)
-    assert logs =~ "Cache miss for key"
-    assert logs =~ "Persisting cache key"
-
     assert Cachex.get!(:gbfs, cache_key) != nil
     assert_in_delta Cachex.ttl!(:gbfs, cache_key), 60_000, 200
 
-    logs = issue_verified_query_and_capture_logs(conn, url)
-    assert logs =~ "Cache hit for key"
+    # # second call must not result into call to third party
+    with_mock HTTPoison, get: mock do
+      run_query(conn, url)
+      assert_not_called(HTTPoison.get(:_, :_, :_))
+    end
 
-    # fake time passed, should re-fetch
+    # fake time passed, which normally results in expiry
     Cachex.del!(:gbfs, cache_key)
 
-    logs = issue_verified_query_and_capture_logs(conn, url)
-    assert logs =~ "Cache miss for key"
-  end
-
-  # TODO: fix #1379, and then later decide what to do with caching for non-200 returns.
-  # Maybe we should cache them still, but with a shorter expiry time
-  @tag :pending
-  test "cache non-200 response with a different TTL?", %{conn: conn} do
-    url = "/gbfs/vcub/station_status.json"
-
-    use_cassette :stub, url: "~r/opendata.bordeaux-metropole.fr/", status_code: 500 do
-      _logs =
-        capture_log(fn ->
-          response =
-            conn
-            |> get(url)
-
-          # see https://github.com/etalab/transport-site/issues/1379, this won't pass for now
-          # we are currently returning 200 where we should not
-          assert response.status == 500
-        end)
+    # last call must again result in call to third party
+    with_mock HTTPoison, get: mock do
+      run_query(conn, url)
+      assert_called_exactly(HTTPoison.get(:_, :_, :_), 1)
     end
   end
+
+  # The proxy currently does not honor the remote HTTP code (#1379).
+  # To be fixed before we implement this test.
+  @tag :pending
+  test "mirrors non-200 status code"
+
+  # To be implemented later, but for now the error handling on that (Sentry etc)
+  # is not clear (#1378)
+  @tag :pending
+  test "does not cache anything if we raise an exception"
 end
