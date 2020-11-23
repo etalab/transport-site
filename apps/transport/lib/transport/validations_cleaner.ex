a5c8560c09446be0bb8535836c5bf502efae590e
antoine-de <antoine.desbordes@gmail.com>
2020-11-23 17:52:20 +0100

    Clean old validations
    The manual validations (not linked to a resource) where never cleaned.
    
    This fixes it (and keep 2 month a manuel validation)

---
+++ apps/transport/lib/transport/validations_cleaner.ex
@@ -0,0 +1,35 @@
+defmodule Transport.ValidationCleaner do
+  @moduledoc """
+  Clean the old hand made validations (not related to one of our resource)
+  """
+  alias DB.{Repo, Validation}
+  import Ecto.Query
+  require Logger
+
+  def clean_old_validations do
+    nb_days_to_keep = Application.get_env(:transport, :nb_days_to_keep_validations)
+
+    limit =
+      DateTime.utc_now()
+      |> DateTime.add(-1 * 24 * 3600 * nb_days_to_keep, :second)
+      |> DateTime.truncate(:second)
+      |> DateTime.to_iso8601()
+
+    nb_validations_before = count_validations()
+    Logger.info("cleaning old validations older than #{limit}.")
+    Logger.info("There are #{nb_validations_before} validation not linked to a resource")
+
+    Validation
+    |> where([v], is_nil(v.resource_id))
+    |> where([v], v.date < ^limit)
+    |> Repo.delete_all()
+
+    Logger.info("cleaned #{nb_validations_before - count_validations()} validation without a resource")
+  end
+
+  defp count_validations,
+    do:
+      Validation
+      |> where([v], is_nil(v.resource_id))
+      |> Repo.aggregate(:count, :id)
+end
---
+++ apps/transport/test/transport/validation_cleaner_test.exs
@@ -0,0 +1,88 @@
+defmodule Transport.ValidationCleanerTest do
+  use ExUnit.Case
+  alias Ecto.Adapters.SQL.Sandbox
+  alias DB.{Repo, Resource, Validation}
+  import Ecto.Query
+
+  def date(prior_days),
+    do:
+      Date.utc_today()
+      |> Date.add(prior_days)
+      |> Date.to_iso8601()
+
+  setup do
+    :ok = Sandbox.checkout(Repo)
+    # the setup is:
+    # 2 resources with validation (one 3 months ago, one 1 day ago)
+    # 2 validations without resource (one 3 months ago, one 1 day ago)
+    {:ok, _} =
+      %Resource{
+        url: "https://link.to/angers.zip",
+        validation: %Validation{
+          details: %{},
+          date: date(-1),
+          max_error: "Error",
+          validation_latest_content_hash: "a_content_hash",
+          data_vis: %{}
+        },
+        metadata: %{},
+        title: "angers.zip",
+        modes: ["ferry"],
+        features: ["tarifs"]
+      }
+      |> Repo.insert()
+
+    {:ok, _} =
+      %Resource{
+        url: "https://link.to/another_gtfs.zip",
+        validation: %Validation{
+          details: %{},
+          date: date(-90),
+          max_error: "Error",
+          validation_latest_content_hash: "a_content_hash",
+          data_vis: %{}
+        },
+        metadata: %{},
+        title: "another_gtfs.zip",
+        modes: ["bus"],
+        features: []
+      }
+      |> Repo.insert()
+
+    {:ok, _} =
+      %Validation{
+        details: %{},
+        date: date(-1),
+        on_the_fly_validation_metadata: %{},
+        data_vis: %{}
+      }
+      |> Repo.insert()
+
+    {:ok, _} =
+      %Validation{
+        details: %{},
+        date: date(-90),
+        on_the_fly_validation_metadata: %{},
+        data_vis: %{}
+      }
+      |> Repo.insert()
+
+    on_exit(fn ->
+      :ok = Sandbox.checkout(Repo)
+      Repo.delete_all(Resource)
+      Repo.delete_all(Validation)
+    end)
+
+    :ok
+  end
+
+  test "check validation cleaning" do
+    assert Repo.aggregate(Validation, :count, :id) == 4
+
+    Transport.ValidationCleaner.clean_old_validations()
+
+    # only the validation without resource created 3 months ago should have been cleaned
+    # the validation linked to resources should never be cleaned
+    assert Repo.aggregate(Validation, :count, :id) == 3
+  end
+end
