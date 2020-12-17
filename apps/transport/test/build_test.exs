b7251a0a97f37d632d88e0e227ceb3abdebb1f50
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-12-17 18:16:39 +0100

    Add test to catch differences in NodeJS version at build time

--- apps/transport/test/build_test.exs
+++ apps/transport/test/build_test.exs
@@ -15,11 +15,21 @@ defmodule TransportWeb.BuildTest do
     version
   end
 
+  def asdf_nodejs_release do
+    [[_, version]] = Regex.scan(~r/nodejs (\d+\.\d+\.\d+)/, tool_versions())
+    version
+  end
+
   test "make sure Elixir version is same for asdf & CI" do
     assert System.version() == asdf_elixir_version()
   end
 
   test "make sure OTP version is same for asdf & CI" do
     assert System.otp_release() == asdf_elixir_release()
+  end
+
+  test "make sure NodeJS version is same for asdf & CI" do
+    {output, 0} = System.cmd("node", ["--version"])
+    assert output |> String.trim() == "v" <> asdf_nodejs_release()
   end
 end
