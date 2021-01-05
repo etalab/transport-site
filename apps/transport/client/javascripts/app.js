c2e15b1042df334d01886cf165d730da8b206f22
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-01-05 18:40:53 +0100

    Make ECMA linter happy (to fix CI)

--- apps/transport/client/javascripts/app.js
+++ apps/transport/client/javascripts/app.js
@@ -1,6 +1,6 @@
 import { Socket } from 'phoenix'
 import LiveSocket from 'phoenix_live_view'
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}});
+const csrfToken = document.querySelector('meta[name=\'csrf-token\']').getAttribute('content')
+const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken } })
 liveSocket.connect()
