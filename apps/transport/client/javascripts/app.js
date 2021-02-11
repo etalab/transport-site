213afaa5de2a8d13a9a62ade85699de797dadc44
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-02-11 18:10:09 +0100

    Fix linter error

--- apps/transport/client/javascripts/app.js
+++ apps/transport/client/javascripts/app.js
@@ -5,9 +5,9 @@ const csrfToken = document.querySelector('meta[name=\'csrf-token\']').getAttribu
 const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken } })
 liveSocket.connect()
 
-// expose liveSocket on window for web console debug logs and latency simulation:            
-// >> liveSocket.enableDebug()            
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session            
-// >> liveSocket.disableLatencySim()            
+// expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
+// >> liveSocket.disableLatencySim()
 
 // window.liveSocket = liveSocket
