5b0b182423cd0fee4ad693abeadc9ced972fdd72
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-02-11 15:24:47 +0100

    Add useful boilerplate for later debugging

--- apps/transport/client/javascripts/app.js
+++ apps/transport/client/javascripts/app.js
@@ -4,3 +4,10 @@ import LiveSocket from 'phoenix_live_view'
 const csrfToken = document.querySelector('meta[name=\'csrf-token\']').getAttribute('content')
 const liveSocket = new LiveSocket('/live', Socket, { params: { _csrf_token: csrfToken } })
 liveSocket.connect()
+
+// expose liveSocket on window for web console debug logs and latency simulation:            
+// >> liveSocket.enableDebug()            
+// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session            
+// >> liveSocket.disableLatencySim()            
+
+// window.liveSocket = liveSocket
