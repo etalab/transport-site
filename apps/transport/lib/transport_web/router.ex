3c8edf933c2fd1740ed33cc3e8f48e639833e79e
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-02-11 15:25:21 +0100

    Remove fetch_flash since we already have fetch_live_flash
    The default boilerplate does not use both, and fetch_live_flash includes the previous one (https://github.com/phoenixframework/phoenix_live_view/blob/v0.15.4/lib/phoenix_live_view/router.ex#L132).

--- apps/transport/lib/transport_web/router.ex
+++ apps/transport/lib/transport_web/router.ex
@@ -11,7 +11,6 @@ defmodule TransportWeb.Router do
   pipeline :browser do
     plug(:accepts, ["html"])
     plug(:fetch_session)
-    plug(:fetch_flash)
     plug(:fetch_live_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
