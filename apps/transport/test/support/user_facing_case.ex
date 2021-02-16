ed357a45afd020f6f1aa8c78603a9ed8354e6bbe
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-02-16 16:23:20 +0100

    Make browser window size deterministic (fixes #1504)

--- apps/transport/test/support/user_facing_case.ex
+++ apps/transport/test/support/user_facing_case.ex
@@ -9,7 +9,10 @@ defmodule TransportWeb.UserFacingCase do
   using do
     quote do
       use Hound.Helpers
-      hound_session()
+      # NOTE: use a deterministic window size to make sure we do not meet failures
+      # due to find_element needing "below the fold" search
+      # See https://github.com/HashNuke/hound/issues/186
+      hound_session(driver: %{chromeOptions: %{args: ["--window-size=1024,768"]}})
     end
   end
 end
