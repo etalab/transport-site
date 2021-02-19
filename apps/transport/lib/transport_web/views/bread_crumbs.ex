348d22033d7548eca709b5ce4bcba1bbdd1cac11
Francis <fchabouis@gmail.com>
2021-02-19 17:04:02 +0100

    better breadcrumbs function names

--- apps/transport/lib/transport_web/views/bread_crumbs.ex
+++ apps/transport/lib/transport_web/views/bread_crumbs.ex
@@ -31,24 +31,24 @@ defmodule TransportWeb.BreadCrumbs do
 
   def render_crumbs(crumbs_element) do
     content_tag :div, class: "breadcrumbs" do
-      render_crumbs_elements_list(crumbs_element)
+      render_crumbs_elements(crumbs_element)
     end
   end
 
-  def render_crumbs_element({text, link}, :list_element) do
+  def render_crumb_element({text, link}, :list_element) do
     content_tag(:li, link(text, to: link), class: "breadcrumbs-element")
   end
 
   # last element does not need a link
-  def render_crumbs_element({text, _link}, :last_element) do
+  def render_crumb_element({text, _link}, :last_element) do
     content_tag(:li, text, class: "breadcrumbs-element")
   end
 
-  def render_crumbs_elements_list([element]) do
-    [render_crumbs_element(element, :last_element)]
+  def render_crumbs_elements([element]) do
+    [render_crumb_element(element, :last_element)]
   end
 
-  def render_crumbs_elements_list([head | tail]) do
-    List.flatten([render_crumbs_element(head, :list_element), render_crumbs_elements_list(tail)])
+  def render_crumbs_elements([head | tail]) do
+    List.flatten([render_crumb_element(head, :list_element), render_crumbs_elements(tail)])
   end
 end
