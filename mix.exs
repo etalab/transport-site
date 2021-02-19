2f02950f7c4ba737b5db4efb90613039bc332bf6
Francis <fchabouis@gmail.com>
2021-02-19 11:33:27 +0100

    update Dialyxir version to 1.1.0

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Transport.MixProject do
   defp deps do
     [
       # NOTE: using master to be able to use `plt_local_path` option
-      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false, git: "https://github.com/jeremyjh/dialyxir.git"},
+      {:dialyxir, "~> 1.1.0", only: :dev, runtime: false, git: "https://github.com/jeremyjh/dialyxir.git"},
       {:excoveralls, "~> 0.10", only: :test}
     ]
   end
