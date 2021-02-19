c4852460457a5ebbdc7be49c1738d0926cf4f358
Francis <fchabouis@gmail.com>
2021-02-19 15:52:25 +0100

    crumbs point to correct link
    last crumb has no link

--- apps/transport/lib/transport_web/views/bread_crumbs.ex
+++ apps/transport/lib/transport_web/views/bread_crumbs.ex
@@ -13,32 +13,41 @@ defmodule TransportWeb.BreadCrumbs do
     [{dgettext("espace-producteurs", "Your producer space"), page_path(conn, :espace_producteur)}]
   end
 
-  def crumbs(conn, :select_resource) do
-    crumbs(conn, :espace_producteur) ++
-    [{dgettext("espace-producteurs", "Sélectionner une resource"), page_path(conn, :espace_producteur)}]
-  end
-
   def crumbs(conn, :new_resource) do
     crumbs(conn, :espace_producteur) ++
     [{dgettext("espace-producteurs", "Nouvelle ressource"), page_path(conn, :espace_producteur)}]
   end
 
-  def crumbs(conn, :update_resource, _dataset_id) do
-    crumbs(conn, :select_resource) ++
+  def crumbs(conn, :select_resource, id) do
+    crumbs(conn, :espace_producteur) ++
+    [{dgettext("espace-producteurs", "Sélectionner une resource"), resource_path(conn, :resources_list, id)}]
+  end
+
+  def crumbs(conn, :update_resource, id) do
+    crumbs(conn, :select_resource, id) ++
     [{dgettext("espace-producteurs", "Mettre à jour une ressource"), page_path(conn, :espace_producteur)}]
   end
 
   def render_crumbs(crumbs_element) do
     content_tag :div, class: "breadcrumbs" do
-      render_crumbs_element(crumbs_element)
+      render_crumbs_elements_list(crumbs_element)
     end
   end
 
-  def render_crumbs_element([{text, link}]) do
+  def render_crumbs_element({text, link}, :list_element) do
     content_tag(:li, link(text, to: link), class: "breadcrumbs-element")
   end
 
-  def render_crumbs_element([head | tail]) do
-    List.flatten([render_crumbs_element([head]), render_crumbs_element(tail)])
+  # last element does not need a link
+  def render_crumbs_element({text, _link}, :last_element) do
+    content_tag(:li, text, class: "breadcrumbs-element")
+  end
+
+  def render_crumbs_elements_list([element]) do
+    [render_crumbs_element(element, :last_element)]
+  end
+
+  def render_crumbs_elements_list([head | tail]) do
+    List.flatten([render_crumbs_element(head, :list_element), render_crumbs_elements_list(tail)])
   end
 end
