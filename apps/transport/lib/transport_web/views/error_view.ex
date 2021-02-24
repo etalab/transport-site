39377a973c67e7bae0a3a342d5b34733ba65673a
Francis <fchabouis@gmail.com>
2021-02-24 17:41:41 +0100

    create a template for 400 family errors
    400 errors are grouped with 404, and are not displayed as 500
    errors anymore

--- apps/transport/lib/transport_web/views/error_view.ex
+++ apps/transport/lib/transport_web/views/error_view.ex
@@ -6,10 +6,15 @@ defmodule TransportWeb.ErrorView do
   end
 
   def render("400.html", assigns) do
-    render(__MODULE__, "internal_server_error.html", assigns)
+    assigns =
+      assigns
+      |> Map.put(:status_message, dgettext("errors", "400: Bad Request"))
+
+    render(__MODULE__, "400_family_errors.html", assigns)
   end
 
   def render("404.html", assigns) do
-    render(__MODULE__, "not_found_error.html", assigns)
+    assigns = assigns |> Map.put(:status_message, dgettext("errors", "404: Page not available"))
+    render(__MODULE__, "400_family_errors.html", assigns)
   end
 end
