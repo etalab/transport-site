590fdea17fe1be292ee4f3eaef69a0224adde553
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-03-01 21:52:08 +0100

    Add tests for all cached stats routes

--- apps/transport/test/transport_web/controllers/stats_controller_test.exs
+++ apps/transport/test/transport_web/controllers/stats_controller_test.exs
@@ -1,29 +1,35 @@
 defmodule TransportWeb.API.StatsControllerTest do
   use TransportWeb.DatabaseCase, cleanup: [:datasets]
-  use TransportWeb.ConnCase, async: false
+  use TransportWeb.ConnCase
   import Mock
 
-  @tag :focus
-  test "GET /api/stats/regions (invokes the cache system)", %{conn: conn} do
-    # return original computed payload
-    mock = fn _, x -> x.() end
+  @cached_features_routes [
+    {"/api/stats", "api-stats-aoms"},
+    {"/api/stats/regions", "api-stats-regions"},
+    {"/api/stats/quality", "api-stats-quality"}
+  ]
 
-    with_mock Transport.Cache.API, fetch: mock do
-      conn = conn |> get("/api/stats/regions")
-      %{"features" => features} = json_response(conn, 200)
-      # NOTE: we'll need to add real data instead, this just tests the caching path
-      assert features == []
-      assert_called_exactly(Transport.Cache.API.fetch(:_, :_), 1)
+  for {route, cache_key} <- @cached_features_routes do
+    test "GET #{route} (invokes the cache system)", %{conn: conn} do
+      # return original computed payload
+      mock = fn unquote(cache_key), x -> x.() end
+
+      with_mock Transport.Cache.API, fetch: mock do
+        conn = conn |> get(unquote(route))
+        %{"features" => features} = json_response(conn, 200)
+        # NOTE: we'll need to add real data instead, this just tests the caching path
+        assert features == []
+        assert_called_exactly(Transport.Cache.API.fetch(:_, :_), 1)
+      end
     end
-  end
 
-  @tag :focus
-  test "GET /api/stats/regions (returns the cached value as is)", %{conn: conn} do
-    mock = fn _, _ -> %{hello: 123} |> Jason.encode!() end
+    test "GET #{route} (returns the cached value as is)", %{conn: conn} do
+      mock = fn unquote(cache_key), _ -> %{hello: 123} |> Jason.encode!() end
 
-    with_mock Transport.Cache.API, fetch: mock do
-      conn = conn |> get("/api/stats/regions")
-      assert json_response(conn, 200) == %{"hello" => 123}
+      with_mock Transport.Cache.API, fetch: mock do
+        conn = conn |> get(unquote(route))
+        assert json_response(conn, 200) == %{"hello" => 123}
+      end
     end
   end
 end
