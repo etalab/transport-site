a26367513ea73274dc0a06b3014aa9d36e749894
Francis <fchabouis@gmail.com>
2021-03-05 16:59:36 +0100

    Apply the column type to the dataset table as well

--- apps/db/priv/repo/migrations/20210305153623_change_resource_last_modified_column_type.exs
+++ apps/db/priv/repo/migrations/20210305153623_change_resource_last_modified_column_type.exs
@@ -2,14 +2,15 @@ defmodule DB.Repo.Migrations.ChangeResourceLastModifiedColumnType do
   use Ecto.Migration
 
   def change do
-    execute(string_to_date(), date_to_string())
+    execute(string_to_date("resource"), date_to_string("resource"))
+    execute(string_to_date("dataset"), date_to_string("dataset"))
   end
 
-  defp string_to_date() do
-    "alter table resource alter last_update type timestamptz using last_update::timestamptz"
+  defp string_to_date(table) do
+    "alter table #{table} alter last_update type timestamptz using last_update::timestamptz"
   end
 
-  defp date_to_string() do
-    "alter table resource alter last_update type varchar(255) using last_update::varchar(255)"
+  defp date_to_string(table) do
+    "alter table #{table} alter last_update type varchar(255) using last_update::varchar(255)"
   end
 end
