86e28e40d33ae6b4c1b3eb3416dc04de6e5f7082
Francis <fchabouis@gmail.com>
2021-03-08 11:53:08 +0100

    Explicitely convert date to binary to control output
    and add some doctests

--- apps/helpers/lib/helpers.ex
+++ apps/helpers/lib/helpers.ex
@@ -28,20 +28,47 @@ defmodule Helpers do
     |> List.last()
   end
 
-  @spec convert_datetime_paris_zone(Calendar.datetime() | nil) :: Calendar.datetime() | binary()
+  @doc """
+  Converts a Calendar.datetime to Paris datetime and then to a binary format ready for display.
+
+  ## Examples
+
+  iex> Helpers.convert_datetime_paris_zone(nil)
+  ""
+
+  iex> {:ok, dt, 0} = DateTime.from_iso8601("2020-03-08T00:00:00Z")
+  {:ok, ~U[2020-03-08 00:00:00Z], 0}
+  iex> Helpers.convert_datetime_paris_zone(dt)
+  "2020-03-08T01:00:00+01:00"
+  """
+  @spec convert_datetime_paris_zone(Calendar.datetime() | nil) :: binary()
   def convert_datetime_paris_zone(nil), do: ""
 
   def convert_datetime_paris_zone(datetime) do
-    Timezone.convert(datetime, "Europe/Paris")
+    with converted_date <- Timezone.convert(datetime, "Europe/Paris"),
+         {:ok, formatted_date} <- Formatter.format(converted_date, "{RFC3339}") do
+      formatted_date
+    else
+      {:error, error} ->
+        Logger.error(error)
+        ""
+    end
   end
 
+  @doc """
+  Converts a binary date to a Paris binary date. Ultimately, this function should disappear because all datetimes in our databases will be datetimes, not strings
+
+  ## Examples
+
+  iex> Helpers.format_datetime("2020-03-08T00:00:00+00:00")
+  "2020-03-08T01:00:00+01:00"
+  """
   @spec format_datetime(binary()) :: binary()
   def format_datetime(nil), do: ""
 
   def format_datetime(date) do
     with {:ok, parsed_date} <- Timex.parse(date, "{ISO:Extended}"),
-         converted_date <- Timezone.convert(parsed_date, "Europe/Paris"),
-         {:ok, formatted_date} <- Formatter.format(converted_date, "{RFC3339}") do
+         formatted_date <- convert_datetime_paris_zone(parsed_date) do
       formatted_date
     else
       {:error, error} ->
