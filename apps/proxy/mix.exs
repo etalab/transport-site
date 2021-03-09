c90a64bc7bfc3936a321a8bfc74f721b4c1c3643
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-03-09 19:06:03 +0100

    Add a new proxy app

---
+++ apps/proxy/mix.exs
@@ -0,0 +1,33 @@
+defmodule Proxy.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :proxy,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.8",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls]
+    ]
+  end
+
+  def application do
+    [
+      mod: {Proxy.Application, []},
+      extra_applications: [:logger, :runtime_tools, :iconv]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    []
+  end
+end
