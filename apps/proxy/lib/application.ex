511ee3e241748959033136d9d0e834c1f8ceca4e
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-03-09 21:03:39 +0100

    Start endpoint

--- apps/proxy/lib/application.ex
+++ apps/proxy/lib/application.ex
@@ -7,9 +7,14 @@ defmodule Proxy.Application do
 
   def start(_type, _args) do
     children = [
+      Proxy.Endpoint
     ]
 
     opts = [strategy: :one_for_one, name: Proxy.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  # NOTE: not implementing `config_change` at this point, but in case
+  # you can read about it here:
+  # https://github.com/phoenixframework/phoenix/issues/3025
 end
