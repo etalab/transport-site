5528411a8eed6dc99102efd4139abe081d5b22a6
Francis <fchabouis@gmail.com>
2021-03-11 17:04:29 +0100

    Add a test for dataset details with data.gouv down
    Co-authored-by: thibaut.barrere@gmail.com <thibaut.barrere@gmail.com>

--- apps/transport/test/transport_web/controllers/dataset_controller_test.exs
+++ apps/transport/test/transport_web/controllers/dataset_controller_test.exs
@@ -2,11 +2,26 @@ defmodule TransportWeb.DatasetControllerTest do
   use TransportWeb.ConnCase, async: false
   use TransportWeb.ExternalCase
   use TransportWeb.DatabaseCase, cleanup: [:datasets]
+  import TransportWeb.Factory
+
+  import Mock
 
   doctest TransportWeb.DatasetController
 
   test "GET /", %{conn: conn} do
     conn = conn |> get(dataset_path(conn, :index))
     assert html_response(conn, 200) =~ "Jeux de données"
+  end
+
+  test "Datasets details page loads even when data.gouv is down", %{conn: conn} do
+    dataset = insert(:dataset, slug: "dataset-slug", tags: [], spatial: "dataset de test")
+
+    with_mocks [
+      {Datagouvfr.Client.Reuses, [], [get: fn dataset -> {:error, "data.gouv is down !"} end]},
+      {Datagouvfr.Client.Discussions, [], [get: fn id -> nil end]}
+    ] do
+      conn = conn |> get(dataset_path(conn, :details, dataset.slug))
+      assert html_response(conn, 200) =~ "dataset de test"
+    end
   end
 end
