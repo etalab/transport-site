40519b7b4438e501082ea9d536caa0b7e0aaca6d
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-03-19 18:08:55 +0100

    Remove config (no more integration tests)

--- apps/transport/test/test_helper.exs
+++ apps/transport/test/test_helper.exs
@@ -3,7 +3,7 @@ Application.ensure_all_started(:hound)
 
 # Exclude all external tests from running (unless RUN_ALL is provided)
 run_all = System.get_env("RUN_ALL") == "1"
-excludes = if run_all, do: [], else: [:integration, :external]
+excludes = if run_all, do: [], else: [:external]
 ExUnit.configure(exclude: excludes ++ [:pending])
 
 {:ok, _} = Application.ensure_all_started(:ex_machina)
