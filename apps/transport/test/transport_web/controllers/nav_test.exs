6384ef10f92334f5b6e7facb1d7ba86d64bb6649
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-03-19 18:22:33 +0100

    Simplify test by using factory

--- apps/transport/test/transport_web/controllers/nav_test.exs
+++ apps/transport/test/transport_web/controllers/nav_test.exs
@@ -1,53 +1,19 @@
 defmodule TransportWeb.NavTest do
   use TransportWeb.ConnCase, async: true
-  alias DB.{AOM, Dataset, Repo, Resource, Validation}
+  import TransportWeb.Factory
   import Mock
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
 
-    {:ok, _} =
-      %Dataset{
-        description: "Un jeu de données",
-        licence: "odc-odbl",
-        title: "Horaires et arrêts du réseau IRIGO - format GTFS",
-        spatial: "Horaires Angers",
-        type: "public-transit",
-        slug: "horaires-et-arrets-du-reseau-irigo-format-gtfs",
-        datagouv_id: "5b4cd3a0b59508054dd496cd",
-        frequency: "yearly",
-        tags: [],
-        resources: [
-          %Resource{
-            url: "https://link.to/angers.zip",
-            validation: %Validation{},
-            metadata: %{},
-            title: "angers.zip"
-          }
-        ],
-        aom: %AOM{nom: "Angers Métropôle"}
-      }
-      |> Repo.insert()
-
-    {:ok, _} =
-      %Dataset{
-        description: "Un autre jeu de données",
-        licence: "odc-odbl",
-        title: "offre de transport du réseau de LAVAL Agglomération (GTFS)",
-        slug: "offre-de-transport-du-reseau-de-laval-agglomeration-gtfs",
-        type: "public-transit",
-        datagouv_id: "5bc493d08b4c416c84a69500",
-        frequency: "yearly",
-        tags: [],
-        resources: [
-          %Resource{
-            url: "https://link.to/angers.zip",
-            validation: %Validation{},
-            metadata: %{}
-          }
-        ]
-      }
-      |> Repo.insert()
+    insert(:dataset,
+      description: "Un jeu de données",
+      spatial: "Horaires Angers",
+      resources: [
+        build(:resource, url: "https://link.to/angers.zip")
+      ],
+      aom: build(:aom)
+    )
 
     :ok
   end
