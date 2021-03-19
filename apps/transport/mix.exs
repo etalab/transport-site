6c4b8f0c744a363a10a3962d578de3911eed3658
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-03-19 18:09:36 +0100

    Remove hound

--- apps/transport/mix.exs
+++ apps/transport/mix.exs
@@ -75,7 +75,6 @@ defmodule Transport.Mixfile do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:cors_plug, "~> 2.0"},
       {:exvcr, "~> 0.10", only: :test},
-      {:hound, "~> 1.0", only: :test},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
       {:ex_aws, "~> 2.1"},
       {:ex_aws_s3, "~> 2.0"},
