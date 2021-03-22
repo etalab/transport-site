eed2f728c85dfa6163a4b7106f71b8f63ba29d86
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-03-22 17:50:10 +0100

    Suppression du besoin de navigateur pour les tests (#1552)
    * Replace slow browser test by faster controller test
    
    * Make test less fragile
    
    * Replace slow browser test by fast controller test
    
    * Replace slow browser test by fast controller test
    
    * Replace slow browser test by controller test
    
    * Fix deprecation warning
    
    * Bump up Floki
    
    * Replace slow browser test by improved controller test
    
    * Update page_controller_test.exs
    
    * Fix buggy assertion (parse_document vs parse_document!)
    
    * Replace slow solution test by equivalent controller test
    
    * Remove pending & obsolete old test
    
    * Remove "solution" tests (all transformed to controller tests)
    
    * Fix warning
    
    * Replace browser-based test by controller test (faster)
    
    * Remove config (no more integration tests)
    
    * Remove now useless helper
    
    * Remove test_integration from build matrix
    
    * Remove hound
    
    * Do not start hound anymore
    
    * Update README.md
    
    * Remove obsolete section configuration
    
    * Simplify test by using factory

---
+++ apps/transport/test/transport_web/controllers/nav_test.exs
@@ -0,0 +1,56 @@
+defmodule TransportWeb.NavTest do
+  use TransportWeb.ConnCase, async: true
+  import TransportWeb.Factory
+  import Mock
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+
+    insert(:dataset,
+      description: "Un jeu de données",
+      spatial: "Horaires Angers",
+      resources: [
+        build(:resource, url: "https://link.to/angers.zip")
+      ],
+      aom: build(:aom)
+    )
+
+    :ok
+  end
+
+  def click_link_by_text(conn, html, text) do
+    doc = Floki.parse_document!(html)
+    [link] = Floki.find(doc, "a:fl-contains('#{text}')")
+    [href] = Floki.attribute(link, "href")
+    conn |> get(href)
+  end
+
+  test "I can list available datasets to find and download transport data", %{conn: conn} do
+    # NOTE: we cannot easily mock the API exchanges, so just stubbing the known bits for now, and
+    # we'll later use a proper stubbed implementation for all tests.
+    with_mock Datagouvfr.Client.Discussions, get: fn _ -> [] end do
+      # browse the home
+      conn = conn |> get("/")
+      html = html_response(conn, 200)
+
+      # I can click to the a list of available datasets
+      conn = click_link_by_text(conn, html, "Voir les jeux de données récents")
+      html = html_response(conn, 200)
+
+      # I can see or read somewhere that the datasets are valid
+      assert html =~ "Jeux de données"
+      conn = click_link_by_text(conn, html, "Horaires Angers")
+      html = html_response(conn, 200)
+
+      assert html =~ "Horaires Angers"
+      assert html =~ "Un jeu de données"
+
+      doc = Floki.parse_document!(html)
+
+      # I can download the dataset
+      [element] = Floki.find(doc, ".download-button")
+      [href] = Floki.attribute(element, "href")
+      assert String.ends_with?(href, ".zip")
+    end
+  end
+end
