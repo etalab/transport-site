c019a6594faaaab7050445975e2781f4d3a380f5
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-03-22 17:05:29 +0100

    Let unlock handle all requests for "proxy" subdomain

--- apps/transport/lib/transport_web/plugs/router.ex
+++ apps/transport/lib/transport_web/plugs/router.ex
@@ -4,6 +4,7 @@ defmodule TransportWeb.Plugs.Router do
   plug(:match)
   plug(:dispatch)
 
+  match(_, host: "proxy.", to: Unlock.Router)
   match("/api/*_", to: TransportWeb.API.Router)
   match("/gbfs/*_", to: GBFS.Router)
   match(_, to: TransportWeb.Router)
