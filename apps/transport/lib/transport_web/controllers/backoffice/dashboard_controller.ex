825f042321e7036e374c14bee57c9e03195352bc
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-04-15 00:37:20 +0200

    Rework the data for easy display

--- apps/transport/lib/transport_web/controllers/backoffice/dashboard_controller.ex
+++ apps/transport/lib/transport_web/controllers/backoffice/dashboard_controller.ex
@@ -8,7 +8,19 @@ defmodule TransportWeb.Backoffice.DashboardController do
   @dashboard_import_count_sql File.read!("lib/queries/dashboard_import_count.sql")
 
   def index(conn, _params) do
+    data =
+      @dashboard_import_count_sql
+      |> DB.Repo.query!()
+
+    import_count_by_dataset_and_by_day =
+      data.rows
+      |> Enum.group_by(
+        fn [dataset_id, _, _] -> dataset_id end,
+        fn [_, date, count] -> {date, count} end
+      )
+      |> Enum.sort_by(fn {key, value} -> key end)
+
     conn
-    |> render("index.html")
+    |> render("index.html", import_count_by_dataset_and_by_day: import_count_by_dataset_and_by_day)
   end
 end
