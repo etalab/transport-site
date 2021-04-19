7e460268514fa03ce0e20a74889fb9e6c9625655
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-04-19 18:35:43 +0200

    Add tests for new HTTP stream

---
+++ apps/shared/test/http_stream_v2_test.exs
@@ -0,0 +1,24 @@
+defmodule HTTPStreamV2.Test do
+  use ExUnit.Case, async: true
+
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
+  test "streams the content and compute expected information", %{bypass: bypass} do
+    Bypass.expect_once(bypass, "GET", "/", fn conn ->
+      conn
+      |> Plug.Conn.put_resp_header("hello", "header")
+      |> Plug.Conn.resp(302, "Contenu éphémère")
+    end)
+
+    url = "http://localhost:#{bypass.port}/"
+    result = HTTPStreamV2.fetch_status_and_hash(url)
+
+    assert result.status == 302
+    assert result.hash == :crypto.hash(:sha256, "Contenu éphémère") |> Base.encode16 |> String.downcase
+    assert result.body_byte_size == ("Contenu éphémère" |> byte_size())
+    assert result.headers |> Enum.filter(fn({a,b}) -> a == "hello" end) == [{"hello", "header"}]
+  end
+end
