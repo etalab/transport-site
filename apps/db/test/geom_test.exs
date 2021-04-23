20e5cc66e434c8ab55a0c2da9ee680b369f4969b
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-04-23 21:47:19 +0200

    Restructure test to allow iteration over N modules

--- apps/db/test/geom_test.exs
+++ apps/db/test/geom_test.exs
@@ -6,27 +6,31 @@ defmodule DB.GeomTest do
 
   import Ecto.Query
 
-  test "geom support for Region" do
-    region = DB.Region |> last() |> DB.Repo.one!()
-    assert region.geom == nil
+  @modules [DB.Region]
 
-    geom =
-      "SRID=4326;POLYGON((55.58320119999998 -21.37236860026815,55.55105419999999 -21.37434010026793,55.53599659999998 -21.36315530026913))"
-      |> Geo.WKT.decode!()
+  Enum.each(@modules, fn tested_module ->
+    test "geom support for #{tested_module |> inspect}" do
+      instance = unquote(tested_module) |> last() |> DB.Repo.one!()
+      assert instance.geom == nil
 
-    assert geom.srid == 4326
-    assert geom.properties == %{}
+      geom =
+        "SRID=4326;POLYGON((55.58320119999998 -21.37236860026815,55.55105419999999 -21.37434010026793,55.53599659999998 -21.36315530026913))"
+        |> Geo.WKT.decode!()
 
-    assert geom.coordinates == [
-             [
-               {55.58320119999998, -21.37236860026815},
-               {55.55105419999999, -21.37434010026793},
-               {55.53599659999998, -21.36315530026913}
+      assert geom.srid == 4326
+      assert geom.properties == %{}
+
+      assert geom.coordinates == [
+               [
+                 {55.58320119999998, -21.37236860026815},
+                 {55.55105419999999, -21.37434010026793},
+                 {55.53599659999998, -21.36315530026913}
+               ]
              ]
-           ]
 
-    region
-    |> Ecto.Changeset.change(%{geom: geom})
-    |> DB.Repo.update!()
-  end
+      instance
+      |> Ecto.Changeset.change(%{geom: geom})
+      |> DB.Repo.update!()
+    end
+  end)
 end
