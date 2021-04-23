d9d686c19e6f6ab72b24b4d065d474f237827287
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-04-23 22:11:47 +0200

    Add comments

--- apps/db/test/geom_test.exs
+++ apps/db/test/geom_test.exs
@@ -11,9 +11,11 @@ defmodule DB.GeomTest do
 
   Enum.each(@modules, fn tested_module ->
     test "geom support for #{tested_module |> inspect}" do
+      # load the test record
       instance = unquote(tested_module) |> last() |> DB.Repo.one!()
       assert instance.geom == nil
 
+      # create a geometry from text (unrelated to Ecto)
       geom =
         "SRID=4326;POLYGON((55.58320119999998 -21.37236860026815,55.55105419999999 -21.37434010026793,55.53599659999998 -21.36315530026913,55.58320119999998 -21.37236860026815))"
         |> Geo.WKT.decode!()
@@ -30,9 +32,12 @@ defmodule DB.GeomTest do
                ]
              ]
 
+      # update the record (geo_postgis should serialize as needed)
       instance
       |> Ecto.Changeset.change(%{geom: geom})
       |> DB.Repo.update!()
+
+      # reload after save, just unserialize as needed, and leave unchanged
       instance = DB.Repo.get!(unquote(tested_module), instance.id)
       assert instance.geom == geom
     end
