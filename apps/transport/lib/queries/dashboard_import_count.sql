fa431df9a41ffa515d3d2cb55e368ffb46046d4b
Francis <fchabouis@gmail.com>
2021-05-05 14:49:46 +0200

    add import failures to the backoffice heatmap

--- apps/transport/lib/queries/dashboard_import_count.sql
+++ apps/transport/lib/queries/dashboard_import_count.sql
@@ -1,5 +1,7 @@
 /*
-  Return the "count" of logs_import for each dataset and each day of a recent date range.
+  Return :
+  - the "count" of logs_import for each dataset and each day of a recent date range (import_counts)
+  - the count of logs_import full success for each dataset and each day of a recent date range (success_counts)
 
   NOTE: If we need to parameterize the date range, but still work with a raw SQL like here
   (easier to test with SQL tooling, without having to rewrite into Ecto SQL), we can have
@@ -15,7 +17,7 @@ from
 	generate_series(current_date - interval '30 day', current_date, '1 day') as day,
 	dataset d),
 
-counts as (
+import_counts as (
 select
 	dataset_id,
 	timestamp::date as date,
@@ -24,13 +26,30 @@ from
 	logs_import
 group by
 	dataset_id,
+	date),
+
+  success_counts as (
+select
+	dataset_id,
+	timestamp::date as date,
+	count(*) as count
+from
+	logs_import
+  WHERE is_success = TRUE
+
+group by
+	dataset_id,
 	date)
 
 select
 	dates.*,
-	coalesce(counts.count, 0) as count
+	coalesce(import_counts.count, 0) as import_counts,
+  coalesce(success_counts.count, 0) as success_counts
 from
 	dates
-left join counts on
-	dates.day = counts.date
-	and dates.dataset_id = counts.dataset_id;
+left join import_counts on
+	dates.day = import_counts.date
+	and dates.dataset_id = import_counts.dataset_id
+left join success_counts on
+	dates.day = success_counts.date
+	and dates.dataset_id = success_counts.dataset_id;
--- apps/transport/lib/transport_web/views/backoffice/dashboard_view.ex
+++ apps/transport/lib/transport_web/views/backoffice/dashboard_view.ex
@@ -1,3 +1,13 @@
 defmodule TransportWeb.Backoffice.DashboardView do
   use TransportWeb, :view
+
+  @spec cell_class({boolean(), boolean()}) :: binary()
+  def cell_class({true, _import_failure}), do: "crash"
+
+  def cell_class({false, import_failure}) do
+    case import_failure do
+      true -> "failure"
+      false -> "success"
+    end
+  end
 end
