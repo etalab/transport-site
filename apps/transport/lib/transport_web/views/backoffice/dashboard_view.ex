c82665b5190155bec114adbf80f3542442a3483c
Francis <fchabouis@gmail.com>
2021-05-06 17:09:55 +0200

    add a 4th color to the heatmap !
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>

--- apps/transport/lib/transport_web/views/backoffice/dashboard_view.ex
+++ apps/transport/lib/transport_web/views/backoffice/dashboard_view.ex
@@ -2,12 +2,11 @@ defmodule TransportWeb.Backoffice.DashboardView do
   use TransportWeb, :view
 
   @spec cell_class({boolean(), boolean()}) :: binary()
-  def cell_class({true, _import_failure}), do: "crash"
+  def cell_class({_import_count = 0, _success_count}), do: "no_import"
 
-  def cell_class({false, import_failure}) do
-    case import_failure do
-      true -> "failure"
-      false -> "success"
-    end
-  end
+  def cell_class({import_count, _success_count = 0}) when import_count > 0, do: "only_failures"
+
+  def cell_class({import_count, success_count}) when import_count != success_count, do: "some_failures"
+
+  def cell_class({import_count, success_count}) when import_count == success_count, do: "only_successes"
 end
