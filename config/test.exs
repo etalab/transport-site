5d4fe86484aa77f32b8c9490ac8138f4c31750bb
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-05-06 20:49:46 +0200

    Disable tzdata auto-update during testing

--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,9 @@ use Mix.Config
 
 alias Datagouvfr.Authentication
 
+# avoid test failures with VCR, as tzdata tries to update
+config :tzdata, :autoupdate, :disabled
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :transport, TransportWeb.Endpoint,
