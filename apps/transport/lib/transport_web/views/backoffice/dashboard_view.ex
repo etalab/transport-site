1898f3b8b01c50bd409472dca2508123dd3903e7
Francis Chabouis <fchabouis@gmail.com>
2021-05-06 18:15:01 +0200

    Ajout d'informations supplémentaires dans le dashboard du backoffice (#1622)
    * rewrite the backoffice dashboard queries using `with`
    
    * add import failures to the backoffice heatmap
    
    * update dashboard description and dataset link
    
    * add a 4th color to the heatmap !
    
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>
    
    * Fix border bug
    
    * Use class instead of inline style
    
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>

--- apps/transport/lib/transport_web/views/backoffice/dashboard_view.ex
+++ apps/transport/lib/transport_web/views/backoffice/dashboard_view.ex
@@ -1,3 +1,12 @@
 defmodule TransportWeb.Backoffice.DashboardView do
   use TransportWeb, :view
+
+  @spec cell_class({boolean(), boolean()}) :: binary()
+  def cell_class({_import_count = 0, _success_count}), do: "no_import"
+
+  def cell_class({import_count, _success_count = 0}) when import_count > 0, do: "only_failures"
+
+  def cell_class({import_count, success_count}) when import_count != success_count, do: "some_failures"
+
+  def cell_class({import_count, success_count}) when import_count == success_count, do: "only_successes"
 end
