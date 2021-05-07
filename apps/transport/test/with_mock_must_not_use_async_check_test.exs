c3b3addc9a973d0a43a833bcf0536882a4ffd642
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-05-07 18:34:00 +0200

    Add a quick check to prevent use of async: true with with_mock

---
+++ apps/transport/test/with_mock_must_not_use_async_check_test.exs
@@ -0,0 +1,23 @@
+defmodule WithMockMustNotUseAsyncCheckTest do
+  @moduledoc """
+  A very cheap and not super reliable way to ensure a developer
+  using "with_mock" together with "async: true" will be warned
+  that this should not be used, because it will cause troubles.
+  """
+  use ExUnit.Case
+
+  test "async must be set to false if you use with_mock" do
+    files =
+      Path.wildcard("../../apps/**/*_test.exs")
+      |> Enum.filter(&potential_incorrect_use_detected?/1)
+
+    assert files == []
+  end
+
+  def potential_incorrect_use_detected?(file) do
+    content = File.read!(file)
+    is_async = String.contains?(content, "async: true")
+    use_mock = String.match?(content, ~r/with_mocks? /)
+    use_mock && is_async
+  end
+end
