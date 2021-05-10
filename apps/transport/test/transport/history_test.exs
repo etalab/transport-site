82235da4fcbfdb92618578f6e2ad1597c265faeb
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-05-10 18:35:48 +0200

    Add first iteration of tests

---
+++ apps/transport/test/transport/history_test.exs
@@ -0,0 +1,34 @@
+defmodule Transport.HistoryTest do
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureLog
+  import TransportWeb.Factory
+  import Ecto.Query
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+  end
+
+  test "backup_resources" do
+    logs =
+      capture_log(fn ->
+        resource =
+          insert(:resource,
+            url: "http://localhost",
+            title: "Hello",
+            format: "GTFS",
+            is_community_resource: false,
+            dataset: insert(:dataset)
+          )
+
+        assert :ok == Transport.History.Backup.backup_resources()
+      end)
+
+    assert logs == []
+  end
+
+  test "history_resources" do
+    dataset = insert(:dataset)
+
+    Transport.History.Fetcher.history_resources(dataset)
+  end
+end
