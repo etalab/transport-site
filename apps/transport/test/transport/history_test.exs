e2c694ae093ed29238f4bb5a31f1b67eec05f58c
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-05-13 00:21:06 +0200

    Finalize first test

--- apps/transport/test/transport/history_test.exs
+++ apps/transport/test/transport/history_test.exs
@@ -11,44 +11,71 @@ defmodule Transport.HistoryTest do
 
   setup :verify_on_exit!
 
+  @tag :focus
   test "backup_resources" do
-    logs =
-      capture_log(fn ->
-        resource =
-          insert(:resource,
-            url: "http://localhost",
-            title: "Hello",
-            format: "GTFS",
-            is_community_resource: false,
-            dataset: insert(:dataset)
-          )
+    resource =
+      insert(:resource,
+        url: resource_url = "http://localhost/the-resource-url",
+        title: "Hello",
+        format: "GTFS",
+        is_community_resource: false,
+        dataset: insert(:dataset),
+        last_update: DateTime.utc_now() |> DateTime.to_iso8601()
+      )
 
-          Transport.ExAWS.Mock
-          # bucket creation
-          |> expect(:request!, fn(request) ->
-            assert %{
-              service: :s3,
-              http_method: :put,
-              path: "/",
-              bucket: "dataset-123",
-              headers: %{"x-amz-acl" => "public-read"}
-            }
-          end)
-          |> expect(:stream!, fn(request) ->
-            assert %{
-              service: :s3,
-              bucket: "dataset-123",
-              http_method: :get,
-              params: %{"prefix" => "Hello"},
-              path: "/"
-            }
-            [%{key: "hello"}]
-          end)
+    Transport.HTTPoison.Mock
+    |> expect(:get, fn resource_url ->
+      {:ok, %{status_code: 200, body: "the-payload"}}
+    end)
 
-        assert :ok == Transport.History.Backup.backup_resources()
-      end)
+    Transport.ExAWS.Mock
+    # bucket creation
+    |> expect(:request!, fn request ->
+      assert %{
+               service: :s3,
+               http_method: :put,
+               path: "/",
+               bucket: "dataset-123",
+               headers: %{"x-amz-acl" => "public-read"}
+             } = request
+    end)
+    |> expect(:stream!, fn request ->
+      assert %{
+               service: :s3,
+               bucket: "dataset-123",
+               http_method: :get,
+               params: %{"prefix" => "Hello"},
+               path: "/"
+             } = request
 
-    assert logs == []
+      [%{key: "hello"}]
+    end)
+    |> expect(:request!, fn request ->
+      assert %{
+               service: :s3,
+               bucket: "dataset-123",
+               http_method: :head,
+               path: "hello"
+             } = request
+
+      %{
+        headers: %{
+          "content-hash" => "some-hash",
+          "updated_at" => DateTime.add(DateTime.utc_now(), -60 * 60 * 24, :second)
+        }
+      }
+    end)
+    |> expect(:request!, fn request ->
+      assert %{
+               service: :s3,
+               bucket: "dataset-123",
+               headers: %{"x-amz-acl" => "public-read"},
+               http_method: :put,
+               body: "the-payload"
+             } = request
+    end)
+
+    assert :ok == Transport.History.Backup.backup_resources()
   end
 
   test "history_resources" do
