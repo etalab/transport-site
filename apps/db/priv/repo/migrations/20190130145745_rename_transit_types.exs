bee76a23efee8c1fa03fb7ea93205840d9d6d5e2
Miryad <59559301+Miryad3108@users.noreply.github.com>
2021-05-21 06:07:42 +0200

    Modification "carsharing-area" en "carpooling-area"

--- apps/db/priv/repo/migrations/20190130145745_rename_transit_types.exs
+++ apps/db/priv/repo/migrations/20190130145745_rename_transit_types.exs
@@ -4,14 +4,14 @@ defmodule DB.Repo.Migrations.RenameTransitTypes do
   def up do
     execute "UPDATE dataset set type='public-transit' where type='DB-statique'"
     execute "UPDATE dataset set type='micro-mobility' where type='bike sharing'"
-    execute "UPDATE dataset set type='carsharing-areas' where type='aires-covoiturage'"
+    execute "UPDATE dataset set type='carpooling-areas' where type='aires-covoiturage'"
     execute "UPDATE dataset set type='charging-stations' where type='borne-recharge'"
   end
 
   def down do
     execute "UPDATE dataset set type='DB-statique' where type='public-transit'"
     execute "UPDATE dataset set type='bike sharing' where type='micro-mobility'"
-    execute "UPDATE dataset set type='aires-covoiturage' where type='carsharing-areas'"
+    execute "UPDATE dataset set type='aires-covoiturage' where type='carpooling-areas'"
     execute "UPDATE dataset set type='borne-recharge' where type='charging-stations'"
   end
 end
