1461ec30b5b67c455e4906f01e2dc89d6254d7f5
Francis <fchabouis@gmail.com>
2021-05-31 14:30:36 +0200

    add two tests for html dataset description generation

--- apps/transport/test/transport_web/controllers/dataset_view_test.exs
+++ apps/transport/test/transport_web/controllers/dataset_view_test.exs
@@ -2,6 +2,19 @@ defmodule TransportWeb.DatasetViewTest do
   use TransportWeb.ConnCase, async: false
   use TransportWeb.ExternalCase
   use TransportWeb.DatabaseCase, cleanup: [:datasets]
+  import TransportWeb.DatasetView
 
   doctest TransportWeb.DatasetView
+
+  test "the html content of a markdown description" do
+    content = "# coucou"
+    dataset = %DB.Dataset{description: content}
+    assert description(dataset) == {:safe, "<h1>coucou</h1>\n"}
+  end
+
+  test "if the html produced is sanitized" do
+    content = "<p \" onmouseout=\"alert('Gotcha!')\">coucou</p>"
+    dataset = %DB.Dataset{description: content}
+    assert description(dataset) == {:safe, "<p>coucou</p>\n"}
+  end
 end
