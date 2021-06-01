64466bfebfb28e4b9b0a4f6fe9e0940921314d1a
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-06-01 17:31:29 +0200

    "warnings as errors" pour les tests transport (#1658)
    * Consider tests warnings as errors
    
    * Fix warnings

--- apps/transport/test/test_helper.exs
+++ apps/transport/test/test_helper.exs
@@ -12,3 +12,8 @@ ExUnit.start()
 ExVCR.Config.cassette_library_dir("test/fixture/cassettes")
 
 Ecto.Adapters.SQL.Sandbox.mode(DB.Repo, :manual)
+
+# Temporary solution to consider warnings as errors during tests,
+# until `mix test --warning-as--errors` is available (Elixir 1.12+)
+# https://github.com/elixir-lang/elixir/issues/3223#issuecomment-751876927
+Code.put_compiler_option(:warnings_as_errors, true)
