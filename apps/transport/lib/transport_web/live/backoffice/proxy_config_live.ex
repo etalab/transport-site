16cf19a541aee082a82bac39e506e1f63c7c14b2
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-06-07 16:26:19 +0200

    Prepare objects for listing

--- apps/transport/lib/transport_web/live/backoffice/proxy_config_live.ex
+++ apps/transport/lib/transport_web/live/backoffice/proxy_config_live.ex
@@ -1,9 +1,26 @@
 defmodule TransportWeb.Backoffice.ProxyConfigLive do
   use Phoenix.LiveView
 
-  def mount(_params, _session, socket) do
+  def mount(_params, session, socket) do
+    socket = assign(socket, :proxy_configuration, get_proxy_configuration(session))
     # TODO: add auth https://hexdocs.pm/phoenix_live_view/security-model.html
-    {:ok, assign(socket, :hello, "World")}
+    {:ok, socket}
+  end
+
+  defp get_proxy_configuration(session) do
+    config_method = Application.fetch_env!(:unlock, :resources)
+    data = config_method.()
+    # TODO: stop using an array here
+    Enum.map(data, fn {slug, [resource]} ->
+      %{
+        unique_slug: slug,
+        proxy_url: get_proxy_resource_url(session, slug),
+        original_url: resource["url"],
+        ttl: resource["ttl"]
+      }
+    end)
+  end
+
   # Hackish stuff to create link to resource. To be replaced by
   # a cleaner and more explicit configuration later.
   def build_session(conn) do
@@ -15,5 +32,10 @@ defmodule TransportWeb.Backoffice.ProxyConfigLive do
     }
   end
 
+  defp get_proxy_resource_url(%{"proxy_base_url" => base_url}, slug) do
+    Path.join(
+      base_url,
+      Unlock.Router.Helpers.resource_path(Unlock.Endpoint, :fetch, slug)
+    )
   end
 end
