5a59bf71c926e8a90d0331f4f0c0ca58f2ecbf27
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-06-07 13:44:12 +0200

    Add TODO

--- apps/transport/lib/transport_web/live/backoffice/proxy_config_live.ex
+++ apps/transport/lib/transport_web/live/backoffice/proxy_config_live.ex
@@ -2,6 +2,7 @@ defmodule TransportWeb.Backoffice.ProxyConfigLive do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
+    # TODO: add auth https://hexdocs.pm/phoenix_live_view/security-model.html
     {:ok, assign(socket, :hello, "World")}
   end
 end
