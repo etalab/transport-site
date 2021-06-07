5810f1f519f7ba2f0f7b612f219f2abbcc5d5667
Francis <fchabouis@gmail.com>
2021-06-07 15:51:05 +0200

    better test description

--- apps/shared/test/http_stream_v2_test.exs
+++ apps/shared/test/http_stream_v2_test.exs
@@ -25,79 +25,81 @@ defmodule HTTPStreamV2.Test do
     assert headers == [{"hello", "header"}]
   end
 
-  test "get a request status by streaming it", %{bypass: bypass} do
-    Bypass.expect(bypass, "GET", "/", fn conn ->
-      conn
-      |> Plug.Conn.put_resp_header("hello", "header")
-      |> Plug.Conn.resp(200, "Contenu de la réponse")
-    end)
+  describe "get a request status by streaming it" do
+    test "simple 200 response", %{bypass: bypass} do
+      Bypass.expect(bypass, "GET", "/", fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("hello", "header")
+        |> Plug.Conn.resp(200, "Contenu de la réponse")
+      end)
 
-    url = "http://localhost:#{bypass.port}/"
+      url = "http://localhost:#{bypass.port}/"
 
-    result = HTTPStreamV2.fetch_status(url)
-    assert result == {:ok, %{status: 200}}
+      result = HTTPStreamV2.fetch_status(url)
+      assert result == {:ok, %{status: 200}}
 
-    result_follow_redirect = HTTPStreamV2.fetch_status_follow_redirect(url)
-    assert result_follow_redirect == {:ok, 200}
-  end
+      result_follow_redirect = HTTPStreamV2.fetch_status_follow_redirect(url)
+      assert result_follow_redirect == {:ok, 200}
+    end
 
-  test "get a redirect request status", %{bypass: bypass} do
-    url = "http://localhost:#{bypass.port}/"
+    test "redirect response", %{bypass: bypass} do
+      url = "http://localhost:#{bypass.port}/"
 
-    Bypass.expect(bypass, "GET", "/", fn conn ->
-      conn
-      |> Plug.Conn.put_resp_header("Location", "#{url}here")
-      |> Plug.Conn.resp(301, "va voir ailleurs si j'y suis")
-    end)
+      Bypass.expect(bypass, "GET", "/", fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("Location", "#{url}here")
+        |> Plug.Conn.resp(301, "va voir ailleurs si j'y suis")
+      end)
 
-    Bypass.expect_once(bypass, "GET", "/here", fn conn ->
-      conn
-      |> Plug.Conn.resp(200, "gagné")
-    end)
+      Bypass.expect_once(bypass, "GET", "/here", fn conn ->
+        conn
+        |> Plug.Conn.resp(200, "gagné")
+      end)
 
-    result = HTTPStreamV2.fetch_status(url)
-    assert result == {:ok, %{status: 301, location: "#{url}here"}}
+      result = HTTPStreamV2.fetch_status(url)
+      assert result == {:ok, %{status: 301, location: "#{url}here"}}
 
-    # get the status following redirection
-    result_follow_redirect = HTTPStreamV2.fetch_status_follow_redirect(url)
-    assert result_follow_redirect == {:ok, 200}
-  end
+      # get the status following redirection
+      result_follow_redirect = HTTPStreamV2.fetch_status_follow_redirect(url)
+      assert result_follow_redirect == {:ok, 200}
+    end
 
-  test "more redirects than allowed", %{bypass: bypass} do
-    url = "http://localhost:#{bypass.port}/"
+    test "more redirects than allowed", %{bypass: bypass} do
+      url = "http://localhost:#{bypass.port}/"
 
-    # setup a test with 2 successive redirects
-    Bypass.expect(bypass, "GET", "/", fn conn ->
-      conn
-      |> Plug.Conn.put_resp_header("Location", "#{url}1")
-      |> Plug.Conn.resp(301, "")
-    end)
+      # setup a test with 2 successive redirects
+      Bypass.expect(bypass, "GET", "/", fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("Location", "#{url}1")
+        |> Plug.Conn.resp(301, "")
+      end)
 
-    Bypass.expect(bypass, "GET", "/1", fn conn ->
-      conn
-      |> Plug.Conn.put_resp_header("Location", "#{url}2")
-      |> Plug.Conn.resp(301, "")
-    end)
+      Bypass.expect(bypass, "GET", "/1", fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("Location", "#{url}2")
+        |> Plug.Conn.resp(301, "")
+      end)
 
-    Bypass.expect(bypass, "GET", "/2", fn conn ->
-      conn
-      |> Plug.Conn.resp(404, "")
-    end)
+      Bypass.expect(bypass, "GET", "/2", fn conn ->
+        conn
+        |> Plug.Conn.resp(404, "")
+      end)
 
-    # test with 1 redirect allowed
-    assert {:error, "maximum number of redirect reached"} == HTTPStreamV2.fetch_status_follow_redirect(url, 1)
+      # test with 1 redirect allowed
+      assert {:error, "maximum number of redirect reached"} == HTTPStreamV2.fetch_status_follow_redirect(url, 1)
 
-    # test with 2 redirects allowed
-    assert {:ok, 404} == HTTPStreamV2.fetch_status_follow_redirect(url, 2)
-  end
+      # test with 2 redirects allowed
+      assert {:ok, 404} == HTTPStreamV2.fetch_status_follow_redirect(url, 2)
+    end
 
-  test "301 response, but location header not provided", %{bypass: bypass} do
-        Bypass.expect(bypass, "GET", "/", fn conn ->
-      conn
-      |> Plug.Conn.resp(301, "Mais ou est le header ?")
-    end)
+    test "redirect response, but location header not provided", %{bypass: bypass} do
+      Bypass.expect(bypass, "GET", "/", fn conn ->
+        conn
+        |> Plug.Conn.resp(301, "Mais ou est le header ?")
+      end)
 
-    url = "http://localhost:#{bypass.port}/"
-    assert {:ok, 301} == HTTPStreamV2.fetch_status_follow_redirect(url)
+      url = "http://localhost:#{bypass.port}/"
+      assert {:ok, 301} == HTTPStreamV2.fetch_status_follow_redirect(url)
+    end
   end
 end
