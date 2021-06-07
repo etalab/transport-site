361009460c1cf1a288b10696db9a44d0c65d99d5
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-06-07 13:44:01 +0200

    Move template to leex file for easier editing

--- apps/transport/lib/transport_web/live/backoffice/proxy_config_live.ex
+++ apps/transport/lib/transport_web/live/backoffice/proxy_config_live.ex
@@ -1,11 +1,7 @@
 defmodule TransportWeb.Backoffice.ProxyConfigLive do
   use Phoenix.LiveView
 
-  def render(assigns) do
-    ~L"HELLO <%= assigns[:hello] %>"
-  end
-
-  def mount(_params, _truc, socket) do
+  def mount(_params, _session, socket) do
     {:ok, assign(socket, :hello, "World")}
   end
 end
