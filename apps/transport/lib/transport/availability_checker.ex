5f6a4e8853e4a7b7a532b71d37c22e73c8d6d5e3
Francis <fchabouis@gmail.com>
2021-06-07 15:50:52 +0200

    change argument name

--- apps/transport/lib/transport/availability_checker.ex
+++ apps/transport/lib/transport/availability_checker.ex
@@ -5,19 +5,19 @@ defmodule Transport.AvailabilityChecker do
   """
 
   @spec available?(map() | binary()) :: boolean
-  def available?(target, use_stream_method \\ false)
+  def available?(target, use_http_streaming \\ false)
 
-  def available?(%{"url" => url}, use_stream_method), do: available?(url, use_stream_method)
+  def available?(%{"url" => url}, use_http_streaming), do: available?(url, use_http_streaming)
 
   def available?(url, false) when is_binary(url) do
     case HTTPoison.head(url, [], follow_redirect: true) do
       {:ok, %HTTPoison.Response{status_code: 200}} -> true
-      {:ok, %HTTPoison.Response{status_code: 405}} -> available?(url, _use_stream_method = true)
+      {:ok, %HTTPoison.Response{status_code: 405}} -> available?(url, _use_http_streaming = true)
       _ -> false
     end
   end
 
-  def available?(url, true = _use_stream_method) do
+  def available?(url, true = _use_http_streaming) do
     case HTTPStreamV2.fetch_status_follow_redirect(url) do
       {:ok, 200} -> true
       _ -> false
