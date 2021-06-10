2bba16c27fdea5c8088a2f5787de0d37c34d38f3
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-06-10 22:24:16 +0200

    Implement headers management
    This is a conservative approach based on a couple of feeds that we need to support right now.

--- apps/unlock/lib/controller.ex
+++ apps/unlock/lib/controller.ex
@@ -1,6 +1,15 @@
 defmodule Unlock.Controller do
   @moduledoc """
-  The main controller for proxying
+  The main controller for proxying, focusing on simplicity and runtime control.
+
+  This currently implements an in-RAM (Cachex) loading of the resource, with a reduced
+  set of headers that we will improve over time.
+
+  Future evolutions will very likely support FTP proxying, disk caching, custom headers.
+
+  Useful resources for later maintenance:
+  - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
+  - https://www.mnot.net/blog/2011/07/11/what_proxies_must_do
   """
 
   use Phoenix.Controller
@@ -9,6 +18,16 @@ defmodule Unlock.Controller do
   def index(conn, _params) do
     text(conn, "Unlock Proxy")
   end
+
+  # for now we use a whitelist which we'll gradually expand.
+  # make sure to avoid including "hop-by-hop" headers here.
+  @forwarded_headers_whitelist [
+    "content-type",
+    "content-length",
+    "date",
+    "last-modified",
+    "etag"
+  ]
 
   def fetch(conn, %{"id" => id}) do
     config = Application.fetch_env!(:unlock, :config_fetcher).fetch_config!()
@@ -20,11 +39,19 @@ defmodule Unlock.Controller do
 
     response = Unlock.HTTP.Client.impl().get!(resource.target_url, [])
 
-    # TODO: handle some response headers at least
-    # TODO: add a bit of in-memory caching
+    # TODO: add a bit of in-memory caching, but forbid too large payloads
     # TODO: handle errors by sending 502 bad gateway
     # TODO: integrate Sentry for error reporting
-    conn
+
+    prepare_response_headers(response.headers)
+    |> Enum.reduce(conn, fn {h,v}, c -> put_resp_header(c,h,v) end)
     |> send_resp(response.status, response.body)
+  end
+
+  # Inspiration (MIT) here https://github.com/tallarium/reverse_proxy_plug
+  defp prepare_response_headers(headers) do
+    headers
+    |> Enum.map(fn {h,v} -> {String.downcase(h) ,v} end)
+    |> Enum.filter(fn {h,_v} -> Enum.member?(@forwarded_headers_whitelist, h) end)
   end
 end
--- apps/unlock/test/controllers/unlock_controller_test.exs
+++ apps/unlock/test/controllers/unlock_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule Unlock.ControllerTest do
-  use ExUnit.Case
+  # async false until we stub Cachex calls or use per-test cache name
+  use ExUnit.Case, async: false
   use Plug.Test
   import Phoenix.ConnTest
   @endpoint Unlock.Endpoint
@@ -20,16 +21,62 @@ defmodule Unlock.ControllerTest do
 
   def setup_proxy_config(config) do
     Unlock.Config.Fetcher.Mock
-    |> stub(:fetch_config!, fn() -> config end)
+    |> stub(:fetch_config!, fn -> config end)
   end
 
   describe "GET /resource/:slug" do
     test "handles a regular read" do
-      # TODO: create clean struct for item
-      setup_proxy_config(%{"some-configure-slug" => stuff})
+      slug = "an-existing-identifier"
 
-      build_conn()
-      |> get("/resource/some-configured-slug")
+      setup_proxy_config(%{
+        slug => %Unlock.Config.Item{
+          identifier: slug,
+          target_url: target_url = "http://localhost/some-remote-resource",
+          ttl: 30
+        }
+      })
+
+      Unlock.HTTP.Client.Mock
+      |> expect(:get!, fn url, _headers = [] ->
+        assert url == target_url
+
+        %Unlock.HTTP.Response{
+          body: "somebody-to-love",
+          status: 207,
+          headers: [
+            {"content-type", "application/json"},
+            {"content-length", "7350"},
+            {"date", "Thu, 10 Jun 2021 19:45:14 GMT"},
+            # unwanted headers
+            {"x-amzn-request-id", "11111111-2222-3333-4444-f4c5846f0a85"},
+            {"x-cache", "Miss from cloudfront"}
+          ]
+        }
+      end)
+
+      resp =
+        build_conn()
+        |> get("/resource/an-existing-identifier")
+
+      assert resp.resp_body == "somebody-to-love"
+      assert resp.status == 207
+
+      # these ones are added by our pipeline for now
+      assert Plug.Conn.get_resp_header(resp, "x-request-id")
+      # we'll provide a finer grained algorithm later if useful, for now assume no cache is done
+      assert Plug.Conn.get_resp_header(resp, "cache-control") == [
+               "max-age=0, private, must-revalidate"
+             ]
+
+      remaining_headers =
+        resp.resp_headers
+        |> Enum.reject(fn {h, v} -> Enum.member?(["x-request-id", "cache-control"], h) end)
+
+      assert remaining_headers == [
+               {"content-type", "application/json"},
+               {"content-length", "7350"},
+               {"date", "Thu, 10 Jun 2021 19:45:14 GMT"}
+             ]
     end
 
     test "handles 404"
