08891f83ea400851ca88949e84740eb176be7a31
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-06-10 23:46:59 +0200

    Implement in-memory caching with hard RAM limit + 404, 500, 502 support

--- apps/unlock/lib/controller.ex
+++ apps/unlock/lib/controller.ex
@@ -15,6 +15,11 @@ defmodule Unlock.Controller do
   use Phoenix.Controller
   require Logger
 
+  defmodule ProxyCacheEntry do
+    @enforce_keys [:body, :headers, :status]
+    defstruct [:body, :headers, :status]
+  end
+
   def index(conn, _params) do
     text(conn, "Unlock Proxy")
   end
@@ -32,16 +37,33 @@ defmodule Unlock.Controller do
   def fetch(conn, %{"id" => id}) do
     config = Application.fetch_env!(:unlock, :config_fetcher).fetch_config!()
 
-    # TODO: handle 404 properly
-    resource = config |> Map.fetch!(id)
-    # TODO: handle 500 properly
-    Logger.info "Proxy match found for id #{id}"
+    resource = Map.get(config, id)
+    if resource do
+      conn
+      |> process_resource(resource)
+    else
+      conn
+      |> send_resp(404, "Not Found")
+    end
+  rescue
+    exception ->
+      # NOTE: handling this here for now because the main endpoint
+      # will otherwise send a full HTML error page. We will have to
+      # hook an unlock-specific handling for this instead.
+      # W
+      Logger.error("An exception occurred (#{exception |> inspect}")
+      conn
+      |> send_resp(500, "Internal Error")
+  end
 
-    response = Unlock.HTTP.Client.impl().get!(resource.target_url, [])
+  # We put a hard limit on what can be cached, and otherwise will just
+  # send back without caching. This means the remote server is less protected
+  # temporarily, but also that we do not blow up our whole architecture due to
+  # RAM consumption
+  @max_allowed_cached_byte_size 20 * 1024 * 1024
 
-    # TODO: add a bit of in-memory caching, but forbid too large payloads
-    # TODO: handle errors by sending 502 bad gateway
-    # TODO: integrate Sentry for error reporting
+  defp process_resource(conn, item) do
+    response = fetch_remote(item)
 
     prepare_response_headers(response.headers)
     |> Enum.reduce(conn, fn {h,v}, c -> put_resp_header(c,h,v) end)
@@ -49,6 +71,47 @@ defmodule Unlock.Controller do
     # if the content-type is incorrect, but is better than nothing.
     |> put_resp_header("content-disposition", "attachment")
     |> send_resp(response.status, response.body)
+  end
+
+  defp fetch_remote(item) do
+    comp_fn = fn _key ->
+      Logger.info("Processing proxy request for identifier #{item.identifier}")
+      try do
+        response = Unlock.HTTP.Client.impl().get!(item.target_url, [])
+        size = byte_size(response.body)
+        if size > @max_allowed_cached_byte_size do
+          Logger.warn("Payload is too large (#{size} bytes > #{@max_allowed_cached_byte_size}). Skipping cache.")
+          {:ignore, response}
+        else
+          {:commit, response}
+        end
+      rescue
+        e ->
+          # NOTE: if an error occurs around the HTTP query, then
+          # we want to track it down and return Bad Gateway
+          {:error, {:computation_error, e, __STACKTRACE__}}
+      end
+    end
+
+    cache_name = Unlock.Cachex
+    cache_key = "resource:#{item.identifier}"
+    {operation, result} = Cachex.fetch(cache_name, cache_key, comp_fn)
+    case operation do
+      :ok ->
+        Logger.info("Proxy response for #{item.identifier} served from cache")
+        result
+      :commit ->
+        Cachex.expire(cache_name, cache_key, :timer.seconds(item.ttl))
+        Logger.info("Value for key #{cache_key} regenerated (will expire in #{item.ttl} seconds)")
+        result
+      :ignore ->
+        Logger.info("Cache has been skipped for proxy response")
+        result
+      :error ->
+        # NOTE: we'll want to have some monitoring here, but not using Sentry
+        # because in case of troubles, we will blow up our quota.
+        %Unlock.HTTP.Response{status: 502, body: "Bad Gateway", headers: [{"content-type", "text/plain"}]}
+    end
   end
 
   # Inspiration (MIT) here https://github.com/tallarium/reverse_proxy_plug
