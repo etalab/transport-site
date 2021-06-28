b64fc0ba6e6cf71bf3157ecc79ef92134e78d88b
Francis <fchabouis@gmail.com>
2021-06-28 11:53:58 +0200

    http 204 (no content) is also ok
    this the response we get after a "delete"

--- apps/datagouvfr/lib/datagouvfr/client.ex
+++ apps/datagouvfr/lib/datagouvfr/client.ex
@@ -28,8 +28,7 @@ defmodule Datagouvfr.Client do
         Logger.debug(fn -> "response: #{inspect(response)}" end)
 
         case response do
-          {:ok, %{status_code: 200, body: body}} -> {:ok, body}
-          {:ok, %{status_code: 201, body: body}} -> {:ok, body}
+          {:ok, %{status_code: status_code, body: body}} when status_code in [200, 201, 204] -> {:ok, body}
           {:ok, %{status_code: _, body: body}} -> {:error, body}
           {:error, error} -> {:error, error}
         end
