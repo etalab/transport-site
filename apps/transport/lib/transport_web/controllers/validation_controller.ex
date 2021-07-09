f729acd9f2220900ac6eb44933ee2f4aa2349253
Lionel ANDRÉ <lionel.andre@gmail.com>
2021-07-09 14:27:38 +0200

    Affichage des alertes de validation sur une carte
    - Enregistrement de la données Validation.data_vis
    - Mutualisation de la factory permettant de construire les données geojson

--- apps/transport/lib/transport_web/controllers/validation_controller.ex
+++ apps/transport/lib/transport_web/controllers/validation_controller.ex
@@ -1,6 +1,7 @@
 defmodule TransportWeb.ValidationController do
   use TransportWeb, :controller
   alias DB.{Repo, Validation}
+  alias Transport.DataVisualization
 
   import TransportWeb.ResourceView, only: [issue_type: 1]
 
@@ -9,8 +10,6 @@ defmodule TransportWeb.ValidationController do
   @err HTTPoison.Error
   @timeout 180_000
 
-  @geojson_converter_url "https://convertisseur.transport.data.gouv.fr/gtfs2geojson_sync"
-
   defp endpoint, do: Application.fetch_env!(:transport, :gtfs_validator_url) <> "/validate"
 
   def index(%Plug.Conn{} = conn, _) do
@@ -18,30 +17,13 @@ defmodule TransportWeb.ValidationController do
   end
 
   def validate(%Plug.Conn{} = conn, %{"upload" => upload_params}) do
-    # geojson converter accepts only zip files
     file_path = upload_params["file"].path
-    file_path_with_extension = file_path <> ".zip"
-    File.rename(file_path, file_path_with_extension)
 
-    geojson_converter_response =
-      @client.post(
-        @geojson_converter_url,
-        {:multipart, [{:file, file_path_with_extension}]},
-        [{"content-type", "multipart/form-data;"}],
-        recv_timeout: @timeout
-      )
-
-    geojson_encoded =
-      case geojson_converter_response do
-        {:ok, %@res{status_code: 200, body: geojson_encoded}} -> geojson_encoded
-        _ -> nil
-      end
-
-    with {:ok, gtfs} <- File.read(file_path_with_extension),
-         {:ok, %@res{status_code: 200, body: body}} <-
-           @client.post(endpoint(), gtfs, [], recv_timeout: @timeout),
+    with {:ok, gtfs} <- File.read(file_path),
+         geojson <- DataVisualization.convert_to_geojson(gtfs),
+         {:ok, %@res{status_code: 200, body: body}} <- @client.post(endpoint(), gtfs, [], recv_timeout: @timeout),
          {:ok, %{"validations" => validations, "metadata" => metadata}} <- Jason.decode(body) do
-      data_vis = validation_data_vis(geojson_encoded, validations)
+      data_vis = DataVisualization.validation_data_vis(geojson, validations)
 
       %Validation{
         date: DateTime.utc_now() |> DateTime.to_string(),
@@ -62,102 +44,6 @@ defmodule TransportWeb.ValidationController do
         conn
         |> put_flash(:error, dgettext("validations", "Unable to validate file"))
         |> redirect(to: validation_path(conn, :index))
-    end
-  end
-
-  @spec validation_data_vis(any, any) :: nil | map
-  def validation_data_vis(nil, _), do: nil
-
-  def validation_data_vis(geojson_encoded, validations) do
-    case Jason.decode(geojson_encoded) do
-      {:ok, geojson} ->
-        data_vis_content(geojson, validations)
-
-      _ ->
-        %{}
-    end
-  end
-
-  def data_vis_content(geojson, validations) do
-    validations
-    |> Map.new(fn {issue_name, issues_list} ->
-      issues_map = get_issues_map(issues_list)
-
-      # create a map with with stop id as keys and geojson features as values
-      features_map =
-        geojson["features"]
-        |> Map.new(fn feature -> {feature["properties"]["id"], feature} end)
-
-      issues_geojson = get_issues_geojson(geojson, issues_map, features_map)
-
-      severity = issues_map |> Map.values() |> Enum.at(0) |> Map.get("severity")
-      # severity is used to customize the markers color in leaflet
-      {issue_name, %{"severity" => severity, "geojson" => issues_geojson}}
-    end)
-  end
-
-  def get_issues_map(issues_list) do
-    # create a map with stops id as keys and issue description as values
-    Map.new(issues_list, fn issue ->
-      simplified_issue = simplified_issue(issue)
-
-      {issue["object_id"], simplified_issue}
-    end)
-  end
-
-  def simplified_issue(issue) do
-    # keep only on related stop in related objects
-    issue
-    |> Map.update("related_objects", [], fn related_objects ->
-      related_objects |> Enum.filter(fn o -> o["object_type"] == "Stop" end) |> List.first()
-    end)
-  end
-
-  def get_issues_geojson(geojson, issues_map, features_map) do
-    # create a geojson for each issue type
-    Map.update(geojson, "features", [], fn _features ->
-      issues_map
-      |> Enum.flat_map(fn {id, issue} ->
-        features_from_issue(issue, id, features_map)
-      end)
-    end)
-  end
-
-  def features_from_issue(issue, id, features_map) do
-    # features contains a list of stops, related_stops and Linestrings
-    # Linestrings are used to link a stop and its related stop
-
-    case features_map[id] do
-      nil ->
-        []
-
-      feature ->
-        properties = Map.put(feature["properties"] || %{}, "details", Map.get(issue, "details"))
-        stop = Map.put(feature, "properties", properties)
-
-        case issue["related_objects"] do
-          %{"id" => id, "name" => _name} ->
-            related_stop = features_map[id]
-
-            stops_link = %{
-              "type" => "Feature",
-              "properties" => %{
-                "details" => Map.get(issue, "details")
-              },
-              "geometry" => %{
-                "type" => "LineString",
-                "coordinates" => [
-                  stop["geometry"]["coordinates"],
-                  related_stop["geometry"]["coordinates"]
-                ]
-              }
-            }
-
-            [stop, related_stop, stops_link]
-
-          _ ->
-            [stop]
-        end
     end
   end
 
@@ -182,7 +68,7 @@ defmodule TransportWeb.ValidationController do
           end
 
         data_vis = validation.data_vis[issue_type]
-        has_features = not (data_vis["geojson"]["features"] == [])
+        has_features = DataVisualization.has_features(data_vis["geojson"])
 
         encoded_data_vis =
           case {has_features, Jason.encode(data_vis)} do
