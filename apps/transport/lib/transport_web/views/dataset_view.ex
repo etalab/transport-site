9d5b2bc8c2d03b9e7700a53af6d23dd995aa5cb3
Francis Chabouis <fchabouis@gmail.com>
2021-08-16 14:11:41 +0200

    Fix bike scooter map (#1740)
    * add a test for the bikes and scooter stats api call
    
    * add scooter along bikes everywhere
    
    * fix test

--- apps/transport/lib/transport_web/views/dataset_view.ex
+++ apps/transport/lib/transport_web/views/dataset_view.ex
@@ -330,7 +330,7 @@ defmodule TransportWeb.DatasetView do
     |> Enum.max_by(fn r -> r.last_update end, fn -> nil end)
   end
 
-  def get_resource_to_display(%Dataset{type: "bike-sharing", resources: resources}) do
+  def get_resource_to_display(%Dataset{type: "bike-scooter-sharing", resources: resources}) do
     resources
     |> Enum.filter(fn r -> r.format == "gbfs" or String.ends_with?(r.url, "gbfs.json") end)
     |> Enum.reject(fn r -> r.is_community_resource end)
