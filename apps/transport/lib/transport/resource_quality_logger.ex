4e1c9effd4d9b3e44ccce69fc23adc07d6062c60
Francis <fchabouis@gmail.com>
2021-08-20 18:18:59 +0200

    log relevant quality information about the resources

---
+++ apps/transport/lib/transport/resource_quality_logger.ex
@@ -0,0 +1,22 @@
+defmodule Transport.ResourceQualityLogger do
+  alias DB.{LogsResourceQuality, Repo, Resource}
+
+  def inserts_all_resources_logs() do
+    stream = Resource |> Repo.stream()
+
+    Repo.transaction(fn ->
+      stream
+      |> Stream.map(fn resource ->
+        %{
+          resource_id: resource.id,
+          is_available: resource.is_available,
+          resource_end_date: resource.end_date,
+          log_date: DateTime.utc_now()
+        }
+      end)
+      |> Stream.chunk_every(500)
+      |> Stream.each(fn changesets -> Repo.insert_all(LogsResourceQuality, changesets) end)
+      |> Enum.to_list()
+    end)
+  end
+end
