930d06b42cae9822d0d35a882652a0ef79f6475a
Francis <fchabouis@gmail.com>
2021-08-20 11:18:14 +0200

    prepare resource_test for mox use

--- apps/db/test/resource_test.exs
+++ apps/db/test/resource_test.exs
@@ -1,6 +1,11 @@
 defmodule DB.ResourceTest do
   use ExUnit.Case, async: true
   alias DB.{Resource, Validation}
+  import Mox
 
   doctest Resource
+
+  setup :verify_on_exit!
+
+  test "validate and save a resource"
 end
