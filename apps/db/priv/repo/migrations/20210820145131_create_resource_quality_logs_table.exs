4a2d9104bc7189bf7b20dc27150828bd4930afbe
Francis <fchabouis@gmail.com>
2021-08-20 18:17:58 +0200

    create the DB table to log resource quality informations

---
+++ apps/db/priv/repo/migrations/20210820145131_create_resource_quality_logs_table.exs
@@ -0,0 +1,16 @@
+defmodule DB.Repo.Migrations.CreateResourceQualityLogsTable do
+  @moduledoc """
+  A table for logging information about all the PAN resources "quality",
+  ie availability, freshness, correctness, etc
+  """
+  use Ecto.Migration
+
+  def change do
+    create table(:logs_resource_quality) do
+      add :resource_id,  references(:resource)
+      add :log_date, :utc_datetime_usec
+      add :resource_end_date, :date
+      add :is_available, :boolean
+    end
+  end
+end
