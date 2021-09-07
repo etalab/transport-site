af485841bc6399f26da76efede9822454bc20ae7
Lionel ANDRÉ <lionel.andre@gmail.com>
2021-09-07 09:08:28 +0200

    Fix gtfs_validator_url configuration Fecth the value from System.get_env

--- config/gtfs_validator.exs
+++ config/gtfs_validator.exs
@@ -2,6 +2,5 @@ use Mix.Config
 
 # Configure GTFS Validator
 config :transport,
-       # by default, use the production validator. This can be overriden with dev.secret.exs
-       gtfs_validator_url: "https://transport-validator.cleverapps.io"
-#       gtfs_validator_url: System.get_env("GTFS_VALIDATOR_URL")
+   # by default, use the production validator. This can be overriden with dev.secret.exs
+   gtfs_validator_url: System.get_env("GTFS_VALIDATOR_URL", "https://transport-validator.cleverapps.io")
