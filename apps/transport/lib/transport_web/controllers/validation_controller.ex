7cddf5df635d98ae5b4f7fa9360625762331a71b
Francis <fchabouis@gmail.com>
2021-09-27 17:14:49 +0200

    update call to geojson converter

--- apps/transport/lib/transport_web/controllers/validation_controller.ex
+++ apps/transport/lib/transport_web/controllers/validation_controller.ex
@@ -45,14 +45,24 @@ defmodule TransportWeb.ValidationController do
   def convert(%Plug.Conn{} = conn, %{"upload" => upload_params}) do
     file_path = upload_params["file"].path
 
-    res = case Rambo.run("external-tools/gtfs-geojson", ["--input",  file_path]) do
-       {:ok, %Rambo{out: x}} -> x
-       x -> "errooooor"
-     end
+    conn =
+      case call_geojson_converter(file_path) do
+        {:ok, res} ->
+          conn
+          |> Plug.Conn.resp(200, res)
+        {:error, err_msg} -> conn |> Plug.Conn.resp(400, err_msg)
+      end
 
-     conn
-      |> Plug.Conn.resp(200, res)
-      |> Plug.Conn.send_resp()
+    conn
+    |> Plug.Conn.send_resp()
+  end
+
+  def call_geojson_converter(file_path) do
+    case Rambo.run("gtfs-geojson", ["--input", file_path]) do
+      {:ok, %Rambo{out: res}} -> {:ok, res}
+      {:error, %Rambo{err: err_msg}} -> {:error, err_msg}
+      {:error, _} = r -> r
+    end
   end
 
   def show(%Plug.Conn{} = conn, %{} = params) do
