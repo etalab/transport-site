cbf8efe9872df115cfa787149f1b5fbab0dde2c0
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-10-05 09:28:56 +0200

    Quick-fix pour exclure les tests nécessitant gtfs-to-geojson (sauf sur le CI) (#1822)
    * Hot-fix to exclude tests requiring specific tooling (only outside CI)
    
    See #1820
    
    * Fix incorrect CI value ("true" and not "1" here) + add doc

--- apps/transport/test/transport_web/controllers/geojson_conversion_controller_test.exs
+++ apps/transport/test/transport_web/controllers/geojson_conversion_controller_test.exs
@@ -4,6 +4,11 @@ defmodule TransportWeb.GeojsonConversionControllerTest do
   doctest TransportWeb.GeojsonConversionController
 
   describe "test the gtfs to geojson conversion" do
+    # NOTE: the :transport_tools tag is just a quick fix to completely skip the tests outside CI, until
+    # we implement a better version of this, allowing to skip by default yet allow local runs
+    # with specific versions (either compiled locally, or Docker-pulled)
+    # See https://github.com/etalab/transport-site/issues/1820
+    @tag :transport_tools
     test "check the conversion binary is accessible" do
       {:error, msg} = GeojsonConversionController.call_geojson_converter("")
 
@@ -11,6 +16,7 @@ defmodule TransportWeb.GeojsonConversionControllerTest do
       assert msg != "rambo exited with 0"
     end
 
+    @tag :transport_tools
     test "we can convert a gtfs" do
       {:ok, msg} = GeojsonConversionController.call_geojson_converter("#{__DIR__}/../../fixture/files/gtfs.zip")
       assert String.contains?(msg, "FeatureCollection")
