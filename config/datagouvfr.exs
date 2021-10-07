a11c9ce5bbc2297de8b964bec4454c3eb74590cb
Francis <fchabouis@gmail.com>
2021-10-07 14:25:15 +0200

    add some details

--- config/datagouvfr.exs
+++ config/datagouvfr.exs
@@ -10,6 +10,14 @@ config :transport,
   datagouvfr_transport_publisher_label: "Point d'Accès National transport.data.gouv.fr"
 
 # Configure data.gouv.fr authentication
+
+# transport acts as a client application of data.gouv.fr. When a user logs in on transport using its
+# data.gouv.fr credentials, it gives the possibility to the transport website to request a token to data.gouv.fr
+# allowing the user to e.g. post a comment on data.gouv.fr from the transport website.
+# Our application (transport) has a client_id and a client_secret used to authenticate our application
+# on data.gouv.fr's authorization server. There is no GUI to request or refresh those 2 informations : they have
+# been personnaly given to us by the data.gouv.fr team.
+
 config :oauth2, Authentication,
   strategy: Authentication,
   site: System.get_env("DATAGOUVFR_SITE"),
