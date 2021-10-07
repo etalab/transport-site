86ca3f923bf7507dee4f53d6e19620d748b4316a
Lionel ANDRÉ <lionel.andre@gmail.com>
2021-10-07 17:33:06 +0200

    Remove test data

--- apps/validator/lib/validator_test.ex
+++ apps/validator/lib/validator_test.ex
@@ -3,22 +3,10 @@ defmodule ValidatorTest do
 
   alias Validator.Gtfs.MobilityDataValidator
 
-  def validate_with_notices, do: execute_validation_and_parse_outputs("gtfs-notices.zip")
-
-  def validate_with_file_not_found,
-    do: execute_validation_and_parse_outputs("does_not_exists.zip")
-
-  def validate_without_notices, do: execute_validation_and_parse_outputs("gtfs-1-.zip")
-
-  defp execute_validation_and_parse_outputs(gtfs_filename),
+  def validate_gtfs(gtfs_file_path),
     do:
-      gtfs_filename
-      |> resolve_gtfs_path()
+      gtfs_file_path
       |> MobilityDataValidator.new()
       |> MobilityDataValidator.execute()
       |> MobilityDataValidator.parse_outputs()
-
-  defp gtfs_validator_directory, do: "/Users/lionel/Devs/projets/etalab/vendors/mobility_data"
-
-  defp resolve_gtfs_path(gtfs_filename), do: Path.join(gtfs_validator_directory(), gtfs_filename)
 end
