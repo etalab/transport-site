ef3078d524fa1bf355111692eb9e5db9280d6d8e
Francis <fchabouis@gmail.com>
2021-10-18 14:42:19 +0200

    oban migrations

---
+++ apps/db/priv/repo/migrations/20211018122851_add_oban_jobs_table.exs
@@ -0,0 +1,14 @@
+defmodule DB.Repo.Migrations.AddObanJobsTable do
+  use Ecto.Migration
+
+  def up do
+    # TODO check Oban migration policy
+    Oban.Migrations.up()
+  end
+
+  # We specify `version: 1` in `down`, ensuring that we'll roll all the way back down if
+  # necessary, regardless of which version we've migrated `up` to.
+  def down do
+    Oban.Migrations.down(version: 1)
+  end
+end
