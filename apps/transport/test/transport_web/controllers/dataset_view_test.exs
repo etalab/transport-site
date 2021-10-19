28c86815bc7f643598f5c4ebf79127144e7723fa
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2021-10-19 16:22:09 +0200

    Simple view test for get_resource_to_display

--- apps/transport/test/transport_web/controllers/dataset_view_test.exs
+++ apps/transport/test/transport_web/controllers/dataset_view_test.exs
@@ -17,4 +17,52 @@ defmodule TransportWeb.DatasetViewTest do
     dataset = %DB.Dataset{description: content}
     assert description(dataset) == {:safe, "<p>\n  coucou</p>\n"}
   end
+
+  test "resource to display for a low emission zone dataset" do
+    dataset_two_geojson = %DB.Dataset{
+      type: "low-emission-zones",
+      resources: [
+        %DB.Resource{
+          id: 1,
+          url: "https://example.com/zfe.geojson",
+          format: "geojson",
+          schema_name: "etalab/schema-zfe"
+        },
+        %DB.Resource{
+          id: 2,
+          url: "https://example.com/voies.geojson",
+          format: "geojson",
+          schema_name: "etalab/schema-zfe"
+        }
+      ]
+    }
+
+    dataset_title_geojson = %DB.Dataset{
+      type: "low-emission-zones",
+      resources: [
+        %DB.Resource{
+          id: 1,
+          url: "https://example.com/zfe.geojson",
+          format: "json",
+          title: "Export de la zone en GeoJSON"
+        }
+      ]
+    }
+
+    dataset_only_roads = %DB.Dataset{
+      type: "low-emission-zones",
+      resources: [
+        %DB.Resource{
+          id: 1,
+          url: "https://example.com/voies.geojson",
+          format: "json",
+          title: "Export des voies en GeoJSON"
+        }
+      ]
+    }
+
+    assert get_resource_to_display(dataset_two_geojson).id == 1
+    assert get_resource_to_display(dataset_title_geojson).id == 1
+    assert get_resource_to_display(dataset_only_roads) == nil
+  end
 end
