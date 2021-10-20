390f8873dbbcafa0f9f22769a3e7bd49d17f8125
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2021-10-20 15:40:17 +0200

    Refactor Smoove GBFS controller (#1867)
    * Refactor Smoove GBFS controller
    
    * Ajout test pour erreur HTTP 500
    
    * Refactor des tests
    
    * Suppression iconv

---
+++ apps/gbfs/test/gbfs/controllers/smoove_controller_test.exs
@@ -0,0 +1,61 @@
+defmodule GBFS.SmooveControllerTest do
+  use GBFS.ConnCase, async: true
+  alias GBFS.Router.Helpers, as: Routes
+  import Mox
+  import GBFS.Checker
+
+  setup :verify_on_exit!
+
+  describe "Smoove GBFS conversion" do
+    test "on gbfs.json", %{conn: conn} do
+      conn = conn |> get(Routes.montpellier_path(conn, :index))
+      body = json_response(conn, 200)
+      check_entrypoint(body)
+    end
+
+    test "on system_information.json", %{conn: conn} do
+      conn = conn |> get(Routes.montpellier_path(conn, :system_information))
+      body = json_response(conn, 200)
+      check_system_information(body)
+    end
+
+    test "on station_information.json", %{conn: conn} do
+      setup_stations_response()
+
+      conn = conn |> get(Routes.montpellier_path(conn, :station_information))
+      body = json_response(conn, 200)
+      check_station_information(body)
+    end
+
+    test "on station_status.json", %{conn: conn} do
+      setup_stations_response()
+
+      conn = conn |> get(Routes.montpellier_path(conn, :station_status))
+      body = json_response(conn, 200)
+      check_station_status(body)
+    end
+
+    test "on invalid response", %{conn: conn} do
+      Transport.HTTPoison.Mock |> expect(:get, fn _url -> {:ok, %HTTPoison.Response{status_code: 500}} end)
+
+      conn = conn |> get(Routes.montpellier_path(conn, :station_status))
+      assert %{"error" => "smoove service unavailable"} == json_response(conn, 502)
+    end
+
+    defp setup_stations_response do
+      Transport.HTTPoison.Mock
+      |> expect(:get, fn url ->
+        assert url == "https://data.montpellier3m.fr/sites/default/files/ressources/TAM_MMM_VELOMAG.xml"
+
+        {:ok,
+         %HTTPoison.Response{
+           status_code: 200,
+           body: """
+            <vcs ver="1"><sl><si na="001 Rue Jules Ferry - Gare Saint-Roch" id="001" la="43.605366" lg="3.881346" av="5" fr="7" to="12"></si></sl></vcs>
+           """,
+           headers: [{"Content-Type", "application/xml"}]
+         }}
+      end)
+    end
+  end
+end
