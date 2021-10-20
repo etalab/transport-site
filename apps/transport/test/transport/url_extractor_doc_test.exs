90fb6780152633fd95bcd5b9c808a753a3aabfba
Francis Chabouis <fchabouis@gmail.com>
2021-10-20 10:19:00 +0200

    Utilisation d'un Mock pour tester url_extractor.ex (#1863)
    * wip : add mock for httpoison
    
    * remove debug statements
    
    * put a correct variable case
    
    * no need to escape \n outside of a doctest !
    
    * use the real HTTPoison module where cassettes are in use

--- apps/transport/test/transport/url_extractor_doc_test.exs
+++ apps/transport/test/transport/url_extractor_doc_test.exs
@@ -1,5 +1,23 @@
 defmodule Transport.UrlExtractorDocTest do
   use ExUnit.Case, async: true
   alias Opendatasoft.UrlExtractor
+  import Mox
   doctest UrlExtractor
+
+  test "get urls in CSV file" do
+    Transport.HTTPoison.Mock |> expect(:head, fn _url -> %HTTPoison.Response{} end)
+
+    res =
+      [{"name,file\ntoulouse,http", %{"id" => "bob"}}, {"stop,lon,lat\n1,48.8,2.3", %{"id" => "bobette"}}]
+      |> UrlExtractor.get_resources_with_url_from_csv()
+
+    assert res == {:ok, [%{"url" => "http", "title" => "http", "id" => "bob"}]}
+  end
+
+  test "get urls in CSV file : no url available" do
+    Transport.HTTPoison.Mock |> expect(:head, fn _url -> %HTTPoison.Response{} end)
+
+    assert UrlExtractor.get_resources_with_url_from_csv([{"stop,lon,lat\\n1,48.8,2.3", %{"id" => "bob"}}]) ==
+             {:error, "No url found"}
+  end
 end
