1536d411c8f0256b112a106bbd42e19111b2ee8e
Francis <fchabouis@gmail.com>
2021-10-27 09:57:59 +0200

    add runtime.exs file

---
+++ config/runtime.exs
@@ -0,0 +1,10 @@
+import Config
+
+# config/runtime.exs is executed for all environments, including
+# during releases. It is executed after compilation and before the
+# system starts, so it is typically used to load production configuration
+# and secrets from environment variables or elsewhere. Do not define
+# any compile-time configuration in here, as it won't be applied.
+
+config :transport,
+  worker: System.get_env("WORKER") || raise "expected the WORKER environment variable to be set"
