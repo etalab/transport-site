97f9618a099afa95bc3d955c4f1cbbf6f03fa359
Francis <fchabouis@gmail.com>
2021-10-27 11:53:15 +0200

    set runtime config for prod and test env

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,17 +6,33 @@ import Config
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
 
-worker = System.get_env("WORKER") || raise "expected the WORKER environment variable to be set"
+if config_env() == :prod do
+  worker = System.get_env("WORKER") || raise "expected the WORKER environment variable to be set"
+  webserver = System.get_env("WEBSERVER") || raise "expected the WEBSERVER environment variable to be set"
 
-config :transport,
-  worker: worker
+  config :transport,
+    worker: worker,
+    webserver: webserver
 
-# scheduled jobs are run in production on the worker only
-# TODO handle case with multiple workers
-scheduled_jobs = case {config_env(), worker} do
-  {:prod, "1"} -> Transport.Scheduler.scheduled_jobs()
-  _ -> []
+  # Scheduled jobs are run in production on the worker only
+  # TODO handle case with multiple workers
+  scheduled_jobs = if worker == "1", do: Transport.Scheduler.scheduled_jobs(), else: []
+
+  config :transport, Transport.Scheduler,
+    jobs: scheduled_jobs
 end
 
-config :transport, Transport.Scheduler,
-  jobs: scheduled_jobs
+if config_env() == :dev do
+  # By default in dev, the application will be both a worker and a webserver
+  worker = System.get_env("WORKER") || "1"
+  webserver = System.get_env("WEBSERVER") || "1"
+
+  config :transport,
+    worker: worker,
+    webserver: webserver,
+end
+
+if config_env() != :test do
+  # TODO queue size could be defined with ENV variable
+  config :transport, Oban, queues: [default: 1]
+end
