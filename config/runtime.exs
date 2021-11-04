fcbd28d38e8d3818ff6e1e3453102fa030b63f87
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-11-04 15:54:08 +0100

    Improve runtime configuration
    The new layout allows to:
    - DRY most of the config steps
    - easily follow the Oban guidelines for testing (https://github.com/sorentwo/oban#testing)
    - ensure the test suite starts running again
    - disable Oban processing when under IEx

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,8 +1,6 @@
 import Config
 
-# TODO: this must be DRYed out
-# TODO: disable processing & plugins if IEx is started, no matter prod
-# or dev (https://github.com/sorentwo/oban/issues/520#issuecomment-883416363)
+require Logger
 
 # config/runtime.exs is executed for all environments, including
 # during releases. It is executed after compilation and before the
@@ -10,53 +8,66 @@ import Config
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
 
-if config_env() == :prod do
-  worker = System.get_env("WORKER") || raise "expected the WORKER environment variable to be set"
-  webserver = System.get_env("WEBSERVER") || raise "expected the WEBSERVER environment variable to be set"
+{worker, webserver} =
+  case config_env() do
+    :prod ->
+      {
+        System.get_env("WORKER") || raise("expected the WORKER environment variable to be set"),
+        System.get_env("WEBSERVER") || raise("expected the WEBSERVER variable to be set")
+      }
 
-  config :transport,
-    worker: worker,
-    webserver: webserver
+    :dev ->
+      # By default in dev, the application will be both a worker and a webserver
+      {
+        System.get_env("WORKER", "1"),
+        System.get_env("WEBSERVER", "1")
+      }
 
-  # Scheduled jobs are run in production on the worker only
-  # TODO handle case with multiple workers
-  scheduled_jobs = if worker == "1", do: Transport.Scheduler.scheduled_jobs(), else: []
+    :test ->
+      {
+        "0",
+        "0"
+      }
+  end
 
-  config :transport, Transport.Scheduler, jobs: scheduled_jobs
+# expose the result so that the application can configure itself from there
+config :transport,
+  worker: worker,
+  webserver: webserver
 
-  # TODO queue sizing should be configurable
-  queues = if worker == "1", do: [default: 1], else: false
-  config :transport, Oban, repo: DB.Repo, plugins: [Oban.Plugins.Pruner], queues: queues
+# Scheduled jobs (via Quantum at this point) are run in production and only on the first worker node
+# https://www.clever-cloud.com/doc/reference/reference-environment-variables/#set-by-the-deployment-process
+if config_env() == :prod && worker && System.fetch_env!("INSTANCE_NUMBER") == "0" do
+  config :transport, Transport.Scheduler, jobs: Transport.Scheduler.scheduled_jobs()
 end
 
+# Inside IEx, we do not want jobs to start processing, nor plugins working.
+# The jobs can be heavy and for instance in production, one person could
+# unknowningly create duplicate RAM heavy jobs. With this trick, we can still
+# enqueue jobs from IEx, but only the real worker will process them
+# See https://github.com/sorentwo/oban/issues/520#issuecomment-883416363
+
+iex_started? = Code.ensure_loaded?(IEx) && IEx.started?()
+base_oban_conf = [repo: DB.Repo]
+
+extra_oban_conf =
+  if worker != "1" || iex_started? || config_env() == :test do
+    [queues: false, plugins: false]
+  else
+    [
+      queues: [default: 25, heavy: 1],
+      plugins: [Oban.Plugins.Pruner]
+    ]
+  end
+
+config :transport, Oban, Keyword.merge(base_oban_conf, extra_oban_conf)
+
+# here we only override specific keys. As documented in https://hexdocs.pm/elixir/master/Config.html#config/2,
+# for keywords there is a recursive deep-merge, which should work nicely here.
 if config_env() == :dev do
-  # By default in dev, the application will be both a worker and a webserver
-  worker = System.get_env("WORKER") || "1"
-  webserver = System.get_env("WEBSERVER") || "1"
-
-  config :transport,
-    worker: worker,
-    webserver: webserver
-
-  queues = if worker == "1", do: [default: 1], else: false
-  config :transport, Oban, repo: DB.Repo, plugins: [Oban.Plugins.Pruner], queues: queues
-
-  watchers =
-    if webserver == "1" do
-      [npm: ["run", "--prefix", "apps/transport/client", "watch"]]
-    else
-      []
-    end
-
   config :transport, TransportWeb.Endpoint,
-    # optionally allowing to override the port is useful to play with 2 nodes locally
+    # optionally allowing to override the port is useful to play with 2 nodes locally, without conflict
     http: [port: System.get_env("PORT", "5000")],
-    debug_errors: true,
-    code_reloader: true,
-    check_origin: false,
-    # NOTE: this is required to ensure code reloading will work.
-    # A page reload is required to trigger this. More apps could
-    # be added when needed here, we just added what we needed.
-    reloadable_apps: [:shared, :db, :transport, :unlock],
-    watchers: watchers
+    #  We also make sure to start the assets watcher only if the webserver is up, to avoid cluttering the logs.
+    watchers: if(webserver == "1", do: [npm: ["run", "--prefix", "apps/transport/client", "watch"]], else: [])
 end
