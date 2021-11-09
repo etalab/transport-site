9328af7b1b8f6569d72c8fd6944a194d5c818a92
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2021-11-09 10:47:57 +0100

    Datagouvfr.Client.Reuses : gestion maintenance

--- apps/datagouvfr/lib/datagouvfr/client/reuses.ex
+++ apps/datagouvfr/lib/datagouvfr/client/reuses.ex
@@ -18,6 +18,9 @@ defmodule Datagouvfr.Client.Reuses do
 
       {:error, %{reason: reason}} ->
         {:error, "Unable to get reuses of dataset #{dataset_id} because of #{reason}"}
+
+      {:error, %Jason.DecodeError{}} ->
+        {:error, "Unable to get reuses of dataset #{dataset_id} could not decode JSON"}
     end
   end
 
