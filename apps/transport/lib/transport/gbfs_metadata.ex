30cc1da995c836bd7d23a5632b338cec266d1b4b
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-11-15 14:00:39 +0100

    Extract query to separate method to make it easier to invoke it from IEx

--- apps/transport/lib/transport/gbfs_metadata.ex
+++ apps/transport/lib/transport/gbfs_metadata.ex
@@ -8,14 +8,22 @@ defmodule Transport.GBFSMetadata do
   import Ecto.Query
   require Logger
 
+  @doc """
+  It is a bit of work, currently, to extract the list of `gbfs.json` endpoints,
+  for instance because `format` is not enough alone to filter them.
+
+  See https://github.com/etalab/transport-site/issues/1891#issuecomment-958888421 for some background.
+  """
+  def gbfs_feeds_query do
+    Resource
+    |> join(:inner, [r], d in Dataset, on: r.dataset_id == d.id)
+    |> where([_r, d], d.type == "bike-scooter-sharing" and d.is_active)
+    |> where([r, _d], like(r.url, "%gbfs.json") or r.format == "gbfs")
+    |> where([r, _d], not fragment("? ~ ?", r.url, "station|free_bike"))
+  end
+
   def set_gbfs_feeds_metadata do
-    resources =
-      Resource
-      |> join(:inner, [r], d in Dataset, on: r.dataset_id == d.id)
-      |> where([_r, d], d.type == "bike-scooter-sharing" and d.is_active)
-      |> where([r, _d], like(r.url, "%gbfs.json") or r.format == "gbfs")
-      |> where([r, _d], not fragment("? ~ ?", r.url, "station|free_bike"))
-      |> Repo.all()
+    resources = gbfs_feeds_query() |> Repo.all()
 
     Logger.info("Fetching details about #{Enum.count(resources)} GBFS feeds")
 
