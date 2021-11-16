a98f5616aad185c3e0cc5a45e2c90d7a198850a6
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-11-16 18:45:31 +0100

    Save finalized version

--- zip_checksum.exs
+++ zip_checksum.exs
@@ -1,8 +1,46 @@
 Mix.install([
-  {:req, "~> 0.2.0"}
+  {:req, "~> 0.2.0"},
+  {:unzip, "~> 0.6.0"}
 ])
 
-defmodule Download do
+defmodule ZipMetaDataExtractor do
+  @moduledoc """
+  A module to extract the metadata out of a zip file on disk. It relies on `unzip`
+  which is able to stream the content of each file, allowing us to easily compute a
+  SHA256 for each entry.
+  """
+  def extract!(file) do
+    zip_file = Unzip.LocalFile.open(file)
+    {:ok, unzip} = Unzip.new(zip_file)
+    # NOTE: `unzip.cd_list` contains crc + filenames & more info, if needed
+    unzip
+    |> Unzip.list_entries
+    |> Enum.map(&enrich(&1, unzip))
+  end
+
+  def enrich(entry, unzip) do
+    algorithm = :sha256
+    checksum = unzip
+    |> Unzip.file_stream!(entry.file_name)
+    |> compute_checksum(algorithm)
+
+    Map.put(entry, algorithm, checksum)
+  end
+
+  def compute_checksum(stream, algorithm) do
+    stream
+    |> Enum.reduce(:crypto.hash_init(algorithm), fn(elm, acc) -> :crypto.hash_update(acc, elm) end)
+    |> :crypto.hash_final()
+    |> Base.encode16()
+    |> String.downcase()
+  end
+
+end
+
+defmodule DemoDownload do
+  @moduledoc """
+  A quick helper to download a given file on disk
+  """
   def download do
     IO.puts("Starting....")
 
@@ -19,23 +57,12 @@ defmodule Download do
     IO.puts("Writing #{filename}...")
     File.write!(filename, response.body)
   end
-
-  def analyse(file) do
-    IO.puts("Analyzing file #{file}...")
-
-    {:ok, result} = :zip.list_dir(file |> to_char_list)
-
-    result
-    |> Enum.reject(&match?({:zip_comment, _}, &1))
-    |> IO.inspect(IEx.inspect_opts())
-
-    {output, 0} = System.cmd("unzip", ["-lv", file])
-    IO.puts(output)
-
-    IO.puts("Done")
-  end
 end
 
-Path.wildcard("*.zip")
-# |> Enum.take(1)
-|> Enum.each(&Download.analyse/1)
+# Uncomment and run twice to get files with different external checksums,
+# but same content inside
+# Download.download()
+
+Path.wildcard("save-*.zip")
+|> Enum.map(&ZipMetaDataExtractor.extract!/1)
+|> IO.inspect(IEx.inspect_opts)
