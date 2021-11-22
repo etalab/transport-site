8dab79d41d2319fa7118476b44e86d724fae3a5b
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-11-22 16:13:05 +0100

    Add boilerplate for integration test [skip ci]

--- apps/transport/test/transport_web/routing/proxy_routing_test.exs
+++ apps/transport/test/transport_web/routing/proxy_routing_test.exs
@@ -1,6 +1,7 @@
 defmodule TransportWeb.ProxyRoutingTest do
   use ExUnit.Case, async: true
   import Phoenix.ConnTest
+  import Mox
   @endpoint TransportWeb.Endpoint
 
   test "accepts proxy. subdomain calls and delegates them to unlock" do
@@ -13,5 +14,17 @@ defmodule TransportWeb.ProxyRoutingTest do
     assert h
 
     assert text_response(conn, 200) =~ ~r/Unlock/
+  end
+
+  @tag :focus
+  test "generates telemetry events" do
+    config = %{"some-slug" => %{}}
+
+    Unlock.Config.Fetcher.Mock
+    |> stub(:fetch_config!, fn -> config end)
+
+    build_conn()
+    |> Map.put(:host, "proxy.example.com")
+    |> get("/resource/some-slug")
   end
 end
