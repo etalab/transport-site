88847ec222b5d950a856319ca352d2a66622defb
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-11-22 12:01:16 +0100

    Add schema

---
+++ apps/db/lib/db/proxy_metric.ex
@@ -0,0 +1,17 @@
+defmodule DB.ProxyMetric do
+  @moduledoc """
+  A quick proxy metric schema to store bucketed events, until a moment may come
+  where we'll outgrow this and use timescaledb or similar instead.
+  """
+  use Ecto.Schema
+  use TypedEctoSchema
+
+  typed_schema "proxy_metrics" do
+    field(:resource_identifier, :string)
+    field(:event, :string)
+    field(:period, :utc_datetime)
+    field(:count, :integer)
+
+    timestamps(type: :utc_datetime_usec)
+  end
+end
