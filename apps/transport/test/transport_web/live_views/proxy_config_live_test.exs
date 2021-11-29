f0d48b5239bea2e9b86198b21ab2227512229a96
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-11-29 17:38:32 +0100

    Add a first test that would have caught the bug I wrote earlier

---
+++ apps/transport/test/transport_web/live_views/proxy_config_live_test.exs
@@ -0,0 +1,75 @@
+defmodule TransportWeb.Backoffice.ProxyConfigLiveTest do
+  use ExUnit.Case, async: true
+
+  import Phoenix.ConnTest
+  import Phoenix.LiveViewTest
+  @endpoint TransportWeb.Endpoint
+  import Mox
+
+  setup do
+    {:ok, conn: build_conn()}
+  end
+
+  def setup_admin_in_session(conn) do
+    conn
+    |> init_test_session(%{
+      current_user: %{
+        "organizations" => [%{"slug" => "equipe-transport-data-gouv-fr"}]
+      }
+    })
+  end
+
+  def setup_proxy_config(slug) do
+    config = %{
+      slug => %Unlock.Config.Item{
+        identifier: slug,
+        target_url: "http://localhost/some-remote-resource",
+        ttl: 10
+      }
+    }
+
+    Unlock.Config.Fetcher.Mock
+    |> stub(:fetch_config!, fn -> config end)
+  end
+
+  test "requires login", %{conn: conn} do
+    conn = get(conn, "/backoffice/proxy-config")
+    assert html_response(conn, 302)
+  end
+
+  test "disconnected and connected mount", %{conn: conn} do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+
+    item_id = "slug"
+    setup_proxy_config(item_id)
+
+    # NOTE: this fakes previous proxy requests, without having to
+    # setup a complete scenario, to prepare the data for the test below
+    Unlock.Controller.Telemetry.trace_request(item_id, :external)
+    Unlock.Controller.Telemetry.trace_request(item_id, :external)
+    Unlock.Controller.Telemetry.trace_request(item_id, :internal)
+
+    conn = setup_admin_in_session(conn)
+    conn = get(conn, "/backoffice/proxy-config")
+
+    response = html_response(conn, 200)
+    assert response =~ "Configuration du Proxy"
+
+    doc = Floki.parse_document!(response)
+    headers = Floki.find(doc, "table thead tr th") |> Enum.map(&Floki.text/1)
+    row = Floki.find(doc, "table tbody tr td") |> Enum.map(&Floki.text/1)
+
+    data = Enum.zip(headers, row) |> Enum.into(%{})
+
+    # NOTE: we might need a sleep here if the assertion fails, because
+    # trace_request is making async calls at time of writings, and the counts
+    # may end up being incorrect
+    assert %{
+             "Identifiant" => "slug",
+             "Req ext 7j" => "2",
+             "Req int 7j" => "1"
+           } = data
+
+    {:ok, _view, _html} = live(conn)
+  end
+end
