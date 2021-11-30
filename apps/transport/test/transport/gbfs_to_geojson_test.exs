11ceec185c4724cac40bd381ed68d6f6a411f45d
Francis Chabouis <fchabouis@gmail.com>
2021-11-30 10:19:43 +0100

    add 2 tests for station_information and station_status

--- apps/transport/test/transport/gbfs_to_geojson_test.exs
+++ apps/transport/test/transport/gbfs_to_geojson_test.exs
@@ -6,30 +6,96 @@ defmodule Transport.GbfsToGeojsonTest do
 
   describe "gbfs to geojson conversion" do
     test "station_information conversion" do
-    gbfs_endpoint = "gbfs.json"
-    Transport.HTTPoison.Mock |> expect(:get!, fn ^gbfs_endpoint ->
-      %{status_code: 200, body: """
-        {
-        "data": {
-          "fr": {
-            "feeds": [
-              {
-                "name": "station_information",
-                "url": "station_information.json"
-              }
-            ]
-          }
-        }
-      }
-      """
-      }
-    end)
+      gbfs_endpoint = "gbfs.json"
+      feeds = [%{"name" => "station_information", "url" => "station_information.json"}]
 
-    set_station_information_expect()
+      set_gbfs_entrypoint_expect(gbfs_endpoint, feeds)
+      set_station_information_expect()
 
-    geojsons = Transport.GbfsToGeojson.gbfs_geojsons(gbfs_endpoint)
-    assert geojsons["stations"] ==
-      [
+      geojsons = Transport.GbfsToGeojson.gbfs_geojsons(gbfs_endpoint)
+      assert geojsons["stations"] == simple_station_information_geojson()
+    end
+
+    test "station_information + station_status conversion" do
+      gbfs_endpoint = "gbfs.json"
+
+      feeds = [
+        %{"name" => "station_information", "url" => "station_information.json"},
+        %{"name" => "station_status", "url" => "station_status.json"}
+      ]
+
+      set_gbfs_entrypoint_expect(gbfs_endpoint, feeds)
+      set_station_information_expect()
+      set_station_status_expect()
+
+      geojsons = Transport.GbfsToGeojson.gbfs_geojsons(gbfs_endpoint)
+
+      assert geojsons["stations"] ==
+               %{
+                 "type" => "FeatureCollection",
+                 "features" => [
+                   %{
+                     "geometry" => %{
+                       "coordinates" => ["-2.3353", "47.26095"],
+                       "type" => "Point"
+                     },
+                     "properties" => %{
+                       "name" => "Rond-point de l'Hippodrome",
+                       "station_id" => "1",
+                       "station_status" => %{
+                         "num_bikes_available" => 0,
+                         "num_docks_available" => 0,
+                         "a_field" => "coucou"
+                       }
+                     },
+                     "type" => "Feature"
+                   },
+                   %{
+                     "geometry" => %{
+                       "coordinates" => ["-2.345135", "47.270352"],
+                       "type" => "Point"
+                     },
+                     "properties" => %{
+                       "name" => "Gare de Pornichet",
+                       "station_id" => "2",
+                       "station_status" => %{
+                         "num_bikes_available" => 3,
+                         "num_docks_available" => 12,
+                         "num_vehicles_available" => 3
+                       }
+                     },
+                     "type" => "Feature"
+                   }
+                 ]
+               }
+    end
+
+
+    test "station_information + failing station_status conversion" do
+      gbfs_endpoint = "gbfs.json"
+
+      feeds = [
+        %{"name" => "station_information", "url" => "station_information.json"},
+        %{"name" => "station_status", "url" => "station_status.json"}
+      ]
+
+      set_gbfs_entrypoint_expect(gbfs_endpoint, feeds)
+      set_station_information_expect()
+      set_station_status_expect("bad response")
+
+      geojsons = Transport.GbfsToGeojson.gbfs_geojsons(gbfs_endpoint)
+
+      # if the station_status is not valid, you should still have a basic geojson
+      # from station_information
+      assert geojsons["stations"] == simple_station_information_geojson()
+    end
+
+  end
+
+  defp simple_station_information_geojson() do
+    %{
+      "type" => "FeatureCollection",
+      "features" => [
         %{
           "geometry" => %{
             "coordinates" => ["-2.3353", "47.26095"],
@@ -37,7 +103,7 @@ defmodule Transport.GbfsToGeojsonTest do
           },
           "properties" => %{
             "name" => "Rond-point de l'Hippodrome",
-            "station_id" => "stn_VWFEvC9XWrvmE2m7kkFkKo"
+            "station_id" => "1"
           },
           "type" => "Feature"
         },
@@ -48,36 +114,92 @@ defmodule Transport.GbfsToGeojsonTest do
           },
           "properties" => %{
             "name" => "Gare de Pornichet",
-            "station_id" => "stn_TdAEzFgrkvHXmQ3U8nRxtt"
+            "station_id" => "2"
           },
           "type" => "Feature"
         }
       ]
-    end
+    }
+  end
+
+  defp set_gbfs_entrypoint_expect(entrypoint_url, feeds) do
+    Transport.HTTPoison.Mock
+    |> expect(:get!, fn ^entrypoint_url ->
+      %{status_code: 200, body: gbfs_entry_point(feeds)}
+    end)
+  end
+
+  defp gbfs_entry_point(feeds) do
+    %{
+      "data" => %{
+        "fr" => %{
+          "feeds" => feeds
+        }
+      }
+    }
+    |> Jason.encode!()
   end
 
   defp set_station_information_expect() do
-    Transport.HTTPoison.Mock |> expect(:get!, fn "station_information.json" ->
-    %{status_code: 200, body: """
-      {
-        "data": {
-          "stations": [
-            {
-              "lat": "47.26095",
-              "lon": "-2.3353",
-              "name": "Rond-point de l'Hippodrome",
-              "station_id": "stn_VWFEvC9XWrvmE2m7kkFkKo"
-            },
-            {
-              "lat": "47.270352",
-              "lon": "-2.345135",
-              "name": "Gare de Pornichet",
-              "station_id": "stn_TdAEzFgrkvHXmQ3U8nRxtt"
-            }
-          ]
+    Transport.HTTPoison.Mock
+    |> expect(:get!, fn "station_information.json" ->
+      %{
+        status_code: 200,
+        body: """
+        {
+          "data": {
+            "stations": [
+              {
+                "lat": "47.26095",
+                "lon": "-2.3353",
+                "name": "Rond-point de l'Hippodrome",
+                "station_id": "1"
+              },
+              {
+                "lat": "47.270352",
+                "lon": "-2.345135",
+                "name": "Gare de Pornichet",
+                "station_id": "2"
+              }
+            ]
+          }
         }
+        """
       }
-      """
+    end)
+  end
+
+  defp set_station_status_expect(custom_response \\ nil) do
+
+    Transport.HTTPoison.Mock
+    |> expect(:get!, fn "station_status.json" ->
+      custom_response ||
+      %{
+        status_code: 200,
+        body: """
+        {
+          "data": {
+            "stations": [
+              {
+                "num_bikes_available": 0,
+                "num_docks_available": 0,
+                "a_field": "coucou",
+                "station_id": "1"
+              },
+              {
+                "num_bikes_available": 3,
+                "num_docks_available": 12,
+                "num_vehicles_available": 3,
+                "station_id": "2"
+              },
+              {
+                "num_bikes_available": 16,
+                "station_id": "does not exist"
+              }
+            ]
+          }
+        }
+        """
       }
     end)
   end
