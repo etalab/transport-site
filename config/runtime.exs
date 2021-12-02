927383cc2157f5f56b7383cf8d34e530ede95889
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2021-12-02 14:50:11 +0100

    Ajout APP_ENV en production (#1949)
    * Ajout APP_ENV en production
    
    * Move to runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -52,6 +52,25 @@ if config_env() == :prod && !iex_started? && worker && System.fetch_env!("INSTAN
   config :transport, Transport.Scheduler, jobs: Transport.Scheduler.scheduled_jobs()
 end
 
+# Make sure that APP_ENV is set in production to distinguish
+# production and staging (both running with MIX_ENV=prod)
+# See https://github.com/etalab/transport-site/issues/1945
+app_env = System.get_env("APP_ENV", "") |> String.to_atom()
+app_env_is_valid = Enum.member?([:production, :staging], app_env)
+
+if config_env() == :prod and not app_env_is_valid do
+  raise("APP_ENV must be set to production or staging while in production")
+end
+
+config :transport,
+  app_env: app_env
+
+app_env_file = Path.join(__DIR__, "#{app_env}.exs")
+
+if File.exists?(app_env_file) do
+  import_config app_env_file
+end
+
 base_oban_conf = [repo: DB.Repo]
 
 extra_oban_conf =
