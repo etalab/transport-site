6ae8e5747d809869bc1bba755c86a53dbc32e182
Thibaut Barrère <thibaut.barrere@gmail.com>
2021-12-06 16:50:56 +0100

    Fix typo

--- apps/transport/lib/transport_web/router.ex
+++ apps/transport/lib/transport_web/router.ex
@@ -62,7 +62,7 @@ defmodule TransportWeb.Router do
   end
 
   scope "/", TransportWeb do
-    pipe_through(:browser, :stop_on_worker_only_node)
+    pipe_through([:browser, :stop_on_worker_only_node])
 
     get("/", PageController, :index)
     get("/real_time", PageController, :real_time)
