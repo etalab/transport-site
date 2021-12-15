3fae68a41af50d230de2d00f1201acf2e2fa899e
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2021-12-15 10:20:57 +0100

    Remove defmock

--- apps/datagouvfr/test/support/mocks.ex
+++ apps/datagouvfr/test/support/mocks.ex
@@ -1,3 +1,2 @@
 Mox.defmock(Datagouvfr.Client.CommunityResources.Mock, for: Datagouvfr.Client.CommunityResources)
 Mox.defmock(Transport.HTTPoison.Mock, for: HTTPoison.Base)
-Mox.defmock(Transport.GBFSMetadata.Mock, for: Transport.GBFSMetadata.Wrapper)
