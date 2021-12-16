c9c5088349296cd280c3d627db9df5aa7d49ec2f
Francis Chabouis <fchabouis@gmail.com>
2021-12-16 14:22:11 +0100

    test enqueue jobs

---
+++ apps/transport/test/transport/jobs/gtfs_to_geojson_converter_job_test.exs
@@ -0,0 +1,43 @@
+defmodule Transport.Jobs.GtfsToGeojsonConverterJobTest do
+  use ExUnit.Case, async: true
+  use Oban.Testing, repo: DB.Repo
+  import DB.Factory
+  # import Ecto.Query
+  # import Mox
+
+  alias Transport.Jobs.{GtfsToGeojsonConverterJob, ResourceHistoryJob, SingleGtfsToGeojsonConverterJob}
+  alias Transport.Test.S3TestUtils
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+    # DB.Repo.delete_all(DB.ResourceHistory)
+    :ok
+  end
+
+  # setup :verify_on_exit!
+  test "gtfs to gejson jobs are enqueued" do
+    %{id: resource_history_id} =
+      insert(:resource_history,
+        datagouv_id: "1",
+        payload: %{"format" => "GTFS"}
+      )
+
+    insert(:resource_history,
+      datagouv_id: "2",
+      payload: %{"format" => "NeTEx"}
+    )
+
+    insert(:resource_history,
+      datagouv_id: "3",
+      payload: %{}
+    )
+
+    S3TestUtils.s3_mocks_create_bucket()
+    :ok = GtfsToGeojsonConverterJob.perform(%{})
+
+    assert_enqueued(
+      [worker: SingleGtfsToGeojsonConverterJob, args: %{"resource_history_id" => resource_history_id}],
+      100
+    )
+  end
+end
