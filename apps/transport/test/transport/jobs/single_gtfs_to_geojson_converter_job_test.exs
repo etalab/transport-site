9e3e3ca07de5468108b8347a423a7f48d6cd0c11
Francis Chabouis <fchabouis@gmail.com>
2021-12-16 15:50:57 +0100

    add a test for a successful conversion

--- apps/transport/test/transport/jobs/single_gtfs_to_geojson_converter_job_test.exs
+++ apps/transport/test/transport/jobs/single_gtfs_to_geojson_converter_job_test.exs
@@ -1,6 +1,7 @@
 defmodule Transport.Jobs.SingleGtfsToGeojsonConverterJobTest do
   use ExUnit.Case, async: true
   import DB.Factory
+  import Mox
   # alias Transport.Jobs.{GtfsToGeojsonConverterJob, ResourceHistoryJob, SingleGtfsToGeojsonConverterJob}
 
   setup do
@@ -38,5 +39,58 @@ defmodule Transport.Jobs.SingleGtfsToGeojsonConverterJobTest do
              Transport.Jobs.SingleGtfsToGeojsonConverterJob.perform(%{
                args: %{"resource_history_id" => resource_history_id}
              })
+  end
+
+  test "launch a conversion" do
+    permanent_url = "https://resource.fr"
+    uuid = "some-nice-uuid"
+
+    # add a resource history
+    %{id: resource_history_id} =
+      insert(:resource_history,
+        datagouv_id: "2",
+        payload: %{"uuid" => uuid, "format" => "GTFS", "permanent_url" => permanent_url, "filename" => "fff"}
+      )
+
+    # mock for the resource download
+    Transport.HTTPoison.Mock
+    |> expect(:get!, 1, fn ^permanent_url, [], [follow_redirect: true] ->
+      %{status_code: 200, body: "this is my GTFS file"}
+    end)
+
+    # mock for the resource conversion
+    Transport.Rambo.Mock
+    |> expect(:run, 1, fn _binary_path, opts ->
+      assert(["--input", _file_path, "--output", geojson_file_path] = opts)
+      File.write!(geojson_file_path, "this my geojson content")
+      {:ok, "this my geojson content"}
+    end)
+
+    # mock for the aws upload
+    Transport.ExAWS.Mock
+    |> expect(:request!, fn %{
+                              service: :s3,
+                              http_method: :put,
+                              path: path,
+                              bucket: _bucket_name,
+                              body: _content,
+                              headers: %{"x-amz-acl" => "public-read"}
+                            } ->
+      assert path |> String.starts_with?("conversions/gtfs-to-geojson/")
+    end)
+
+    # job succeed
+    assert :ok ==
+             Transport.Jobs.SingleGtfsToGeojsonConverterJob.perform(%{
+               args: %{"resource_history_id" => resource_history_id}
+             })
+
+    # a data_conversion row is recorded ✌️‍
+    DB.DataConversion
+    |> DB.Repo.get_by!(
+      convert_from: "GTFS",
+      convert_to: "GeoJSON",
+      resource_history_uuid: uuid
+    )
   end
 end
