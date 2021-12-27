10f6bb4cc902702f508a553ccdc1997281b4b492
Francis Chabouis <fchabouis@gmail.com>
2021-12-27 18:39:02 +0100

    add a test for it

--- apps/transport/test/transport/jobs/gtfs_to_geojson_converter_job_test.exs
+++ apps/transport/test/transport/jobs/gtfs_to_geojson_converter_job_test.exs
@@ -2,6 +2,7 @@ defmodule Transport.Jobs.GtfsToGeojsonConverterJobTest do
   use ExUnit.Case, async: true
   use Oban.Testing, repo: DB.Repo
   import DB.Factory
+  import Mox
 
   alias Transport.Jobs.{GtfsToGeojsonConverterJob, SingleGtfsToGeojsonConverterJob}
   alias Transport.Test.S3TestUtils
@@ -10,7 +11,8 @@ defmodule Transport.Jobs.GtfsToGeojsonConverterJobTest do
     Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
   end
 
-  # setup :verify_on_exit!
+  setup :verify_on_exit!
+
   test "gtfs to gejson jobs are enqueued" do
     %{id: resource_history_id} =
       insert(:resource_history,
@@ -28,11 +30,27 @@ defmodule Transport.Jobs.GtfsToGeojsonConverterJobTest do
       payload: %{}
     )
 
+    # this resource_history should not get enqueued for conversion,
+    # as a matching data_conversion already exists
+    %{id: not_enqueued_resource_history_id} =
+      insert(:resource_history,
+        datagouv_id: "4",
+        payload: %{"format" => "GTFS", uuid: uuid = Ecto.UUID.generate()}
+      )
+
+    # here is the matching data conversion
+    insert(:data_conversion, convert_from: "GTFS", convert_to: "GeoJSON", resource_history_uuid: uuid, payload: %{})
+
     S3TestUtils.s3_mocks_create_bucket()
     :ok = GtfsToGeojsonConverterJob.perform(%{})
 
     assert_enqueued(
       [worker: SingleGtfsToGeojsonConverterJob, args: %{"resource_history_id" => resource_history_id}],
+      100
+    )
+
+    refute_enqueued(
+      [worker: SingleGtfsToGeojsonConverterJob, args: %{"resource_history_id" => not_enqueued_resource_history_id}],
       100
     )
   end
