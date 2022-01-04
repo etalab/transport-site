4e91846fa1d121f66f01ce2343adb4e293cfc704
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-01-04 10:00:53 +0100

    Ajout de classes interagissant avec schema.datagouv et JSON Schema  (#2021)
    * Work on Transport.Shared.Schemas
    
    * Work on JSONSchemaValidator
    
    * Add validate method
    
    * Refactor tests

---
+++ apps/shared/lib/validation/jsonschema_validator.ex
@@ -0,0 +1,47 @@
+defmodule Shared.Validation.JSONSchemaValidator do
+  @moduledoc """
+  Works with JSON schemas:
+  - load them from schema.data.gouv.fr
+  - validate payloads
+  """
+  import Transport.Shared.Schemas
+
+  defmodule ErrorFormatter do
+    @moduledoc """
+    Format JSON Schema errors.
+
+    See https://hexdocs.pm/ex_json_schema/readme.html#validation-error-formats
+    """
+    alias ExJsonSchema.Validator.Error
+
+    @spec format(ExJsonSchema.Validator.errors()) :: [String.t()]
+    def format(errors) do
+      errors
+      |> Enum.map(fn %Error{error: error, path: path} ->
+        "#{path}: #{to_string(error)}"
+      end)
+      |> Enum.sort()
+    end
+  end
+
+  def load_jsonschema_for_schema(schema_name) do
+    unless Enum.member?(json_schemas_names(), schema_name) do
+      raise "#{schema_name} is not a JSONSchema"
+    end
+
+    schema_name |> read_latest_schema() |> ExJsonSchema.Schema.resolve()
+  end
+
+  @spec validate(ExJsonSchema.Schema.Root.t(), map()) :: map()
+  def validate(schema, payload) do
+    errors =
+      case ExJsonSchema.Validator.validate(schema, payload, error_formatter: ErrorFormatter) do
+        :ok -> []
+        {:error, errors} -> errors
+      end
+
+    %{"has_errors" => not Enum.empty?(errors), "errors_count" => Enum.count(errors), "errors" => errors}
+  end
+
+  defp json_schemas_names, do: Map.keys(schemas_by_type("jsonschema"))
+end
---
+++ apps/shared/test/support/cache_case.ex
@@ -0,0 +1,23 @@
+defmodule Shared.CacheCase do
+  @moduledoc """
+  This module defines the test case to be used by
+  tests that require access to the Cachex cache.
+  """
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      use ExUnit.Case, async: false
+      import Shared.Application, only: [cache_name: 0]
+      import Mox
+
+      setup :verify_on_exit!
+      setup :set_mox_from_context
+
+      setup do
+        Cachex.clear(cache_name())
+        on_exit(fn -> Cachex.clear(cache_name()) end)
+      end
+    end
+  end
+end
