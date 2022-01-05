638c48ba812fb36f36da109378597e4150a624ae
Francis Chabouis <fchabouis@gmail.com>
2022-01-05 17:53:27 +0100

    Le site n'utilise plus les geojson des ressources communautaires (#2020)
    * add geojson file size information in payload
    
    * function to get a resource related geojson
    
    * same thing for a dataset
    
    * add assign for resource controller
    
    * add assign for dataset controller
    
    * update resource view existing functions accordingly
    
    * plug the new function for resources
    
    * use the new function for dataset details page
    
    * add a test
    
    * add condition on convert_to format
    
    * update test
    
    * format file
    
    * add nil case (can happen in tests)
    
    * don't match, assert equal
    
    * use the updated_at field of resource_history
    
    so we know when was the last check
    
    * update the tests
    
    * use that new information
    
    * update test
    
    * new function to convert seconds to hours and minutes
    
    * finally add the information to the webpage
    
    * mix format
    
    * credo + warning
    
    * simplify test
    
    * correct file_size computation
    
    * use a dedicated field for freshness information
    
    * valide_at should be filled when creating resource !
    
    * change field name
    
    * correct merge issue
    
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>

--- apps/transport/test/transport/jobs/resource_history_job_test.exs
+++ apps/transport/test/transport/jobs/resource_history_job_test.exs
@@ -49,7 +49,7 @@ defmodule Transport.Test.Transport.Jobs.ResourceHistoryJobTest do
     end
 
     test "with the latest ResourceHistory matching" do
-      %{datagouv_id: datagouv_id} =
+      %{id: resource_history_id, datagouv_id: datagouv_id} =
         resource_history =
         insert(:resource_history,
           datagouv_id: "1",
@@ -58,7 +58,9 @@ defmodule Transport.Test.Transport.Jobs.ResourceHistoryJobTest do
 
       assert 1 == count_resource_history()
       assert ResourceHistoryJob.is_same_resource?(resource_history, zip_metadata())
-      refute ResourceHistoryJob.should_store_resource?(%DB.Resource{datagouv_id: datagouv_id}, zip_metadata())
+
+      assert {false, %{id: ^resource_history_id}} =
+               ResourceHistoryJob.should_store_resource?(%DB.Resource{datagouv_id: datagouv_id}, zip_metadata())
     end
 
     test "with the latest ResourceHistory matching but for a different datagouv_id" do
@@ -89,7 +91,9 @@ defmodule Transport.Test.Transport.Jobs.ResourceHistoryJobTest do
       assert ResourceHistoryJob.should_store_resource?(%DB.Resource{datagouv_id: datagouv_id}, zip_metadata())
 
       %DB.ResourceHistory{id: latest_rh_id} |> DB.Repo.delete()
-      refute ResourceHistoryJob.should_store_resource?(%DB.Resource{datagouv_id: datagouv_id}, zip_metadata())
+
+      assert {false, _} =
+               ResourceHistoryJob.should_store_resource?(%DB.Resource{datagouv_id: datagouv_id}, zip_metadata())
     end
 
     test "with the latest ResourceHistory not matching" do
@@ -230,10 +234,12 @@ defmodule Transport.Test.Transport.Jobs.ResourceHistoryJobTest do
                  "zip_metadata" => ^expected_zip_metadata,
                  "uuid" => _uuid,
                  "download_datetime" => _download_datetime
-               }
+               },
+               last_up_to_date_at: last_up_to_date_at
              } = DB.ResourceHistory |> DB.Repo.one!()
 
       assert permanent_url == Transport.S3.permanent_url(:history, filename)
+      refute is_nil(last_up_to_date_at)
     end
 
     test "does not store resource again when it did not change" do
@@ -249,10 +255,11 @@ defmodule Transport.Test.Transport.Jobs.ResourceHistoryJobTest do
           is_community_resource: false
         )
 
-      insert(:resource_history,
-        datagouv_id: datagouv_id,
-        payload: %{"zip_metadata" => zip_metadata()}
-      )
+      %{id: resource_history_id, updated_at: updated_at} =
+        insert(:resource_history,
+          datagouv_id: datagouv_id,
+          payload: %{"zip_metadata" => zip_metadata()}
+        )
 
       Transport.HTTPoison.Mock
       |> expect(:get, fn ^resource_url, _headers, options ->
@@ -263,6 +270,12 @@ defmodule Transport.Test.Transport.Jobs.ResourceHistoryJobTest do
       assert 1 == count_resource_history()
       assert :ok == perform_job(ResourceHistoryJob, %{datagouv_id: datagouv_id})
       assert 1 == count_resource_history()
+
+      # check the updated_at field has been updated.
+      assert DB.ResourceHistory
+             |> DB.Repo.get!(resource_history_id)
+             |> Map.get(:updated_at)
+             |> DateTime.diff(updated_at, :microsecond) > 0
 
       Transport.Test.TestUtils.ensure_no_tmp_files!("resource_")
     end
