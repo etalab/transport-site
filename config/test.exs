645618901ce76558b5cfaaf6d7681a03da93a654
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-01-07 12:04:58 +0100

    Validate TableSchema resources

--- config/test.exs
+++ config/test.exs
@@ -35,6 +35,7 @@ config :transport,
   gbfs_metadata_impl: Transport.Shared.GBFSMetadata.Mock,
   availability_checker_impl: Transport.AvailabilityChecker.Mock,
   jsonschema_validator_impl: Shared.Validation.JSONSchemaValidator.Mock,
+  tableschema_validator_impl: Shared.Validation.TableSchemaValidator.Mock,
   schemas_impl: Transport.Shared.Schemas.Mock,
   notifications_api_token: "secret",
   s3_buckets: %{
