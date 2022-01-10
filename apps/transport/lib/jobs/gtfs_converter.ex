10fa84b76c2699ea488a3c5a70cdd1e12a3c9c7d
Francis Chabouis <fchabouis@gmail.com>
2022-01-10 11:37:18 +0100

    format file

--- apps/transport/lib/jobs/gtfs_converter.ex
+++ apps/transport/lib/jobs/gtfs_converter.ex
@@ -1,37 +1,40 @@
 defmodule Transport.Jobs.GtfsConverter do
-@moduledoc """
-Provides some functions to convert GTFS to another format
-"""
-alias DB.{Repo, ResourceHistory}
-import Ecto.Query
+  @moduledoc """
+  Provides some functions to convert GTFS to another format
+  """
+  alias DB.{Repo, ResourceHistory}
+  import Ecto.Query
 
-@spec enqueue_all_conversion_jobs(binary(), module()) :: :ok
-def enqueue_all_conversion_jobs(format, conversionJob) do
-  query =
-    ResourceHistory
-    |> where(
-      [_r],
-      fragment("""
-      payload ->>'format'='GTFS'
-      AND
-      payload ->>'uuid' NOT IN
-      (SELECT resource_history_uuid::text FROM data_conversion WHERE convert_from='GTFS' and convert_to='?')
-      """, ^format)
-    )
-    |> select([r], r.id)
+  @spec enqueue_all_conversion_jobs(binary(), module()) :: :ok
+  def enqueue_all_conversion_jobs(format, conversionJob) when format in ["GeoJSON", "NeTEx"] do
+    query =
+      ResourceHistory
+      |> where(
+        [_r],
+        fragment(
+          """
+          payload ->>'format'='GTFS'
+          AND
+          payload ->>'uuid' NOT IN
+          (SELECT resource_history_uuid::text FROM data_conversion WHERE convert_from='GTFS' and convert_to=?)
+          """,
+          ^format
+        )
+      )
+      |> select([r], r.id)
 
-  stream = Repo.stream(query)
+    stream = Repo.stream(query)
 
-  Repo.transaction(fn ->
-    stream
-    |> Stream.each(fn id ->
-      %{"resource_history_id" => id}
-      |> conversionJob.new()
-      |> Oban.insert()
+    Repo.transaction(fn ->
+      stream
+      |> Stream.each(fn id ->
+        %{"resource_history_id" => id}
+        |> conversionJob.new()
+        |> Oban.insert()
+      end)
+      |> Stream.run()
     end)
-    |> Stream.run()
-  end)
 
-  :ok
-end
+    :ok
+  end
 end
