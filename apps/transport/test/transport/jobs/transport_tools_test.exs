a20199a921527c4d0330614587fb6693ab8eb6f2
Francis Chabouis <fchabouis@gmail.com>
2022-01-20 10:34:27 +0100

    add a test to zip a folder

--- apps/transport/test/transport/jobs/transport_tools_test.exs
+++ apps/transport/test/transport/jobs/transport_tools_test.exs
@@ -6,7 +6,7 @@ defmodule TransportWeb.TransportToolsTest do
     :ok
   end
 
-  describe "test we can launch tools from transport-tools folder" do
+  describe "we can launch tools from transport-tools folder" do
     # NOTE: the :transport_tools tag is just a quick fix to completely skip the tests outside CI, until
     # we implement a better version of this, allowing to skip by default yet allow local runs
     # with specific versions (either compiled locally, or Docker-pulled)
@@ -33,6 +33,27 @@ defmodule TransportWeb.TransportToolsTest do
       :ok = Transport.GtfsToNeTExConverter.convert("#{__DIR__}/../../fixture/files/gtfs.zip", netex_dir)
       assert File.dir?(netex_dir)
       File.rm_rf!(netex_dir)
+    end
+  end
+
+  describe "Necessary command lines tools are installed on the machine" do
+    @tag :transport_tools
+    test "zip a simple folder" do
+      zip_archive_path = [System.tmp_dir!(), "subfolder", "myzip.zip"] |> Path.join()
+      content_folder_path = [System.tmp_dir!(), "subfolder", "folder_to_zip"] |> Path.join()
+
+      content_folder_path
+      |> File.mkdir_p!()
+
+      File.write(Path.join([content_folder_path, "a_file"]), "the file content")
+
+      # if the zip command is not available in the container, this will fail
+      :ok = Transport.FolderZipper.zip(content_folder_path, zip_archive_path)
+
+      # we check the content to be sure files are saved inside the zip
+      # with path relative to "folder_to_zip", and we don't save absolute paths (ie /tmp/subfolder/folder_to_zip/a_file)
+      zip_file = Unzip.LocalFile.open(zip_archive_path)
+      {:ok, %Unzip{cd_list: %{"a_file" => _map}}} = Unzip.new(zip_file)
     end
   end
 end
