3706e28d8c5ca34db815261574fd3402d58c949f
Francis Chabouis <fchabouis@gmail.com>
2022-02-02 15:22:24 +0100

    review comments

--- apps/transport/lib/transport_web/views/resource_view.ex
+++ apps/transport/lib/transport_web/views/resource_view.ex
@@ -148,8 +148,8 @@ defmodule TransportWeb.ResourceView do
 
   def date_to_string(conn, %Date{} = date) do
     case get_session(conn, :locale) do
-      "fr" -> "#{date.day}/#{date.month}/#{date.year}"
-      _ -> "#{date.month}/#{date.day}/#{date.year}"
+      "fr" -> Calendar.strftime(date, "%d/%m/%y")
+      _ -> Calendar.strftime(date, "%m/%d/%y")
     end
   end
 end
