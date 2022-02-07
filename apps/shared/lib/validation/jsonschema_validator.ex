d52d865189c6c50880c90cd4132263e7b51593e3
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-02-07 14:11:42 +0100

    Run "mix format"

--- apps/shared/lib/validation/jsonschema_validator.ex
+++ apps/shared/lib/validation/jsonschema_validator.ex
@@ -67,7 +67,11 @@ defmodule Shared.Validation.JSONSchemaValidator do
         {:error, errors} -> errors
       end
 
-    %{"has_errors" => not Enum.empty?(errors), "errors_count" => Enum.count(errors), "errors" => errors |> Enum.take(100)}
+    %{
+      "has_errors" => not Enum.empty?(errors),
+      "errors_count" => Enum.count(errors),
+      "errors" => errors |> Enum.take(100)
+    }
   end
 
   defp json_schemas_names, do: Map.keys(schemas_by_type("jsonschema"))
