3ba5b10d854b4420c5670b7b58a61e7f8104811b
Francis Chabouis <fchabouis@gmail.com>
2022-02-09 16:48:01 +0100

    clean temp file

--- apps/transport/lib/jobs/gtfs_to_db.ex
+++ apps/transport/lib/jobs/gtfs_to_db.ex
@@ -9,24 +9,30 @@ defmodule Transport.Jobs.GtfsToDB do
 
     file_stream = Transport.Unzip.S3File.get_file_stream("stops.txt", filename, bucket_name)
 
+    # we should try to stream directly to Ecto instead of saving the file locally...
     temp_file = "#{uuid}_stops.txt"
-    file_stream |> Stream.into(File.stream!(temp_file)) |> Stream.run()
 
-    DB.Repo.transaction(fn ->
-      File.stream!(temp_file, [:trim_bom])
-      |> CSV.decode!(headers: true)
-      |> Stream.map(fn r ->
-        %{
-          stop_id: Map.fetch!(r, "stop_id"),
-          stop_name: Map.fetch!(r, "stop_name"),
-          stop_lat: Map.fetch!(r, "stop_lat") |> String.to_float(),
-          stop_lon: Map.fetch!(r, "stop_lon") |> String.to_float(),
-          location_type: Map.fetch!(r, "location_type")
-        }
+    try do
+      file_stream |> Stream.into(File.stream!(temp_file)) |> Stream.run()
+
+      DB.Repo.transaction(fn ->
+        File.stream!(temp_file, [:trim_bom])
+        |> CSV.decode!(headers: true)
+        |> Stream.map(fn r ->
+          %{
+            stop_id: Map.fetch!(r, "stop_id"),
+            stop_name: Map.fetch!(r, "stop_name"),
+            stop_lat: Map.fetch!(r, "stop_lat") |> String.to_float(),
+            stop_lon: Map.fetch!(r, "stop_lon") |> String.to_float(),
+            location_type: Map.fetch!(r, "location_type")
+          }
+        end)
+        |> Stream.chunk_every(1000)
+        |> Stream.each(fn chunk -> DB.Repo.insert_all(DB.GtfsStops, chunk) end)
+        |> Stream.run()
       end)
-      |> Stream.chunk_every(1000)
-      |> Stream.each(fn chunk -> DB.Repo.insert_all(DB.GtfsStops, chunk) end)
-      |> Stream.run()
-    end)
+    after
+      File.rm(temp_file)
+    end
   end
 end
