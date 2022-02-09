553f256892e81c8a2145c65deabd149609d4a512
Francis Chabouis <fchabouis@gmail.com>
2022-02-09 16:08:22 +0100

    properly fill stops table from file

---
+++ apps/transport/lib/S3/unzip.ex
@@ -0,0 +1,52 @@
+defmodule Transport.Unzip.S3File do
+  @moduledoc """
+  Read a remote zip file stored on a S3 bucket, as explained here
+  https://hexdocs.pm/unzip/readme.html
+  """
+
+  defstruct [:path, :bucket, :s3_config]
+  alias __MODULE__
+
+  def new(path, bucket, s3_config) do
+    %S3File{path: path, bucket: bucket, s3_config: s3_config}
+  end
+
+  def get_file_stream(file_name, zip_name, bucket_name) do
+    aws_s3_config =
+      ExAws.Config.new(:s3,
+        access_key_id: [Application.fetch_env!(:ex_aws, :access_key_id), :instance_role],
+        secret_access_key: [Application.fetch_env!(:ex_aws, :secret_access_key), :instance_role]
+      )
+
+    file = new(zip_name, bucket_name, aws_s3_config)
+    {:ok, unzip} = Unzip.new(file)
+    Unzip.file_stream!(unzip, file_name)
+  end
+end
+
+defimpl Unzip.FileAccess, for: Transport.Unzip.S3File do
+  alias ExAws.S3
+
+  def size(file) do
+    %{headers: headers} = S3.head_object(file.bucket, file.path) |> ExAws.request!(file.s3_config)
+
+    size =
+      headers
+      |> Enum.find(fn {k, _} -> String.downcase(k) == "content-length" end)
+      |> elem(1)
+      |> String.to_integer()
+
+    {:ok, size}
+  end
+
+  def pread(file, offset, length) do
+    {_, chunk} =
+      S3.Download.get_chunk(
+        %S3.Download{bucket: file.bucket, path: file.path, dest: nil},
+        %{start_byte: offset, end_byte: offset + length - 1},
+        file.s3_config
+      )
+
+    {:ok, chunk}
+  end
+end
---
+++ apps/transport/lib/jobs/gtfs_to_db.ex
@@ -0,0 +1,32 @@
+defmodule Transport.Jobs.GtfsToDB do
+  @moduledoc """
+  Get the content of a GTFS ResourceHistory, store it in the DB
+  """
+
+  def fill_stop_from_resource_history(resource_history_id) do
+    %{payload: %{"uuid" => uuid, "filename" => filename}} = DB.ResourceHistory |> DB.Repo.get!(resource_history_id)
+    bucket_name = Transport.S3.bucket_name(:history)
+
+    file_stream = Transport.Unzip.S3File.get_file_stream("stops.txt", filename, bucket_name)
+
+    temp_file = "#{uuid}_stops.txt"
+    file_stream |> Stream.into(File.stream!(temp_file)) |> Stream.run()
+
+    DB.Repo.transaction(fn ->
+      File.stream!(temp_file, [:trim_bom])
+      |> CSV.decode!(headers: true)
+      |> Stream.map(fn r ->
+        %{
+          stop_id: Map.fetch!(r, "stop_id"),
+          stop_name: Map.fetch!(r, "stop_name"),
+          stop_lat: Map.fetch!(r, "stop_lat") |> String.to_float(),
+          stop_lon: Map.fetch!(r, "stop_lon") |> String.to_float(),
+          location_type: Map.fetch!(r, "location_type")
+        }
+       end)
+      |> Stream.chunk_every(1000)
+      |> Stream.each(fn chunk -> DB.Repo.insert_all(DB.GtfsStops, chunk) end)
+      |> Stream.run()
+    end)
+  end
+end
