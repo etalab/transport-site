2436961f5d56fdd55a12d7ab98985a537f9fff08
Francis Chabouis <fchabouis@gmail.com>
2022-02-10 16:49:44 +0100

    format files

--- apps/db/lib/db/gtfs_stop_times.ex
+++ apps/db/lib/db/gtfs_stop_times.ex
@@ -6,11 +6,11 @@ defmodule DB.GtfsStopTimes do
   use TypedEctoSchema
 
   typed_schema "gtfs_stop_times" do
-      belongs_to(:data_import, DB.GtfsImport)
-      field(:trip_id, :binary)
-      field(:stop_id, :binary)
-      field(:stop_sequence, :integer)
-      field(:arrival_time, EctoInterval)
-      field(:departure_time, EctoInterval)
+    belongs_to(:data_import, DB.GtfsImport)
+    field(:trip_id, :binary)
+    field(:stop_id, :binary)
+    field(:stop_sequence, :integer)
+    field(:arrival_time, EctoInterval)
+    field(:departure_time, EctoInterval)
   end
 end
