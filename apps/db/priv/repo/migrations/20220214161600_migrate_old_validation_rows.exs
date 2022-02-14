5b11c5a21563189e3342b40a935016301b610269
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-02-14 16:22:38 +0100

    Add migration for existing data

---
+++ apps/db/priv/repo/migrations/20220214161600_migrate_old_validation_rows.exs
@@ -0,0 +1,15 @@
+defmodule DB.Repo.Migrations.AddTitleResourceHistory do
+  use Ecto.Migration
+
+  def up do
+    execute """
+    update validations
+    set on_the_fly_validation_metadata = on_the_fly_validation_metadata || jsonb_build_object('state', 'completed', 'type', 'gtfs')
+    where not on_the_fly_validation_metadata ? 'state'
+    """
+  end
+
+  def down do
+    IO.puts("no going back")
+  end
+end
