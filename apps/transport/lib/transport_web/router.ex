3fe9583b36b5f4d86753008a8714ad63e56d799f
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-02-23 21:37:27 +0100

    Make sure we can health-check the worker too
    This requires a refactoring to move up the chain.

--- apps/transport/lib/transport_web/router.ex
+++ apps/transport/lib/transport_web/router.ex
@@ -37,10 +37,6 @@ defmodule TransportWeb.Router do
 
   get("/swaggerui", OpenApiSpex.Plug.SwaggerUI, path: "/api/openapi")
 
-  scope "/health-check" do
-    get("/", TransportWeb.HealthCheckController, :index)
-  end
-
   scope "/", TransportWeb do
     pipe_through(:browser)
 
--- apps/transport/test/transport_web/routing/health_check_test.exs
+++ apps/transport/test/transport_web/routing/health_check_test.exs
@@ -1,19 +1,19 @@
-defmodule TransportWeb.HealthCheckControllerTest do
+defmodule TransportWeb.HealthCheckTest do
   use TransportWeb.ConnCase, async: false
   use TransportWeb.DatabaseCase, cleanup: [:datasets]
   import Mock
 
   test "GET /health-check", %{conn: conn} do
     conn = get(conn, "/health-check")
-    body = json_response(conn, 200)
-    assert body == %{"database" => "OK"}
+    body = text_response(conn, 200)
+    assert body == "DATABASE: OK"
   end
 
   test "GET /health-check (with db not available)", %{conn: conn} do
     with_mock Ecto.Adapters.SQL, query!: fn _, _, _ -> raise DBConnection.ConnectionError end do
       conn = get(conn, "/health-check")
-      body = json_response(conn, 500)
-      assert body == %{"database" => "KO"}
+      body = text_response(conn, 500)
+      assert body == "DATABASE: KO"
     end
   end
 end
