27f23992635963742ad3c8d2c4d504902d9dc652
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-02-25 17:33:26 +0100

    Add doc

--- apps/transport/lib/mailjet/email_host.ex
+++ apps/transport/lib/mailjet/email_host.ex
@@ -1,5 +1,15 @@
 defmodule Transport.RuntimeConfig.EmailHost do
-  # See https://github.com/etalab/transport-site/issues/2154
+  @moduledoc """
+  Emails can be sent from various machines (site vs workers), and each machine
+  has its own host name. When sending emails with links to the site, though, by
+  default the url construction (based on the endpoint settings) will be incorrect
+  for our use.
+
+  This module helps ensure one can get a base URI with the correct email host name,
+  to be used with `Router.Helpers`.
+
+  See https://github.com/etalab/transport-site/issues/2154 for some historical context.
+  """
 
   def email_host(
         endpoint \\ TransportWeb.Endpoint,
