003263a24b715ece56e03e9fa9a2bd93e93a8f2d
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-02-25 17:22:03 +0100

    Mix format

--- apps/transport/lib/mailjet/email_host.ex
+++ apps/transport/lib/mailjet/email_host.ex
@@ -1,7 +1,10 @@
 defmodule Transport.RuntimeConfig.EmailHost do
   # See https://github.com/etalab/transport-site/issues/2154
 
-  def email_host(endpoint \\ TransportWeb.Endpoint, email_host_name \\ Application.fetch_env!(:transport, :email_host_name)) do
+  def email_host(
+        endpoint \\ TransportWeb.Endpoint,
+        email_host_name \\ Application.fetch_env!(:transport, :email_host_name)
+      ) do
     endpoint.url()
     |> URI.parse()
     |> Map.put(:host, email_host_name)
