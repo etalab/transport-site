943e45e93a7ca5b41915f4f97447de0be8c4586b
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-03-01 09:58:16 +0100

    Mix format

--- apps/transport/test/transport/screens_test.exs
+++ apps/transport/test/transport/screens_test.exs
@@ -9,6 +9,7 @@ defmodule Transport.ScreensTest do
   test "it works" do
     resource_history_uuid = Ecto.UUID.generate()
     resource_datagouv_id = Ecto.UUID.generate()
+
     %{id: resource_history_id} =
       insert(:resource_history,
         datagouv_id: resource_datagouv_id,
@@ -20,12 +21,11 @@ defmodule Transport.ScreensTest do
       convert_to: "NeTEx",
       resource_history_uuid: resource_history_uuid,
       payload: %{
-        "resource_datagouv_id": resource_datagouv_id
+        resource_datagouv_id: resource_datagouv_id
       }
     )
 
     query = """
-
     SELECT
     resource_history_id,
     resource_datagouv_id,
@@ -34,7 +34,7 @@ defmodule Transport.ScreensTest do
     ELSE
       FALSE
     END AS conversion_found
-  FROM (
+    FROM (
     SELECT
       id AS resource_history_id,
       datagouv_id AS resource_datagouv_id,
