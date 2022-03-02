a90aa585c6207daad9bbfd3121421303f8f1e398
Francis Chabouis <fchabouis@gmail.com>
2022-03-02 18:37:18 +0100

    remove additionnal field

--- apps/db/priv/repo/migrations/20220228115606_resource_history_created_at.exs
+++
@@ -1,9 +0,0 @@
-defmodule DB.Repo.Migrations.ResourceHistoryCreatedAt do
-  use Ecto.Migration
-
-  def change do
-    alter table(:resource_history) do
-      add(:created_at, :utc_datetime_usec)
-    end
-  end
-end
