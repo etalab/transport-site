b2d4511c19b0941cfe877bd8243ebf595736abe2
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-03-08 13:43:04 +0100

    Rework UI

--- apps/transport/lib/transport_web/live/on_demand_validation_select_live.ex
+++ apps/transport/lib/transport_web/live/on_demand_validation_select_live.ex
@@ -12,8 +12,6 @@ defmodule TransportWeb.Live.OnDemandValidationSelectLive do
   import TransportWeb.Router.Helpers
   import TransportWeb.ValidationController, only: [select_options: 0]
 
-  @default_selected "gtfs"
-
   def mount(_params, %{"locale" => locale} = _session, socket) do
     Gettext.put_locale(locale)
 
@@ -21,32 +19,45 @@ defmodule TransportWeb.Live.OnDemandValidationSelectLive do
      socket
      |> socket_data(%{
        select_options: select_options(),
-       changeset: Ecto.Changeset.cast({%{url: "", file: ""}, %{url: :string, file: :string}}, %{}, [:url, :file])
+       changeset: Ecto.Changeset.cast({%{url: "", type: ""}, %{url: :string, type: :string}}, %{}, [:url, :type])
      })}
   end
 
   def handle_params(params, _uri, socket) do
-    type = params["type"]
-    selected = type || @default_selected
-    {:noreply, socket |> socket_data(%{type: type, selected: selected})}
+    {:noreply, socket |> socket_data(%{changeset: new_changeset(socket, params)})}
   end
 
   def self_path(socket) do
-    live_path(socket, __MODULE__, %{"type" => socket_value(socket, :type)})
+    fields = form_fields(socket)
+    live_path(socket, __MODULE__, fields |> Map.reject(fn {_, v} -> v in ["", nil] end))
   end
 
   def socket_data(socket, data \\ nil) do
     socket = socket |> assign(data || %{})
 
-    socket |> assign(input_type: determine_input_type(socket_value(socket, :type)))
+    socket |> assign(input_type: determine_input_type(form_value(socket, :type)))
   end
 
   def determine_input_type(type) when type in ["gbfs"], do: "link"
   def determine_input_type(_), do: "file"
 
   def handle_event("form_changed", %{"upload" => params}, socket) do
-    socket = socket |> socket_data(%{type: Map.get(params, "type")})
+    socket = socket |> socket_data(%{changeset: new_changeset(socket, params)})
     {:noreply, socket |> push_patch(to: self_path(socket))}
+  end
+
+  defp form_value(socket, field) do
+    Ecto.Changeset.get_field(socket_value(socket, :changeset), field)
+  end
+
+  defp form_fields(socket) do
+    changeset = socket_value(socket, :changeset)
+    Map.merge(changeset.data(), changeset.changes())
+  end
+
+  defp new_changeset(socket, params) do
+    changeset = socket_value(socket, :changeset)
+    changeset |> Ecto.Changeset.change(params |> Map.new(fn {k, v} -> {String.to_atom(k), v} end))
   end
 
   defp socket_value(%Phoenix.LiveView.Socket{assigns: assigns}, key), do: Map.get(assigns, key)
