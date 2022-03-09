17361943699e7326e1e607079622365bab44810b
Francis Chabouis <fchabouis@gmail.com>
2022-03-09 09:38:16 +0100

    add cases

--- apps/shared/lib/date_time_display.ex
+++ apps/shared/lib/date_time_display.ex
@@ -16,14 +16,16 @@ defmodule Shared.DateTimeDisplay do
   iex> format_date("2022-03-01", "en")
   "03/01/2022"
   """
-  @spec format_date(binary | Date.t(), <<_::16>>) :: binary
+  @spec format_date(binary | Date.t(), binary() | nil) :: binary
   def format_date(%Date{} = date, "fr"), do: Calendar.strftime(date, "%d/%m/%Y")
-
+  def format_date(%Date{} = date, nil), do: format_date(date, "fr")
   def format_date(%Date{} = date, "en"), do: Calendar.strftime(date, "%m/%d/%Y")
 
   def format_date(date, locale) when is_binary(date) do
     date |> Date.from_iso8601!() |> format_date(locale)
   end
+
+  def format_date(nil, _), do: ""
 
   @doc """
   Display a date from a DateTime
@@ -36,6 +38,8 @@ defmodule Shared.DateTimeDisplay do
   def format_datetime_to_date(%DateTime{} = dt, locale) do
     DateTime.to_date(dt) |> format_date(locale)
   end
+
+  def format_datetime_to_date(nil, _), do: ""
 
   @doc """
   Formats a naive date time for display
@@ -53,6 +57,10 @@ defmodule Shared.DateTimeDisplay do
     Calendar.strftime(ndt, "%d/%m/%Y à %Hh%M")
   end
 
+  def format_naive_datetime(%NaiveDateTime{} = ndt, nil) do
+    format_naive_datetime(ndt, "fr")
+  end
+
   def format_naive_datetime(%NaiveDateTime{} = ndt, "en") do
     Calendar.strftime(ndt, "%m/%d/%Y at %H:%M")
   end
@@ -60,6 +68,8 @@ defmodule Shared.DateTimeDisplay do
   def format_naive_datetime(naive_datetime, locale) when is_binary(naive_datetime) do
     naive_datetime |> NaiveDateTime.from_iso8601!() |> format_naive_datetime(locale)
   end
+
+  def format_naive_datetime(nil, _), do: ""
 
   @doc """
   Formats a date time for display.
@@ -80,6 +90,10 @@ defmodule Shared.DateTimeDisplay do
     Calendar.strftime(dt, "%d/%m/%Y à %Hh%M UTC")
   end
 
+  def format_datetime_to_utc(%DateTime{} = dt, nil) do
+    format_datetime_to_utc(dt, "fr")
+  end
+
   def format_datetime_to_utc(%DateTime{} = dt, "en") do
     Calendar.strftime(dt, "%m/%d/%Y at %H:%M UTC")
   end
@@ -89,12 +103,16 @@ defmodule Shared.DateTimeDisplay do
     format_datetime_to_utc(dt, locale)
   end
 
+  def format_datetime_to_utc(nil, _), do: ""
+
   @doc """
   Converts a binary naive date time to a binary date time having the Paris timezone
 
   iex> format_naive_datetime_to_paris_tz("2022-03-01T15:30:00")
   "2022-03-01T15:30:00+01:00"
   """
+  def format_naive_datetime_to_paris_tz(nil), do: ""
+
   def format_naive_datetime_to_paris_tz(naive_datetime) do
     naive_datetime
     |> Timex.parse!("{ISO:Extended}")
