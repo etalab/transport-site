faa956e39aed218c00219f85f5f101426120696e
Francis Chabouis <fchabouis@gmail.com>
2022-03-10 17:05:52 +0100

    add a test

---
+++ apps/shared/test/hasher_test.exs
@@ -0,0 +1,12 @@
+defmodule HasherTest do
+  use ExUnit.Case
+
+  test "hash by streaming a local file" do
+    content = "coucou"
+    hash = :crypto.hash(:sha256, content) |> Base.encode16() |> String.downcase()
+    file_path = System.tmp_dir!() |> Path.join("coucou_file")
+    File.write!(file_path, content)
+
+    assert Hasher.get_file_hash(file_path) == hash
+  end
+end
