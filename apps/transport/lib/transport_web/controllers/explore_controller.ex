52edc44bb724ae8c9cc834efdc44ce1f84b8ecd9
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-03-11 06:12:20 +0100

    Add demo bottom

--- apps/transport/lib/transport_web/controllers/explore_controller.ex
+++ apps/transport/lib/transport_web/controllers/explore_controller.ex
@@ -4,6 +4,7 @@ defmodule TransportWeb.ExploreController do
   def vehicle_positions(conn, _params) do
     conn
     |> assign(:extra_script_tags, [TransportWeb.ExploreView, "_head_script_tags.html"])
+    |> assign(:extra_bottom_script_tags, [TransportWeb.ExploreView, "_bottom_script_tags.html"])
     |> render("vehicle_positions.html")
   end
 end
