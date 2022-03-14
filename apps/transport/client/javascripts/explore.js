c41a18de3fdbf2e6b7984a3339718bf32b894aa0
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-03-14 15:09:26 +0100

    Connect from client

--- apps/transport/client/javascripts/explore.js
+++ apps/transport/client/javascripts/explore.js
@@ -1,3 +1,14 @@
 import { Socket } from 'phoenix'
 
 let socket = new Socket("/socket", { params: { token: window.userToken } });
+socket.connect();
+let channel = socket.channel("explore", {})
+channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+
+channel.on("hello", payload => {
+    console.log("hello received", payload);
+})
+
+export default socket
