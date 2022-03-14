02098234a57339ebba61fe447ff5570e588d8c14
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-03-14 16:05:19 +0100

    Replace deckgl demo by our usual maps constructs

--- apps/transport/client/javascripts/explore.js
+++ apps/transport/client/javascripts/explore.js
@@ -11,4 +11,21 @@ channel.on("vehicle-positions", payload => {
     console.log(payload);
 })
 
+import Leaflet from 'leaflet'
+import 'leaflet.pattern'
+
+const Mapbox = {
+    url: 'https://api.mapbox.com/styles/v1/istopopoki/ckg98kpoc010h19qusi9kxcct/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
+    accessToken: 'pk.eyJ1IjoiaXN0b3BvcG9raSIsImEiOiJjaW12eWw2ZHMwMGFxdzVtMWZ5NHcwOHJ4In0.VvZvyvK0UaxbFiAtak7aVw',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors <a href="https://spdx.org/licenses/ODbL-1.0.html">ODbL</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 20
+}
+
+const map = Leaflet.map("map", { renderer: Leaflet.canvas() }).fitBounds([[51.1, -4.9], [41.2, 9.8]])
+L.tileLayer(Mapbox.url, {
+    accessToken: Mapbox.accessToken,
+    attribution: Mapbox.attribution,
+    maxZoom: Mapbox.maxZoom
+}).addTo(map)
+
 export default socket
