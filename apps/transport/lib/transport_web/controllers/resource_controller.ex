baf705fd0af5b34da37436e59152460d97ec317c
Francis Chabouis <fchabouis@gmail.com>
2022-03-16 14:28:33 +0100

    Carte générique pour les geojsons 🗺️ (#2217)
    * rename js function
    
    * prepare the code for a generic geojson handling
    
    * create a generic geojson map
    
    * set leaflet popup overflow to auto
    
    * function to get most recent resource history infos
    
    * download resource from cellar
    
    * store resource history file size for non zip resources
    
    * extract file_size from resource_history
    
    * do not repeat default values
    
    * handle to big to show files
    
    * adapt map size to screen size
    
    * add function to check if geojson viz is available
    
    * add message in resource card if viz available
    
    * let the color by default
    
    * use the file_size to render the map or not
    
    * translation
    
    * lint
    
    * update function name
    
    * update resource history factory
    
    * add some tests
    
    * add an additional guard
    
    * remove unused variable
    
    * add translation
    
    * update message
    
    * fill filesize for zip resources as well
    
    * rename to filesize
    
    * spell visualization with a "z"

--- apps/transport/lib/transport_web/controllers/resource_controller.ex
+++ apps/transport/lib/transport_web/controllers/resource_controller.ex
@@ -18,6 +18,7 @@ defmodule TransportWeb.ResourceController do
     conn =
       conn
       |> assign(:uptime_per_day, DB.ResourceUnavailability.uptime_per_day(resource, availability_number_days()))
+      |> assign(:resource_history_infos, DB.ResourceHistory.latest_resource_history_infos(id))
       |> assign(:gtfs_rt_feed, gtfs_rt_feed(conn, resource))
       |> put_resource_flash(resource.dataset.is_active)
 
