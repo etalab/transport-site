e918edb7b73ce3bad76a0736556796d7011c5f7f
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-03-22 15:56:58 +0100

    Remove credo warning

--- apps/transport/lib/transport_web/presence.ex
+++ apps/transport/lib/transport_web/presence.ex
@@ -2,4 +2,8 @@ defmodule TransportWeb.Presence do
   use Phoenix.Presence,
     otp_app: :transport,
     pubsub_server: TransportWeb.PubSub
+
+  @moduledoc """
+  A "Presence" (user tracking) module for the app.
+  """
 end
