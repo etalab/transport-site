46e392f1e7cb1541e134bb340e0c4f0f2926d59c
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-03-25 10:40:25 +0100

    Add Backfill.ResourceHistoryMetadataNonGTFS

---
+++ apps/transport/lib/jobs/backfill/backfill_metadata_non_gtfs_resource_history.ex
@@ -0,0 +1,54 @@
+defmodule Transport.Jobs.Backfill.ResourceHistoryMetadataNonGTFS do
+  @moduledoc """
+  Backfill of ResourceHistory payload to fix metadata for
+  non-GTFS resources with metadata specific to GTFS.
+
+  See https://github.com/etalab/transport-site/issues/2258
+  """
+  use Oban.Worker
+  import Ecto.Query
+  alias DB.{Repo, ResourceHistory}
+
+  @backfill_delay 1
+
+  @impl true
+  def perform(%{args: %{"resource_history_id" => resource_history_id, "backfill" => true}}) do
+    with :ok <- perform(%{args: %{"resource_history_id" => resource_history_id}}) do
+      case fetch_next(resource_history_id) do
+        next_id when is_integer(next_id) ->
+          %{resource_history_id: next_id, backfill: true} |> new(schedule_in: @backfill_delay) |> Oban.insert()
+
+        nil ->
+          :ok
+      end
+    end
+  end
+
+  def perform(%{args: %{"resource_history_id" => resource_history_id}}) do
+    update_resource_history(resource_history_id)
+    :ok
+  end
+
+  def fetch_next(resource_history_id) do
+    ResourceHistory
+    |> where([rh], rh.id > ^resource_history_id)
+    |> where([rh], fragment("payload ->>'format' != 'GTFS' and payload->'resource_metadata' \\? 'modes'"))
+    |> order_by(asc: :id)
+    |> limit(1)
+    |> select([rh], rh.id)
+    |> Repo.one()
+  end
+
+  def update_resource_history(resource_history_id) do
+    ResourceHistory |> Repo.get!(resource_history_id) |> do_update()
+  end
+
+  def do_update(%ResourceHistory{payload: %{"format" => format, "resource_metadata" => %{"modes" => _}} = payload} = rh)
+      when format != "GTFS" do
+    rh
+    |> Ecto.Changeset.change(%{payload: payload |> Map.put("resource_metadata", nil)})
+    |> Repo.update!()
+  end
+
+  def do_update(%ResourceHistory{} = rh), do: rh
+end
---
+++ apps/transport/test/transport/jobs/backfill/backfill_metadata_non_gtfs_resource_history_test.exs
@@ -0,0 +1,62 @@
+defmodule Transport.Jobs.Backfill.ResourceHistoryMetadataNonGTFSTest do
+  use ExUnit.Case
+  import Transport.Jobs.Backfill.ResourceHistoryMetadataNonGTFS
+  import DB.Factory
+  import Mox
+
+  setup do
+    Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+  end
+
+  setup :verify_on_exit!
+
+  test "updates a non GTFS ResourceHistory" do
+    resource_history =
+      insert(:resource_history, %{
+        datagouv_id: datagouv_id = "datagouv_id",
+        payload: %{
+          "format" => "NeTEx",
+          "other_field" => "other_value",
+          "resource_metadata" => %{
+            "modes" => [],
+            "end_date" => nil,
+            "networks" => [],
+            "has_fares" => false,
+            "has_shapes" => false,
+            "start_date" => nil,
+            "lines_count" => 0
+          }
+        }
+      })
+
+    update_resource_history(resource_history.id)
+
+    res = DB.Repo.reload!(resource_history)
+
+    assert res.datagouv_id == datagouv_id
+    assert res.payload == %{"format" => "NeTEx", "other_field" => "other_value", "resource_metadata" => nil}
+  end
+
+  test "leaves GTFS ResourceHistory alone" do
+    resource_history =
+      insert(:resource_history, %{
+        datagouv_id: "datagouv_id",
+        payload: %{
+          "format" => "GTFS",
+          "other_field" => "other_value",
+          "resource_metadata" => %{
+            "modes" => [],
+            "end_date" => nil,
+            "networks" => [],
+            "has_fares" => false,
+            "has_shapes" => false,
+            "start_date" => nil,
+            "lines_count" => 0
+          }
+        }
+      })
+
+    update_resource_history(resource_history.id)
+    assert DB.Repo.reload!(resource_history) == resource_history
+  end
+end
