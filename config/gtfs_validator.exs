d2dbf75e877e6138cd785ece283366e8998a7d56
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-03-31 09:36:49 +0200

    Formattage (sans changement) des fichiers de configuration

--- config/gtfs_validator.exs
+++ config/gtfs_validator.exs
@@ -2,5 +2,5 @@ import Config
 
 # Configure GTFS Validator
 config :transport,
-   # by default, use the production validator. This can be overriden with dev.secret.exs
-   gtfs_validator_url: System.get_env("GTFS_VALIDATOR_URL", "https://transport-validator.cleverapps.io")
+  # by default, use the production validator. This can be overriden with dev.secret.exs
+  gtfs_validator_url: System.get_env("GTFS_VALIDATOR_URL", "https://transport-validator.cleverapps.io")
