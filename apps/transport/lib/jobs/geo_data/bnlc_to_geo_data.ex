95414092eb8d91706cd0b307ea3892297e7fcb42
Francis Chabouis <fchabouis@gmail.com>
2022-04-06 17:57:00 +0200

    job to import the BNLC

---
+++ apps/transport/lib/jobs/geo_data/bnlc_to_geo_data.ex
@@ -0,0 +1,76 @@
+defmodule Transport.Jobs.BNLCToGeoData do
+  @moduledoc """
+  Job in charge of taking the content of the BNLC (Base Nationale de Covoiturage) and storing it
+  in the geo_data table
+  """
+  use Oban.Worker, max_attempts: 3
+  import Ecto.Query
+  alias NimbleCSV.RFC4180, as: CSV
+  require Logger
+
+  @bnlc_dataset 253
+
+  @impl Oban.Worker
+  def perform(%Oban.Job{}) do
+    # get resource id
+    %{type: "carpooling-areas", resources: [%{id: resource_id}]} =
+      DB.Dataset |> preload(:resources) |> DB.Repo.get!(@bnlc_dataset)
+
+    %{id: latest_resource_history_id, payload: %{"permanent_url" => permanent_url}} =
+      DB.ResourceHistory.latest_resource_history(resource_id)
+
+    current_geo_data_import = DB.GeoDataImport.dataset_latest_geo_data_import(@bnlc_dataset)
+    import_replace_data(permanent_url, latest_resource_history_id, current_geo_data_import)
+    :ok
+  end
+
+  defp import_replace_data(_url, new_resource_history_id, %{resource_history_id: current_resource_history_id})
+       when new_resource_history_id <= current_resource_history_id do
+    Logger.info("nothing new to import for BNLC")
+    :ok
+  end
+
+  defp import_replace_data(url, new_resource_history_id, current_geo_data_import) do
+    Logger.info("New BNLC content detected...update content")
+
+    DB.Repo.transaction(fn ->
+      unless is_nil(current_geo_data_import) do
+        # thanks to cascading delete, it will also clean geo_data table corresponding entries
+        current_geo_data_import |> DB.Repo.delete!()
+      end
+
+      %{id: geo_data_import_id} =
+        DB.Repo.insert!(%DB.GeoDataImport{resource_history_id: new_resource_history_id, publish: true})
+
+      %{status_code: 200, body: body} = HTTPoison.get!(url)
+
+      body
+      |> CSV.parse_string(skip_headers: false)
+      |> Stream.transform([], fn r, acc ->
+        if acc == [] do
+          {%{}, r}
+        else
+          {[acc |> Enum.zip(r) |> Enum.into(%{})], acc}
+        end
+      end)
+      |> Stream.map(fn m ->
+        %{
+          geo_data_import_id: geo_data_import_id,
+          geom: %Geo.Point{
+            coordinates:
+              {m["Xlong"] |> String.replace([" ", " "], "") |> String.to_float(),
+               m["Ylat"] |> String.replace([" ", " "], "") |> String.to_float()},
+            properties: %{},
+            srid: 4326
+          },
+          payload: m |> Map.drop(["Xlong", "Ylat"])
+        }
+      end)
+      |> Stream.chunk_every(1000)
+      |> Stream.each(fn chunk -> DB.Repo.insert_all(DB.GeoData, chunk) end)
+      |> Stream.run()
+    end)
+
+    :ok
+  end
+end
