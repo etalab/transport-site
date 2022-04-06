f5f11ba8f68d745153c14ddeabdf1ffdb44ec42f
Francis Chabouis <fchabouis@gmail.com>
2022-04-06 17:56:23 +0200

    migration correction

--- apps/db/priv/repo/migrations/20220406125936_add_geo_data.exs
+++ apps/db/priv/repo/migrations/20220406125936_add_geo_data.exs
@@ -2,16 +2,16 @@ defmodule DB.Repo.Migrations.AddGeoData do
   use Ecto.Migration
 
   def change do
-    create table(:geo_data) do
-      add :geom, :geometry
-      add :payload, :map
-      add :geo_data_import_id, references(:geo_data, on_delete: :delete_all)
-
-    end
-
     create table(:geo_data_import) do
       add :resource_history_id, references(:resource_history)
       add :publish, :boolean
     end
+
+    create table(:geo_data) do
+      add :geom, :geometry
+      add :payload, :map
+      add :geo_data_import_id, references(:geo_data_import, on_delete: :delete_all)
+    end
+
   end
 end
