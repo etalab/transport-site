f53be873e3db0f8c75fed3c29a456f0db9c53733
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-04-07 16:28:45 +0200

    Write files

--- apps/transport/lib/jobs/consolidate_lez_job.ex
+++ apps/transport/lib/jobs/consolidate_lez_job.ex
@@ -4,7 +4,7 @@ defmodule Transport.Jobs.ConsolidateLEZsJob do
   """
   use Oban.Worker, max_attempts: 3
   import Ecto.Query
-  alias DB.{Dataset, Resource, Repo}
+  alias DB.{Dataset, Repo, Resource, ResourceHistory}
 
   @zfe_schema_name "etalab/schema-zfe"
 
@@ -14,12 +14,23 @@ defmodule Transport.Jobs.ConsolidateLEZsJob do
     :ok
   end
 
-  def run() do
+  def run do
     relevant_resources()
     |> Enum.group_by(&type/1)
     |> Enum.into(%{}, fn {type, resources} ->
       {type, consolidate_features(resources)}
     end)
+    |> write_files()
+  end
+
+  def write_files(consolidated_data) do
+    write_file(consolidated_data |> Map.fetch!("aire") |> Jason.encode!(), "aires.json")
+    write_file(consolidated_data |> Map.fetch!("voie") |> Jason.encode!(), "voies.json")
+  end
+
+  def write_file(content, filename) do
+    dst_path = System.tmp_dir!() |> Path.join(filename)
+    dst_path |> File.write!(content)
   end
 
   def type(%Resource{} = resource) do
@@ -48,13 +59,25 @@ defmodule Transport.Jobs.ConsolidateLEZsJob do
   def consolidate_features(resources) do
     %{
       type: "FeatureCollection",
-      features: resources |> Enum.map(&content_features/1)
+      features: resources |> Enum.flat_map(&content_features/1)
     }
   end
 
-  def content_features(%Resource{url: url}) do
+  def content_features(%Resource{} = resource) do
+    # TO DO: enforce `has_errors` to `false`
+    %ResourceHistory{payload: %{"permanent_url" => url, "resource_metadata" => %{"validation" => %{"has_errors" => _}}}} =
+      latest_resource_history(resource)
+
     %HTTPoison.Response{status_code: 200, body: body} = http_client().get!(url, [], follow_redirect: true)
     body |> Jason.decode!() |> Map.fetch!("features")
+  end
+
+  def latest_resource_history(%Resource{datagouv_id: datagouv_id}) do
+    ResourceHistory
+    |> where([rh], rh.datagouv_id == ^datagouv_id)
+    |> order_by(desc: :inserted_at)
+    |> limit(1)
+    |> Repo.one!()
   end
 
   defp http_client, do: Transport.Shared.Wrapper.HTTPoison.impl()
