7879524bcf5662346edf5a42d571587bcc587d88
Francis Chabouis <fchabouis@gmail.com>
2022-04-11 12:17:01 +0200

    correct place for timestamps

--- apps/db/priv/repo/migrations/20220406125936_add_geo_data.exs
+++ apps/db/priv/repo/migrations/20220406125936_add_geo_data.exs
@@ -5,13 +5,13 @@ defmodule DB.Repo.Migrations.AddGeoData do
     create table(:geo_data_import) do
       add :resource_history_id, references(:resource_history)
       add :publish, :boolean
+      timestamps([type: :utc_datetime_usec])
     end
 
     create table(:geo_data) do
       add :geom, :geometry
       add :payload, :map
       add :geo_data_import_id, references(:geo_data_import, on_delete: :delete_all)
-      timestamps([type: :utc_datetime_usec])
     end
 
   end
