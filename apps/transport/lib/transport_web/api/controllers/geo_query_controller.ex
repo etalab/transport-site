a9744b3539c3a75f43446d55c605e8d6d22a733d
Francis Chabouis <fchabouis@gmail.com>
2022-04-12 16:33:16 +0200

    add some cachex for the api response

--- apps/transport/lib/transport_web/api/controllers/geo_query_controller.ex
+++ apps/transport/lib/transport_web/api/controllers/geo_query_controller.ex
@@ -2,7 +2,8 @@ defmodule TransportWeb.API.GeoQueryController do
   use TransportWeb, :controller
 
   def index(conn, %{"data" => "bnlc"}) do
-    geojson = DB.GeoData.geo_data_as_geojson(2)
+    get_geojson = fn -> DB.GeoData.geo_data_as_geojson(2) end
+    geojson = Transport.Cache.API.fetch(:bnlc_data, get_geojson)
     conn |> json(geojson)
   end
 end
