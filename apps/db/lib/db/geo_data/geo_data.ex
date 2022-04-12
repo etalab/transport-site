dd2974c7bf2cb52303e07b60a54d881e55fa4f3b
Francis Chabouis <fchabouis@gmail.com>
2022-04-12 14:19:54 +0200

    add geo query

--- apps/db/lib/db/geo_data/geo_data.ex
+++ apps/db/lib/db/geo_data/geo_data.ex
@@ -4,10 +4,26 @@ defmodule DB.GeoData do
   """
   use Ecto.Schema
   use TypedEctoSchema
+  import Ecto.Query
 
   typed_schema "geo_data" do
     field(:geom, Geo.PostGIS.Geometry) :: Geo.geometry()
     field(:payload, :map)
     belongs_to(:geo_data_import, DB.GeoDataImport)
+  end
+
+  def geo_data_as_geojson(geo_data_import_id) do
+    subquery = from g in DB.GeoData, where: g.geo_data_import_id == ^geo_data_import_id
+
+    query =
+      from(g in subquery(subquery),
+        select:
+          fragment(
+            "json_build_object('type', 'FeatureCollection', 'features', json_agg(ST_AsGeoJSON(?)::json))",
+            g
+          )
+      )
+
+    query |> DB.Repo.one()
   end
 end
