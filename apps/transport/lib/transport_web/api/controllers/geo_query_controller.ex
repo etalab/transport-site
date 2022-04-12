3c5b08b75fa33cb146b206e1228b7ac993ff2d6f
Francis Chabouis <fchabouis@gmail.com>
2022-04-12 16:43:43 +0200

    update controller

--- apps/transport/lib/transport_web/api/controllers/geo_query_controller.ex
+++ apps/transport/lib/transport_web/api/controllers/geo_query_controller.ex
@@ -1,8 +1,21 @@
 defmodule TransportWeb.API.GeoQueryController do
   use TransportWeb, :controller
+  import Ecto.Query
 
   def index(conn, %{"data" => "bnlc"}) do
-    get_geojson = fn -> DB.GeoData.geo_data_as_geojson(2) end
+    transport_publisher_label = Application.fetch_env!(:transport, :datagouvfr_transport_publisher_label)
+
+    get_geojson = fn ->
+      %{id: id} =
+        DB.Dataset
+        |> where([d], d.type == "carpooling-areas" and d.organization == ^transport_publisher_label)
+        |> DB.Repo.one!()
+
+      id
+      |> DB.GeoDataImport.dataset_latest_geo_data_import()
+      |> DB.GeoData.geo_data_as_geojson()
+    end
+
     geojson = Transport.Cache.API.fetch(:bnlc_data, get_geojson)
     conn |> json(geojson)
   end
