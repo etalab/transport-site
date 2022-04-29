1b408d2a2ddcdef10e9daa8c104c7188c26ec9ea
Francis Chabouis <fchabouis@gmail.com>
2022-04-29 17:49:58 +0200

    update multi_validation model

--- apps/db/priv/repo/migrations/20220429092956_set_up_validation_v2.exs
+++ apps/db/priv/repo/migrations/20220429092956_set_up_validation_v2.exs
@@ -3,12 +3,13 @@ defmodule DB.Repo.Migrations.SetUpValidationV2 do
 
   def change do
     create table(:multi_validations) do
-      add :validation_timestamp, :utc_datetime, null: false
+      add :validation_timestamp, :utc_datetime_usec, null: false
       add :validator, :text, null: false
       add :transport_tools_version, :text
       add :command, :text
       add :result, :jsonb
       add :data_vis, :jsonb
+      add :metadata, :jsonb
 
       add :resource_id, references(:resource, on_delete: :delete_all)
       add :resource_history_id, references(:resource_history, on_delete: :delete_all)
