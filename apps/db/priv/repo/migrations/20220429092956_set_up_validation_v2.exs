e0fed483bdc8de48c8cd75111f6e12e992d3b87b
Francis Chabouis <fchabouis@gmail.com>
2022-04-29 14:36:49 +0200

    change :string to :text (avoid size limit)

--- apps/db/priv/repo/migrations/20220429092956_set_up_validation_v2.exs
+++ apps/db/priv/repo/migrations/20220429092956_set_up_validation_v2.exs
@@ -4,19 +4,19 @@ defmodule DB.Repo.Migrations.SetUpValidationV2 do
   def change do
     create table(:multi_validations) do
       add :validation_timestamp, :utc_datetime, null: false
-      add :validator, :string, null: false
-      add :transport_tools_version, :string
-      add :command, :string
+      add :validator, :text, null: false
+      add :transport_tools_version, :text
+      add :command, :text
       add :result, :jsonb
       add :data_vis, :jsonb
 
       add :resource_id, references(:resource, on_delete: :delete_all)
       add :resource_history_id, references(:resource_history, on_delete: :delete_all)
-      add :validated_data_name, :string
+      add :validated_data_name, :text
 
       add :secondary_resource_id, references(:resource, on_delete: :delete_all)
       add :secondary_resource_history_id, references(:resource_history, on_delete: :delete_all)
-      add :secondary_validated_data_name, :string
+      add :secondary_validated_data_name, :text
 
       timestamps([type: :utc_datetime_usec])
     end
