61e3aec4ce5eecbfdd618b9dcf9280b5b6d7419a
Francis Chabouis <fchabouis@gmail.com>
2022-05-03 15:58:58 +0200

    add test for already_validated?

---
+++ apps/db/test/multi_validation_test.exs
@@ -0,0 +1,22 @@
+defmodule DB.MultiValidationTest do
+  use ExUnit.Case, async: true
+  doctest DB.MultiValidation
+  import DB.Factory
+
+  setup do
+    Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+  end
+
+  test "already validated" do
+    %{id: resource_history_id} = resource_history = insert(:resource_history)
+
+    refute DB.MultiValidation.already_validated?(resource_history, Transport.Validators.Dummy)
+
+    # wrong validator name
+    insert(:multi_validation, %{validator: "coucou", resource_history_id: resource_history_id})
+    refute DB.MultiValidation.already_validated?(resource_history, Transport.Validators.Dummy)
+
+    insert(:multi_validation, %{validator: Transport.Validators.Dummy.validator_name(), resource_history_id: resource_history_id})
+    assert DB.MultiValidation.already_validated?(resource_history, Transport.Validators.Dummy)
+  end
+end
--- apps/transport/lib/validators/validator.ex
+++ apps/transport/lib/validators/validator.ex
@@ -8,3 +8,13 @@ defmodule Transport.Validators.Validator do
   @callback validate(any()) :: :ok | {:error, any()}
   @callback validator_name() :: binary()
 end
+
+defmodule Transport.Validators.Dummy do
+  @behaviour Transport.Validators.Validator
+
+  @impl Transport.Validators.Validator
+  def validate(_), do: :ok
+
+  @impl Transport.Validators.Validator
+  def validator_name, do: "dummy validator"
+end
