fd7d48d2c8c32ffb0b62d0b28eb0cfaeeb864e92
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-05-03 10:38:46 +0200

    replace content

--- config/database.exs
+++ config/database.exs
@@ -1,10 +1,10 @@
 import Config
 
-config :db, DB.Repo,
+config :transport, DB.Repo,
   url: System.get_env("PG_URL") || "ecto://postgres:postgres@localhost/transport_repo",
   # NOTE: this default pool_size is overriden by "prod.exs" !
   pool_size: (System.get_env("PG_POOL_SIZE") || "10") |> String.to_integer(),
   types: DB.PostgrexTypes
 
-config :db, ecto_repos: [DB.Repo]
+config :transport, ecto_repos: [DB.Repo]
 config :transport, ecto_repos: [DB.Repo]
