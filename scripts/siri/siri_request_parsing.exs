3957ac32ee36a5d5a53a97d0653bf4c2a856cc8d
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-05-10 15:32:07 +0200

    Save WIP for transparent XML round-trip (decode/encode)

--- scripts/siri/siri_request_parsing.exs
+++ scripts/siri/siri_request_parsing.exs
@@ -1,1 +1,21 @@
+Mix.install([
+  # for UUID generation
+  {:ecto, "~> 3.8.2"},
+  # parser and encoder
+  {:saxy, "~> 1.4"}
+])
 Code.require_file(__DIR__ |> (Path.join "/siri_queries.exs"))
+
+timestamp = DateTime.utc_now() |> DateTime.to_iso8601()
+message_id = "Test::Message::#{Ecto.UUID.generate()}"
+requestor_ref = "MY-REF"
+
+request = SIRI.stop_points_discovery(timestamp, requestor_ref, message_id)
+
+{:ok, parsed_request} = Saxy.SimpleForm.parse_string(request, cdata_as_characters: false)
+# TODO: add encoding
+# TODO: verify input encoding (saxy only support UTF-8)
+encoded_request = Saxy.encode!(parsed_request, prolog: [version: "1.0", encoding: "UTF-8"])
+
+File.write!("input.xml", request)
+File.write!("output.xml", encoded_request)
