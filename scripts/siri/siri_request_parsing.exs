b07baba2f4c57f82e010a6b8dc4c1210cf3c3c26
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-05-13 15:19:08 +0200

    Create a round-trip situation

--- scripts/siri/siri_request_parsing.exs
+++ scripts/siri/siri_request_parsing.exs
@@ -13,9 +13,15 @@ requestor_ref = "MY-REF"
 request = SIRI.stop_points_discovery(timestamp, requestor_ref, message_id)
 
 {:ok, parsed_request} = Saxy.SimpleForm.parse_string(request, cdata_as_characters: false)
-# TODO: add encoding
 # TODO: verify input encoding (saxy only support UTF-8)
 encoded_request = Saxy.encode!(parsed_request, version: "1.0", encoding: :utf8)
 
-File.write!("input.xml", request)
-File.write!("output.xml", encoded_request)
+import ExUnit.Assertions
+
+# see https://github.com/qcam/saxy/issues/101
+encoded_request = encoded_request |> String.replace(~S(encoding="utf-8"?>), ~S(encoding="UTF-8"?>) <> "\n") |> String.trim()
+request = request |> String.trim()
+# File.write!("input.xml", request |> String.trim())
+# File.write!("output.xml", encoded_request |> String.trim())
+# System.shell("colordiff input.xml output.xml", into: IO.stream())
+assert request == encoded_request
