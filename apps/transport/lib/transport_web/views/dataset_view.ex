690d6d5f14c6b02edee08000387b6ba444599880
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-05-24 09:06:14 +0200

    Dataset#details lien vers temps réel transport public (#2407)
    * Dataset#details lien vers temps réel transport public
    
    * Add template

--- apps/transport/lib/transport_web/views/dataset_view.ex
+++ apps/transport/lib/transport_web/views/dataset_view.ex
@@ -329,6 +329,12 @@ defmodule TransportWeb.DatasetView do
     |> Enum.filter(&Resource.is_documentation?/1)
   end
 
+  def is_real_time_public_transit?(%Dataset{type: "public-transit"} = dataset) do
+    not Enum.empty?(real_time_official_resources(dataset))
+  end
+
+  def is_real_time_public_transit?(%Dataset{}), do: false
+
   def community_resources(dataset), do: Dataset.community_resources(dataset)
 
   def licence_url("fr-lo"),
