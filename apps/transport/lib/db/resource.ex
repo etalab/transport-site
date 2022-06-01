7106218b6d0f860dff119c84c2748b02211b31eb
Francis Chabouis <fchabouis@gmail.com>
2022-06-01 11:56:52 +0200

    Réparer la carte IRVE (#2423)
    * fix irve map
    
    * modify the on_delete action
    
    * reject documentation for display
    
    * fix credo warnings
    
    * check against true to return boolean
    
    * Update apps/transport/lib/transport_web/views/dataset_view.ex
    
    Co-authored-by: Antoine Augusti <antoine.augusti@beta.gouv.fr>
    
    * better readability
    
    * format
    
    Co-authored-by: Antoine Augusti <antoine.augusti@beta.gouv.fr>

--- apps/transport/lib/db/resource.ex
+++ apps/transport/lib/db/resource.ex
@@ -625,6 +625,10 @@ defmodule DB.Resource do
   def is_documentation?(%__MODULE__{type: "documentation"}), do: true
   def is_documentation?(_), do: false
 
+  @spec is_community_resource?(__MODULE__.t()) :: boolean
+  def is_community_resource?(%__MODULE__{is_community_resource: true}), do: true
+  def is_community_resource?(_), do: false
+
   @spec is_real_time?(__MODULE__.t()) :: boolean
   def is_real_time?(%__MODULE__{} = resource) do
     is_gtfs_rt?(resource) or is_gbfs?(resource) or is_siri_lite?(resource) or is_siri?(resource)
