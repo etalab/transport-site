e28214936cfac7a16390391e6e41e4bbcfa821ce
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-06-16 11:21:24 +0200

    Adapt code

--- apps/unlock/lib/siri.ex
+++ apps/unlock/lib/siri.ex
@@ -19,14 +19,14 @@ defmodule Unlock.SIRI do
     "<root>\u0044<hello></root>"
     """
 
-    def replace_requestor_ref(data, config) do
+    def replace_requestor_ref(data, config = %{new_requestor_ref: new_requestor_ref}) do
       case data do
         a = {tag, attributes, [some_text]} when is_binary(some_text) ->
           [last | _] = tag |> String.split(":") |> Enum.reverse()
 
           if last == "RequestorRef" do
             # TODO: add check & stop processing if incorrect requestor ref is detected
-            {tag, attributes, [config.after]}
+            {tag, attributes, [new_requestor_ref]}
           else
             a
           end
