4c7cf35294616e40a051ba3c6414f713906dbd8d
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-07-08 16:27:17 +0200

    Remove dead code

--- apps/shared/test/schemas_test.exs
+++ apps/shared/test/schemas_test.exs
@@ -51,13 +51,6 @@ defmodule Transport.Shared.SchemasTest do
     assert_in_delta Cachex.ttl!(cache_name(), cache_key), :timer.seconds(expected_ttl), :timer.seconds(1)
   end
 
-  defp setup_schema_response(expected_url) do
-    Transport.HTTPoison.Mock
-    |> expect(:get!, fn ^expected_url ->
-      %HTTPoison.Response{body: ~s({"foo": "bar"}), status_code: 200}
-    end)
-  end
-
   defp setup_schemas_response do
     url = "https://schema.data.gouv.fr/schemas.json"
 
