a0b67d43e411877dae22571d27cf2c3745905bd5
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-07-21 10:20:15 +0200

    Ignore files stored by GTFS-RT MultiValidation

--- apps/transport/lib/jobs/backfill/remove_gtfs_rt_snapshots.ex
+++ apps/transport/lib/jobs/backfill/remove_gtfs_rt_snapshots.ex
@@ -3,17 +3,18 @@ defmodule Transport.Jobs.Backfill.RemoveGTFSRTSnapshots do
   Delete GTFS-RT files that have been stored in the object storage with the v1 validation.
   """
   use Oban.Worker
+  alias Transport.Validators.GTFSRT
+  import Ecto.Query
   @bucket_feature :history
 
   @impl true
   def perform(%{}) do
     relevant_file_keys()
-    |> Stream.each(&Transport.S3.delete_object!(@bucket_feature, &1))
-    |> Stream.run()
+    |> Enum.each(&Transport.S3.delete_object!(@bucket_feature, &1))
   end
 
   def count_relevant_files do
-    relevant_file_keys() |> Enum.to_list() |> Enum.count()
+    relevant_file_keys() |> Enum.count()
   end
 
   def relevant_file_keys do
@@ -26,5 +27,17 @@ defmodule Transport.Jobs.Backfill.RemoveGTFSRTSnapshots do
     # See https://github.com/etalab/transport-site/blob/3c870a42d0e6212f181cc69818755b5871f7f62c/apps/transport/lib/jobs/gtfs_rt_validation_job.ex#L207-L211
     |> Stream.filter(&String.match?(&1.key, ~r/\.2022(\d){4}\.(\d){6}\.(\d){6}\.bin$/))
     |> Stream.map(& &1.key)
+    |> MapSet.new()
+    |> MapSet.difference(multi_validation_gtfs_rt_filenames())
+  end
+
+  def multi_validation_gtfs_rt_filenames do
+    validator_name = GTFSRT.validator_name()
+
+    DB.MultiValidation.base_query()
+    |> where([mv], mv.validator == ^validator_name and is_nil(mv.oban_args))
+    |> select(fragment("result->'files'->'gtfs_rt_filename'"))
+    |> DB.Repo.all()
+    |> MapSet.new()
   end
 end
