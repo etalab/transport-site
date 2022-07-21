7c2206d1627498b369b9817d5fc0eb398f2e9582
Francis Chabouis <fchabouis@gmail.com>
2022-07-21 18:07:11 +0200

    remove import

--- apps/transport/lib/transport_web/views/resource_view.ex
+++ apps/transport/lib/transport_web/views/resource_view.ex
@@ -5,7 +5,6 @@ defmodule TransportWeb.ResourceView do
   import Phoenix.Controller, only: [current_url: 2]
   import TransportWeb.BreadCrumbs, only: [breadcrumbs: 1]
   import TransportWeb.DatasetView, only: [schema_url: 1, errors_count: 1, warnings_count: 1]
-  import DB.Resource, only: [has_errors_details?: 1]
   import DB.ResourceUnavailability, only: [floor_float: 2]
   import Shared.DateTimeDisplay, only: [format_datetime_to_paris: 2]
   import Shared.Validation.TableSchemaValidator, only: [validata_web_url: 1]
