f0b17c859fed899d7cdb7d9bace0130019ea6275
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-07-23 15:57:01 +0200

    Create git_diff_experiment.exs

---
+++ scripts/git_diff_experiment.exs
@@ -0,0 +1,10 @@
+Mix.install([
+  {:git_diff, "~> 0.6.3"},
+  {:req, "~> 0.3.0"}
+])
+
+base =
+  "https://transport-data-gouv-fr-resource-history-prod.cellar-c2.services.clever-cloud.com/b5873d02-043e-447a-8433-0a3be706efc8/b5873d02-043e-447a-8433-0a3be706efc8.20220607.120433.286304.zip"
+
+new =
+  "https://transport-data-gouv-fr-resource-history-prod.cellar-c2.services.clever-cloud.com/b5873d02-043e-447a-8433-0a3be706efc8/b5873d02-043e-447a-8433-0a3be706efc8.20220608.180932.003492.zip"
