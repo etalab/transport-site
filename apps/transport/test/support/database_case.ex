21ca8dad13f9c443f7c953ebefb6e1fef51938c0
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-07-25 17:28:47 +0200

    Make test more deterministic by fixating ids for now

--- apps/transport/test/support/database_case.ex
+++ apps/transport/test/support/database_case.ex
@@ -31,32 +31,36 @@ defmodule TransportWeb.DatabaseCase do
           Sandbox.mode(Repo, {:shared, self()})
         end
 
-        Repo.insert(%Region{nom: "Pays de la Loire"})
-        Repo.insert(%Region{nom: "Auvergne-Rhône-Alpes"})
-        Repo.insert(%Region{nom: "Île-de-France"})
+        Repo.insert!(%Region{id: 1000, nom: "Pays de la Loire"})
+        Repo.insert!(%Region{id: 1001, nom: "Auvergne-Rhône-Alpes"})
+        Repo.insert!(%Region{id: 1002, nom: "Île-de-France"})
 
-        Repo.insert(%AOM{
+        Repo.insert!(%AOM{
+          id: 1003,
           insee_commune_principale: "53130",
           nom: "Laval",
           region: Repo.get_by(Region, nom: "Pays de la Loire"),
           composition_res_id: 1
         })
 
-        Repo.insert(%AOM{
+        Repo.insert!(%AOM{
+          id: 1004,
           insee_commune_principale: "38185",
           nom: "Grenoble",
           region: Repo.get_by(Region, nom: "Auvergne-Rhône-Alpes"),
           composition_res_id: 2
         })
 
-        Repo.insert(%AOM{
+        Repo.insert!(%AOM{
+          id: 1005,
           insee_commune_principale: "36044",
           nom: "Châteauroux",
           region: Repo.get_by(Region, nom: "Auvergne-Rhône-Alpes"),
           composition_res_id: 3
         })
 
-        Repo.insert(%Commune{
+        Repo.insert!(%Commune{
+          id: 1006,
           insee: "38185",
           nom: "Grenoble",
           wikipedia: "fr:Grenoble",
@@ -64,7 +68,8 @@ defmodule TransportWeb.DatabaseCase do
           aom_res_id: 2
         })
 
-        Repo.insert(%Commune{
+        Repo.insert!(%Commune{
+          id: 1007,
           insee: "36044",
           nom: "Châteauroux",
           wikipedia: "fr:Châteauroux",
@@ -72,7 +77,8 @@ defmodule TransportWeb.DatabaseCase do
           aom_res_id: 3
         })
 
-        Repo.insert(%Commune{
+        Repo.insert!(%Commune{
+          id: 1008,
           insee: "63096",
           nom: "Chas",
           wikipedia: "fr:Chas",
@@ -80,14 +86,16 @@ defmodule TransportWeb.DatabaseCase do
           aom_res_id: 3
         })
 
-        Repo.insert(%AOM{
+        Repo.insert!(%AOM{
+          id: 1009,
           insee_commune_principale: "75056",
           nom: "Île-de-France Mobilités",
           region: Repo.get_by(Region, nom: "Île-de-France"),
           composition_res_id: 4
         })
 
-        Repo.insert(%Commune{
+        Repo.insert!(%Commune{
+          id: 1010,
           insee: "36063",
           nom: "Déols",
           wikipedia: "fr:Déols",
