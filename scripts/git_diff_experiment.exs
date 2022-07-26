43f06611e273b8d9e71ae701e190f2608bd17a50
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-07-26 11:45:29 +0200

    Run git diff & parse output with git_diff package

--- scripts/git_diff_experiment.exs
+++ scripts/git_diff_experiment.exs
@@ -16,10 +16,26 @@ defmodule GTFSCompare do
   end
 
   def maybe_unpack(local_file, local_folder) do
-    unless File.exists?(local_folder), do: File.mkdir!(local_folder)
+    unless File.exists?(local_folder) do
+      File.mkdir!(local_folder)
 
-    {output, 0} =
-      System.cmd("unzip", ["../" <> local_file], cd: local_folder, stderr_to_stdout: true)
+      {output, 0} =
+        System.cmd("unzip", ["../" <> local_file], cd: local_folder, stderr_to_stdout: true)
+    end
+  end
+
+  # inspired by hexdiff
+  def git_compare(base, new) do
+    case System.cmd("git", [
+           "diff",
+           "--no-index",
+           "--no-color",
+           base,
+           new
+         ]) do
+      {"", 0} -> {:ok, nil}
+      {output, 1} -> {:ok, output}
+    end
   end
 end
 
@@ -30,9 +46,26 @@ new =
   "https://transport-data-gouv-fr-resource-history-prod.cellar-c2.services.clever-cloud.com/b5873d02-043e-447a-8433-0a3be706efc8/b5873d02-043e-447a-8433-0a3be706efc8.20220608.180932.003492.zip"
 
 unless File.exists?("downloads"), do: File.mkdir!("downloads")
+
+# TODO: clean-up stuff (handle proper temp folders etc)
 GTFSCompare.maybe_download(base, "downloads/base.zip")
 GTFSCompare.maybe_download(new, "downloads/new.zip")
-GTFSCompare.maybe_unpack("base.zip", "downloads/base.zip.unpacked")
-GTFSCompare.maybe_unpack("new.zip", "downloads/new.zip.unpacked")
+GTFSCompare.maybe_unpack("base.zip", base_folder = "downloads/base.zip.unpacked")
+GTFSCompare.maybe_unpack("new.zip", new_folder = "downloads/new.zip.unpacked")
 
-IO.puts("ok")
+IO.puts("Comparing...")
+
+{:ok, output} =
+  GTFSCompare.git_compare(
+    base_stop = base_folder <> "/stop_times.txt",
+    new_stop = new_folder <> "/stop_times.txt"
+  )
+
+if output do
+  GitDiff.parse_patch(output, relative_from: base_stop, relative_to: new_stop)
+  |> IO.inspect(IEx.inspect_opts())
+else
+  IO.puts("No diff in stop times")
+end
+
+IO.puts("Done compare...")
