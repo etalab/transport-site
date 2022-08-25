7aff3b615ddbfafe8519318197267e96de36be7a
Francis Chabouis <fchabouis@gmail.com>
2022-08-25 17:34:15 +0200

    Update apps/transport/lib/db/resource.ex
    Co-authored-by: Antoine Augusti <antoine.augusti@beta.gouv.fr>

--- apps/transport/lib/db/resource.ex
+++ apps/transport/lib/db/resource.ex
@@ -646,8 +646,7 @@ defmodule DB.Resource do
   end
 
   @spec has_schema?(__MODULE__.t()) :: boolean
-  def has_schema?(%__MODULE__{schema_name: schema_name}) when not is_nil(schema_name), do: true
-  def has_schema?(_), do: false
+  def has_schema?(%__MODULE__{schema_name: schema_name}), do: not is_nil(schema_name)
 
   @spec ttl(__MODULE__.t()) :: integer() | nil
   def ttl(%__MODULE__{format: "gbfs", metadata: %{"ttl" => ttl}})
