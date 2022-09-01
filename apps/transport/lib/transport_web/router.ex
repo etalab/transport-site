2d61b5ce91caccf573c7f8eabfe55c80858205cf
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-09-01 17:29:25 +0200

    Avoid compile-time config evaluation
    Moving to runtime allows us to work with Elixir releases later.
    
    I'm not filtering on web-server only because the place where the plug is used is already web-server specific (the worker part is stopped earlier in the stack).
    
    I'm not filtering on Mix.env either because we want to be able to verify the behaviour in dev (using `/etc/hosts`) and test, to later fix the numerous broken tests.

--- apps/transport/lib/transport_web/router.ex
+++ apps/transport/lib/transport_web/router.ex
@@ -8,10 +8,7 @@ defmodule TransportWeb.Router do
   end
 
   pipeline :browser do
-    plug(PlugCanonicalHost,
-      if: {Transport.Application, :webserver_only? && Mix.env() == :prod},
-      canonical_host: Application.fetch_env!(:transport, :domain_name)
-    )
+    plug(:canonical_host)
 
     plug(:accepts, ["html"])
     plug(:fetch_session)
@@ -278,5 +275,12 @@ defmodule TransportWeb.Router do
       |> redirect(to: Helpers.page_path(conn, :login, redirect_path: current_path(conn)))
       |> halt()
     end
+  end
+
+  # see https://github.com/remi/plug_canonical_host#usage
+  defp canonical_host(conn, _) do
+    host = Application.fetch_env!(:transport, :domain_name)
+    opts = PlugCanonicalHost.init(canonical_host: host)
+    PlugCanonicalHost.call(conn, opts)
   end
 end
