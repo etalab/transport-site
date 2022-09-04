9c14478374bd8ee4367af716d51b54ce20b65a38
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-09-04 15:58:49 +0200

    Fix GBFSLive (manual test)

--- apps/transport/lib/transport_web/live/backoffice/gbfs_live.ex
+++ apps/transport/lib/transport_web/live/backoffice/gbfs_live.ex
@@ -3,7 +3,6 @@ defmodule TransportWeb.Backoffice.GBFSLive do
   A view able to display the current running configuration of the proxy.
   """
   use Phoenix.LiveView
-  use Phoenix.HTML
   alias Transport.Telemetry
   import TransportWeb.Backoffice.JobsLive, only: [ensure_admin_auth_or_redirect: 3]
   import TransportWeb.Router.Helpers
