395c28683a3666574bd3e5f79b58f8e898e9a56d
Francis Chabouis <fchabouis@gmail.com>
2022-09-09 11:37:42 +0200

    simplify validators function

--- apps/transport/lib/validators/validator_selection.ex
+++ apps/transport/lib/validators/validator_selection.ex
@@ -27,10 +27,10 @@ defmodule Transport.ValidatorsSelection.Impl do
   end
 
   @doc """
-  Get a list of validators to run for a `DB.ResourceHistory`, `DB.Resource`, a map of format and schema or just a format
+  Get a list of validators to run for a `DB.ResourceHistory`, `DB.Resource`, a map of format and schema
   """
   @impl Transport.ValidatorsSelection
-  @spec validators(binary() | DB.ResourceHistory.t() | DB.Resource.t() | map()) :: list()
+  @spec validators(DB.ResourceHistory.t() | DB.Resource.t() | map()) :: list()
   def validators(%DB.ResourceHistory{payload: payload}) do
     validators(%{format: Map.get(payload, "format"), schema_name: Map.get(payload, "schema_name")})
   end
@@ -39,21 +39,11 @@ defmodule Transport.ValidatorsSelection.Impl do
     validators(%{format: format, schema_name: schema_name})
   end
 
-  def validators(%{format: format, schema_name: schema_name}) do
-    validators = validators(format)
+  def validators(%{format: "GTFS"}), do: [Validators.GTFSTransport]
+  def validators(%{format: "gtfs-rt"}), do: [Validators.GTFSRT]
+  def validators(%{format: "gbfs"}), do: [Validators.GBFSValidator]
 
-    if Enum.empty?(validators) and not is_nil(schema_name) do
-      validators_for_schema(schema_name)
-    else
-      validators
-    end
-  end
-
-  def validators(format) do
-    format |> get_validators(formats_and_validators())
-  end
-
-  defp validators_for_schema(schema_name) do
+  def validators(%{schema_name: schema_name}) do
     cond do
       Schemas.is_tableschema?(schema_name) ->
         [Transport.Validators.TableSchema]
@@ -64,15 +54,5 @@ defmodule Transport.ValidatorsSelection.Impl do
       true ->
         []
     end
-  end
-
-  @doc """
-  iex> validators("GBFS", %{"GBFS" => ["v1", "v2"], "GTFS" => ["v3"]})
-  ["v1", "v2"]
-  iex> validators("GBFS", %{"GTFS" => ["v1"]})
-  []
-  """
-  def get_validators(format, formats_and_validators) do
-    formats_and_validators |> Map.get(format, [])
   end
 end
