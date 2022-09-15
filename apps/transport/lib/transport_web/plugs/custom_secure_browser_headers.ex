bc1b1aa61b8ccceecb64881a31cf9bd584486dac
Francis Chabouis <fchabouis@gmail.com>
2022-09-15 13:01:49 +0200

    Update apps/transport/lib/transport_web/plugs/custom_secure_browser_headers.ex
    Co-authored-by: Antoine Augusti <antoine.augusti@beta.gouv.fr>

--- apps/transport/lib/transport_web/plugs/custom_secure_browser_headers.ex
+++ apps/transport/lib/transport_web/plugs/custom_secure_browser_headers.ex
@@ -35,7 +35,7 @@ defmodule TransportWeb.Plugs.CustomSecureBrowserHeaders do
           font-src 'self';
           img-src 'self' data: https://api.mapbox.com https://static.data.gouv.fr https://www.data.gouv.fr;
           script-src 'self' 'unsafe-eval' 'unsafe-inline' https://stats.data.gouv.fr/matomo.js;
-          style-src 'self' 'unsafe-inline'
+          style-src 'self'
           """
 
         :staging ->
