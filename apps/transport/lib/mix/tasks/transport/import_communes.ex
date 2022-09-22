f1f6c5d54841bb9a7def96f428218668cfd96bcc
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-09-22 15:56:31 +0200

    Ajout tache Mix pour import de communes

---
+++ apps/transport/lib/mix/tasks/transport/import_communes.ex
@@ -0,0 +1,113 @@
+defmodule Mix.Tasks.Transport.ImportCommunes do
+  @moduledoc """
+  Import the EPCI file to get the relation between the cities and the EPCI
+  """
+  use Mix.Task
+  import Ecto.Query
+  alias Ecto.Changeset
+  alias DB.{Commune, Region, Repo}
+  require Logger
+
+  @communes_geojson_url "http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/communes-1000m.geojson"
+  # See https://github.com/etalab/decoupage-administratif
+  @communes_url "https://unpkg.com/@etalab/decoupage-administratif@2.2.1/data/communes.json"
+
+  def insert_or_update_commune(
+        %{"code" => insee, "nom" => nom, "region" => region_insee},
+        regions,
+        geojsons
+      ) do
+    insee
+    |> get_or_create_commune()
+    |> Changeset.change(%{
+      insee: insee,
+      nom: nom,
+      region_id: Map.fetch!(regions, region_insee),
+      geom: build_geometry(geojsons, insee)
+    })
+    |> Repo.insert_or_update(force: true)
+  end
+
+  defp regions_by_insee do
+    Region |> Repo.all() |> Enum.into(%{}, fn region -> {region.insee, region.id} end)
+  end
+
+  defp geojson_by_insee do
+    @communes_geojson_url
+    |> HTTPoison.get!()
+    |> Map.fetch!(:body)
+    |> Jason.decode!()
+    |> Map.fetch!("features")
+    |> Enum.into(%{}, fn record -> {record["properties"]["code"], record["geometry"]} end)
+  end
+
+  defp get_or_create_commune(insee) do
+    Commune
+    |> Repo.get_by(insee: insee)
+    |> case do
+      nil ->
+        %Commune{}
+
+      commune ->
+        commune
+    end
+  end
+
+  defp build_geometry(geojsons, insee) do
+    {:ok, geom} = Geo.PostGIS.Geometry.cast(Map.fetch!(geojsons, insee))
+    %{geom | srid: 4326}
+  end
+
+  defp load_etalab_communes(region_insees) do
+    @communes_url
+    |> HTTPoison.get!()
+    |> Map.fetch!(:body)
+    |> Jason.decode!()
+    |> Enum.filter(&(&1["type"] == "commune-actuelle" and &1["region"] in region_insees))
+  end
+
+  def run(_params) do
+    Logger.info("Importing communes")
+
+    Mix.Task.run("app.start")
+
+    regions = regions_by_insee()
+    geojsons = geojson_by_insee()
+    region_insees = regions |> Map.keys()
+
+    etalab_communes = load_etalab_communes(region_insees)
+    etalab_insee = etalab_communes |> Enum.map(& &1["code"])
+    communes_insee = Commune |> select([c], c.insee) |> Repo.all()
+
+    nb_new = etalab_insee |> MapSet.new() |> MapSet.difference(MapSet.new(communes_insee)) |> Enum.count()
+    removed_communes = communes_insee |> MapSet.new() |> MapSet.difference(MapSet.new(etalab_insee)) |> Enum.into([])
+    nb_removed = removed_communes |> Enum.count()
+
+    Logger.info("#{nb_new} new communes")
+    Logger.info("#{nb_removed} communes should be removed")
+
+    duplicates_insee =
+      Commune
+      |> group_by([c], c.insee)
+      |> having([c], count(c.id) > 1)
+      |> select([c], c.insee)
+      |> Repo.all()
+
+    Logger.info("#{Enum.count(duplicates_insee)} communes are duplicates by INSEE code")
+
+    Commune |> where([c], c.insee in ^duplicates_insee) |> Repo.delete_all()
+    Commune |> where([c], c.insee in ^removed_communes) |> Repo.delete_all()
+
+    disable_trigger()
+    etalab_communes |> Enum.each(&insert_or_update_commune(&1, regions, geojsons))
+    Logger.info("Finished. Enabling trigger and refreshing views.")
+    enable_trigger()
+  end
+
+  defp disable_trigger, do: Repo.query!("ALTER TABLE commune DISABLE TRIGGER refresh_places_commune_trigger;")
+
+  defp enable_trigger do
+    Repo.query!("ALTER TABLE commune DISABLE TRIGGER refresh_places_commune_trigger;")
+    Repo.query!("REFRESH MATERIALIZED VIEW places;")
+  end
+end
