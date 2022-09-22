36cf8995402157493a93a41c3e5178a1af16dc56
Francis Chabouis <fchabouis@gmail.com>
2022-09-22 16:58:08 +0200

    corresponding controller

--- apps/transport/lib/transport_web/controllers/backoffice/dataset_controller.ex
+++ apps/transport/lib/transport_web/controllers/backoffice/dataset_controller.ex
@@ -135,6 +135,21 @@ defmodule TransportWeb.Backoffice.DatasetController do
     |> redirect_to_index()
   end
 
+  @spec force_validate_gtfs_transport(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def force_validate_gtfs_transport(%Plug.Conn{} = conn, _args) do
+    ImportDataWorker.force_validate_gtfs_transport()
+
+    conn
+    |> put_flash(
+      :info,
+      dgettext(
+        "backoffice_dataset",
+        "GTFS Transport Validator has been force launched on all GTFS resources"
+      )
+    )
+    |> redirect_to_index()
+  end
+
   ## Private functions
 
   @spec flash(:ok | {:ok, any} | {:error, any}, Plug.Conn.t(), iodata(), iodata()) :: Plug.Conn.t()
