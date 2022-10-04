b7df8bc6350eb50342fd412c50679bc5bdf79ca0
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-10-04 10:15:33 +0200

    Wire sample query

--- apps/transport/lib/transport_web/live/siri_querier_live.ex
+++ apps/transport/lib/transport_web/live/siri_querier_live.ex
@@ -5,11 +5,25 @@ defmodule TransportWeb.Live.SIRIQuerierLive do
   require Logger
 
   def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:siri_query, generate_query("check_status"))
+
     {:ok, socket}
   end
 
-  def handle_event(event, params, socket) do
-    Logger.info("Got event #{event |> inspect} with params #{params |> inspect}")
+  def handle_event("generate", params, socket) do
+    %{"query_template" => template} = params
     {:noreply, socket}
+  end
+
+  # TODO: instead of using the string-based XML generation, use the safer "builder-based" approach,
+  # and keep our string-based XMLs as test fixtures to lock down the builder behaviour.
+  defp generate_query("check_status") do
+    # must conform to https://www.w3.org/TR/xmlschema-2/#dateTime
+    timestamp = DateTime.utc_now() |> DateTime.to_iso8601()
+    message_id = "Test::Message::#{Ecto.UUID.generate()}"
+    requestor_ref = "$$YOUR_REQUESTOR_REF$$"
+    Transport.SIRI.check_status(timestamp, message_id, requestor_ref)
   end
 end
