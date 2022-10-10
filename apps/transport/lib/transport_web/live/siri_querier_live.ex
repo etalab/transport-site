d82614dccd0bcde27505568bf1cc956b5ab04ff3
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-10-10 17:35:30 +0200

    Implement all parameter-less queries

--- apps/transport/lib/transport_web/live/siri_querier_live.ex
+++ apps/transport/lib/transport_web/live/siri_querier_live.ex
@@ -5,63 +5,70 @@ defmodule TransportWeb.Live.SIRIQuerierLive do
   require Logger
 
   def mount(_params, _session, socket) do
-    {:ok, socket |> get_some_test_config_in_there}
+    socket = socket |> get_some_test_config_in_there()
+
+    {:ok, socket}
   end
 
+  @doc """
+  Temporary code to auto-fill one access point based on proxy config
+  """
   def get_some_test_config_in_there(socket) do
-    # temporary code used to feed something in there
-    config = Application.fetch_env!(:unlock, :config_fetcher).fetch_config!() |> Map.values()
-    config = Enum.filter(config, fn x -> %Unlock.Config.Item.SIRI{} = x end)
+    socket
+    |> assign(:endpoint_url, get_one_siri_proxy_url(socket))
+    |> assign(:requestor_ref, Application.fetch_env!(:unlock, :siri_public_requestor_ref))
+    |> assign(:query_template, "CheckStatus")
+    |> assign(:query_template_choices, ["CheckStatus", "LinesDiscovery", "StopPointsDiscovery"])
+  end
 
-    base_url = TransportWeb.Backoffice.ProxyConfigLive.proxy_base_url(socket)
-    # TODO: create a double list from both proxied & non proxied elements, setting the requestor_ref automatically
-    # TODO: do not fill anything unless we have a "transport" member
-    config =
-      config
-      |> Enum.map(fn item ->
-        %{
-          endpoint_url: TransportWeb.Backoffice.ProxyConfigLive.get_proxy_resource_url(base_url, item.identifier)
-        }
+  def get_one_siri_proxy_url(socket) do
+    item =
+      Application.fetch_env!(:unlock, :config_fetcher).fetch_config!()
+      |> Map.values()
+      |> Enum.find(fn
+        %Unlock.Config.Item.SIRI{} -> true
+        _ -> false
       end)
 
-    urls = config |> Enum.map(& &1.endpoint_url)
-
-    socket
-    |> assign(:endpoint_url, urls |> List.first())
-    |> assign(:endpoint_urls, urls)
-    |> assign(:requestor_ref, Application.fetch_env!(:unlock, :siri_public_requestor_ref))
-  end
-
-  def handle_event("change_form", params, socket) do
-    socket =
-      socket
-      |> assign(:requestor_ref, params["config"]["requestor_ref"])
-      |> assign(:endpoint_url, params["config"]["endpoint_url"])
-
-    {:noreply, socket}
+    base_url = TransportWeb.Backoffice.ProxyConfigLive.proxy_base_url(socket)
+    TransportWeb.Backoffice.ProxyConfigLive.get_proxy_resource_url(base_url, item.identifier)
   end
 
   def handle_event("generate_query", _params, socket) do
     socket =
       socket
-      |> assign(:siri_query, generate_query("check_status", socket.assigns[:requestor_ref]))
+      |> assign(
+        :siri_query,
+        generate_query(socket.assigns[:query_template], socket.assigns[:requestor_ref])
+      )
 
     {:noreply, socket}
   end
 
-  def handle_event("execute_query", _params, socket) do
-    query = socket.assigns[:siri_query]
-    IO.puts(query)
+  def handle_event("change_form", params, socket) do
+    socket =
+      socket
+      |> assign(:endpoint_url, params["config"]["endpoint_url"])
+      |> assign(:requestor_ref, params["config"]["requestor_ref"])
+      |> assign(:query_template, params["config"]["query_template"])
+
     {:noreply, socket}
   end
+
+  def build_timestamp, do: DateTime.utc_now() |> DateTime.to_iso8601()
+  def build_message_id, do: "Test::Message::#{Ecto.UUID.generate()}"
 
   # TODO: instead of using the string-based XML generation, use the safer "builder-based" approach,
   # and keep our string-based XMLs as test fixtures to lock down the builder behaviour.
-  defp generate_query("check_status", requestor_ref) do
-    # must conform to https://www.w3.org/TR/xmlschema-2/#dateTime
-    timestamp = DateTime.utc_now() |> DateTime.to_iso8601()
-    message_id = "Test::Message::#{Ecto.UUID.generate()}"
-    requestor_ref = requestor_ref
-    Transport.SIRI.check_status(timestamp, message_id, requestor_ref)
+  defp generate_query("CheckStatus", requestor_ref) do
+    Transport.SIRI.check_status(build_timestamp(), build_message_id(), requestor_ref)
+  end
+
+  defp generate_query("StopPointsDiscovery", requestor_ref) do
+    Transport.SIRI.stop_points_discovery(build_timestamp(), build_message_id(), requestor_ref)
+  end
+
+  defp generate_query("LinesDiscovery", requestor_ref) do
+    Transport.SIRI.lines_discovery(build_timestamp(), build_message_id(), requestor_ref)
   end
 end
