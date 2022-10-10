8fb38cb13c78c3ba8831893cd7b9c9a8fc4c1eca
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-10-10 17:05:49 +0200

    up/down migration Nouméa

--- apps/transport/priv/repo/migrations/20221004125601_set_noumea_siren.exs
+++ apps/transport/priv/repo/migrations/20221004125601_set_noumea_siren.exs
@@ -1,7 +1,11 @@
 defmodule DB.Repo.Migrations.SetNoumeaSiren do
   use Ecto.Migration
 
-  def change do
+  def up do
     execute("UPDATE commune SET siren = '200012508' WHERE nom = 'Nouméa';")
+  end
+
+  def down do
+    execute("UPDATE commune SET siren = null WHERE nom = 'Nouméa';")
   end
 end
