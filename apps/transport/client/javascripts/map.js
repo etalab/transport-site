34e7e688bb7da16c9e905105a8633362dadff008
Francis Chabouis <fchabouis@gmail.com>
2022-10-10 11:02:07 +0200

    use object shorthand notation

--- apps/transport/client/javascripts/map.js
+++ apps/transport/client/javascripts/map.js
@@ -70,8 +70,8 @@ function getAomsFG (featureFunction, style, filter = null) {
         .then(response => {
             const geoJSON = Leaflet.geoJSON(response, {
                 onEachFeature: featureFunction,
-                style: style,
-                filter: filter,
+                style,
+                filter,
                 pane: 'aoms'
             })
             aomsFeatureGroup.addLayer(geoJSON)
@@ -89,7 +89,7 @@ function getRegionsFG (featureFunction, style) {
         .then(response => {
             const geoJSON = Leaflet.geoJSON(response, {
                 onEachFeature: featureFunction,
-                style: style,
+                style,
                 pointToLayer: (_point, _) => null
             })
             regionsFeatureGroup.addLayer(geoJSON)
@@ -129,7 +129,7 @@ function displayQuality (featureFunction, style) {
         .then(response => {
             const geoJSON = Leaflet.geoJSON(response, {
                 onEachFeature: featureFunction,
-                style: style
+                style
             })
             qualityFeatureGroup.addLayer(geoJSON)
         })
