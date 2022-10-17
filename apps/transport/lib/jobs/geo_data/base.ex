5ab34950c46e8aef1ac953b0a7b8b9be266a98e1
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-10-17 11:46:23 +0200

    Refactor

--- apps/transport/lib/jobs/geo_data/base.ex
+++ apps/transport/lib/jobs/geo_data/base.ex
@@ -21,30 +21,33 @@ defmodule Transport.Jobs.BaseGeoData do
   def needs_import?(_, nil), do: true
 
   def import_replace_data(%DB.Resource{id: resource_id, dataset_id: dataset_id}, prepare_data_for_insert_fn) do
-    Logger.info("New content detected...update content")
-
-    %{id: latest_resource_history_id, payload: %{"permanent_url" => permanent_url}} =
-      latest_resource_history = DB.ResourceHistory.latest_resource_history(resource_id)
-
+    latest_resource_history = DB.ResourceHistory.latest_resource_history(resource_id)
     current_geo_data_import = DB.GeoDataImport.dataset_latest_geo_data_import(dataset_id)
 
-    needs_import = needs_import?(latest_resource_history, current_geo_data_import)
-
-    DB.Repo.transaction(fn ->
-      if needs_import do
-        unless is_nil(current_geo_data_import) do
-          # thanks to cascading delete, it will also clean geo_data table corresponding entries
-          current_geo_data_import |> DB.Repo.delete!()
-        end
-
-        %{id: geo_data_import_id} = DB.Repo.insert!(%DB.GeoDataImport{resource_history_id: latest_resource_history_id})
-        http_client = Transport.Shared.Wrapper.HTTPoison.impl()
-        %{status_code: 200, body: body} = http_client.get!(permanent_url)
-        insert_data(body, geo_data_import_id, prepare_data_for_insert_fn)
-      end
-    end)
+    if needs_import?(latest_resource_history, current_geo_data_import) do
+      Logger.info("New content detected...update content")
+      perform_import(current_geo_data_import, latest_resource_history, prepare_data_for_insert_fn)
+    end
 
     :ok
+  end
+
+  defp perform_import(
+         current_geo_data_import,
+         %DB.ResourceHistory{id: latest_resource_history_id, payload: %{"permanent_url" => permanent_url}},
+         prepare_data_for_insert_fn
+       ) do
+    DB.Repo.transaction(fn ->
+      unless is_nil(current_geo_data_import) do
+        # thanks to cascading delete, it will also clean geo_data table corresponding entries
+        current_geo_data_import |> DB.Repo.delete!()
+      end
+
+      %{id: geo_data_import_id} = DB.Repo.insert!(%DB.GeoDataImport{resource_history_id: latest_resource_history_id})
+      http_client = Transport.Shared.Wrapper.HTTPoison.impl()
+      %{status_code: 200, body: body} = http_client.get!(permanent_url)
+      insert_data(body, geo_data_import_id, prepare_data_for_insert_fn)
+    end)
   end
 
   # keep 6 digits for WGS 84, see https://en.wikipedia.org/wiki/Decimal_degrees#Precision
