ea357d1044ae002c9c2b1a9462f4ab65b9fd8063
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-10-18 09:27:05 +0200

    Work on paths

--- apps/transport/lib/transport_web/controllers/explore_controller.ex
+++ apps/transport/lib/transport_web/controllers/explore_controller.ex
@@ -1,13 +1,9 @@
 defmodule TransportWeb.ExploreController do
   use TransportWeb, :controller
 
-  def index(conn, _params) do
-    conn
-    |> render("explore.html")
-  end
+  def index(conn, _params), do: conn |> render("explore.html")
 
   def vehicle_positions(conn, _params) do
-    conn
-    |> redirect(to: explore_path(conn, :index))
+    conn |> redirect(to: explore_path(conn, :index))
   end
 end
