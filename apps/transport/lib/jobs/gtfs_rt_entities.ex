6b5772c044a2389d6cc53343d895b6ce152f56e8
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-10-18 14:25:00 +0200

    GTFSRTEntitiesJob : ne plus stocker dans resource.metadata

--- apps/transport/lib/jobs/gtfs_rt_entities.ex
+++ apps/transport/lib/jobs/gtfs_rt_entities.ex
@@ -27,16 +27,12 @@ end
 defmodule Transport.Jobs.GTFSRTEntitiesJob do
   @moduledoc """
   Job in charge of keeping track of which entities are present
-  in a GTFS-RT feed over the last 7 days by leveraging what has
-  been seen and stored in the metadata and decoding
-  the feed when the job is performed.
+  in a GTFS-RT feed.
   """
   use Oban.Worker, max_attempts: 3
   require Logger
   alias DB.{Repo, Resource}
   alias Transport.GTFSRT
-
-  @entities_metadata_key "entities_last_seen"
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"resource_id" => id}}) do
@@ -47,34 +43,27 @@ defmodule Transport.Jobs.GTFSRTEntitiesJob do
     :ok
   end
 
-  defp present_entities(count_entities) do
+  @doc """
+  Identifies which entities are present in a feed from a count of entities.
+
+  ## Examples
+
+  iex> present_entities(%{})
+  []
+  iex> present_entities(%{service_alerts: 0, vehicle_positions: 1})
+  ["vehicle_positions"]
+  iex> present_entities(%{service_alerts: 2, vehicle_positions: 1})
+  ["service_alerts", "vehicle_positions"]
+  """
+  def present_entities(count_entities) do
     count_entities |> Map.filter(fn {_, v} -> v > 0 end) |> Enum.map(fn {k, _} -> Atom.to_string(k) end)
   end
 
-  def process_feed({:ok, feed}, %Resource{metadata: metadata} = resource) do
-    metadata = metadata || %{}
+  def process_feed({:ok, %TransitRealtime.FeedMessage{} = feed}, %Resource{id: resource_id}) do
     count_entities = feed |> GTFSRT.count_entities()
 
-    # this will be deleted later
-    # ⬇️⬇️⬇️
-    new_entities =
-      compute_new_entities(
-        Map.get(metadata, @entities_metadata_key, %{}),
-        count_entities,
-        DateTime.utc_now()
-      )
-
-    resource
-    |> Resource.changeset(%{
-      metadata: Map.put(metadata, @entities_metadata_key, new_entities),
-      features: Map.keys(new_entities)
-    })
-    |> Repo.update!()
-
-    # ⬆️⬆️⬆️
-
     %DB.ResourceMetadata{
-      resource_id: resource.id,
+      resource_id: resource_id,
       metadata: count_entities,
       features: present_entities(count_entities)
     }
@@ -83,28 +72,6 @@ defmodule Transport.Jobs.GTFSRTEntitiesJob do
 
   def process_feed({:error, _}, %Resource{id: id}) do
     Logger.error("Cannot decode GTFS-RT feed for resource##{id}")
-  end
-
-  @doc """
-  Compute entities that have been seen in the feed over the last
-  `days_to_keep()` days, using:
-  - existing_entities: `%{"trip_updates" => "2022-03-18 15:43:40.963443Z", "vehicle_positions" => "2022-03-18 15:43:40.963443Z"}`
-  - entities_in_feed: `%{trip_updates: 4, vehicle_positions: 0, service_alerts: 0}`
-
-  Will return the same type as `existing_entities`.
-  """
-  def compute_new_entities(existing_entities, entities_in_feed, now) do
-    entities_present = present_entities(entities_in_feed)
-
-    entities_still_valid =
-      existing_entities
-      |> Map.filter(fn {_, v} ->
-        {:ok, dt, 0} = DateTime.from_iso8601(v)
-        period_start = datetime_limit()
-        DateTime.compare(dt, period_start) == :gt
-      end)
-
-    Map.merge(entities_still_valid, entities_present |> Enum.into(%{}, &{&1, now |> DateTime.to_string()}))
   end
 
   def days_to_keep, do: 7
