7fb178cbd110bbfb7b62f6a66a7b720c200f7ec4
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-10-28 14:23:34 +0200

    Rename class

--- apps/transport/test/transport_web/controllers/api/datasets_controller_test.exs
+++ apps/transport/test/transport_web/controllers/api/datasets_controller_test.exs
@@ -1,4 +1,4 @@
-defmodule TransportWeb.DatasetControllerTest do
+defmodule TransportWeb.API.DatasetControllerTest do
   use TransportWeb.ConnCase, async: true
   use TransportWeb.DatabaseCase, cleanup: [:datasets]
   alias TransportWeb.API.Router.Helpers
