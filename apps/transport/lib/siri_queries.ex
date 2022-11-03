103ed010467efe2cc0072669b1d096697e5fc4ed
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-11-03 18:13:50 +0100

    Move GetStopMonitoring to XML builder

--- apps/transport/lib/siri_queries.ex
+++ apps/transport/lib/siri_queries.ex
@@ -119,26 +119,30 @@ defmodule Transport.SIRI do
   end
 
   def get_stop_monitoring(timestamp, requestor_ref, message_identifier, stop_ref) do
-    """
-    #{prolog()}
-    <S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
-    <S:Body>
-        <sw:GetStopMonitoring xmlns:sw="http://wsdl.siri.org.uk" xmlns:siri="http://www.siri.org.uk/siri">
-            <ServiceRequestInfo>
-                <siri:RequestTimestamp>#{timestamp}</siri:RequestTimestamp>
-                <siri:RequestorRef>#{requestor_ref}</siri:RequestorRef>
-                <siri:MessageIdentifier>#{message_identifier}</siri:MessageIdentifier>
-            </ServiceRequestInfo>
-            <Request>
-                <siri:RequestTimestamp>#{timestamp}</siri:RequestTimestamp>
-                <siri:MessageIdentifier>#{message_identifier}</siri:MessageIdentifier>
-                <siri:MonitoringRef>#{stop_ref}</siri:MonitoringRef>
-                <siri:StopVisitTypes>all</siri:StopVisitTypes>
-            </Request>
-        </sw:GetStopMonitoring>
-    </S:Body>
-    </S:Envelope>
-    """
+    doc =
+      element("S:Envelope", @top_level_namespaces, [
+        element("S:Body", [], [
+          element("sw:GetStopMonitoring", @request_namespaces, [
+            element("ServiceRequestInfo", [], [
+              element("siri:RequestTimestamp", [], timestamp),
+              element("siri:RequestorRef", [], requestor_ref),
+              element("siri:MessageIdentifier", [], message_identifier)
+            ]),
+            element(
+              "Request",
+              [],
+              [
+                element("siri:RequestTimestamp", [], timestamp),
+                element("siri:MessageIdentifier", [], message_identifier),
+                element("siri:MonitoringRef", [], stop_ref),
+                element("siri:StopVisitTypes", [], "all")
+              ]
+            )
+          ])
+        ])
+      ])
+
+    Saxy.encode!(doc)
   end
 
   def get_general_message(timestamp, requestor_ref, message_identifier) do
