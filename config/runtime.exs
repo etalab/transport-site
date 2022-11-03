60ac253b190142ebeb9c920006320fa45cc5a357
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2022-11-03 09:26:08 -0400

    Ajout job quotidien avec JDD à référencer (#2742)

--- config/runtime.exs
+++ config/runtime.exs
@@ -111,6 +111,7 @@ oban_crontab_all_envs =
         {"30 7 * * *", Transport.Jobs.GBFSMultiValidationDispatcherJob},
         {"45 */3 * * *", Transport.Jobs.ResourceHistoryJSONSchemaValidationJob},
         {"15 */3 * * *", Transport.Jobs.ResourceHistoryTableSchemaValidationJob},
+        {"5 6 * * *", Transport.Jobs.NewDatagouvDatasetsJob},
         {"0 6 * * *", Transport.Jobs.NewDatasetNotificationsJob}
       ]
 
