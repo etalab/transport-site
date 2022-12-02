5a1686cfe06122849bb233cc26d0dbaf7a6dda76
Francis Chabouis <fchabouis@gmail.com>
2022-12-02 16:34:19 +0100

    update tables definition

--- apps/transport/priv/repo/migrations/20221201165336_create_dataset_history.exs
+++ apps/transport/priv/repo/migrations/20221201165336_create_dataset_history.exs
@@ -5,15 +5,18 @@ defmodule DB.Repo.Migrations.CreateDatasetHistory do
     create table(:dataset_history) do
       add :dataset_id, references(:dataset, on_delete: :nothing)
       add :dataset_datagouv_id, :text
-      add :timestamp, :utc_datetime_usec
       add :payload, :jsonb
+
+      timestamps([type: :utc_datetime_usec])
     end
 
     create table(:dataset_history_resources) do
       add :dataset_history_id, references(:dataset_history)
       add :resource_id, references(:resource, on_delete: :nothing)
       add :resource_history_id, references(:resource_history, on_delete: :nothing)
+      add :resource_history_last_up_to_date_at, :utc_datetime_usec
       add :resource_metadata_id, references(:resource_metadata, on_delete: :nothing)
+      add :validation_id, references(:multi_validation, on_delete: :nothing)
       add :payload, :jsonb
     end
   end
