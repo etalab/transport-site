3634653155ca6986ba64ee558c3bd8a9d05cf313
Thibaut Barrère <thibaut.barrere@gmail.com>
2022-12-05 17:23:15 +0100

    Save sequential processing (without delete of prior copies)

--- scripts/download_resource_history_files.exs
+++ scripts/download_resource_history_files.exs
@@ -9,7 +9,7 @@ import Ecto.Query
 # - use gtfs_to_db once downloaded
 # - look at Transport.Jobs.BNLCToGeoData and Transport.Jobs.BaseGeoData.import_replace_data
 
-defmodule SyncS3LatestResourceHistory do
+defmodule Tooling do
   # for each active dataset, grab all resources with a resource history.
   def query() do
     DB.Dataset.base_query()
@@ -17,11 +17,13 @@ defmodule SyncS3LatestResourceHistory do
     |> DB.ResourceHistory.join_resource_with_latest_resource_history()
     |> where([resource: r], r.format == "GTFS")
     |> select([resource_history: rh], rh)
-    |> DB.Repo.all()
   end
+end
 
+defmodule SyncS3LatestResourceHistory do
   def sync!(minio_folder) do
-    query
+    Tooling.query()
+    |> DB.Repo.all()
     # TODO: task sync max concurrency (this is slow)
     |> Enum.each(fn rh ->
       # create local minio bucket, if needed
@@ -48,18 +50,33 @@ defmodule SyncS3LatestResourceHistory do
   end
 end
 
+defmodule IngestResourceHistoryGTFSData do
+  def process(rh) do
+    try do
+      data_import_id = Transport.Jobs.GtfsToDB.import_gtfs_from_resource_history(rh.id, :stops)
+      {:ok, data_import_id}
+    rescue
+      e ->
+        {:error, e}
+    end
+  end
+
+  def ingest!() do
+    Tooling.query()
+    # |> limit(50)
+    |> DB.Repo.all()
+    |> Enum.map(fn rh ->
+      Logger.info("Trying streaming...")
+      process(rh)
+    end)
+  end
+end
+
 # # create a local S3 copy (via MinIO) of each latest ResourceHistory file so
 # # that the production database dump can be used locally with a matching local S3 file
 SyncS3LatestResourceHistory.sync!(Path.expand("~/data"))
 
-# records =
-#   |> Enum.drop(1)
-#   |> Enum.take(1)
-#   |> Enum.each(fn rh ->
+IngestResourceHistoryGTFSData.ingest!()
+|> IO.inspect(IEx.inspect_opts())
 
-#     Logger.info("Trying streaming...")
-#     data_import_id = Transport.Jobs.GtfsToDB.import_gtfs_from_resource_history(rh.id, :stops)
-#     Logger.info("OK (data_import_id=#{data_import_id}")
-#   end)
-
-Logger.info("Done...")
+# TODO: handle deletion of duplicate (DELETE USING, CTE, or what is done in BLNC import)
