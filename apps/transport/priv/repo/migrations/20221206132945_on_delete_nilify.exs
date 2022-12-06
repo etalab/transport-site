ee15b37161b0ec8e63fd3f646a850bd0deab22ba
Francis Chabouis <fchabouis@gmail.com>
2022-12-06 15:34:19 +0100

    format

--- apps/transport/priv/repo/migrations/20221206132945_on_delete_nilify.exs
+++ apps/transport/priv/repo/migrations/20221206132945_on_delete_nilify.exs
@@ -3,15 +3,27 @@ defmodule DB.Repo.Migrations.OnDeleteNilify do
 
   def change do
     alter table(:dataset_history) do
-      modify :dataset_id, references(:dataset, on_delete: :nilify_all), from: references(:dataset, on_delete: :nothing)
+      modify(:dataset_id, references(:dataset, on_delete: :nilify_all),
+        from: references(:dataset, on_delete: :nothing)
+      )
     end
 
     alter table(:dataset_history_resources) do
-      modify :resource_id, references(:resource, on_delete: :nilify_all), from: references(:resource, on_delete: :nothing)
-      modify :resource_history_id, references(:resource_history, on_delete: :nilify_all), from: references(:resource_history, on_delete: :nothing)
-      modify :resource_metadata_id, references(:resource_metadata, on_delete: :nilify_all), from: references(:resource_metadata, on_delete: :nothing)
-      modify :validation_id, references(:multi_validation, on_delete: :nilify_all), from: references(:multi_validation, on_delete: :nothing)
-    end
+      modify(:resource_id, references(:resource, on_delete: :nilify_all),
+        from: references(:resource, on_delete: :nothing)
+      )
 
+      modify(:resource_history_id, references(:resource_history, on_delete: :nilify_all),
+        from: references(:resource_history, on_delete: :nothing)
+      )
+
+      modify(:resource_metadata_id, references(:resource_metadata, on_delete: :nilify_all),
+        from: references(:resource_metadata, on_delete: :nothing)
+      )
+
+      modify(:validation_id, references(:multi_validation, on_delete: :nilify_all),
+        from: references(:multi_validation, on_delete: :nothing)
+      )
+    end
   end
 end
