ed7eaa01b6087db1fe21b8d67febb60a821b28dd
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2022-12-12 17:17:59 +0100

    API : ajout de 2 champs pour les ressources

--- apps/transport/lib/transport_web/api/controllers/datasets_controller.ex
+++ apps/transport/lib/transport_web/api/controllers/datasets_controller.ex
@@ -297,9 +297,11 @@ defmodule TransportWeb.API.DatasetController do
       end
 
     %{
+      "id" => resource.id,
       "datagouv_id" => resource.datagouv_id,
       "title" => resource.title,
       "updated" => Shared.DateTimeDisplay.format_naive_datetime_to_paris_tz(resource.last_update),
+      "is_available" => resource.is_available,
       "url" => resource.latest_url,
       "original_url" => resource.url,
       "end_calendar_validity" => metadata_content && Map.get(metadata, "end_date"),
