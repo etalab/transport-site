f8351a9f270651302302f2835ee4a20f39568ee7
Francis Chabouis <fchabouis@gmail.com>
2022-12-28 14:52:34 +0100

    adapt test

--- apps/transport/test/transport_web/controllers/dataset_controller_test.exs
+++ apps/transport/test/transport_web/controllers/dataset_controller_test.exs
@@ -79,6 +79,11 @@ defmodule TransportWeb.DatasetControllerTest do
 
   test "has_validity_period?" do
     assert TransportWeb.DatasetView.has_validity_period?(%DB.ResourceHistory{
+             validations: [%{metadata: %{metadata: %{"start_date" => "2022-02-17", "end_date" => "2023-02-17"}}}]
+           })
+
+    # end_date is missing
+    refute TransportWeb.DatasetView.has_validity_period?(%DB.ResourceHistory{
              validations: [%{metadata: %{metadata: %{"start_date" => "2022-02-17"}}}]
            })
 
