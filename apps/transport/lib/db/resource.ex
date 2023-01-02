3a0d9ffb7cf24202ea77a79539bdd28e12f80095
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-01-02 10:01:21 +0100

    Update apps/transport/lib/db/resource.ex
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>

--- apps/transport/lib/db/resource.ex
+++ apps/transport/lib/db/resource.ex
@@ -434,9 +434,7 @@ defmodule DB.Resource do
   """
   def guess_requestor_ref(%__MODULE__{url: url} = resource) do
     if is_siri?(resource) do
-      host_to_key =
-        :transport
-        |> Application.fetch_env!(:public_siri_host_mappings)
+      host_to_key = Application.fetch_env!(:transport, :public_siri_host_mappings)
 
       resource_host = URI.parse(url).host
 
