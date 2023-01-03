4c0208221fcc10a500b598a93617fa12d48a46d2
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-01-03 08:57:43 +0100

    Success while implementing one test around GtfsToDB import part

--- apps/transport/test/transport/jobs/gtfs_import_job_test.exs
+++ apps/transport/test/transport/jobs/gtfs_import_job_test.exs
@@ -10,7 +10,11 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportJobTest do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
   end
 
-  test "stops import" do
+  def data_import_count do
+    DB.Repo.aggregate(DB.DataImport, :count, :id)
+  end
+
+  test "import stops" do
     %{id: dataset_id} = insert(:dataset, %{datagouv_id: "xxx", datagouv_title: "coucou"})
     %{id: resource_id} = insert(:resource, dataset_id: dataset_id)
     %{id: resource_history_id} = insert(:resource_history, %{
@@ -18,6 +22,26 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportJobTest do
       payload: %{"filename" => "some-file.zip"}
     })
 
-    data_import_id = Transport.Jobs.GtfsToDB.import_gtfs_from_resource_history(resource_history_id, :stops)
+    # TODO: reuse common code from Transport.Unzip.S3
+    Transport.Unzip.S3.Mock
+    |> expect(:get_file_stream, fn(file_in_zip, zip_file, bucket) ->
+      # from payload
+      assert zip_file == "some-file.zip"
+      # from config
+      assert bucket == "transport-data-gouv-fr-resource-history-test"
+
+      # stub with a local file
+      path = Path.dirname(__ENV__.file) |> Path.join("../../fixture/files/gtfs_import.zip")
+      zip_file = Unzip.LocalFile.open(path)
+      {:ok, unzip} = Unzip.new(zip_file)
+      Unzip.file_stream!(unzip, file_in_zip)
+    end)
+
+    assert data_import_count() == 0
+    _data_import_id = Transport.Jobs.GtfsToDB.import_gtfs_from_resource_history(resource_history_id, :stops)
+    assert data_import_count() == 1
+
+    # TODO: add test to ensure only one data import is kept, and only for the latest resource history. Previous
+    # ones should be deleted
   end
 end
