b38ba4ce0e9195472f861fb7e1de336baa11c8a9
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-01-05 10:46:28 +0100

    Mix format & simplification

--- apps/transport/test/transport/jobs/gtfs_import_job_test.exs
+++ apps/transport/test/transport/jobs/gtfs_import_job_test.exs
@@ -59,14 +59,14 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportJobTest do
   def setup_mox(zip_filename) do
     # NOTE: it will be possible to reuse common code from Transport.Unzip.S3 in there
     Transport.Unzip.S3.Mock
-    |> expect(:get_file_stream, fn(file_in_zip, zip_file, bucket) ->
+    |> expect(:get_file_stream, fn file_in_zip, zip_file, bucket ->
       # from payload
       assert zip_file == zip_filename
       # from config
       assert bucket == "transport-data-gouv-fr-resource-history-test"
 
       # stub with a local file
-      path = Path.dirname(__ENV__.file) |> Path.join("../../fixture/files/gtfs_import.zip")
+      path = "#{__DIR__}/../../fixture/files/gtfs_import.zip"
       zip_file = Unzip.LocalFile.open(path)
       {:ok, unzip} = Unzip.new(zip_file)
       Unzip.file_stream!(unzip, file_in_zip)
@@ -76,7 +76,9 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportJobTest do
   test "import stops" do
     %{id: dataset_id} = insert(:dataset, %{datagouv_id: "xxx", datagouv_title: "coucou"})
     %{id: resource_id} = insert(:resource, dataset_id: dataset_id)
-    %{id: resource_history_id} = insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-file.zip"}})
+
+    %{id: resource_history_id} =
+      insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-file.zip"}})
 
     setup_mox("some-file.zip")
     assert data_import_ids() == []
@@ -89,21 +91,28 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportJobTest do
     assert data_import_ids() == [second_data_import_id]
 
     # subsequent import for a new resource_history_id on same resource should also remove previous imports
-    %{id: new_resource_history_id} = insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-new-file.zip"}})
+    %{id: new_resource_history_id} =
+      insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-new-file.zip"}})
+
     setup_mox("some-new-file.zip")
     third_data_import_id = ImportStops.import_stops(new_resource_history_id)
     assert data_import_ids() == [third_data_import_id]
 
     # other resources should not be impacted by import
-     setup_mox("some-other-file.zip")
+    setup_mox("some-other-file.zip")
     %{id: other_dataset_id} = insert(:dataset, %{datagouv_id: "xxx"})
     %{id: other_resource_id} = insert(:resource, dataset_id: other_dataset_id)
-    %{id: other_resource_history_id} = insert(:resource_history, %{resource_id: other_resource_id, payload: %{"filename" => "some-other-file.zip"}})
+
+    %{id: other_resource_history_id} =
+      insert(:resource_history, %{resource_id: other_resource_id, payload: %{"filename" => "some-other-file.zip"}})
+
     other_data_import_id = ImportStops.import_stops(other_resource_history_id)
 
     assert data_import_ids() == [third_data_import_id, other_data_import_id]
 
-    %{id: new_resource_history_id} = insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-new-file.zip"}})
+    %{id: new_resource_history_id} =
+      insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-new-file.zip"}})
+
     setup_mox("some-new-file.zip")
     fourth_data_import_id = ImportStops.import_stops(new_resource_history_id)
     assert data_import_ids() == [other_data_import_id, fourth_data_import_id]
