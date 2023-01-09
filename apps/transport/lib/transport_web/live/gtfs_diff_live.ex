7260db50a6cb0dfffae189437c6e4568adb7ffcc
Francis Chabouis <fchabouis@gmail.com>
2023-01-09 16:24:22 +0100

    improve widget behavior

--- apps/transport/lib/transport_web/live/gtfs_diff_live.ex
+++ apps/transport/lib/transport_web/live/gtfs_diff_live.ex
@@ -8,23 +8,29 @@ defmodule TransportWeb.GTFSDiffLive do
     ~H"""
     <div class={show(@urls)}>
       <strong>Comparer 2 GTFS</strong>
-      <%= for i <- 0..1 do %>
-        <% url = Enum.at(@urls, i) || "" %>
-        <div class="gtfs-diff-url pt-6">
-          <input type="text" value={url} name="url1" id="url1" placeholder={"url#{i+1}"} />
-          <%= if url != "" do %>
-            <i class="icon icon--trash" phx-click="delete_url" phx-value-index={i} aria-hidden="true"></i>
-          <% end %>
-        </div>
+      <form phx-change="manual_input">
+        <%= for i <- 0..1 do %>
+          <% url = Enum.at(@urls, i) || "" %>
+          <div class="gtfs-diff-url pt-6">
+            <input type="text" value={url} name={"url#{i+1}"} id={"url#{i+1}"} placeholder="url" />
+            <%= if url != "" do %>
+              <i class="icon icon--trash" phx-click="delete_url" phx-value-index={i} aria-hidden="true"></i>
+            <% end %>
+          </div>
+        <% end %>
+      </form>
+      <%= if is_nil(@gtfs_diff_id) and not Enum.any?(@urls, &is_nil/1) do %>
+        <button class="button-outline mt-12" phx-click="compare"><strong>Comparer</strong></button>
       <% end %>
-      <button class="button" phx-click="compare">Comparer</button>
       <%= if @job_executing do %>
         <i class="icon icon--spinner" aria-hidden="true"></i>
       <% end %>
       <%= if @gtfs_diff_id do %>
-        <.link navigate={Routes.gtfs_diff_path(@socket, :show, @gtfs_diff_id)} target="_blank">
-          Voir la différence
-        </.link>
+        <div class="mt-12">
+          <.link navigate={Routes.gtfs_diff_path(@socket, :show, @gtfs_diff_id)} target="_blank" id="see-diff">
+            <strong>Voir la différence</strong>
+          </.link>
+        </div>
       <% end %>
     </div>
     """
@@ -36,7 +42,11 @@ defmodule TransportWeb.GTFSDiffLive do
       :ok = Oban.Notifier.listen([:gossip])
     end
 
-    socket = socket |> assign(:urls, [nil, nil]) |> assign(:job_executing, false) |> assign(:gtfs_diff_id, nil)
+    socket =
+      socket
+      |> assign(:urls, [nil, nil])
+      |> assign(:job_executing, false)
+      |> assign(:gtfs_diff_id, nil)
 
     {:ok, socket}
   end
@@ -51,7 +61,10 @@ defmodule TransportWeb.GTFSDiffLive do
     {:noreply, assign(socket, :urls, urls)}
   end
 
-  def handle_info({:notification, :gossip, %{"complete" => job_id, "gtfs_diff_id" => gtfs_diff_id}}, socket) do
+  def handle_info(
+        {:notification, :gossip, %{"complete" => job_id, "gtfs_diff_id" => gtfs_diff_id}},
+        socket
+      ) do
     if socket.assigns.job_id == job_id do
       {:noreply, socket |> assign(:job_executing, false) |> assign(:gtfs_diff_id, gtfs_diff_id)}
     else
@@ -72,7 +85,14 @@ defmodule TransportWeb.GTFSDiffLive do
         "1" -> [urls |> Enum.at(0), nil]
       end
 
-    {:noreply, assign(socket, :urls, updated_urls)}
+    socket = socket |> assign(:urls, updated_urls) |> assign(:gtfs_diff_id, nil)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("manual_input", params, socket) do
+    socket = assign(socket, :urls, [params["url1"], params["url2"]])
+    {:noreply, socket}
   end
 
   def handle_event("compare", _, socket) do
