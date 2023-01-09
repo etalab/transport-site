00dc104b3d143caf619b76b1e223cc09a72faf90
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-01-09 14:46:47 +0100

    Import and provide some feedback + tests

--- apps/transport/lib/jobs/gtfs_import_stops_job.ex
+++ apps/transport/lib/jobs/gtfs_import_stops_job.ex
@@ -4,6 +4,24 @@ defmodule Transport.Jobs.GTFSImportStopsJob do
 
   @impl Oban.Worker
   def perform(%Oban.Job{}) do
-    :ok
+    active_datasets_resource_history_items =
+      DB.Dataset.base_query()
+      |> DB.Resource.join_dataset_with_resource()
+      |> DB.ResourceHistory.join_resource_with_latest_resource_history()
+      |> where([resource: r], r.format == "GTFS")
+      |> select([resource_history: rh], rh)
+      |> DB.Repo.all()
+
+    result =
+      active_datasets_resource_history_items
+      |> Enum.map(fn rh ->
+        # TODO: wrap errors
+        # TODO: create a global summary
+        data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(rh.id)
+        {:ok, %{resource_history_id: rh.id, data_import_id: data_import_id}}
+      end)
+
+    # TODO: delete all DataImport not referenced by active datasets items
+    {:ok, result}
   end
 end
--- apps/transport/test/transport/jobs/gtfs_import_stops_job_test.exs
+++ apps/transport/test/transport/jobs/gtfs_import_stops_job_test.exs
@@ -3,6 +3,7 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportJobTest do
   use Oban.Testing, repo: DB.Repo
   import DB.Factory
   import Mox
+  import Transport.Test.FileStreamUtils
 
   setup :verify_on_exit!
 
@@ -10,7 +11,17 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportJobTest do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
   end
 
-  test "import" do
-    assert :ok == perform_job(Transport.Jobs.GTFSImportStopsJob, %{})
+  test "import without error" do
+    %{id: dataset_id} = insert(:dataset, %{datagouv_id: "xxx", datagouv_title: "coucou"})
+    %{id: resource_id} = insert(:resource, dataset_id: dataset_id, format: "GTFS")
+
+    %{id: resource_history_id} =
+      insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-file.zip"}})
+
+    setup_get_file_stream_mox("some-file.zip")
+
+    {:ok, result} = perform_job(Transport.Jobs.GTFSImportStopsJob, %{})
+
+    [{:ok, %{resource_history_id: ^resource_history_id}}] = result
   end
 end
