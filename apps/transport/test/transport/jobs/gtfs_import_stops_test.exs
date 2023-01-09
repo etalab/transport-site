00ef863bbd63a96e06c17bdb7b6c44d9adbda7cd
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-01-09 13:39:22 +0100

    Module d'import unitaire GTFS stops avec suppression + outils associés (#2851)
    * Update MinIO template to work with homebrew-based install (I had troubles with Docker)
    
    * Save first MinIO-based sync of latest ResourceHistory assets
    
    So that I can work at scale locally.
    
    * Add helper to stream just the stops
    
    * Extract code to module and fix cache bug
    
    * Save sequential processing (without delete of prior copies)
    
    * Automatically delete gtfs tables when a given DataImport is deleted
    
    * Add quick logs
    
    * Save very WIP
    
    * Save WIP before switching to another branch (some refactoring is needed)
    
    * Add note
    
    * Make get_file_stream mox'able
    
    * Success while implementing one test around GtfsToDB import part
    
    * Make sure to delete previous data_import for same resource_history_id
    
    * Add more tests
    
    * Make tests more reliable (order)
    
    * Add bits of documentation
    
    * Delete previous DataImport for the same resource
    
    * Increase coverage
    
    * Make credo happy
    
    * Mix format
    
    * Remove TODO
    
    * Mix format & simplification
    
    * Mix format
    
    * Make credo happy & mix format
    
    * Refactor code to proper modules & rename accordingly
    
    * Remove now legacy code (replaced by ImportStops)
    
    * Rename for clarity
    
    * Update gtfs_import_stops_test.exs
    
    * Update apps/transport/test/transport/jobs/gtfs_import_stops_test.exs
    
    Co-authored-by: Francis Chabouis <fchabouis@gmail.com>
    
    * Improve minio template
    
    * Update gtfs_import_stops.ex
    
    * DRY code (careful - code isn't under test)
    
    * Update gtfs_import_stops.ex
    
    * Avoid join
    
    * Format code
    
    Co-authored-by: Francis Chabouis <fchabouis@gmail.com>

---
+++ apps/transport/test/transport/jobs/gtfs_import_stops_test.exs
@@ -0,0 +1,79 @@
+defmodule Transport.Test.Transport.Jobs.GTFSImportStopsTest do
+  use ExUnit.Case, async: true
+  use Oban.Testing, repo: DB.Repo
+  import DB.Factory
+  import Mox
+  import Ecto.Query
+
+  setup :verify_on_exit!
+
+  setup do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+  end
+
+  def data_import_ids do
+    DB.Repo.all(from(di in DB.DataImport, select: di.id, order_by: [asc: di.id]))
+  end
+
+  def setup_mox(zip_filename) do
+    # NOTE: it will be possible to reuse common code from Transport.Unzip.S3 in there
+    Transport.Unzip.S3.Mock
+    |> expect(:get_file_stream, fn file_in_zip, zip_file, bucket ->
+      # from payload
+      assert zip_file == zip_filename
+      # from config
+      assert bucket == "transport-data-gouv-fr-resource-history-test"
+
+      # stub with a local file
+      path = "#{__DIR__}/../../fixture/files/gtfs_import.zip"
+      zip_file = Unzip.LocalFile.open(path)
+      {:ok, unzip} = Unzip.new(zip_file)
+      Unzip.file_stream!(unzip, file_in_zip)
+    end)
+  end
+
+  test "import stops" do
+    %{id: dataset_id} = insert(:dataset, %{datagouv_id: "xxx", datagouv_title: "coucou"})
+    %{id: resource_id} = insert(:resource, dataset_id: dataset_id)
+
+    %{id: resource_history_id} =
+      insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-file.zip"}})
+
+    setup_mox("some-file.zip")
+    assert data_import_ids() == []
+    first_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(resource_history_id)
+    assert data_import_ids() == [first_data_import_id]
+
+    # subsequent import must remove the previous import for same resource_history_id
+    setup_mox("some-file.zip")
+    second_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(resource_history_id)
+    assert data_import_ids() == [second_data_import_id]
+
+    # subsequent import for a new resource_history_id on same resource should also remove previous imports
+    %{id: new_resource_history_id} =
+      insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-new-file.zip"}})
+
+    setup_mox("some-new-file.zip")
+    third_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(new_resource_history_id)
+    assert data_import_ids() == [third_data_import_id]
+
+    # other resources should not be impacted by import
+    setup_mox("some-other-file.zip")
+    %{id: other_dataset_id} = insert(:dataset, %{datagouv_id: "yyy"})
+    %{id: other_resource_id} = insert(:resource, dataset_id: other_dataset_id)
+
+    %{id: other_resource_history_id} =
+      insert(:resource_history, %{resource_id: other_resource_id, payload: %{"filename" => "some-other-file.zip"}})
+
+    other_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(other_resource_history_id)
+
+    assert data_import_ids() == [third_data_import_id, other_data_import_id]
+
+    %{id: new_resource_history_id} =
+      insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-new-file.zip"}})
+
+    setup_mox("some-new-file.zip")
+    fourth_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(new_resource_history_id)
+    assert data_import_ids() == [other_data_import_id, fourth_data_import_id]
+  end
+end
---
+++ scripts/download_resource_history_files.exs
@@ -0,0 +1,58 @@
+# NOTE: temporary script to help work locally with large amounts of GTFS.
+require Logger
+
+Logger.info("Starting...")
+
+import Ecto.Query
+
+# Useful notes to create the global import job later:
+# - we could look at "last up to date" to ensure we only have fresh data in database
+# - use gtfs_to_db once downloaded
+# - look at Transport.Jobs.BNLCToGeoData and Transport.Jobs.BaseGeoData.import_replace_data
+
+defmodule Tooling do
+  # for each active dataset, grab all resources with a resource history.
+  def query() do
+    DB.Dataset.base_query()
+    |> DB.Resource.join_dataset_with_resource()
+    |> DB.ResourceHistory.join_resource_with_latest_resource_history()
+    |> where([resource: r], r.format == "GTFS")
+    |> select([resource_history: rh], rh)
+  end
+end
+
+defmodule SyncS3LatestResourceHistory do
+  def sync!(minio_folder) do
+    Tooling.query()
+    |> DB.Repo.all()
+    # TODO: task sync max concurrency (this is slow)
+    |> Enum.each(fn rh ->
+      # create local minio bucket, if needed
+      bucket_name = Transport.S3.bucket_name(:history)
+      base_path = Path.join(minio_folder, bucket_name)
+      unless File.exists?(base_path), do: File.mkdir_p!(base_path)
+
+      # simple check based on file presence on disk
+      file_path = Path.join(base_path, rh.payload["filename"])
+
+      # TODO: replace by head request
+      if File.exists?(file_path) do
+        Logger.info("File already downloaded, skipping...")
+      else
+        Logger.info("Downloading file...")
+        Logger.info(file_path)
+
+        %HTTPoison.Response{status_code: 200, body: body} =
+          Transport.Shared.Wrapper.HTTPoison.impl().get!(rh.payload["permanent_url"])
+
+        Transport.S3.upload_to_s3!(:history, body, rh.payload["filename"])
+      end
+    end)
+  end
+end
+
+Logger.configure(level: :info)
+
+# # create a local S3 copy (via MinIO) of each latest ResourceHistory file so
+# # that the production database dump can be used locally with a matching local S3 file
+SyncS3LatestResourceHistory.sync!(Path.expand("~/data"))
