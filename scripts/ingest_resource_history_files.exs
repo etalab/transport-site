969f4c3f521e7a84d88f1f5678bd4a2b33f9b87d
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-01-10 16:25:14 +0100

    Make it easy to dump report (with or without import)

--- scripts/ingest_resource_history_files.exs
+++ scripts/ingest_resource_history_files.exs
@@ -1,9 +1,21 @@
 Logger.configure(level: :info)
 
-outcome =
-  Transport.Jobs.GTFSImportStopsJob.refresh_all()
-  |> IO.inspect(IEx.inspect_opts())
+import Ecto.Query
 
-# Next steps:
-# - count errors
-# - report errors in aggregated fashion
+defmodule Report do
+  def report(data_import_batch_id) do
+    DB.Repo.get_by(DB.DataImportBatch, id: data_import_batch_id).summary["result"]
+  end
+end
+
+batch_id =
+  unless System.get_env("SKIP_IMPORT") == "1" do
+    %{data_import_batch_id: batch_id} = Transport.Jobs.GTFSImportStopsJob.refresh_all()
+    batch_id
+  else
+    %{id: batch_id} = DB.Repo.one(from(dib in DB.DataImportBatch, order_by: [desc: dib.id], limit: 1))
+    batch_id
+  end
+
+Report.report(batch_id)
+|> IO.inspect(IEx.inspect_opts())
