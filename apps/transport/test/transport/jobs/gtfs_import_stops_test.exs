46a2973de168903aa9ecc0c20476474c9ecbb1f2
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-01-16 12:03:46 +0100

    Structuration import GTFS en batch job + dashboard de reporting (#2920)
    * Implement boilerplate
    
    * Extract helper before reuse
    
    * Import and provide some feedback + tests
    
    * Refactor and wrap errors to prepare for reporting
    
    * Add reporting structure
    
    * Create rich serializable report
    
    * Make it easy to dump report (with or without import)
    
    * Add a sorted dashboard to show latest data import batch results
    
    * Fix tags
    
    * Remove todo and add test
    
    * Fix credo errors
    
    * Ajout bouton backoffice
    
    * Correction bug
    
    * Passage en français
    
    * Update gettext
    
    * Notify subscribed jobs
    
    * Add a way to trigger refresh job from backoffice (with feedback)
    
    * Correctifs Oban
    
    * Update apps/transport/lib/transport_web/live/backoffice/data_import_batch_report_live.ex
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    * Update apps/transport/test/transport/jobs/gtfs_import_stops_job_test.exs
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>

--- apps/transport/test/transport/jobs/gtfs_import_stops_test.exs
+++ apps/transport/test/transport/jobs/gtfs_import_stops_test.exs
@@ -4,7 +4,7 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportStopsTest do
   import DB.Factory
   import Mox
   import Ecto.Query
-
+  import Transport.Test.FileStreamUtils
   setup :verify_on_exit!
 
   setup do
@@ -15,23 +15,6 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportStopsTest do
     DB.Repo.all(from(di in DB.DataImport, select: di.id, order_by: [asc: di.id]))
   end
 
-  def setup_mox(zip_filename) do
-    # NOTE: it will be possible to reuse common code from Transport.Unzip.S3 in there
-    Transport.Unzip.S3.Mock
-    |> expect(:get_file_stream, fn file_in_zip, zip_file, bucket ->
-      # from payload
-      assert zip_file == zip_filename
-      # from config
-      assert bucket == "transport-data-gouv-fr-resource-history-test"
-
-      # stub with a local file
-      path = "#{__DIR__}/../../fixture/files/gtfs_import.zip"
-      zip_file = Unzip.LocalFile.open(path)
-      {:ok, unzip} = Unzip.new(zip_file)
-      Unzip.file_stream!(unzip, file_in_zip)
-    end)
-  end
-
   test "import stops" do
     %{id: dataset_id} = insert(:dataset, %{datagouv_id: "xxx", datagouv_title: "coucou"})
     %{id: resource_id} = insert(:resource, dataset_id: dataset_id)
@@ -39,13 +22,13 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportStopsTest do
     %{id: resource_history_id} =
       insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-file.zip"}})
 
-    setup_mox("some-file.zip")
+    setup_get_file_stream_mox("some-file.zip")
     assert data_import_ids() == []
     first_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(resource_history_id)
     assert data_import_ids() == [first_data_import_id]
 
     # subsequent import must remove the previous import for same resource_history_id
-    setup_mox("some-file.zip")
+    setup_get_file_stream_mox("some-file.zip")
     second_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(resource_history_id)
     assert data_import_ids() == [second_data_import_id]
 
@@ -53,12 +36,12 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportStopsTest do
     %{id: new_resource_history_id} =
       insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-new-file.zip"}})
 
-    setup_mox("some-new-file.zip")
+    setup_get_file_stream_mox("some-new-file.zip")
     third_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(new_resource_history_id)
     assert data_import_ids() == [third_data_import_id]
 
     # other resources should not be impacted by import
-    setup_mox("some-other-file.zip")
+    setup_get_file_stream_mox("some-other-file.zip")
     %{id: other_dataset_id} = insert(:dataset, %{datagouv_id: "yyy"})
     %{id: other_resource_id} = insert(:resource, dataset_id: other_dataset_id)
 
@@ -72,7 +55,7 @@ defmodule Transport.Test.Transport.Jobs.GTFSImportStopsTest do
     %{id: new_resource_history_id} =
       insert(:resource_history, %{resource_id: resource_id, payload: %{"filename" => "some-new-file.zip"}})
 
-    setup_mox("some-new-file.zip")
+    setup_get_file_stream_mox("some-new-file.zip")
     fourth_data_import_id = Transport.GTFSImportStops.import_stops_and_remove_previous(new_resource_history_id)
     assert data_import_ids() == [other_data_import_id, fourth_data_import_id]
   end
