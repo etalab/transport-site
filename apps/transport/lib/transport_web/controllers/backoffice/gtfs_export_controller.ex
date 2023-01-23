a9e39cd59774dfcee6c9c6f9cbc5dc9072f4f371
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-01-23 11:48:09 +0100

    Wire export & remove todos (will work without for now)

--- apps/transport/lib/transport_web/controllers/backoffice/gtfs_export_controller.ex
+++ apps/transport/lib/transport_web/controllers/backoffice/gtfs_export_controller.ex
@@ -2,12 +2,9 @@ defmodule TransportWeb.Backoffice.GTFSExportController do
   use TransportWeb, :controller
   require Logger
 
-  # TODO: use real data
-  # TODO: make sure output is streamed/chunked
-  # TODO: make sure output is zipped if possible
-  # TODO: add test for controller
   def export(%Plug.Conn{} = conn, _params) do
-    csv_data = [%{stop_lon: 1.17, stop_lat: -39.6}] |> CSV.encode(headers: true) |> Enum.join("")
+    data_import_ids = Transport.GTFSExportStops.data_import_ids()
+    csv_data = Transport.GTFSExportStops.export_stops_report(data_import_ids)
 
     conn
     |> put_resp_content_type("text/csv")
