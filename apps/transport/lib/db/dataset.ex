7a82cf30c7f804dbebd51f0385c6e92e57225be3
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-02-21 11:43:21 +0100

    Try to fix Dialyzer

--- apps/transport/lib/db/dataset.ex
+++ apps/transport/lib/db/dataset.ex
@@ -6,7 +6,7 @@ defmodule DB.Dataset do
   There are also trigger on update on aom and region that will force an update on this model
   so the search vector is up-to-date.
   """
-  alias DB.{AOM, Commune, DatasetGeographicView, LogsImport, Region, Repo, Resource}
+  alias DB.{AOM, Commune, DatasetGeographicView, LogsImport, NotificationSubscription, Region, Repo, Resource}
   alias Phoenix.HTML.Link
   import Ecto.{Changeset, Query}
   import TransportWeb.Gettext
@@ -57,7 +57,7 @@ defmodule DB.Dataset do
 
     has_many(:resources, Resource, on_replace: :delete, on_delete: :delete_all)
     has_many(:logs_import, LogsImport, on_replace: :delete, on_delete: :delete_all)
-    has_many(:notification_subscriptions, DB.NotificationSubscription, on_delete: :delete_all)
+    has_many(:notification_subscriptions, NotificationSubscription, on_delete: :delete_all)
     # A dataset can be "parent dataset" of many AOMs
     has_many(:child_aom, AOM, foreign_key: :parent_dataset_id)
   end
