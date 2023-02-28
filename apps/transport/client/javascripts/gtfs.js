f04d64526caf687cd06b59cbd52741a5b9a4c708
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-02-28 16:48:49 +0100

    Fix linting

--- apps/transport/client/javascripts/gtfs.js
+++ apps/transport/client/javascripts/gtfs.js
@@ -1,7 +1,7 @@
 import { Socket } from 'phoenix'
 import Leaflet from 'leaflet'
 import { LeafletLayer } from 'deck.gl-leaflet'
-import { ScatterplotLayer } from '@deck.gl/layers';
+import { ScatterplotLayer } from '@deck.gl/layers'
 
 import { MapView } from '@deck.gl/core'
 
@@ -16,51 +16,50 @@ const Mapbox = {
 }
 
 const metropolitanFranceBounds = [[51.1, -4.9], [41.2, 9.8]]
-const map = Leaflet.map('map', { renderer: Leaflet.canvas() });
+const map = Leaflet.map('map', { renderer: Leaflet.canvas() })
 
 Leaflet.tileLayer(Mapbox.url, {
     accessToken: Mapbox.accessToken,
     attribution: Mapbox.attribution,
     maxZoom: Mapbox.maxZoom
-}).addTo(map);
+}).addTo(map)
 
 const deckGLLayer = new LeafletLayer({
     views: [new MapView({ repeat: true })],
     layers: []
 })
-map.addLayer(deckGLLayer);
+map.addLayer(deckGLLayer)
 
 // triggered both by "zoomend" and the end of move
-map.on('moveend', function(event) {
-    var bounds = map.getBounds();
-    var a = map.latLngToLayerPoint([bounds.getNorth(), bounds.getWest()]);
-    var b = map.latLngToLayerPoint([bounds.getSouth(), bounds.getEast()]);
-    var width_pixels = b.x - a.x;
-    var height_pixels = b.y - a.y;
+map.on('moveend', function (event) {
+    const bounds = map.getBounds()
+    const a = map.latLngToLayerPoint([bounds.getNorth(), bounds.getWest()])
+    const b = map.latLngToLayerPoint([bounds.getSouth(), bounds.getEast()])
+    const widthPixels = b.x - a.x
+    const heightPixels = b.y - a.y
 
-    var params = new URLSearchParams({
-        "width_pixels": width_pixels,
-        "height_pixels": height_pixels,
-        "south": bounds.getSouth(),
-        "east": bounds.getEast(),
-        "west": bounds.getWest(),
-        "north": bounds.getNorth()
-    });
+    const params = new URLSearchParams({
+        width_pixels: widthPixels,
+        height_pixels: heightPixels,
+        south: bounds.getSouth(),
+        east: bounds.getEast(),
+        west: bounds.getWest(),
+        north: bounds.getNorth()
+    })
 
-    var url = `/explore/gtfs-stops-data?${params}`;
+    const url = `/explore/gtfs-stops-data?${params}`
 
     // https://coolors.co/gradient-palette/2655ff-ff9822?number=5 and https://coolors.co/gradient-palette/ff9822-ce1313?number=5
-    var palette = [[38,85,255], [92,102,200], [147,119,145], [201,135,89], [255,152,34], [243,119,30], [231,86,27], [218,52,23], [206,19,19]];
+    const palette = [[38, 85, 255], [92, 102, 200], [147, 119, 145], [201, 135, 89], [255, 152, 34], [243, 119, 30], [231, 86, 27], [218, 52, 23], [206, 19, 19]]
 
-    var colorFunc = function(v) {
-        return palette[Math.min(Math.floor(v * 10), 9)];
-    };
+    const colorFunc = function (v) {
+        return palette[Math.min(Math.floor(v * 10), 9)]
+    }
 
     fetch(url)
         .then(data => data.json())
         .then(json => {
-
-            var data = json;
+            const data = json
             const maxCount = Math.max(...data.map(a => a[2]))
             const scatterplotLayer = new ScatterplotLayer({
                 id: 'scatterplot-layer',
@@ -76,13 +75,12 @@ map.on('moveend', function(event) {
                 getRadius: d => 2,
                 getFillColor: d => colorFunc(maxCount < 3 ? 0 : d[2] / maxCount),
                 getLineColor: d => colorFunc(maxCount < 3 ? 0 : d[2] / maxCount)
-            });
-            deckGLLayer.setProps({ layers: [scatterplotLayer] });
+            })
+            deckGLLayer.setProps({ layers: [scatterplotLayer] })
         })
         .catch(e => console.log(e))
 })
 
 map.fitBounds(metropolitanFranceBounds)
-
 
 export default socket
