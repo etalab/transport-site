adcff4183eb00186e6b4aa67ff9448935df77d4c
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-03-09 14:27:35 +0100

    Add missing migration file

---
+++ apps/transport/priv/repo/migrations/20230309102424_add_title_contact.exs
@@ -0,0 +1,13 @@
+defmodule DB.Repo.Migrations.AddTitleContact do
+  use Ecto.Migration
+
+  def change do
+    alter table(:contact) do
+      # `first_name` and `last_name` can now be `null`
+      modify :first_name, :string, null: true, from: {:string, null: false}
+      modify :last_name, :string, null: true, from: {:string, null: false}
+      # add new `column` for contacts that are not "real humans", for example mailing lists
+      add :title, :string, null: true
+    end
+  end
+end
