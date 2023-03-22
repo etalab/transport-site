916c9e157a3af8eb61cbf1da8806cc6679d133ed
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-03-22 09:57:29 +0100

    Add migration

---
+++ apps/transport/priv/repo/migrations/20230322080214_dataset_change_types_date_cols.exs
@@ -0,0 +1,13 @@
+defmodule DB.Repo.Migrations.DatasetChangeTypesDateCols do
+  use Ecto.Migration
+
+  def up do
+    execute "ALTER TABLE dataset ALTER COLUMN created_at TYPE timestamp without time zone USING created_at::timestamp;"
+    execute "ALTER TABLE dataset ALTER COLUMN last_update TYPE timestamp without time zone USING last_update::timestamp;"
+  end
+
+  def down do
+    execute "ALTER TABLE dataset ALTER COLUMN created_at TYPE varchar;"
+    execute "ALTER TABLE dataset ALTER COLUMN last_update TYPE varchar;"
+  end
+end
