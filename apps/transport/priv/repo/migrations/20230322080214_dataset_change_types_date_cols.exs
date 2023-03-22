4436cc622d7afa6d5bfe326b13353ea497d6d617
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-03-22 16:09:25 +0100

    I can write migrations

--- apps/transport/priv/repo/migrations/20230322080214_dataset_change_types_date_cols.exs
+++ apps/transport/priv/repo/migrations/20230322080214_dataset_change_types_date_cols.exs
@@ -2,12 +2,16 @@ defmodule DB.Repo.Migrations.DatasetChangeTypesDateCols do
   use Ecto.Migration
 
   def up do
-    execute "ALTER TABLE dataset ALTER COLUMN created_at TYPE timestamp without time zone SET NOT NULL USING created_at::timestamp;"
-    execute "ALTER TABLE dataset ALTER COLUMN last_update TYPE timestamp without time zone SET NOT NULL USING last_update::timestamp;"
+    execute "ALTER TABLE dataset ALTER COLUMN created_at TYPE timestamp without time zone USING created_at::timestamp;"
+    execute "ALTER TABLE dataset ALTER COLUMN last_update TYPE timestamp without time zone USING last_update::timestamp;"
+    execute "ALTER TABLE dataset ALTER COLUMN created_at SET NOT NULL;"
+    execute "ALTER TABLE dataset ALTER COLUMN last_update SET NOT NULL;"
   end
 
   def down do
     execute "ALTER TABLE dataset ALTER COLUMN created_at TYPE varchar;"
     execute "ALTER TABLE dataset ALTER COLUMN last_update TYPE varchar;"
+    execute "ALTER TABLE dataset ALTER COLUMN created_at DROP NOT NULL;"
+    execute "ALTER TABLE dataset ALTER COLUMN last_update DROP NOT NULL;"
   end
 end
