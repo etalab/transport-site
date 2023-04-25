b7f47842528e83327b74e3469c287d91f0543a71
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-04-25 17:40:32 +0200

    Affiche ressources associées sur resource#details

--- apps/transport/lib/db/resource_related.ex
+++ apps/transport/lib/db/resource_related.ex
@@ -3,11 +3,21 @@ defmodule DB.ResourceRelated do
   A module to deal with `DB.Resource` related to other `DB.Resource`
   """
   use TypedEctoSchema
+  import TransportWeb.Gettext
 
   @primary_key false
   typed_schema "resource_related" do
     belongs_to(:resource_src, DB.Resource, foreign_key: :resource_src_id, references: :id)
     belongs_to(:resource_dst, DB.Resource, foreign_key: :resource_dst_id, references: :id)
     field(:reason, Ecto.Enum, values: [:gtfs_rt_gtfs])
+  end
+
+  def reason_to_str(%__MODULE__{reason: reason}) do
+    Map.fetch!(
+      %{
+        :gtfs_rt_gtfs => dgettext("db-resource-related", "Associated GTFS file")
+      },
+      reason
+    )
   end
 end
