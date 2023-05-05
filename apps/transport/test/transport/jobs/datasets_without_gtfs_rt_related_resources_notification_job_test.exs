491785925effd7615ed8e39ac0315c7ffd3f5c25
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2023-05-05 15:33:15 +0200

    Notification GTFS-RT vers GTFS : prend en compte JDD à jour (#3118)

--- apps/transport/test/transport/jobs/datasets_without_gtfs_rt_related_resources_notification_job_test.exs
+++ apps/transport/test/transport/jobs/datasets_without_gtfs_rt_related_resources_notification_job_test.exs
@@ -13,35 +13,48 @@ defmodule Transport.Test.Transport.Jobs.DatasetsWithoutGTFSRTRelatedResourcesNot
 
   test "relevant_datasets" do
     # 2 GTFS and 2 GTFS-RT with resource_related set for all GTFS-RT
-    %{id: d1_id} = insert(:dataset, is_active: true)
-    # 1 GTFS and 1 GTFS-RT
-    %{id: d2_id} = insert(:dataset, is_active: true)
-    # 2 GTFS and 1 GTFS-RT
-    %{id: d3_id} = insert(:dataset, is_active: true)
-    gtfs_1_1 = insert(:resource, dataset_id: d1_id, is_community_resource: false, format: "GTFS")
-    insert(:resource, dataset_id: d1_id, is_community_resource: false, format: "GTFS")
+    %{dataset: %DB.Dataset{id: d1_id} = d1, resource: %DB.Resource{format: "GTFS"} = gtfs_1_1} =
+      insert_up_to_date_resource_and_friends()
+
+    insert_up_to_date_resource_and_friends(dataset: d1)
     gtfs_rt_1 = insert(:resource, dataset_id: d1_id, is_community_resource: false, format: "gtfs-rt")
     gtfs_rt_2 = insert(:resource, dataset_id: d1_id, is_community_resource: false, format: "gtfs-rt")
-
-    insert(:resource, dataset_id: d2_id, is_community_resource: false, format: "GTFS")
-    insert(:resource, dataset_id: d2_id, is_community_resource: false, format: "gtfs-rt")
-
-    insert(:resource, dataset_id: d3_id, is_community_resource: false, format: "GTFS")
-    insert(:resource, dataset_id: d3_id, is_community_resource: false, format: "GTFS")
-    insert(:resource, dataset_id: d3_id, is_community_resource: false, format: "gtfs-rt")
 
     insert(:resource_related, resource_src: gtfs_rt_1, resource_dst: gtfs_1_1, reason: :gtfs_rt_gtfs)
     insert(:resource_related, resource_src: gtfs_rt_2, resource_dst: gtfs_1_1, reason: :gtfs_rt_gtfs)
 
-    assert [%DB.Dataset{id: ^d3_id}] =
+    # 1 GTFS and 1 GTFS-RT
+    %{dataset: %DB.Dataset{id: d2_id}, resource: %DB.Resource{format: "GTFS"}} =
+      insert_up_to_date_resource_and_friends()
+
+    insert(:resource, dataset_id: d2_id, is_community_resource: false, format: "gtfs-rt")
+
+    # 1 up-to-date GTFS, 1 outdated GTFS and 1 GTFS-RT
+    %{dataset: %DB.Dataset{id: d3_id} = d3, resource: %DB.Resource{format: "GTFS"}} =
+      insert_up_to_date_resource_and_friends()
+
+    insert_outdated_resource_and_friends(dataset: d3)
+    insert(:resource, dataset_id: d3_id, is_community_resource: false, format: "gtfs-rt")
+
+    # 2 GTFS and 1 GTFS-RT, without resource_related set
+    %{dataset: %DB.Dataset{id: d4_id} = d4, resource: %DB.Resource{format: "GTFS"}} =
+      insert_up_to_date_resource_and_friends()
+
+    insert_up_to_date_resource_and_friends(dataset: d4)
+    insert(:resource, dataset_id: d4_id, is_community_resource: false, format: "gtfs-rt")
+
+    assert [%DB.Dataset{id: ^d4_id}] =
              DatasetsWithoutGTFSRTRelatedResourcesNotificationJob.relevant_datasets() |> Enum.sort()
   end
 
   test "perform" do
-    dataset = insert(:dataset, is_active: true, custom_title: "Super JDD")
-    insert(:resource, dataset: dataset, is_community_resource: false, format: "GTFS")
-    insert(:resource, dataset: dataset, is_community_resource: false, format: "GTFS")
+    %{dataset: %DB.Dataset{id: dataset_id} = dataset, resource: %DB.Resource{format: "GTFS"}} =
+      insert_up_to_date_resource_and_friends(custom_title: "Super JDD")
+
+    insert_up_to_date_resource_and_friends(dataset: dataset)
     insert(:resource, dataset: dataset, is_community_resource: false, format: "gtfs-rt")
+
+    assert [%DB.Dataset{id: ^dataset_id}] = DatasetsWithoutGTFSRTRelatedResourcesNotificationJob.relevant_datasets()
 
     Transport.EmailSender.Mock
     |> expect(:send_mail, fn "transport.data.gouv.fr",
