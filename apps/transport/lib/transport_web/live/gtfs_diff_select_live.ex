a0588e523df54cbb3b495e23fee134dbce3554ee
Francis Chabouis <fchabouis@gmail.com>
2023-05-05 15:11:14 +0200

    add some human friendly explanations

--- apps/transport/lib/transport_web/live/gtfs_diff_select_live.ex
+++ apps/transport/lib/transport_web/live/gtfs_diff_select_live.ex
@@ -79,9 +79,13 @@ defmodule TransportWeb.Live.GTFSDiffSelectLive do
 
     %{status_code: 200, body: body} = http_client.get!(diff_file_url)
     diff = Transport.GTFSDiff.parse_diff_output(body)
-    diff_summary = diff |> diff_summary()
 
-    {:noreply, socket |> assign(:diff_summary, diff_summary)}
+    socket =
+      socket
+      |> assign(:diff_summary, diff |> diff_summary())
+      |> assign(:diff_explanations, diff |> diff_explanations())
+
+    {:noreply, socket}
   end
 
   # catch-all
@@ -99,6 +103,79 @@ defmodule TransportWeb.Live.GTFSDiffSelectLive do
     |> Enum.sort_by(fn {{_, _, target}, _} -> order |> Map.fetch!(target) end)
     |> Enum.group_by(fn {{_file, action, _target}, _n} -> action end)
   end
+
+  def explanation_add_file(explanations, %{"action" => "add", "file" => file, "target" => "file"}) do
+    [~s(Un fichier nommé "#{file}" a été ajouté) | explanations]
+  end
+
+  def explanation_add_file(explanations, _), do: explanations
+
+  def explanation_delete_file(explanations, %{"action" => "delete", "file" => file, "target" => "file"}) do
+    [~s(Le fichier nommé "#{file}" a été supprimé) | explanations]
+  end
+
+  def explanation_delete_file(explanations, _), do: explanations
+
+  def explanation_update_stop_name(
+        explanations,
+        %{
+          "action" => "update",
+          "file" => "stops.txt",
+          "target" => "row",
+          "identifier" => %{"stop_id" => stop_id},
+          "new_value" => %{"stop_name" => new_stop_name},
+          "initial_value" => %{"stop_name" => initial_stop_name}
+        }
+      ) do
+    [
+      ~s([stops.txt] Le nom du stop_id #{stop_id} a été modifié. Nom initial : "#{initial_stop_name}", nouvelle valeur : "#{new_stop_name}")
+      | explanations
+    ]
+  end
+
+  def explanation_update_stop_name(explanations, _) do
+    explanations
+  end
+
+  def explanation_stop_wheelchair_access(
+        explanations,
+        %{
+          "action" => "update",
+          "file" => "stops.txt",
+          "target" => "row",
+          "identifier" => %{"stop_id" => stop_id},
+          "new_value" => %{"wheelchair_boarding" => new_wheelchair_boarding},
+          "initial_value" => %{"wheelchair_boarding" => initial_wheelchair_boarding}
+        }
+      )
+      when new_wheelchair_boarding in ["1", "2"] do
+    [
+      ~s([stops.txt] Information sur l'accessibilité rajoutée au stop_id #{stop_id}, ancienne valeur : "#{initial_wheelchair_boarding}", nouvelle valeur : "#{new_wheelchair_boarding}")
+      | explanations
+    ]
+  end
+
+  def explanation_stop_wheelchair_access(explanations, _), do: explanations
+
+  def diff_explanations(diffs) do
+    diffs
+    |> Enum.flat_map(fn diff ->
+      diff =
+        diff
+        |> Map.update("initial_value", %{}, &try_jason_decode(&1))
+        |> Map.update("new_value", %{}, &try_jason_decode(&1))
+        |> Map.update("identifier", %{}, &try_jason_decode(&1))
+
+      []
+      |> explanation_add_file(diff)
+      |> explanation_delete_file(diff)
+      |> explanation_update_stop_name(diff)
+      |> explanation_stop_wheelchair_access(diff)
+    end)
+  end
+
+  def try_jason_decode(""), do: ""
+  def try_jason_decode(input), do: Jason.decode!(input)
 
   defp upload_to_s3(file_path, path) do
     Transport.S3.upload_to_s3!(:gtfs_diff, File.read!(file_path), path)
