63b83c8a299eac3cc919c91d45f1356119c7f1da
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-05-09 18:12:24 +0200

    Mix format

--- apps/transport/lib/transport/gtfs_data.ex
+++ apps/transport/lib/transport/gtfs_data.ex
@@ -79,7 +79,8 @@ defmodule Transport.GTFSData do
     12 => {0.00171661376953125, 0.0011294445140095472}
   }
 
-  def create_gtfs_stops_materialized_view(zoom_level) when is_integer(zoom_level) and zoom_level in 1..12 do
+  def create_gtfs_stops_materialized_view(zoom_level)
+      when is_integer(zoom_level) and zoom_level in 1..12 do
     {:ok, %{rows: [[count]], num_rows: 1}} =
       Ecto.Adapters.SQL.query(DB.Repo, """
       select count(*)
