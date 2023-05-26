77b1f08ce325fe84a24e7f4bc91364ccc5e49eb0
Francis Chabouis <fchabouis@gmail.com>
2023-05-26 09:51:01 +0200

    create test file

---
+++ apps/transport/test/transport/jobs/dataset_quality_score_test.exs
@@ -0,0 +1,9 @@
+defmodule Transport.Test.Transport.Jobs.DatasetQualityScoreTest do
+  use ExUnit.Case, async: true
+  doctest Transport.Jobs.DatasetQualityScore, import: true
+
+  describe "description_of_tests" do
+    test "definition_of_this_test" do
+    end
+  end
+end
