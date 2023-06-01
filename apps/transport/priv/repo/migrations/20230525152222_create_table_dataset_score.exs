7eef4f97a7808bc8897ff36ab82941b1c97197cb
Francis Chabouis <fchabouis@gmail.com>
2023-06-01 18:52:47 +0200

    add on_delete action

--- apps/transport/priv/repo/migrations/20230525152222_create_table_dataset_score.exs
+++ apps/transport/priv/repo/migrations/20230525152222_create_table_dataset_score.exs
@@ -3,7 +3,7 @@ defmodule DB.Repo.Migrations.CreateTableDatasetScore do
 
   def change do
     create table(:dataset_score) do
-      add :dataset_id, references(:dataset)
+      add :dataset_id, references(:dataset, on_delete: :delete_all)
       add :topic, :string
       add :score, :float
       add :timestamp, :utc_datetime_usec
