8d32979772d05369a9a601fc27d75162bdf73d11
Francis Chabouis <fchabouis@gmail.com>
2023-06-01 17:40:29 +0200

    add some tests to the changeset

---
+++ apps/transport/test/db/dataset_score_test.exs
@@ -0,0 +1,29 @@
+defmodule DB.DatasetScoreTest do
+  use ExUnit.Case
+
+  describe "save dataset score" do
+    test "some fields are mandatory" do
+      changeset = %DB.DatasetScore{} |> DB.DatasetScore.changeset(%{})
+
+      assert %{
+               valid?: false,
+               errors: [
+                 dataset_id: {"can't be blank", [validation: :required]},
+                 topic: {"can't be blank", [validation: :required]},
+                 score: {"can't be blank", [validation: :required]},
+                 timestamp: {"can't be blank", [validation: :required]}
+               ]
+             } = changeset
+    end
+
+    test "score has constraints" do
+      changeset = %DB.DatasetScore{} |> DB.DatasetScore.changeset(%{dataset_id: 1, topic: "freshness", score: 2.0, timestamp: DateTime.utc_now()})
+      assert %{
+        valid?: false,
+        errors: [
+          score: {"must be beetween 0.0 and 1.0", []},
+        ]
+      } = changeset
+    end
+  end
+end
