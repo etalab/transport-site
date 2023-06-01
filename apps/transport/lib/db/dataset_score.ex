3b4a333f9bb020cb568601788e7f093df03faeef
Francis Chabouis <fchabouis@gmail.com>
2023-06-01 17:40:19 +0200

    create a changeset function for score

--- apps/transport/lib/db/dataset_score.ex
+++ apps/transport/lib/db/dataset_score.ex
@@ -5,14 +5,25 @@ defmodule DB.DatasetScore do
   use Ecto.Schema
   use TypedEctoSchema
   import Ecto.Query
+  import Ecto.Changeset
 
   typed_schema "dataset_score" do
     belongs_to(:dataset, DB.Dataset)
-    field(:topic, :string)
+    field(:topic, Ecto.Enum, values: [:freshness])
     field(:score, :float)
     field(:timestamp, :utc_datetime_usec)
     field(:details, :map)
   end
+
+  def changeset(%DB.DatasetScore{} = dataset_score, attrs) do
+    dataset_score
+    |> cast(attrs, [:dataset_id, :topic, :score, :timestamp, :details])
+    |> validate_required([:dataset_id, :topic, :score, :timestamp])
+    |> validate_change(:score, &between_0_and_1/2)
+  end
+
+  def between_0_and_1(:score, score) when score >= 0.0 and score <= 1.0, do: []
+  def between_0_and_1(:score, _score), do: [score: "must be beetween 0.0 and 1.0"]
 
   def base_query, do: from(ds in DB.DatasetScore, as: :dataset_score)
 end
