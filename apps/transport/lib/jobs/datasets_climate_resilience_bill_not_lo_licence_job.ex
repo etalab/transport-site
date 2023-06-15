a7806898bcbbf7c69c343a4404c59ba7f02a5f5c
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-06-15 14:32:08 +0200

    Job quotidien identifiant JDD article 122 avec licence non LO

---
+++ apps/transport/lib/jobs/datasets_climate_resilience_bill_not_lo_licence_job.ex
@@ -0,0 +1,47 @@
+defmodule Transport.Jobs.DatasetsClimateResilienceBillNotLOLicenceJob do
+  @moduledoc """
+  Job in charge of sending email notifications to our bizdev team
+  when datasets are subject to a compulsory data integration obligation (article 122)
+  and switched to an inappropriate licence.
+  """
+  use Oban.Worker, max_attempts: 3, tags: ["moderation"]
+  import Ecto.Query
+
+  @impl Oban.Worker
+  def perform(%Oban.Job{}) do
+    datasets = relevant_datasets()
+    remove_climate_resilience_bill_tag(datasets)
+
+    Transport.EmailSender.impl().send_mail(
+      "transport.data.gouv.fr",
+      Application.get_env(:transport, :contact_email),
+      Application.get_env(:transport, :bizdev_email),
+      Application.get_env(:transport, :contact_email),
+      "Jeux de données article 122 avec licence inapproprié",
+      "",
+      Phoenix.View.render_to_string(
+        TransportWeb.EmailView,
+        "datasets_climate_resilience_bill_inappropriate_licence.html",
+        datasets: datasets
+      )
+    )
+
+    :ok
+  end
+
+  def relevant_datasets do
+    DB.Dataset.base_query()
+    |> where([dataset: d], fragment("'loi-climat-resilience' = any(?)", d.custom_tags))
+    |> DB.Repo.all()
+    |> Enum.reject(&DB.Dataset.has_licence_ouverte?/1)
+  end
+
+  def remove_climate_resilience_bill_tag(datasets) do
+    ids = datasets |> Enum.map(& &1.id)
+
+    DB.Dataset.base_query()
+    |> where([dataset: d], d.id in ^ids)
+    |> update([dataset: d], set: [custom_tags: fragment("array_remove(?, 'loi-climat-resilience')", d.custom_tags)])
+    |> DB.Repo.update_all([])
+  end
+end
