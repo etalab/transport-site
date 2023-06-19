4f4d62159c1ea7dbf64ff10b09d24b75b8b9455c
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-06-19 08:50:58 +0200

    Save test script

---
+++ scripts/gtfs_expiry.exs
@@ -0,0 +1,31 @@
+import Ecto.Query
+
+content =
+  DB.Resource.base_query()
+  |> where(format: "GTFS")
+  |> join(:left, [resource: r], rh in DB.ResourceHistory,
+    on: rh.resource_id == r.id,
+    as: :resource_history
+  )
+  |> join(:left, [resource_history: rh], mv in DB.MultiValidation,
+    on: mv.resource_history_id == rh.id,
+    as: :mv
+  )
+  |> distinct([resource: r], r.id)
+  |> order_by([resource: r, resource_history: rh],
+    asc: r.id,
+    desc: rh.inserted_at
+  )
+  |> join(:left, [mv: mv], rm in DB.ResourceMetadata, on: rm.multi_validation_id == mv.id, as: :rm)
+  # |> limit(1)
+  |> select([resource: r, rm: rm], %{
+    resource_id: r.id,
+    inserted_at: fragment("?::date::text", rm.inserted_at),
+    start_date: fragment("?->>'start_date'", rm.metadata),
+    end_date: fragment("?->>'end_date'", rm.metadata)
+  })
+  |> DB.Repo.all()
+  |> Enum.map(fn x -> x[:end_date] end)
+  |> Enum.join("\n")
+
+File.write!("gtfs-expiry-dates.csv", content)
