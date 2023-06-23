2094750474f323a5b3059de76fb5f24104d7e2fe
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-06-23 12:19:50 +0200

    Modification du markdown handler pour gérer les retours à la ligne simple

--- apps/transport/test/transport_web/views/markdown_handler_test.exs
+++ apps/transport/test/transport_web/views/markdown_handler_test.exs
@@ -7,6 +7,17 @@ defmodule TransportWeb.MarkdownHandlerTest do
     assert content |> MarkdownHandler.markdown_to_safe_html!() == {:safe, "<h1>\nBonjour</h1>\n\n  alert(\"xxx\")\n"}
   end
 
+  test "a markdown keeps linebreaks" do
+    content_with_r = "Bonjour,\r\n\r\nLes données seront périmées à la fin du mois d'Août."
+    content_with_only_n = "Bonjour  \nmerci de votre vigilance."
+
+    assert content_with_r |> MarkdownHandler.markdown_to_safe_html!() ==
+             {:safe, "<p>\nBonjour,</p>\n<p>\nLes données seront périmées à la fin du mois d’Août.</p>\n"}
+
+    assert content_with_only_n |> MarkdownHandler.markdown_to_safe_html!() ==
+             {:safe, "<p>\nBonjour  <br />\nmerci de votre vigilance.</p>\n"}
+  end
+
   test "renders tables" do
     content = """
     State | Abbrev
