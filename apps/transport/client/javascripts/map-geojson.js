6492f53079943d5d5b760355ddbf6f6b32d12833
Francis Chabouis <fchabouis@gmail.com>
2023-06-27 15:34:27 +0200

    Update mapbox vector tiles url and key (#3253)
    * update mapbox vector tiles url and key
    
    * DRY mapbox credentials
    
    ---------
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>

--- apps/transport/client/javascripts/map-geojson.js
+++ apps/transport/client/javascripts/map-geojson.js
@@ -1,22 +1,14 @@
 import L from 'leaflet'
-
-/**
- * Represents a Mapbox object.
- * @type {Object}
- */
-const Mapbox = {
-    url: 'https://api.mapbox.com/styles/v1/istopopoki/ckg98kpoc010h19qusi9kxcct/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
-    accessToken: 'pk.eyJ1IjoiaXN0b3BvcG9raSIsImEiOiJjaW12eWw2ZHMwMGFxdzVtMWZ5NHcwOHJ4In0.VvZvyvK0UaxbFiAtak7aVw',
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors <a href="https://spdx.org/licenses/ODbL-1.0.html">ODbL</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 20
-}
+import { Mapbox } from './mapbox-credentials'
 
 function initilizeMap (id) {
     const map = L.map(id, { renderer: L.canvas() }).setView([46.505, 2], 5)
     L.tileLayer(Mapbox.url, {
         accessToken: Mapbox.accessToken,
         attribution: Mapbox.attribution,
-        maxZoom: Mapbox.maxZoom
+        maxZoom: Mapbox.maxZoom,
+        tileSize: Mapbox.tileSize,
+        zoomOffset: Mapbox.zoomOffset
     }).addTo(map)
 
     const markersfg = L.featureGroup().addTo(map)
--- apps/transport/client/javascripts/map.js
+++ apps/transport/client/javascripts/map.js
@@ -1,16 +1,6 @@
 import Leaflet from 'leaflet'
 import 'leaflet.pattern'
-
-/**
- * Represents a Mapbox object.
- * @type {Object}
- */
-const Mapbox = {
-    url: 'https://api.mapbox.com/styles/v1/istopopoki/ckg98kpoc010h19qusi9kxcct/tiles/256/{z}/{x}/{y}?access_token={accessToken}',
-    accessToken: 'pk.eyJ1IjoiaXN0b3BvcG9raSIsImEiOiJjaW12eWw2ZHMwMGFxdzVtMWZ5NHcwOHJ4In0.VvZvyvK0UaxbFiAtak7aVw',
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors <a href="https://spdx.org/licenses/ODbL-1.0.html">ODbL</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 20
-}
+import { Mapbox } from './mapbox-credentials'
 
 const regionsUrl = '/api/stats/regions'
 const aomsUrl = '/api/stats/'
@@ -31,7 +21,9 @@ const makeMapOnView = (id, view) => {
     Leaflet.tileLayer(Mapbox.url, {
         accessToken: Mapbox.accessToken,
         attribution: Mapbox.attribution,
-        maxZoom: Mapbox.maxZoom
+        maxZoom: Mapbox.maxZoom,
+        tileSize: Mapbox.tileSize,
+        zoomOffset: Mapbox.zoomOffset
     }).addTo(map)
 
     return map
