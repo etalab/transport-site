c1a80dfc364f8716eb95184990515d5ea18f653d
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-07-11 19:35:56 +0200

    add Oban job

--- apps/transport/lib/jobs/geo_data/irve_to_geo_data.ex
+++ apps/transport/lib/jobs/geo_data/irve_to_geo_data.ex
@@ -2,8 +2,20 @@ defmodule Transport.Jobs.IRVEToGeoData do
   @moduledoc """
   Job in charge of taking the charge stations stored in the Base nationale des Infrastructures de Recharge pour Véhicules Électriques and storing the result in the `geo_data` table.
   """
+
+  use Oban.Worker, max_attempts: 3
   import Ecto.Query
   alias NimbleCSV.RFC4180, as: CSV
+  require Logger
+
+
+  def perform(%{}) do # This could be shared with some options between files
+    [resource] = relevant_dataset() |> DB.Dataset.official_resources() |> Enum.filter(&(&1.format == "csv"))
+
+    Transport.Jobs.BaseGeoData.import_replace_data(resource, &prepare_data_for_insert/2)
+
+    :ok
+  end
 
 
   def prepare_data_for_insert(body, geo_data_import_id) do
