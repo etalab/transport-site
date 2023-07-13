560e2bcb46b79f0bc0bc829188227b26ac01105e
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-07-13 10:42:18 +0200

    Add import test

--- apps/transport/test/transport/jobs/geo_data/irve_to_geodata_test.exs
+++ apps/transport/test/transport/jobs/geo_data/irve_to_geodata_test.exs
@@ -16,6 +16,14 @@ defmodule Transport.Jobs.IRVEToGeoDataTest do
   ,,info2@example.com,,info2@example.com,,Giberville Sud,FRIONE4171,FRIONE4171,Giberville Sud,Station dédiée à la recharge rapide,"Aire de Giberville Sud, A13, km 220, 14730 Giberville",,"[-0.276864, 49.166746]",5,FRIONE41715,FRIONE41715,50,false,true,true,true,false,false,true,true,,,Accès libre,false,24/7,Accessibilité inconnue,inconnu,false,,,2021-11-20,EF connector is available at the site separately,2023-07-11,false,2023-07-11T03:08:58.394000+00:00,64060c2ac773dcf3fabbe5d2,b11113db-875d-41c7-8673-0cf8ad43e917,eco-movement,-0.276864,49.166746,,,False,False
   """
 
+  @dataset_info %{
+    type: "charging-stations",
+    custom_title: "Infrastructures de Recharge pour Véhicules Électriques - IRVE",
+    organization: "Etalab",
+    organization_id: "534fff75a3a7292c64a77de4"
+  }
+
+
   test "import an IRVE to the DB" do
     %{id: id} = insert(:geo_data_import)
     # Uncomment to test only the prepare_data_for_insert function
@@ -39,13 +47,78 @@ defmodule Transport.Jobs.IRVEToGeoDataTest do
   end
 
   test "Finds the relevant dataset" do
-    %DB.Dataset{id: dataset_id} =
-      insert(:dataset, %{
-        type: "charging-stations",
-        custom_title: "Infrastructures de Recharge pour Véhicules Électriques - IRVE",
-        organization: "Etalab",
-        organization_id: "534fff75a3a7292c64a77de4"
+     %DB.Dataset{id: dataset_id} = insert(:dataset, @dataset_info)
+     assert %DB.Dataset{id: ^dataset_id} = IRVEToGeoData.relevant_dataset()
+  end
+
+  test "IRVE data update logic" do
+    now = DateTime.utc_now()
+    now_100 = now |> DateTime.add(-100)
+    now_50 = now |> DateTime.add(-50)
+    now_25 = now |> DateTime.add(-25)
+
+    assert [] = DB.GeoData |> DB.Repo.all()
+    assert [] = DB.GeoDataImport |> DB.Repo.all()
+
+    %DB.Dataset{id: dataset_id} = insert(:dataset, @dataset_info)
+
+    # We don’t want to match community resources (but want to have them in base)
+    insert(:resource, %{
+      dataset_id: dataset_id,
+      is_community_resource: true,
+      datagouv_id: "8d9398ae-3037-48b2-be19-412c24561fbb"
+    })
+
+    %{id: resource_id} =
+      insert(:resource, %{
+        dataset_id: dataset_id,
+        datagouv_id: "8d9398ae-3037-48b2-be19-412c24561fbb",
+        format: "csv"
       })
-    assert %DB.Dataset{id: ^dataset_id} = IRVEToGeoData.relevant_dataset()
+
+    %{id: id_0} =
+      insert(:resource_history, %{
+        resource_id: resource_id,
+        inserted_at: now_100,
+        payload: %{"dataset_id" => dataset_id, "permanent_url" => "url"}
+      })
+
+    # another random resource history, just in case
+    insert(:resource_history, %{inserted_at: now_25, payload: %{"dataset_id" => dataset_id + 5}})
+
+    # download IRVE Mock
+    Transport.HTTPoison.Mock
+    |> expect(:get!, 2, fn "url" -> %HTTPoison.Response{status_code: 200, body: @irve_content} end)
+
+    # launch job
+    Transport.Jobs.IRVEToGeoData.perform(%{})
+
+
+    # data is imported
+    [%{id: geo_data_import_1, resource_history_id: ^id_0}] = DB.GeoDataImport |> DB.Repo.all()
+    assert DB.GeoData |> DB.Repo.all() |> Enum.count() == 2
+
+    # relaunch job
+    Transport.Jobs.IRVEToGeoData.perform(%{})
+
+    # no change
+    [%{id: ^geo_data_import_1}] = DB.GeoDataImport |> DB.Repo.all()
+
+    # new (more recent) resource history
+    %{id: id_1} =
+      insert(:resource_history, %{
+        resource_id: resource_id,
+        inserted_at: now_50,
+        payload: %{"dataset_id" => dataset_id, "permanent_url" => "url"}
+      })
+
+    # relaunch job
+    Transport.Jobs.IRVEToGeoData.perform(%{})
+
+    # geo_data and geo_data_import are updated accordingly
+    [%{id: geo_data_import_2, resource_history_id: ^id_1}] = DB.GeoDataImport |> DB.Repo.all()
+    assert geo_data_import_2 !== geo_data_import_1
+
+    [%{geo_data_import_id: ^geo_data_import_2},%{geo_data_import_id: ^geo_data_import_2}] = DB.GeoData |> DB.Repo.all()
   end
 end
