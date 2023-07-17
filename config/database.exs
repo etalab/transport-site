0d4825ddabcdc9a8f56c9c48d6f71e072fba10e6
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-07-17 15:12:13 +0200

    Increase pool size and remove logs

--- config/database.exs
+++ config/database.exs
@@ -3,8 +3,9 @@ import Config
 config :transport, DB.Repo,
   url: System.get_env("PG_URL") || "ecto://postgres:postgres@localhost/transport_repo",
   # NOTE: this default pool_size is overriden by "prod.exs" !
-  pool_size: (System.get_env("PG_POOL_SIZE") || "10") |> String.to_integer(),
-  types: DB.PostgrexTypes
+  pool_size: 50,
+  types: DB.PostgrexTypes,
+  log: false
 
 config :transport, ecto_repos: [DB.Repo]
 config :transport, ecto_repos: [DB.Repo]
