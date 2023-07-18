af96015b12a117e6d205ed135adc0b6a1d3be495
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-07-18 15:23:18 +0200

    Add ecto_erd tool (for dev)

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Transport.MixProject do
       {:excoveralls, "~> 0.10", only: :test},
       {:saxy, "~> 1.5"},
       {:appsignal, "~> 2.0"},
-      {:appsignal_phoenix, "~> 2.0"}
+      {:appsignal_phoenix, "~> 2.0"},
+      {:ecto_erd, "~> 0.5.0", only: [:dev]}
     ]
   end
 
