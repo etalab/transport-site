0ee1666b412de649978ef3e396202bc70cea003b
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-07-19 15:07:54 +0200

    Change type pour dataset.legal_owner_company_siren

---
+++ apps/transport/priv/repo/migrations/20230719124102_dataset_legal_owner_company_siren_type.exs
@@ -0,0 +1,9 @@
+defmodule DB.Repo.Migrations.DatasetLegalOwnerCompanySirenType do
+  use Ecto.Migration
+
+  def change do
+    alter table(:dataset) do
+      modify :legal_owner_company_siren, :string, size: 9, from: :integer
+    end
+  end
+end
