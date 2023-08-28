e11717f078f73ba7ec9f753755c5593c4157f17e
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-08-28 16:31:23 +0200

    AOMs : pas d'espaces pour les départements

---
+++ apps/transport/priv/repo/migrations/20230828142610_aom_trim_departement.exs
@@ -0,0 +1,11 @@
+defmodule DB.Repo.Migrations.AomTrimDepartement do
+  use Ecto.Migration
+
+  def up do
+    execute "update aom set departement = trim(departement)"
+  end
+
+  def down do
+    IO.puts("No going back")
+  end
+end
