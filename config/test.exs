68b725cc1ff65b65e1e0118beca4baca546af15d
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-09-11 15:52:15 +0200

    explain swoosh adapter in test.exs
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>

--- config/test.exs
+++ config/test.exs
@@ -119,7 +119,13 @@ config(
   email_sender_impl: Transport.EmailSender.Mock,
   siri_query_generator_impl: Transport.SIRIQueryGenerator.Mock
 )
-
+# The Swoosh test adapter works a bit like an embryo of Mox. 
+# See: https://github.com/swoosh/swoosh/blob/main/lib/swoosh/adapters/test.ex
+#
+# It won't be as flexible in all situations (see https://github.com/swoosh/swoosh/issues/66).
+# 
+# If this causes issues, we will have instead to create
+# a behaviour/wrapper around `Transport.Mailer`
 config :transport, Transport.Mailer, adapter: Swoosh.Adapters.Test
 
 # avoid logging
