6fce0ab8ccbf34080030103d9fb00e30dab2dd0f
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-09-11 19:33:38 +0200

    Implement example of streaming

--- scripts/req_stream.exs
+++ scripts/req_stream.exs
@@ -2,16 +2,20 @@ Mix.install([
   {:req, "~> 0.4.0"}
 ])
 
-_large_file_url = "https://www.data.gouv.fr/fr/datasets/r/c83ba91e-2cd1-40f7-a632-eb0a76d83c49"
-small_file_url = "https://httpbin.org/range/100000"
+large_file_url = "https://www.data.gouv.fr/fr/datasets/r/c83ba91e-2cd1-40f7-a632-eb0a76d83c49"
+# small_file_url = "https://httpbin.org/range/100000"
 
-url = _large_file_url
+url = large_file_url
+# url = small_file_url
 
-# downloading directly to disk
+# Download file to disk via an IO.Stream
 file = File.stream!("test.data", [:write])
-response = Req.get!(large_file_url, into: file)
-File.close(file)
-# the body is not here (well it is, but as a stream)
-IO.inspect(response, IEx.inspect_opts)
-
-IO.puts "ok"
+try do
+  # NOTE: decode_body is apparently disabled on response streaming
+  # https://hexdocs.pm/req/Req.Steps.html#decode_body/1
+  response = Req.get!(url, into: file)
+  # :body is just a ref to the File.Stream
+  IO.inspect(response, IEx.inspect_opts)
+after
+  :ok = File.close(file)
+end
