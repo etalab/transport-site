1d0ca8aa8b2077d114d15057e039059adda47f61
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-09-14 11:56:00 +0200

    Implémentation Swoosh (#3451)
    * start using swoosh (preview not working)
    
    * working preview
    
    * Working swoosh POC for contact
    
    * Add tests for contact controller
    
    * working example for resources changed job
    
    * restore Transport.EmailSender
    
    * mix format & mix credo
    
    * make mix credo happy
    
    * remove comments
    
    * improve documentation
    
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>
    
    * explain swoosh adapter in test.exs
    
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>
    
    * Add explicit hackney dependency
    
    * mix format
    
    * Move configuration to runtime.exs
    
    * replace get_env by fetch_env
    
    * Update config/runtime.exs
    
    * use fetch_env
    
    * Update apps/transport/lib/transport_web/controllers/contact_controller.ex
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    * Update apps/transport/lib/transport_web/controllers/contact_controller.ex
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    * Update config/runtime.exs
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    * Update apps/transport/lib/jobs/resources_changed_notification_job.ex
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    * Update apps/transport/lib/jobs/resources_changed_notification_job.ex
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    * Update apps/transport/lib/transport_web/router.ex
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    * rename demande in question everywhere
    
    * default config for swoosh in mail.exs
    
    * Add readme
    
    * new syntax for notification test
    
    * change contact email test
    
    * Update config/dev.exs
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    * Update README.md
    
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
    
    ---------
    
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>
    Co-authored-by: Antoine Augusti <antoine.augusti@transport.data.gouv.fr>

--- apps/transport/lib/transport_web/controllers/contact_controller.ex
+++ apps/transport/lib/transport_web/controllers/contact_controller.ex
@@ -13,16 +13,10 @@ defmodule TransportWeb.ContactController do
     |> redirect(to: params["redirect_path"] || page_path(conn, :index))
   end
 
-  def send_mail(conn, %{"email" => email, "topic" => subject, "demande" => demande} = params) do
-    case Transport.EmailSender.impl().send_mail(
-           "PAN, Formulaire Contact",
-           Application.get_env(:transport, :contact_email),
-           Application.get_env(:transport, :contact_email),
-           email,
-           subject,
-           demande,
-           ""
-         ) do
+  def send_mail(conn, %{"email" => email, "topic" => subject, "question" => question} = params) do
+    contact_email = TransportWeb.ContactEmail.contact(email, subject, question)
+
+    case Transport.Mailer.deliver(contact_email) do
       {:ok, _} ->
         conn
         |> put_flash(:info, gettext("Your email has been sent, we will contact you soon"))
@@ -41,5 +35,18 @@ defmodule TransportWeb.ContactController do
     conn
     |> put_flash(:error, gettext("There has been an error, try again later"))
     |> redirect(to: params["redirect_path"] || page_path(conn, :index))
+  end
+end
+
+defmodule TransportWeb.ContactEmail do
+  import Swoosh.Email
+
+  def contact(email, subject, question) do
+    new()
+    |> from({"PAN, Formulaire Contact", Application.fetch_env!(:transport, :contact_email)})
+    |> to(Application.fetch_env!(:transport, :contact_email))
+    |> reply_to(email)
+    |> subject(subject)
+    |> text_body(question)
   end
 end
