0f660efe1744b32441323c101a2ddf7f9c83b188
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-09-21 10:17:11 +0200

    mix format

--- scripts/notion_link_databases.exs
+++ scripts/notion_link_databases.exs
@@ -19,6 +19,7 @@ defmodule NotionClient do
 
   def get_notion_database_content(database_id, start_cursor: start_cursor) do
     Process.sleep(350)
+
     Req.post!(
       "https://api.notion.com/v1/databases/#{database_id}/query",
       [json: %{"start_cursor" => start_cursor}] ++ notion_headers()
@@ -44,16 +45,16 @@ defmodule NotionClient do
     results_content_follow_pagination(database_id, body["next_cursor"], acc ++ body["results"])
   end
 
-
-
   def list_of_tuples_text_ref_real_id(results) do
     results
     |> Enum.map(fn line ->
       property = line |> get_in(["properties", @org_id_column_name])
-      org_text_id = case property["type"] do
-        "rich_text" -> property |> get_in(["rich_text"]) |> hd |> get_in(["plain_text"])
-        "number" -> property |> get_in(["number"])
-      end
+
+      org_text_id =
+        case property["type"] do
+          "rich_text" -> property |> get_in(["rich_text"]) |> hd |> get_in(["plain_text"])
+          "number" -> property |> get_in(["number"])
+        end
 
       {org_text_id, line["id"]}
     end)
@@ -61,6 +62,7 @@ defmodule NotionClient do
 
   def patch_organisation_relation_property(entry_id, organisation_id) do
     Process.sleep(500)
+
     Req.patch!(
       "https://api.notion.com/v1/pages/#{entry_id}",
       [
@@ -75,23 +77,31 @@ defmodule NotionClient do
 
   def link_organisations_to_contacts(organisation_database_id, contact_database_id) do
     # 1.Get the organisation database from Notion API, then transform it to get a map with their ID and slug
-    IO.puts"Getting organisations..."
-    map_of_orgs_ids = NotionClient.results_content_follow_pagination(organisation_database_id)
-    |> NotionClient.list_of_tuples_text_ref_real_id()
-    |> Map.new()
+    IO.puts("Getting organisations...")
+
+    map_of_orgs_ids =
+      NotionClient.results_content_follow_pagination(organisation_database_id)
+      |> NotionClient.list_of_tuples_text_ref_real_id()
+      |> Map.new()
 
     IO.inspect(map_of_orgs_ids)
 
     # 2. Get the contact database from Notion API, then transform it to get a list of tuples
-    IO.puts"Getting contacts..."
-    list_of_tuples_of_contacts = NotionClient.results_content_follow_pagination(contact_database_id) |> NotionClient.list_of_tuples_text_ref_real_id()
+    IO.puts("Getting contacts...")
+
+    list_of_tuples_of_contacts =
+      NotionClient.results_content_follow_pagination(contact_database_id)
+      |> NotionClient.list_of_tuples_text_ref_real_id()
+
     IO.inspect(map_of_orgs_ids)
 
     # 3. For each contact, patch it with the organisation ID
-    IO.puts"Linking contacts to organisations..."
+    IO.puts("Linking contacts to organisations...")
+
     list_of_tuples_of_contacts
     |> Enum.each(fn {org_text_id, contact_id} ->
       IO.puts("Linking #{contact_id} to #{org_text_id}")
+
       case Map.get(map_of_orgs_ids, org_text_id) do
         nil -> IO.puts("No organisation ref #{org_text_id} found for #{contact_id}")
         org_id -> NotionClient.patch_organisation_relation_property(contact_id, org_id)
