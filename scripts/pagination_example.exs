7917a3fde3f970f188cb44a9f066c384d38dc983
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-09-26 14:43:05 +0200

    Update pagination_example.exs

--- scripts/pagination_example.exs
+++ scripts/pagination_example.exs
@@ -10,14 +10,21 @@ defmodule EnvConfig do
   end
 end
 
+# A simple example of HTTP pagination of an API, using tools provided by Elixir.
+# We build "streams" which are lazy-evaluated enumerables.
 defmodule NotionClient do
   def http_options(notion_secret) do
     [auth: {:bearer, notion_secret}, headers: [{"Notion-Version", "2022-06-28"}]]
   end
 
-  def database_items(table_id, notion_secret) do
-    base_url = "https://api.notion.com/v1/databases/#{table_id}/query"
+  def build_base_url(table_id), do: "https://api.notion.com/v1/databases/#{table_id}/query"
 
+  # an example of creating a stream (lazy-evaluted) based on HTTP pagination via `Stream.resource/3`
+  # https://hexdocs.pm/elixir/Stream.html#resource/3
+  def database_items_via_stream_resource(table_id, notion_secret) do
+    base_url = build_base_url(table_id)
+
+    # function called by `Stream.resource`, until it returns {:halt, ...}
     next_fn = fn
       :done ->
         {:halt, nil}
@@ -39,6 +46,39 @@ defmodule NotionClient do
       fn _ -> nil end
     )
   end
+
+  # Since we have a simple case, we can even just use `Stream.unfold`
+  # https://hexdocs.pm/elixir/Stream.html#unfold/2
+  def database_items_via_stream_unfold(table_id, notion_secret) do
+    base_url = build_base_url(table_id)
+
+    req = fn
+      nil -> 
+        nil
+      {url, start_cursor} ->
+        # https://developers.notion.com/reference/pagination
+        json = %{page_size: 100}
+        json = if start_cursor, do: Map.put(json, :start_cursor, start_cursor), else: json
+        options = http_options(notion_secret) |> Keyword.put(:json, json)
+        %{status: 200, body: body} = Req.post!(url, options)
+        acc = if (c = body["next_cursor"]), do: {url, c}, else: nil
+        {body["results"], acc}
+    end
+
+    Stream.unfold({base_url, nil}, req)
+    # https://hexdocs.pm/elixir/Stream.html#flat_map/2 is needed
+    |> Stream.flat_map(fn(x) -> x end)
+  end
+end
+
+defmodule Mapper do
+  def run(stream) do
+    stream
+    |> Stream.map(fn x -> x["properties"]["Nom"]["title"] |> List.first() |> Map.fetch!("plain_text") end)
+    |> Stream.with_index()
+    |> Stream.each(&IO.inspect(&1, IEx.inspect_opts))
+    |> Stream.run()
+  end
 end
 
 %{
@@ -46,11 +86,14 @@ end
   "NOTION_ORGANIZATIONS_TABLE_ID" => notion_org_table_id
 } = EnvConfig.read!()
 
-organizations = NotionClient.database_items(notion_org_table_id, notion_secret)
+IO.puts "========= database_items_via_stream_resource ========="
 
-organizations
-#|> Stream.take(2)
-|> Stream.map(fn x -> x["properties"]["Nom"]["title"] |> List.first() |> Map.fetch!("plain_text") end)
-|> Stream.with_index()
-|> Stream.each(&IO.inspect(&1, IEx.inspect_opts))
-|> Stream.run()
+NotionClient.database_items_via_stream_resource(notion_org_table_id, notion_secret)
+|> Stream.take(3)
+|> Mapper.run()
+
+IO.puts "========= database_items_via_stream_unfold ========="
+
+NotionClient.database_items_via_stream_unfold(notion_org_table_id, notion_secret)
+|> Stream.take(2)
+|> Mapper.run()
