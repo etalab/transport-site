59d63bc7d8daa7f70ddac061f0d98db7c593b6e8
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-10-16 15:25:15 +0200

    Stop using aliases (very confusing in that case, see #3539)

--- apps/transport/lib/transport_web/api/controllers/gtfs_stops_controller.ex
+++ apps/transport/lib/transport_web/api/controllers/gtfs_stops_controller.ex
@@ -1,8 +1,6 @@
 defmodule TransportWeb.API.GTFSStopsController do
   use TransportWeb, :controller
   alias OpenApiSpex.Operation
-  alias TransportWeb.API.Schemas.GeoJSONResponse
-  alias TransportWeb.API.Schemas.ErrorJSONResponse
 
   @max_points 20_000
   # NOTE: summary cannot include formatting apparently.
@@ -47,8 +45,8 @@ defmodule TransportWeb.API.GTFSStopsController do
         Operation.parameter(:east, :query, :number, "East (longitude)")
       ],
       responses: %{
-        200 => Operation.response("GeoJSON", "application/json", GeoJSONResponse),
-        422 => Operation.response("ErrorJSON", "application/json", ErrorJSONResponse)
+        200 => Operation.response("GeoJSON", "application/json", TransportWeb.API.Schemas.GeoJSONResponse),
+        422 => Operation.response("ErrorJSON", "application/json", TransportWeb.API.Schemas.ErrorJSONResponse)
       }
     }
 
