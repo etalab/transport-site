e669fce284b6ccd83a644bb92cc5612253cfa84b
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-10-24 17:15:49 +0200

    mix format

--- apps/transport/lib/transport_web/api/controllers/gtfs_stops_controller.ex
+++ apps/transport/lib/transport_web/api/controllers/gtfs_stops_controller.ex
@@ -34,7 +34,8 @@ defmodule TransportWeb.API.GTFSStopsController do
       tags: ["gtfs"],
       summary:
         "#{@experimental_summary} Lists stops from all GTFS files of transport.data.gouv.fr found inside a bounding box.",
-      description: ~s"#{@experimental_description}
+      description:
+        ~s"#{@experimental_description}
                       This call returns the GTFS stops from all the datasets of transport.data.gouv.fr as a GeoJSON FeatureCollection.
                       found inside the provided bounding box, up to #{@max_points} points (above that threshold, an error will be returned). The dataset ID is present in the answer amongst other data.
                       This endpoint (with some slight differences) is used to power the map at https://transport.data.gouv.fr/explore/gtfs-stops.
