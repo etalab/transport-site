c9dfdf201dd4db93261be69df7c287fe18dfcbea
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-10-30 11:45:23 +0100

    Move plugs to TransportWeb.Endpoint

--- apps/transport/lib/transport_web/router.ex
+++ apps/transport/lib/transport_web/router.ex
@@ -10,15 +10,6 @@ defmodule TransportWeb.Router do
 
   pipeline :browser_no_csp do
     plug(:canonical_host)
-
-    plug(RemoteIp, headers: ["x-forwarded-for"])
-
-    plug(TransportWeb.Plugs.BlockUserAgent,
-      log_user_agent: System.get_env("LOG_USER_AGENT", "false"),
-      block_user_agent_keywords: System.get_env("BLOCK_USER_AGENT_KEYWORDS", "")
-    )
-
-    plug(PhoenixDDoS)
     plug(:accepts, ["html"])
     plug(:fetch_session)
     plug(:fetch_live_flash)
