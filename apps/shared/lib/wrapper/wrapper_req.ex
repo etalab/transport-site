d78cd257d2de5ce3b0d363a946d62c7df4d0a10d
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-11-06 22:39:26 +0100

    Rework behaviour

--- apps/shared/lib/wrapper/wrapper_req.ex
+++ apps/shared/lib/wrapper/wrapper_req.ex
@@ -1,14 +1,16 @@
-defmodule Transport.Shared.Wrapper.Req do
-  @moduledoc """
-  A Req wrapper to facilitate the use of mocks.
-  """
-  def impl, do: Application.get_env(:transport, :req_impl)
-
+defmodule Transport.Req.Behaviour do
   # Req apparently does not define a Behaviour that we can Mox directly, recommending
   # instead to mock "above" (via some wrapper) or "below" (via bypass)
   # See https://github.com/wojtekmach/req/issues/143
-  defmodule Behaviour do
-    # Ref: https://github.com/wojtekmach/req/blob/b40de7b7a0e7cc97a2c398ffcc42aa14962f3963/lib/req.ex#L545
-    @callback get(url() | keyword() | Req.Request.t(), options :: keyword()) :: {:ok, Req.Response.t()} | {:error, Exception.t()}
-  end
+  # Ref: https://github.com/wojtekmach/req/blob/b40de7b7a0e7cc97a2c398ffcc42aa14962f3963/lib/req.ex#L545
+  @type url() :: URI.t() | String.t()
+  # Simplified version for our needs
+  @callback get(url(), options :: keyword()) :: {:ok, Req.Response.t()} | {:error, Exception.t()}
+end
+
+defmodule Transport.Req do
+  def impl, do: Application.get_env(:transport, :req_impl, __MODULE__)
+
+  @behaviour Transport.Req.Behaviour
+  defdelegate get(url, options), to: Req
 end
