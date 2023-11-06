975e55055469414264fa514e5613dfac6c838e83
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-11-06 11:57:38 +0100

    Run mix format

--- scripts/go.exs
+++ scripts/go.exs
@@ -2,16 +2,18 @@ import Ecto.Query
 
 Logger.configure(level: :info)
 
-resource = DB.Resource
-|> order_by(desc: :id)
-|> limit(1)
-|> DB.Repo.one!()
+resource =
+  DB.Resource
+  |> order_by(desc: :id)
+  |> limit(1)
+  |> DB.Repo.one!()
+
 # |> IO.inspect(IEx.inspect_opts)
 
-IO.puts resource.url
+IO.puts(resource.url)
 
 job = %Oban.Job{args: %{"resource_id" => resource.id}}
 
 Transport.Jobs.ResourceHistoryJob.perform(job)
 
-IO.puts "done!"
+IO.puts("done!")
