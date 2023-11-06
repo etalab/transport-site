3877b829c95895716f6741896cb105eabfa85713
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-11-06 22:40:46 +0100

    Mix format

--- apps/transport/lib/S3/s3.ex
+++ apps/transport/lib/S3/s3.ex
@@ -42,7 +42,7 @@ defmodule Transport.S3 do
   @spec stream_to_s3!(bucket_feature(), binary(), binary(), acl: atom()) :: any()
   def stream_to_s3!(feature, local_path, upload_path, options \\ []) do
     Logger.debug("Streaming #{local_path} to #{upload_path}")
-    options = Keyword.validate!(options,  acl: "public-read")
+    options = Keyword.validate!(options, acl: "public-read")
 
     local_path
     |> ExAws.S3.Upload.stream_file()
