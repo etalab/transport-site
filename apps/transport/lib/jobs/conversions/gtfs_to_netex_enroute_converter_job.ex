6edd92decd07f51ddb6121de433d8ec3fe1ae85f
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2023-11-08 10:42:58 +0100

    GTFSToNeTExEnRouteConverterJob : gère tous les cas pour next_polling_attempt (#3588)

--- apps/transport/lib/jobs/conversions/gtfs_to_netex_enroute_converter_job.ex
+++ apps/transport/lib/jobs/conversions/gtfs_to_netex_enroute_converter_job.ex
@@ -170,10 +170,12 @@ defmodule Transport.Jobs.GTFSToNeTExEnRouteConverterJob do
   30
   iex> next_polling_attempt_seconds(500)
   30
+  iex> Enum.each(1..@max_attempts, &next_polling_attempt_seconds/1)
+  :ok
   """
   def next_polling_attempt_seconds(current_attempt) when current_attempt < 12, do: 10
 
-  def next_polling_attempt_seconds(current_attempt) when current_attempt >= 13 and current_attempt < @max_attempts,
+  def next_polling_attempt_seconds(current_attempt) when current_attempt >= 12 and current_attempt < @max_attempts,
     do: 30
 
   def conversion_exists?(%DB.ResourceHistory{payload: %{"uuid" => rh_uuid}}) do
