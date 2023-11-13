046a3f9ac8f7234f039a3a98451f9b97a5b61b60
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-11-13 21:05:55 +0100

    Use a state file to properly analyse status codes

--- scripts/go.exs
+++ scripts/go.exs
@@ -7,10 +7,17 @@ defmodule Downloader do
     [:req, :legacy]
     |> Enum.each(fn mode ->
       file_path = Path.join(folder, "#{mode}-#{resource.id}.dat")
+      state_file_path = file_path <> ".state"
 
-      unless File.exists?(file_path) do
-        IO.puts("Saving #{file_path}")
-        Transport.Jobs.ResourceHistoryJob.download_resource(mode, resource, file_path)
+      unless File.exists?(state_file_path) do
+        outcome =
+          Transport.Jobs.ResourceHistoryJob.download_resource(mode, resource, file_path)
+
+        unless outcome |> elem(0) == :ok do
+          File.rm(file_path)
+        end
+
+        File.write!(state_file_path, outcome |> inspect)
       end
     end)
   end
@@ -19,6 +26,9 @@ end
 folder = Path.join(__ENV__.file, "../cache-dir") |> Path.expand()
 
 resources
+# |> Enum.filter(fn x -> x.id == 79628 end)
 |> Enum.each(fn resource ->
   Downloader.handle(folder, resource)
 end)
+
+IO.puts("============ done =============")
