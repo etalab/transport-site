389a2686aada81c783eb72165190e9328c560e85
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-11-14 13:35:09 +0100

    Save WIP

--- scripts/go.exs
+++ scripts/go.exs
@@ -27,14 +27,44 @@ end
 
 folder = Path.join(__ENV__.file, "../cache-dir") |> Path.expand()
 
-resources
-|> Task.async_stream(
-  fn x -> Downloader.handle(folder, x) end,
-  max_concurrency: 10,
-  timeout: :infinity
-)
-|> Stream.map(fn {:ok, x} -> x end)
-|> Stream.each(fn x -> IO.inspect(x, IEx.inspect_opts()) end)
-|> Stream.run()
+stream =
+  resources
+  |> Task.async_stream(
+    fn x -> Downloader.handle(folder, x) end,
+    max_concurrency: 10,
+    timeout: :infinity
+  )
+  |> Stream.map(fn {:ok, x} -> x end)
+
+# TODOS:
+# - compter ceux qui sont 200 (donc :ok) des deux côtés, et vérifier la cohérence de la donnée approximativement
+
+defmodule Comparer do
+  # compute checksum, using a checksum file for persistence since this is a costly operation
+  def checksum(file, :cached) do
+    checksum_file = file <> ".checksum"
+
+    unless File.exists?(checksum_file) do
+      checksum = :crypto.hash(:md5, File.read!(file)) |> Base.encode16()
+      File.write!(checksum_file, checksum)
+    end
+
+    File.read!(checksum_file)
+  end
+end
+
+stream
+|> Stream.filter(fn x -> match?([{:ok, _, _}, {:ok, _, _}], x) end)
+|> Stream.filter(fn x ->
+  [{:ok, file_1, _}, {:ok, file_2, _}] = x
+  Comparer.checksum(file_1, :cached) != Comparer.checksum(file_2, :cached)
+end)
+|> Enum.count()
+|> IO.puts()
+
+# - compter ceux qui sont 200 que pour httpoison, et par pour req: ce sont des régressions à étudier. Voir le contenu,
+#   mais aussi l'état. Je pense que les erreurs d'encodage d'urls porteront là dessus.
+# - compter ceux qui sont 200 pour req, mais pas pour httpoison. Vérifier que ce sont bien des améliorations
+# - voir ceux qui sont non-200 des deux côtés, et comparer.
 
 IO.puts("============ done =============")
