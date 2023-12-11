6d7c1dbc0fa9da6eb136a7eaef0c786b7711ffe4
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-12-11 11:55:45 +0100

    remove comments

--- apps/transport/lib/transport_web/controllers/aoms.ex
+++ apps/transport/lib/transport_web/controllers/aoms.ex
@@ -21,8 +21,8 @@ defmodule TransportWeb.AOMSController do
   def index(conn, _params), do: render(conn, "index.html", aoms: aoms())
 
   @spec prepare_aom({AOM.t(), binary()}, list(), list()) :: map()
-  defp prepare_aom({aom, nom_commune}, datasets, aggregated_datasets) do
-    all_datasets = datasets ++ aggregated_datasets
+  defp prepare_aom({aom, nom_commune}, gtfs_datasets, aggregated_datasets) do
+    all_datasets = gtfs_datasets ++ aggregated_datasets
 
     datasets_up_to_date =
       all_datasets
@@ -56,7 +56,9 @@ defmodule TransportWeb.AOMSController do
 
   @spec aoms :: [map()]
   def aoms do
-    datasets =
+    # Let’s fetch all GTFS datasets.
+    # This doesn’t include GTFS-Flex, although they are in public-transport and have a GTFS format on resource.
+    gtfs_datasets =
       Dataset.base_query()
       |> Dataset.join_from_dataset_to_metadata(Transport.Validators.GTFSTransport.validator_name())
       |> join(:left, [dataset: d], aom in AOM, on: d.aom_id == aom.id, as: :aom)
@@ -72,20 +74,12 @@ defmodule TransportWeb.AOMSController do
       )
       |> Repo.all()
 
-    datasets |> Enum.group_by(& &1.dataset_id) |> Map.values |> Enum.map(&length/1) |> Enum.max |> dbg(label: "size of each group")
+    gtfs_datasets_by_aom_id = gtfs_datasets |> Enum.group_by(& &1.aom_id)
+    gtfs_dataset_by_dataset_id = gtfs_datasets |> Map.new(&{&1.dataset_id, &1})
 
-    datasets_by_aom_id = datasets |> Enum.group_by(& &1.aom_id)
-    dataset_by_dataset_id = datasets |> Map.new(&{&1.dataset_id, &1})
-
-    # dbg(datasets_by_dataset_id, label: "datasets_by_datasets_id")
-    # Datasets with a key dataset to find them easily. The AOM ID inside is AOM as territory.
-    # datasets_by_dataset_id #=> %{
-    #   518 => [%{aom_id: nil, dataset_id: 518, end_date: xxx, has_realtime: true}],
-    #   401 => [%{aom_id: 950, dataset_id: 401, has_realtime: false}],
-    #   117 => [%{aom_id: 107, dataset_id: 117, has_realtime: false}],
-    #   377 => [%{aom_id: nil, dataset_id: 377, has_realtime: true}],
-
-    repo_results =
+    # Some AOM data is present on aggregated datasets: the region publishes instead of the AOM.
+    # In this case, there is at least 2 legal owners on the dataset
+    aggregated_datasets_in_db =
       AOM
       |> join(:inner, [aom], d in assoc(aom, :legal_owners_dataset), as: :legal_owners_dataset)
       |> where(
@@ -93,43 +87,28 @@ defmodule TransportWeb.AOMSController do
         d.id in subquery(
           Dataset.base_query()
           |> join(:inner, [dataset: d], aom in assoc(d, :legal_owners_aom), as: :aom)
+          |> join(:inner, [dataset: d], resource in assoc(d, :resources), as: :resource)
           |> group_by([dataset: d], d.id)
           |> having([aom: a], count(a.id) >= 2)
+          |> where([resource: r], r.format in ["GTFS", "NeTex"])
           |> select([dataset: d], d.id)
         )
       )
       |> select([aom, legal_owners_dataset: d], %{aom_id: aom.id, dataset_id: d.id})
       |> Repo.all()
 
-      # List of tupples AOM / Legal owner dataset but as in the subquery we limit to only one dataset per AOM ? No.
-      dbg(repo_results, label: "repo_results")
-      # repo_results #=> [
-      #   %{aom_id: 5, dataset_id: 787},
-      #   %{aom_id: 9, dataset_id: 732},
-      #   %{aom_id: 15, dataset_id: 787},
-      #   %{aom_id: 17, dataset_id: 976},
-      #   %{aom_id: 19, dataset_id: 146},
-
-      repo_results |> Enum.group_by(& &1.aom_id) |> Map.values |> Enum.map(&length/1) |> Enum.max |> dbg(label: "size of each group")
-
-      aggregated_datasets =
-      repo_results
+    aggregated_datasets =
+      aggregated_datasets_in_db
       |> Enum.group_by(& &1.aom_id, fn %{dataset_id: dataset_id} ->
-        # This is really not good: if there is an non GTFS dataset associated with an AOM, we still put it in the list
-        # So AOM will be considered as "done" even if it's not the case.
-        dataset_by_dataset_id |> Map.get(dataset_id, %{dataset_id: dataset_id, end_date: nil, has_realtime: false})
+        Map.get(
+          # Let’s enrich aggregated datasets with the GTFS dataset metadata if we have it
+          gtfs_dataset_by_dataset_id,
+          dataset_id,
+          # In case of a Netex or GTFS flex dataset, we don’t have the end_date
+          # We could have the realtime info by redoing the SQL query behing aggregated_datasets_in_db
+          %{dataset_id: dataset_id, end_date: nil, has_realtime: false}
+        )
       end)
-
-    dbg(aggregated_datasets, label: "aggregated_datasets")
-    # We take back the list of AOMS from the repo result, and enrich it
-    # aggregated_datasets #=> %{
-    # 289 => [%{aom_id: nil, dataset_id: 732, has_realtime: false}],
-    # 309 => [%{aom_id: nil, dataset_id: 732, has_realtime: false}],
-    # 704 => [%{aom_id: nil, dataset_id: 176, has_realtime: false}],
-    # 621 => [%{aom_id: nil, dataset_id: 979, has_realtime: false}],
-    # 219 => [%{aom_id: nil, dataset_id: 38, has_realtime: false}],
-    # 492 => [%{aom_id: nil, dataset_id: 979, has_realtime: false}],
-    # 311 => [%{aom_id: nil, dataset_id: 176, has_realtime: false}],
 
     AOM
     |> join(:left, [aom], c in Commune, on: aom.insee_commune_principale == c.insee)
@@ -137,7 +116,11 @@ defmodule TransportWeb.AOMSController do
     |> select([aom, commune], {aom, commune.nom})
     |> Repo.all()
     |> Enum.map(fn {aom, nom_commune} ->
-      prepare_aom({aom, nom_commune}, Map.get(datasets_by_aom_id, aom.id, []), Map.get(aggregated_datasets, aom.id, []))
+      prepare_aom(
+        {aom, nom_commune},
+        Map.get(gtfs_datasets_by_aom_id, aom.id, []),
+        Map.get(aggregated_datasets, aom.id, [])
+      )
     end)
   end
 
