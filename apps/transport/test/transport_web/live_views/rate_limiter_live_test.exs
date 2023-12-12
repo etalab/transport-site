93831001f37aa3f44201c751c41bd2dcfffd1262
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-12-12 12:11:14 +0100

    Add test: removing IP from jail

--- apps/transport/test/transport_web/live_views/rate_limiter_live_test.exs
+++ apps/transport/test/transport_web/live_views/rate_limiter_live_test.exs
@@ -1,6 +1,7 @@
 defmodule TransportWeb.Backoffice.RateLimiterLiveTest do
   use ExUnit.Case, async: true
   use TransportWeb.LiveCase
+  import Phoenix.LiveViewTest
   import TransportWeb.ConnCase, only: [setup_admin_in_session: 1]
 
   @endpoint TransportWeb.Endpoint
@@ -27,5 +28,24 @@ defmodule TransportWeb.Backoffice.RateLimiterLiveTest do
     assert [] == TransportWeb.Backoffice.RateLimiterLive.ips_in_jail()
     PhoenixDDoS.Jail.send(~c"108.128.238.17", {nil, %{jail_time: {1, :hour}}})
     assert ["108.128.238.17"] == TransportWeb.Backoffice.RateLimiterLive.ips_in_jail()
+  end
+
+  test "bail out IP", %{conn: conn} do
+    ip = "108.128.238.17"
+    ip |> to_charlist() |> PhoenixDDoS.Jail.send({nil, %{jail_time: {1, :hour}}})
+    {:ok, view, html} = conn |> setup_admin_in_session() |> live(@url)
+    assert [ip] == TransportWeb.Backoffice.RateLimiterLive.ips_in_jail()
+
+    assert html =~ ip
+
+    assert view
+           |> element("button", "Retirer de la jail")
+           |> render_click()
+
+    assert [] == TransportWeb.Backoffice.RateLimiterLive.ips_in_jail()
+
+    send(view.pid, :update_data)
+
+    refute render(view) =~ ip
   end
 end
