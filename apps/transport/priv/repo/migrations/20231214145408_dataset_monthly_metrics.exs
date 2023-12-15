27e77416ecb915517f882fe5b0dc5fa475d73c88
Antoine Augusti <antoine.augusti@beta.gouv.fr>
2023-12-15 16:09:01 +0100

    Improve migration

--- apps/transport/priv/repo/migrations/20231214145408_dataset_monthly_metrics.exs
+++ apps/transport/priv/repo/migrations/20231214145408_dataset_monthly_metrics.exs
@@ -4,11 +4,13 @@ defmodule DB.Repo.Migrations.DatasetMonthlyMetrics do
   def change do
     create table(:dataset_monthly_metrics) do
       add(:dataset_datagouv_id, references(:dataset, column: :datagouv_id, type: :string, on_delete: :delete_all),
-        null: false
+        null: false,
+        size: 50
       )
 
-      add(:year_month, :string, null: false)
-      add(:metric_name, :string, null: false)
+      # Example: 2023-12
+      add(:year_month, :string, null: false, size: 7)
+      add(:metric_name, :string, null: false, size: 50)
       add(:count, :integer, null: false)
       timestamps(type: :utc_datetime_usec)
     end
