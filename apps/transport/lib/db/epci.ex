a6e3652c9232a35ea74041def8692adff655ff75
Vincent Degove <vincent.degove@beta.gouv.fr>
2023-12-27 11:49:44 +0100

    Document EPCI

--- apps/transport/lib/db/epci.ex
+++ apps/transport/lib/db/epci.ex
@@ -1,9 +1,10 @@
 defmodule DB.EPCI do
   @moduledoc """
-  EPCI schema
+  EPCI schema.
 
-  Link the EPCI to some Communes.
-  The EPCI are loaded once and for all by the task transport/lib/transport/import_epci.ex
+  The EPCI are loaded by the task transport/lib/transport/import_epci.ex.
+  The EPCI imported are only "à fiscalité propre". This excludes Etablissements Publics Territoriaux.
+  This allows to have a 1 to 1 relation between a commune and an EPCI.
   """
   use Ecto.Schema
   use TypedEctoSchema
