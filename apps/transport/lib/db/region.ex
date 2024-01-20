febe7f2f9fb9beb8aaa82aef579447d1b67070a1
Vincent Degove <vincent.degove@beta.gouv.fr>
2024-01-20 17:43:45 +0100

    count datasets as legal owners in regions

--- apps/transport/lib/db/region.ex
+++ apps/transport/lib/db/region.ex
@@ -9,6 +9,7 @@ defmodule DB.Region do
   use TypedEctoSchema
   alias DB.{AOM, Dataset, Departement}
   alias Geo.MultiPolygon
+  import Ecto.Query
 
   typed_schema "region" do
     field(:nom, :string)
@@ -18,5 +19,26 @@ defmodule DB.Region do
     has_many(:aoms, AOM)
     has_many(:departements, Departement, foreign_key: :region_insee, references: :insee)
     has_one(:datasets, Dataset)
+  end
+
+  def count_datasets_by_region_as_legal_owners do
+    subquery =
+      from(d in "dataset_region_legal_owner",
+        # Ignore the TER dataset
+        where: d.dataset_id != 239,
+        group_by: d.region_id,
+        select: %{region_id: d.region_id, count: count(d.dataset_id)}
+      )
+
+    query =
+      from(r in DB.Region,
+        # Ignore the National region
+        where: r.id != 14,
+        left_join: drlo in subquery(subquery),
+        on: r.id == drlo.region_id,
+        select: [r.id, drlo.count |> coalesce(0)]
+      )
+
+    query
   end
 end
--- apps/transport/priv/repo/migrations/20231227110412_remove-is-completed-from-region.exs
+++ apps/transport/priv/repo/migrations/20231227110412_remove-is-completed-from-region.exs
@@ -3,7 +3,7 @@ defmodule :"Elixir.DB.Repo.Migrations.Remove-is-completed-from-region" do
 
   def change do
     alter table(:region) do
-      remove :is_completed, :boolean
+      remove(:is_completed, :boolean)
     end
   end
 end
