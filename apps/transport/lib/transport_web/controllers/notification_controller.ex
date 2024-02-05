b5ae62d8cef85550d4ffcdd075f7f22f8b816a4d
Vincent Degove <vincent.degove@beta.gouv.fr>
2024-02-05 14:58:51 +0100

    change methods in controller

--- apps/transport/lib/transport_web/controllers/notification_controller.ex
+++ apps/transport/lib/transport_web/controllers/notification_controller.ex
@@ -45,12 +45,43 @@ defmodule TransportWeb.NotificationController do
   defp notification_subscriptions_for_datasets(datasets, current_contact) do
     dataset_ids = datasets |> Enum.map(& &1.id)
 
-    notification_list = get_notifications(dataset_ids, current_contact)
+    subscriptions_list = load_subscriptions_for_datasets_and_contact(dataset_ids, current_contact)
 
-    notification_list
+    subscriptions_list
     |> Enum.sort_by(&{&1.dataset.custom_title, &1.reason})
     |> Enum.group_by(& &1.dataset)
-    |> Map.new(fn {dataset, subscriptions} -> {dataset, Enum.group_by(subscriptions, & &1.reason)} end)
+    |> Map.new(fn {dataset, subscriptions} -> {dataset, group_by_reason_and_contact(subscriptions, current_contact)} end)
+  end
+
+  defp load_subscriptions_for_datasets_and_contact(dataset_ids, current_contact) do
+    DB.NotificationSubscription.base_query()
+    |> DB.NotificationSubscription.join_with_contact()
+    |> preload(:contact)
+    |> preload(:dataset)
+    |> where(
+      [notification_subscription: ns, contact: c],
+      ns.dataset_id in ^dataset_ids and not is_nil(ns.dataset_id) and
+        ns.role == :producer and
+        c.organization == ^current_contact.organization
+    )
+    |> DB.Repo.all()
+  end
+
+  defp group_by_reason_and_contact(subscriptions, current_contact) do
+    subscriptions
+    |> Enum.group_by(& &1.reason)
+    |> Map.new(fn {reason, subscriptions} -> {reason, subgroup_by_contact(subscriptions, current_contact)} end)
+  end
+
+  defp subgroup_by_contact(subscriptions, current_contact) do
+    {user_subscriptions, team_subscriptions} = Enum.split_with(subscriptions, & &1.contact == current_contact)
+    user_subscription =
+      case user_subscriptions do
+      [] -> nil
+      [subscription] -> subscription
+    end
+    %{user_subscription: user_subscription, team_subscriptions: team_subscriptions}
+
   end
 
   def delete_for_dataset(%Plug.Conn{assigns: %{current_user: %{"id" => datagouv_user_id}}} = conn, %{
@@ -107,19 +138,5 @@ defmodule TransportWeb.NotificationController do
     |> select([notification_subscription: ns], ns.reason)
     |> DB.Repo.all()
     |> Enum.map(&to_string/1)
-  end
-
-  defp get_notifications(dataset_ids, current_contact) do
-    DB.NotificationSubscription.base_query()
-    |> DB.NotificationSubscription.join_with_contact()
-    |> preload(:contact)
-    |> preload(:dataset)
-    |> where(
-      [notification_subscription: ns, contact: c],
-      ns.dataset_id in ^dataset_ids and not is_nil(ns.dataset_id) and
-        ns.role == :producer and
-        c.organization == ^current_contact.organization
-    )
-    |> DB.Repo.all()
   end
 end
