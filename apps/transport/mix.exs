d8283b51395541f8f5c812a432f238161cf63b68
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-03-21 13:21:55 +0100

    Bump image

--- apps/transport/mix.exs
+++ apps/transport/mix.exs
@@ -144,7 +144,7 @@ defmodule Transport.Mixfile do
       {:phoenix_ddos, "~> 1.1"},
       # Highly recommended, this will makes sure we get the correct remote_ip
       {:remote_ip, "~> 1.1"},
-      {:image, "~> 0.42.0"}
+      {:image, "~> 0.42"}
     ]
   end
 end
