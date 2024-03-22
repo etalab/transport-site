034fff759e99384406e692a59f4e91d643a6f86c
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-03-22 14:20:45 +0100

    Add doc

--- apps/transport/lib/config/runtime_configuration.ex
+++ apps/transport/lib/config/runtime_configuration.ex
@@ -1,9 +1,26 @@
 defmodule Transport.RuntimeConfiguration do
+  @moduledoc """
+  A module allowing us to bring some structure & testing to what is happening
+  inside `runtime.exs`.
+  """
+
   defmodule SystemEnvProvider do
+    @moduledoc """
+    A behaviour wrapping the sub-part of `System.get_env`  calls required by
+    the current `runtime.exs` configuration. We define this boundary/interface
+    to help stub the environment variables during tests, and ensure we can get
+    robust well-tested production configuration.
+    """
+
     @callback get_env(String.t()) :: String.t() | nil
     @callback get_env(String.t(), String.t()) :: String.t() | nil
 
     defmodule RealImpl do
+      @moduledoc """
+      The real implementation of the `SystemEnvProvider` behaviour, delegating
+      calls to `System.get_env` etc.
+      """
+
       @behaviour SystemEnvProvider
       defdelegate get_env(key), to: System
       defdelegate get_env(key, default), to: System
