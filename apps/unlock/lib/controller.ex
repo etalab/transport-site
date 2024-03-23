c9b7eb32de281f224fd68a65a6dc6b9ff589e29a
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-03-23 17:24:00 +0100

    Implement parsing & recomposing of ids only

--- apps/unlock/lib/controller.ex
+++ apps/unlock/lib/controller.ex
@@ -103,16 +103,53 @@ defmodule Unlock.Controller do
   defp process_resource(%{method: "GET"} = conn, %Unlock.Config.Item.Aggregate{} = item) do
     Telemetry.trace_request(item.identifier, :external)
 
-    item.feeds
-    |> Task.async_stream(fn(sub_item) ->
-      Telemetry.trace_request(item.identifier <> ":" <> sub_item["identifier"], :internal)
-      Logger.debug("Fetching aggregated sub-item #{sub_item["identifier"]} at #{sub_item["target_url"]}")
-      %{status: status} = get_with_maybe_redirect(sub_item |> Map.fetch!("target_url"))
-      Logger.debug("#{sub_item["identifier"]} responded with HTTP code #{status}")
-    end, max_concurrency: 10)
-    |> Stream.run()
+    headers = ["id_pdc_itinerance"]
 
-    send_resp(conn, 501, "Not Yet Implemented")
+    rows =
+      item.feeds
+      |> Task.async_stream(
+        fn sub_item ->
+          Telemetry.trace_request(item.identifier <> ":" <> sub_item["identifier"], :internal)
+          Logger.debug("Fetching aggregated sub-item #{sub_item["identifier"]} at #{sub_item["target_url"]}")
+          %{status: status, body: body} = get_with_maybe_redirect(sub_item |> Map.fetch!("target_url"))
+          Logger.debug("#{sub_item["identifier"]} responded with HTTP code #{status} (#{body |> byte_size} bytes)")
+          process_csv_payload(body)
+        end,
+        max_concurrency: 10,
+        # this is the default, but highlighted for maintenance clarity
+        ordered: true
+      )
+      |> Stream.map(fn {:ok, stream} -> stream end)
+      |> Stream.concat()
+
+    body_response =
+      [headers]
+      |> Stream.concat(rows)
+      |> Enum.into([])
+      |> NimbleCSV.RFC4180.dump_to_iodata()
+
+    send_resp(conn, 200, body_response)
+  end
+
+  # NOTE: we could avoid "decoding" the payload, but doing so will allow us
+  # to integrate live validation (e.g. of id_pdc_itinerance against static database)
+  # more easily, with less refactoring.
+  def process_csv_payload(body, options \\ []) do
+    [headers | rows] = NimbleCSV.RFC4180.parse_string(body, skip_headers: false)
+
+    # SEE: https://specs.frictionlessdata.io/table-schema/#descriptor
+    # The order of elements in fields array SHOULD be the order of fields in the CSV file.
+    # The number of elements in fields array SHOULD be the same as the number of fields in the CSV file.
+
+    # once we assert that, the rest of the processing is easy
+    # TODO: handle errors gracefully
+    [id_field = "id_pdc_itinerance" | _rest] = headers
+
+    # Only keeping the id for now, on purpose
+    rows = if options[:limit], do: Stream.take(rows, options[:limit]), else: rows
+
+    rows
+    |> Stream.map(fn [id | _rest] -> [id] end)
   end
 
   # `Finch` does not support redirects, and we likely will want to support data gouv stable urls
