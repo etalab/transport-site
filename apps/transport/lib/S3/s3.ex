554a2630629b507b74b2bf9b81f642e80e07130c
Frédéric Menou <frederic.menou@transport.beta.gouv.fr>
2024-03-28 14:36:30 +0100

    Pouvoir tester avec un S3 local

--- apps/transport/lib/S3/s3.ex
+++ apps/transport/lib/S3/s3.ex
@@ -13,8 +13,13 @@ defmodule Transport.S3 do
 
   @spec permanent_url(bucket_feature(), binary()) :: binary()
   def permanent_url(feature, path \\ "") do
-    host = :io_lib.format(Application.fetch_env!(:ex_aws, :cellar_url), [bucket_name(feature)])
-    host |> to_string() |> URI.merge(path) |> URI.to_string()
+    base_url = :io_lib.format(Application.fetch_env!(:ex_aws, :cellar_url), [bucket_name(feature)]) |> to_string()
+
+    if String.length(path) > 0 do
+      base_url |> URI.parse() |> URI.append_path("/" <> path) |> URI.to_string()
+    else
+      base_url
+    end
   end
 
   @spec bucket_names() :: [binary()]
