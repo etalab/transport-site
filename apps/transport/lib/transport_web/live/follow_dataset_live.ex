d13209378cfc29a2fdb703e1de5b60f6269ac8ae
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2024-03-29 10:06:33 +0100

    PR comments

--- apps/transport/lib/transport_web/live/follow_dataset_live.ex
+++ apps/transport/lib/transport_web/live/follow_dataset_live.ex
@@ -1,6 +1,10 @@
 defmodule TransportWeb.Live.FollowDatasetLive do
   @moduledoc """
-  A follow/unfollow button for datasets
+  A follow/unfollow button for dataset reusers.
+
+  This button is displayed when:
+  - the user is authenticated
+  - the user is not a producer of the current dataset (cannot be a reuser of your own dataset)
   """
   use Phoenix.LiveView
   import Ecto.Query
@@ -8,7 +12,7 @@ defmodule TransportWeb.Live.FollowDatasetLive do
   @impl true
   def render(assigns) do
     ~H"""
-    <div :if={not is_nil(@current_user) and not @is_producer} class="follow-dataset-icon">
+    <div :if={not is_nil(@current_user) and not @producer?} class="follow-dataset-icon">
       <i class={@heart_class} phx-click="toggle"></i>
     </div>
     """
@@ -16,21 +20,26 @@ defmodule TransportWeb.Live.FollowDatasetLive do
 
   @impl true
   def mount(_params, %{"current_user" => current_user, "dataset_id" => dataset_id}, socket) do
-    contact = contact(current_user)
-    dataset = DB.Repo.get!(DB.Dataset, dataset_id)
-    follows_dataset = DB.DatasetFollower.follows_dataset?(contact, dataset)
-
     socket =
-      assign(socket, %{
-        contact: contact,
-        dataset: dataset,
-        current_user: current_user,
-        follows_dataset: follows_dataset,
-        is_producer: producer?(contact, dataset),
-        heart_class: heart_class(follows_dataset: follows_dataset)
+      socket
+      |> assign(%{
+        contact: contact(current_user),
+        dataset: DB.Repo.get!(DB.Dataset, dataset_id),
+        current_user: current_user
       })
+      |> set_computed_assigns()
 
     {:ok, socket}
+  end
+
+  defp set_computed_assigns(%Phoenix.LiveView.Socket{assigns: %{dataset: dataset, contact: contact}} = socket) do
+    follows_dataset? = DB.DatasetFollower.follows_dataset?(contact, dataset)
+
+    assign(socket, %{
+      follows_dataset?: follows_dataset?,
+      producer?: producer?(contact, dataset),
+      heart_class: heart_class(follows_dataset?: follows_dataset?)
+    })
   end
 
   @impl true
@@ -41,28 +50,26 @@ defmodule TransportWeb.Live.FollowDatasetLive do
           assigns: %{
             dataset: %DB.Dataset{} = dataset,
             contact: %DB.Contact{} = contact,
-            follows_dataset: follows_dataset
+            follows_dataset?: follows_dataset?
           }
         } = socket
       ) do
-    if follows_dataset do
+    if follows_dataset? do
       DB.DatasetFollower.unfollow!(contact, dataset)
     else
       DB.DatasetFollower.follow!(contact, dataset, source: :follow_button)
     end
 
-    {:noreply,
-     assign(socket, %{
-       follows_dataset: not follows_dataset,
-       heart_class: heart_class(follows_dataset: not follows_dataset)
-     })}
+    {:noreply, set_computed_assigns(socket)}
   end
 
+  # Case where the user is not authenticated
   defp contact(nil = _current_user), do: nil
 
   defp contact(%{"id" => datagouv_user_id} = _current_user),
     do: DB.Repo.get_by!(DB.Contact, datagouv_user_id: datagouv_user_id)
 
+  # Case where the user is not authenticated
   defp producer?(nil, _), do: false
 
   defp producer?(%DB.Contact{id: contact_id}, %DB.Dataset{organization_id: organization_id}) do
@@ -72,6 +79,6 @@ defmodule TransportWeb.Live.FollowDatasetLive do
     |> DB.Repo.exists?()
   end
 
-  defp heart_class(follows_dataset: false), do: "fa fa-heart fa-2x icon---animated-heart"
-  defp heart_class(follows_dataset: true), do: heart_class(follows_dataset: false) <> " active"
+  defp heart_class(follows_dataset?: false), do: "fa fa-heart fa-2x icon---animated-heart"
+  defp heart_class(follows_dataset?: true), do: heart_class(follows_dataset?: false) <> " active"
 end
