6a93ac3e5d9e151712cb2ffa4f1dcc2814370538
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-04-08 23:55:05 +0200

    Refactor for testing

--- apps/unlock/lib/dynamic_irve_schema.ex
+++ apps/unlock/lib/dynamic_irve_schema.ex
@@ -1,11 +1,15 @@
 defmodule Unlock.DynamicIRVESchema do
-  # builds the field list based on the actual schema
-  def build_schema_fields_list do
+  def schema_content do
     __ENV__.file
     |> Path.join("../../meta/schema-irve-dynamique.json")
     |> Path.expand()
     |> File.read!()
     |> Jason.decode!()
+  end
+
+  # builds the field list based on the actual schema
+  def build_schema_fields_list do
+    schema_content()
     |> Map.fetch!("fields")
     |> Enum.map(fn(field) -> Map.fetch!(field, "name") end)
   end
