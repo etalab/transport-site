9faf599fda6d5aaa34f28769b2b0becce022d4df
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2024-04-22 10:28:45 +0200

    Espace réutilisateur : middleware auth (#3885)

--- apps/transport/test/transport_web/controllers/reuser_space_controller_test.exs
+++ apps/transport/test/transport_web/controllers/reuser_space_controller_test.exs
@@ -2,20 +2,30 @@ defmodule TransportWeb.ReuserSpaceControllerTest do
   use TransportWeb.ConnCase, async: true
   import DB.Factory
 
+  @home_url reuser_space_path(TransportWeb.Endpoint, :espace_reutilisateur)
+
   setup do
     Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
   end
 
-  test "espace_reutilisateur", %{conn: conn} do
-    contact = insert_contact(%{datagouv_user_id: Ecto.UUID.generate()})
+  describe "espace_reutilisateur" do
+    test "logged out", %{conn: conn} do
+      conn = conn |> get(@home_url)
+      assert "/login/explanation?" <> URI.encode_query(redirect_path: @home_url) == redirected_to(conn, 302)
+      assert Phoenix.Flash.get(conn.assigns.flash, :info) =~ "Vous devez être préalablement connecté"
+    end
 
-    content =
-      conn
-      |> Plug.Test.init_test_session(%{current_user: %{"id" => contact.datagouv_user_id}})
-      |> get(reuser_space_path(conn, :espace_reutilisateur))
-      |> html_response(200)
+    test "logged in", %{conn: conn} do
+      contact = insert_contact(%{datagouv_user_id: Ecto.UUID.generate()})
 
-    # Feedback form is displayed
-    refute content |> Floki.parse_document!() |> Floki.find("form.feedback-form") |> Enum.empty?()
+      content =
+        conn
+        |> Plug.Test.init_test_session(%{current_user: %{"id" => contact.datagouv_user_id}})
+        |> get(@home_url)
+        |> html_response(200)
+
+      # Feedback form is displayed
+      refute content |> Floki.parse_document!() |> Floki.find("form.feedback-form") |> Enum.empty?()
+    end
   end
 end
