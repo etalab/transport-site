486ef9c2df63dcd8612c3319bb67d9e898f52151
Vincent Degove <vincent.degove@beta.gouv.fr>
2024-04-26 15:20:31 +0200

    Remove todo comment

--- apps/transport/priv/repo/migrations/20240422145836_create_user_feedback.exs
+++ apps/transport/priv/repo/migrations/20240422145836_create_user_feedback.exs
@@ -7,7 +7,6 @@ defmodule Transport.Repo.Migrations.CreateUserFeedback do
       add(:explanation, :text, null: false)
       add(:email, :binary)
       add(:feature, :string, null: false)
-      # make the following line optional
       add(:contact_id, references(:contact, on_delete: :nilify_all))
 
       timestamps(type: :utc_datetime_usec)
