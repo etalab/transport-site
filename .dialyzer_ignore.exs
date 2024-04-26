9f8a85a234a34619204fd0653795a819736f659f
Vincent Degove <vincent.degove@beta.gouv.fr>
2024-04-26 14:04:25 +0200

    Ignore Dialyzer error and move file

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -5,5 +5,6 @@
   # EctoInterval raises an unknown_type error
   ~r/gtfs_stop_times.ex/,
   # Cloak.Ecto.SHA256 and DB.Encrypted.Binary raise an unknown_type error
-  {"lib/db/contact.ex", :unknown_type, 0}
+  {"lib/db/contact.ex", :unknown_type, 0},
+  {"lib/db/user_feedback.ex", :unknown_type, 0}
 ]
