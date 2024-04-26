09fe521e7081b63a276b374df1a77d46922dd7a0
Vincent Degove <vincent.degove@beta.gouv.fr>
2024-04-26 14:55:35 +0200

    Link to cloak_ecto issue for dialyzer problem

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -5,6 +5,7 @@
   # EctoInterval raises an unknown_type error
   ~r/gtfs_stop_times.ex/,
   # Cloak.Ecto.SHA256 and DB.Encrypted.Binary raise an unknown_type error
+  # See https://github.com/danielberkompas/cloak_ecto/issues/55
   {"lib/db/contact.ex", :unknown_type, 0},
   {"lib/db/user_feedback.ex", :unknown_type, 0}
 ]
