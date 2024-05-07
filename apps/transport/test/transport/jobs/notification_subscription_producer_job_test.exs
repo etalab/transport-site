d3fca4be8e67515faac94161f4b3f49e522f72f8
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2024-05-07 10:53:51 +0200

    NotificationSubscriptionProducerJob : supprime puis recrée les abonnements

--- apps/transport/test/transport/jobs/notification_subscription_producer_job_test.exs
+++ apps/transport/test/transport/jobs/notification_subscription_producer_job_test.exs
@@ -9,9 +9,9 @@ defmodule Transport.Test.Transport.Jobs.NotificationSubscriptionProducerJobTest 
   end
 
   test "updates relevant notification subscriptions" do
-    reuser = insert_contact()
+    %DB.Contact{id: reuser_id} = insert_contact()
 
-    producer =
+    %DB.Contact{id: producer_id} =
       insert_contact(%{
         organizations: [
           %{
@@ -26,31 +26,76 @@ defmodule Transport.Test.Transport.Jobs.NotificationSubscriptionProducerJobTest 
         ]
       })
 
-    dataset = insert(:dataset, organization_id: org_id)
+    %DB.Dataset{id: dataset_id} = insert(:dataset, organization_id: org_id)
 
-    ns_reuser =
-      insert(:notification_subscription, %{
-        reason: :expiration,
-        source: :admin,
-        dataset_id: dataset.id,
-        contact_id: reuser.id,
-        role: :reuser
-      })
+    insert(:notification_subscription, %{
+      reason: :expiration,
+      source: :admin,
+      dataset_id: dataset_id,
+      contact_id: reuser_id,
+      role: :reuser
+    })
 
-    # Should be updated: the contact is a producer for this dataset and
-    # the subscription's role is set to `reuser`
-    ns_producer =
-      insert(:notification_subscription, %{
-        reason: :expiration,
-        source: :admin,
-        dataset_id: dataset.id,
-        contact_id: producer.id,
-        role: :reuser
-      })
+    # Should be deleted: the contact is a producer for this dataset and
+    # the subscription's role is set to `reuser`.
+    # We should create subscriptions for this (contact_id, dataset_id) for all
+    # producer reasons
+    insert(:notification_subscription, %{
+      reason: :expiration,
+      source: :admin,
+      dataset_id: dataset_id,
+      contact_id: producer_id,
+      role: :reuser
+    })
+
+    # Should be left untouched: this is a platform-wide reason
+    insert(:notification_subscription, %{
+      reason: :new_dataset,
+      source: :admin,
+      dataset_id: nil,
+      contact_id: producer_id,
+      role: :reuser
+    })
 
     assert :ok == perform_job(NotificationSubscriptionProducerJob, %{})
 
-    assert [%DB.NotificationSubscription{role: :reuser}, %DB.NotificationSubscription{role: :producer}] =
-             DB.Repo.reload!([ns_reuser, ns_producer])
+    assert [
+             %DB.NotificationSubscription{
+               reason: :expiration,
+               source: :admin,
+               role: :reuser,
+               contact_id: ^reuser_id,
+               dataset_id: ^dataset_id
+             },
+             %DB.NotificationSubscription{
+               role: :producer,
+               reason: :dataset_with_error,
+               source: :admin,
+               contact_id: ^producer_id,
+               dataset_id: ^dataset_id
+             },
+             %DB.NotificationSubscription{
+               reason: :expiration,
+               source: :admin,
+               role: :producer,
+               contact_id: ^producer_id,
+               dataset_id: ^dataset_id
+             },
+             %DB.NotificationSubscription{
+               reason: :new_dataset,
+               source: :admin,
+               role: :reuser,
+               contact_id: ^producer_id,
+               dataset_id: nil
+             },
+             %DB.NotificationSubscription{
+               reason: :resource_unavailable,
+               source: :admin,
+               role: :producer,
+               contact_id: ^producer_id,
+               dataset_id: ^dataset_id
+             }
+           ] =
+             DB.NotificationSubscription |> DB.Repo.all() |> Enum.sort_by(&{&1.contact_id, &1.reason})
   end
 end
