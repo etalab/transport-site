74411c1cdde5c21eede429d645c49634ea1245f2
Frédéric Menou <frederic.menou@transport.data.gouv.fr>
2024-05-16 17:39:43 +0200

    restore_db: ignore pg extensions

--- restore_db.sh
+++ restore_db.sh
@@ -5,6 +5,19 @@
 # Usage ./restore_db.sh <db_name> <host> <user_name> <password> <absolute_path_to_backup>
 # or the ./restore_db.sh <absolute_path_to_backup> if the default options are ok for you
 # the latest production backup can be fetched on "transport-site-postgresql" in clevercloud
+#
+# With the flag `--skip-extensions`, you can also skip extensions restoration as those might require administrative
+# rights your pg user doesn't have. Example:
+# ./restore_db.sh --skip-extensions <path_to_backup>
+# The flag must be the first arg.
+
+skip_extensions=false
+
+if [ "$1" = "--skip-extensions" ]
+then
+  skip_extensions=true
+  shift 1
+fi
 
 if test "$#" -eq 1; then
     DB_NAME="transport_repo"
@@ -19,11 +32,18 @@ elif test "$#" -eq 5; then
     export PGPASSWORD=$4
     BACKUP_PATH=$5
 else
-    echo "Usage: ./restore_db.sh <db_name> <host> <user_name> <password> <absolute_path_to_backup>"
+    echo "Usage: ./restore_db.sh (--skip-extensions) <db_name> <host> <user_name> <password> <absolute_path_to_backup>"
     exit 1
 fi
 
-pg_restore -h "$HOST" -U "$USER_NAME" -d "$DB_NAME" --format=c --no-owner --clean --no-acl "$BACKUP_PATH"
+if [ "$skip_extensions" = true ]
+then
+  pg_restore -l "$BACKUP_PATH" -f ./pg.list
+  pg_restore -h "$HOST" -U "$USER_NAME" -d "$DB_NAME" --format=c --no-owner --clean --use-list ./pg.list --no-acl "$BACKUP_PATH"
+else
+  pg_restore -h "$HOST" -U "$USER_NAME" -d "$DB_NAME" --format=c --no-owner --clean --no-acl "$BACKUP_PATH"
+fi
+
 
 echo "Truncating contact table"
 psql -h "$HOST" -U "$USER_NAME" -d "$DB_NAME" -c 'TRUNCATE TABLE contact CASCADE'
@@ -40,3 +60,8 @@ fi
 
 # Don't let database files hang around
 rm "$BACKUP_PATH"
+
+if [ "$skip_extensions" = true ]
+then
+  rm ./pg.list
+fi
