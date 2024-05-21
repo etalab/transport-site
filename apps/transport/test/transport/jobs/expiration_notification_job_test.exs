d666e273d0e8dc6856fdd0dd96007dfc9d54d846
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2024-05-21 18:23:35 +0200

    Notification d'expiration quotidienne pour les réutilisateurs

---
+++ apps/transport/test/transport/jobs/expiration_notification_job_test.exs
@@ -0,0 +1,153 @@
+defmodule Transport.Test.Transport.Jobs.ExpirationNotificationJobTest do
+  use ExUnit.Case, async: true
+  use Oban.Testing, repo: DB.Repo
+  import DB.Factory
+  import Swoosh.TestAssertions
+  alias Transport.Jobs.ExpirationNotificationJob
+
+  doctest ExpirationNotificationJob, import: true
+
+  setup do
+    Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+  end
+
+  test "perform: dispatches other jobs" do
+    today = Date.utc_today()
+    today_str = today |> Date.to_string()
+    a_week_ago = Date.add(today, -7)
+    yesterday = Date.add(today, -1)
+    a_week_from_now = Date.add(today, 7)
+    %{dataset: %DB.Dataset{} = d1} = insert_resource_and_friends(today)
+    %{dataset: %DB.Dataset{} = d2} = insert_resource_and_friends(yesterday)
+    %{dataset: %DB.Dataset{} = d3} = insert_resource_and_friends(a_week_ago)
+    %{dataset: %DB.Dataset{} = d4} = insert_resource_and_friends(a_week_from_now)
+
+    %DB.Contact{} = c1 = insert_contact()
+    %DB.Contact{id: c2_id} = c2 = insert_contact()
+
+    # Subscriptions for `c1`: should not match because:
+    # - is a producer for a relevant expiration delay (+7)
+    # - is a reuser but for an ignored expiration delay
+    # - is a reuser for an irrelevant reason for a matching dataset
+    insert(:notification_subscription,
+      contact_id: c1.id,
+      dataset_id: d1.id,
+      reason: :dataset_with_error,
+      role: :reuser,
+      source: :user
+    )
+
+    insert(:notification_subscription,
+      contact_id: c1.id,
+      dataset_id: d2.id,
+      reason: :expiration,
+      role: :reuser,
+      source: :user
+    )
+
+    insert(:notification_subscription,
+      contact_id: c1.id,
+      dataset_id: d4.id,
+      reason: :expiration,
+      role: :producer,
+      source: :user
+    )
+
+    # Subscriptions for `c2`: matches for expiration today and for +7
+    insert(:notification_subscription,
+      contact_id: c2.id,
+      dataset_id: d1.id,
+      reason: :expiration,
+      role: :reuser,
+      source: :user
+    )
+
+    insert(:notification_subscription,
+      contact_id: c2.id,
+      dataset_id: d4.id,
+      reason: :expiration,
+      role: :reuser,
+      source: :user
+    )
+
+    assert %{-7 => [d3.id], 0 => [d1.id], 7 => [d4.id]} ==
+             ExpirationNotificationJob.gtfs_expiring_on_target_dates(today)
+
+    assert :ok == perform_job(ExpirationNotificationJob, %{}, inserted_at: DateTime.utc_now())
+
+    assert [
+             %Oban.Job{
+               worker: "Transport.Jobs.ExpirationNotificationJob",
+               args: %{"contact_id" => ^c2_id, "digest_date" => ^today_str},
+               conflict?: false,
+               state: "available"
+             }
+           ] = all_enqueued()
+  end
+
+  test "perform for a specific contact_id" do
+    today = Date.utc_today()
+    a_week_ago = Date.add(today, -7)
+    yesterday = Date.add(today, -1)
+    a_week_from_now = Date.add(today, 7)
+    %{dataset: %DB.Dataset{id: d1_id} = d1} = insert_resource_and_friends(today)
+    %{dataset: %DB.Dataset{} = d2} = insert_resource_and_friends(yesterday)
+    %{dataset: %DB.Dataset{} = d3} = insert_resource_and_friends(a_week_ago)
+    %{dataset: %DB.Dataset{id: d4_id} = d4} = insert_resource_and_friends(a_week_from_now)
+
+    %DB.Contact{email: contact_email} = contact = insert_contact()
+
+    insert(:notification_subscription,
+      contact_id: contact.id,
+      dataset_id: d1.id,
+      reason: :expiration,
+      role: :reuser,
+      source: :user
+    )
+
+    insert(:notification_subscription,
+      contact_id: contact.id,
+      dataset_id: d2.id,
+      reason: :expiration,
+      role: :reuser,
+      source: :user
+    )
+
+    insert(:notification_subscription,
+      contact_id: contact.id,
+      dataset_id: d4.id,
+      reason: :expiration,
+      role: :reuser,
+      source: :user
+    )
+
+    assert %{-7 => [d3.id], 0 => [d1.id], 7 => [d4.id]} ==
+             ExpirationNotificationJob.gtfs_expiring_on_target_dates(today)
+
+    assert :ok ==
+             perform_job(ExpirationNotificationJob, %{contact_id: contact.id, digest_date: today},
+               inserted_at: DateTime.utc_now()
+             )
+
+    assert_email_sent(fn %Swoosh.Email{
+                           subject: "Suivi des jeux de données favoris arrivant à expiration",
+                           from: {"transport.data.gouv.fr", "contact@transport.data.gouv.fr"},
+                           to: [{"", ^contact_email}],
+                           text_body: nil,
+                           html_body: html
+                         } ->
+      html = String.replace(html, "\n", "")
+
+      assert html =~
+               ~s|<p>Bonjour,</p><p>Voici un résumé de vos jeux de données favoris arrivant à expiration.</p><strong>Jeux de données périmant demain :</strong><ul><li><a href="http://127.0.0.1:5100/datasets/#{d1.slug}">Hello</a></li></ul><br/><strong>Jeux de données périmant dans 7 jours :</strong><ul><li><a href="http://127.0.0.1:5100/datasets/#{d4.slug}">Hello</a></li></ul>|
+
+      assert html =~
+               ~s|vous pouvez paramétrer vos alertes sur le suivi de vos jeux de données favoris depuis votre <a href="http://127.0.0.1:5100/espace_reutilisateur?utm_source=transactional_email&amp;utm_medium=email&amp;utm_campaign=expiration_reuser">Espace réutilisateur</a>|
+    end)
+
+    assert [
+             %DB.Notification{reason: :expiration, email: ^contact_email, dataset_id: ^d1_id},
+             %DB.Notification{reason: :expiration, email: ^contact_email, dataset_id: ^d4_id}
+           ] = DB.Notification |> DB.Repo.all() |> Enum.sort_by(& &1.dataset_id)
+  end
+end
