fd846362894980df77c40440ae693d608026104c
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-05-26 23:20:26 +0200

    Download all NeTEx

--- scripts/index_resources.exs
+++ scripts/index_resources.exs
@@ -15,41 +15,68 @@ defmodule StreamTools do
     |> Stream.each(function)
     |> Stream.map(fn {item, _index} -> item end)
   end
+
+  def show_one(stream, index) do
+    StreamTools.each_with_index(stream, fn {item, i} ->
+      if index == i do
+        IO.puts("====== échantillon ======")
+        IO.inspect(item, IEx.inspect_opts())
+        IO.puts("")
+      end
+    end)
+  end
 end
 
 df =
   resources
-  |> StreamTools.each_with_index(fn {item, index} ->
-    if index == 0 do
-      IO.puts("====== échantillon ======")
-      IO.inspect(item, IEx.inspect_opts())
-      IO.puts("")
-    end
-  end)
   |> Enum.map(fn r ->
     %{
       id: r.id,
       url: r.url,
       title: r.title,
-      supposed_format: r.format,
+      unverified_format: r.format,
       description: r.description
     }
   end)
   |> Explorer.DataFrame.new()
-  |> IO.inspect(IEx.inspect_opts())
 
-df[:supposed_format]
-|> IO.inspect(IEx.inspect_opts())
-|> Explorer.Series.frequencies()
-|> IO.inspect(IEx.inspect_opts())
-
-# incorporate macros
 require Explorer.DataFrame
 
 netex =
   df
-  |> Explorer.DataFrame.filter(supposed_format == "NeTEx")
-  |> IO.inspect(IEx.inspect_opts())
+  |> Explorer.DataFrame.filter(unverified_format == "NeTEx")
+  |> Explorer.DataFrame.to_rows_stream()
+  |> Task.async_stream(
+    fn r ->
+      url = r["url"]
+      file = Path.join("cache-dir", "resource-#{r["id"]}.dat")
+      status_file = file <> ".status.json"
 
-netex[:url]
-|> IO.inspect(IEx.inspect_opts())
+      unless File.exists?(status_file) do
+        IO.puts("Saving #{url}")
+        url = if String.contains?(url, "|"), do: URI.encode(url), else: url
+
+        %{status: status} =
+          Transport.HTTPClient.get!(url, decode_body: false, compressed: false, into: File.stream!(file))
+
+        File.write!(status_file, %{status: status} |> Jason.encode!())
+      end
+
+      %{"status" => status} = File.read!(status_file) |> Jason.decode!()
+
+      r
+      |> Map.put("http_status", status)
+      |> Map.put("local_path", file)
+    end,
+    max_concurrency: 10,
+    timeout: 50_000
+  )
+  |> Stream.map(fn {:ok, result} -> result end)
+  |> Stream.take(1)
+  |> Stream.map(fn r ->
+    metadata =
+      r["local_path"]
+      |> Transport.ZipMetaDataExtractor.extract!()
+      |> IO.inspect(IEx.inspect_opts())
+  end)
+  |> Stream.run()
