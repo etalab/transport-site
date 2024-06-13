30e33286b465faea6fa8d654aa853b574dcc1bd1
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-06-13 17:24:44 +0200

    Add module

---
+++ apps/transport/lib/irve/aggregator.ex
@@ -0,0 +1,7 @@
+defmodule Transport.IRVE.Aggregator do
+  @moduledoc """
+  A module responsible for aggregating all the available resources for a given schema.
+
+  Helps creating a non-deduped aggregate, useful for diagnosis.
+  """
+end
