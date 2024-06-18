7bccc9c5c5299d5438a7cfd26a5bc6101b523348
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-06-18 15:05:23 +0200

    Enforce explicit passing of schema file path or url

--- apps/transport/lib/irve/frictionless_cli_validator.ex
+++ apps/transport/lib/irve/frictionless_cli_validator.ex
@@ -4,17 +4,15 @@ defmodule Transport.IRVE.FrictionlessCLIValidator do
   only used by IRVE, and no attempt is made to make it more generic than needed.
   """
 
-  @latest_static_irve_schema "https://schema.data.gouv.fr/schemas/etalab/schema-irve-static/latest/schema-static.json"
-
   @doc """
   File location can be a file path or a url.
 
   Schema as well, which allows local caching of the schema to avoid numerous HTTP requests when
   validating a lot of files.
   """
-  def validate(file_location, schema \\ @latest_static_irve_schema) do
+  def validate(file_location, schema_file_path_or_url) do
     cmd = "frictionless"
-    args = ["validate", file_location, "--schema", schema, "--json", "--format", "csv"]
+    args = ["validate", file_location, "--schema", schema_file_path_or_url, "--json", "--format", "csv"]
 
     {output, result} = System.cmd(cmd, args)
 
