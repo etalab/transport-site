29e8954915b3d255f90ca4d71f964a92be071e88
Vincent Degove <vincent.degove@beta.gouv.fr>
2024-06-20 18:34:47 +0200

    Add a daily job

---
+++ apps/transport/lib/jobs/update_counter_cache_job.ex
@@ -0,0 +1,14 @@
+defmodule Transport.Jobs.UpdateCounterCacheJob do
+  @moduledoc """
+  This job is in charge updating the counter cache for `DB.Resource` with the modes
+  """
+  use Oban.Worker, max_attempts: 3
+
+  @impl Oban.Worker
+  @spec perform(Oban.Job.t()) :: :ok
+  def perform(%Oban.Job{args: args}) when is_nil(args) or args == %{} do
+    Transport.CounterCache.cache_modes_on_resources()
+
+    :ok
+  end
+end
