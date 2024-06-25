8a2ac534a70c2267c266368cfdd8b523b17aedac
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-06-25 14:02:51 +0200

    Delete original script

--- scripts/hotfix_modes_indexation.exs
+++
@@ -1,9 +0,0 @@
-alias Transport.CounterCache
-
-# interesting ids I kept around for experimenting with
-ids_with_heterogeneous_modes = [614, 362, 146]
-
-# TODO: add a couple of unit tests
-# TODO: schedule a job
-# MAYBE: discussion a better cache invalidation
-CounterCache.cache_modes_on_resources() |> IO.inspect(IEx.inspect_opts())
