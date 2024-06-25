8275d2cd8a403f13c128b5073658a93097e36e6a
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-06-25 14:02:18 +0200

    Go private for all methods, for maintenance clarity
    As a consequence, replace `@doc` by comments.

--- apps/transport/lib/transport/counter_cache.ex
+++ apps/transport/lib/transport/counter_cache.ex
@@ -17,41 +17,35 @@ defmodule Transport.CounterCache do
     |> apply_all_updates!()
   end
 
-  @doc """
-  Build the query to retrieve one line per "resource <-> modes" with its dataset (for GTFS resources only).
-  """
+  # Build the query to retrieve one line per "resource <-> modes" with its dataset (for GTFS resources only).
   @spec resources_with_modes([integer()] | nil) :: Ecto.Query.t()
-  def resources_with_modes(optional_dataset_ids \\ nil) do
+  defp resources_with_modes(optional_dataset_ids \\ nil) do
     DB.Dataset.base_query()
     |> maybe_where(optional_dataset_ids)
     |> DB.Dataset.join_from_dataset_to_metadata(Transport.Validators.GTFSTransport.validator_name())
   end
 
-  @doc """
-  Prepare a list of maps compatible with a bulk values update (https://hexdocs.pm/ecto/Ecto.Query.API.html#values/2)
-  """
+  # Prepare a list of maps compatible with a bulk values update (https://hexdocs.pm/ecto/Ecto.Query.API.html#values/2)
   @spec prepare_update_values(Ecto.Query.t()) :: Ecto.Query.t()
-  def prepare_update_values(query) do
+  defp prepare_update_values(query) do
     query
     |> select([resource: r, metadata: m], %{resource_id: r.id, resource_gtfs_modes: m.modes})
   end
 
-  @doc """
-  Given a list of planned updates, for each resource, in batch, update the `counter_cache`
-  field to contain a JSONB map with `gtfs_modes` array. Example of SQL generated:
-
-  ```sql
-  UPDATE "resource" AS r
-  SET "counter_cache" = jsonb_build_object('gtfs_modes', v."resource_gtfs_modes")
-  FROM (
-    VALUES (ARRAY['tramway'], 80705), (ARRAY['bus', 'funicular'], 80223)
-  ) AS v ("resource_gtfs_modes", "resource_id")
-  WHERE r."id" = v."resource_id";
-  ```
-  """
+  # Given a list of planned updates, for each resource, in batch, update the `counter_cache`
+  # field to contain a JSONB map with `gtfs_modes` array. Example of SQL generated:
+  #
+  # ```sql
+  # UPDATE "resource" AS r
+  # SET "counter_cache" = jsonb_build_object('gtfs_modes', v."resource_gtfs_modes")
+  # FROM (
+  #   VALUES (ARRAY['tramway'], 80705), (ARRAY['bus', 'funicular'], 80223)
+  # ) AS v ("resource_gtfs_modes", "resource_id")
+  # WHERE r."id" = v."resource_id";
+  # ```
   @type update_resource_modes_list :: [%{resource_id: integer(), resource_gtfs_modes: [String.t()]}]
   @spec apply_all_updates!(update_resource_modes_list) :: any()
-  def apply_all_updates!(updates) do
+  defp apply_all_updates!(updates) do
     types = %{resource_id: :integer, resource_gtfs_modes: {:array, :string}}
 
     from(r in DB.Resource,
@@ -62,6 +56,6 @@ defmodule Transport.CounterCache do
     |> DB.Repo.update_all([])
   end
 
-  def maybe_where(query, nil), do: query
-  def maybe_where(query, dataset_ids), do: query |> where([dataset: d], d.id in ^dataset_ids)
+  defp maybe_where(query, nil), do: query
+  defp maybe_where(query, dataset_ids), do: query |> where([dataset: d], d.id in ^dataset_ids)
 end
