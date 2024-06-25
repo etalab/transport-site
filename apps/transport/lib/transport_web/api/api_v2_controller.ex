c6c977c16984a23b6d87f4755be5e1003ecb9fdc
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-06-25 14:47:49 +0200

    Output resources

--- apps/transport/lib/transport_web/api/api_v2_controller.ex
+++ apps/transport/lib/transport_web/api/api_v2_controller.ex
@@ -3,6 +3,26 @@ defmodule TransportWeb.API.V2.Controller do
 
   @spec resources(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def resources(conn, _params) do
-    json(conn, %{"hello" => "world"})
+    items = DB.Repo.all(DB.Resource)
+    json(conn, format_items(items))
   end
+
+  defp format_items(items) do
+    items
+    |> Enum.map(&format_item(&1))
+  end
+
+  defp format_item(%DB.Resource{} = item) do
+    %{
+      # despite potential stability issues:
+      # https://github.com/etalab/transport-site/issues/1946
+      #
+      # the `datagouv_id` cannot be safely used to uniquely designate a resource:
+      # https://github.com/etalab/transport-site/issues/4022
+      #
+      # so I'm exposing the primary key instead for the moment
+      id: item.id,
+      dataset_id: item.dataset_id
+    }
+end
 end
