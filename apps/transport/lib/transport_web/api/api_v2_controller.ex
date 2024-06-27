25e9f0ef25654c44d03dc21b4ac3d5dddca2e80f
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-06-27 23:03:39 +0200

    Allow to filter resources by format

--- apps/transport/lib/transport_web/api/api_v2_controller.ex
+++ apps/transport/lib/transport_web/api/api_v2_controller.ex
@@ -1,10 +1,45 @@
 defmodule TransportWeb.API.V2.Controller do
   use Phoenix.Controller, namespace: TransportWeb
+  import Ecto.Query
 
   @spec resources(Plug.Conn.t(), map()) :: Plug.Conn.t()
-  def resources(conn, _params) do
-    items = DB.Repo.all(DB.Resource)
+  def resources(conn, params) do
+    items =
+      DB.Resource
+      |> filter(params)
+      |> DB.Repo.all()
+
     json(conn, format_items(items))
+  end
+
+  # TODO: reply HTTP 422 when something incorrect is detected
+  def filter(query, params) do
+    {declared_format, remaining} = Map.pop(params, "declared_format")
+    %{} = remaining
+
+    query
+    |> ecto_filter("declared_format", parse_param("declared_format", declared_format))
+  end
+
+  def parse_param("declared_format", nil), do: nil
+
+  def parse_param("declared_format", value) do
+    {:ok, value} = PostgrestQueryParser.parse(value)
+    value
+  end
+
+  defp ecto_filter(query, "declared_format", nil), do: query
+
+  defp ecto_filter(query, "declared_format", [:eq, value]) do
+    IO.puts("filtering where format is #{value}")
+
+    query
+    |> where([r], fragment("lower(?)", r.format) == ^value)
+  end
+
+  defp ecto_filter(query, "declared_format", [:in, values]) do
+    query
+    |> where([r], fragment("lower(?)", r.format) in ^values)
   end
 
   defp format_items(items) do
@@ -22,7 +57,9 @@ defmodule TransportWeb.API.V2.Controller do
       #
       # so I'm exposing the primary key instead for the moment
       id: item.id,
-      dataset_id: item.dataset_id
+      dataset_id: item.dataset_id,
+      declared_format: (item.format || "unknown") |> String.downcase(),
+      title: item.title
     }
-end
+  end
 end
