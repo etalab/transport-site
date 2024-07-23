e1fc240dd08240cfbe990a78a06c9688c1a21abb
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2024-07-23 11:46:50 +0200

    Corrige attribution OpenStreetMap (#4078)

--- apps/transport/client/javascripts/mapbox-credentials.js
+++ apps/transport/client/javascripts/mapbox-credentials.js
@@ -1,7 +1,7 @@
 export const Mapbox = {
     url: 'https://api.mapbox.com/styles/v1/transport-pan/clj8j9fla009701pie4nrfo62/tiles/{tileSize}/{z}/{x}/{y}?access_token={accessToken}',
     accessToken: 'pk.eyJ1IjoidHJhbnNwb3J0LXBhbiIsImEiOiJjbGo4anJodWUxOXY0M3BxeWo3bHlrMXoxIn0.qFfjiswVf2TaLQ2YmB-Mnw',
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors <a href="https://spdx.org/licenses/ODbL-1.0.html">ODbL</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors <a href="https://spdx.org/licenses/ODbL-1.0.html">ODbL</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 20,
     tileSize: 512,
     zoomOffset: -1
