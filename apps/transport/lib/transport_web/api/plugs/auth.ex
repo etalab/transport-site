8fa81574ac7ad67ae130e71ed60bbd1726d2a5be
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2024-10-10 16:04:37 +0200

    API : proxy et auth pour l'API du validateur GTFS transport

---
+++ apps/transport/lib/transport_web/api/plugs/auth.ex
@@ -0,0 +1,36 @@
+defmodule TransportWeb.API.Plugs.Auth do
+  @moduledoc """
+  A very simple plug handling authorization for HTTP requests.
+  It gets the list of (client, token) from an environment variable and does not check quotas.
+  """
+  import Plug.Conn
+
+  def init(default), do: default
+
+  def call(%Plug.Conn{} = conn, _) do
+    case find_client(get_req_header(conn, "authorization")) do
+      {:ok, client} ->
+        conn |> assign(:client, client)
+
+      :error ->
+        conn
+        |> put_status(:unauthorized)
+        |> Phoenix.Controller.json(%{error: "You must set a valid Authorization header"})
+        |> halt()
+    end
+  end
+
+  defp find_client([authorization]) do
+    # Expected format: `client1:secret_token;client2:other_token`
+    Application.fetch_env!(:transport, :api_auth_clients)
+    |> String.split(";")
+    |> Enum.map(&(&1 |> String.split(":") |> List.to_tuple()))
+    |> Enum.find_value(:error, fn {client, secret} ->
+      if Plug.Crypto.secure_compare(authorization, secret) do
+        {:ok, client}
+      end
+    end)
+  end
+
+  defp find_client(_), do: :error
+end
