dd99d43b1fb3ed3ea5e935575a7c6f6e93579114
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-10-29 18:49:01 +0100

    Leverage IRVE schema to build Explorer dtypes

--- scripts/irve/data-frame.exs
+++ scripts/irve/data-frame.exs
@@ -5,11 +5,35 @@ IO.puts("OK")
 
 sample_url = "https://www.data.gouv.fr/fr/datasets/r/e9bb3424-77cd-40ba-8bbd-5a19362d0365"
 
+# Note: cached in development if you set `irve_consolidation_caching: true` in `dev.secret.exs`
 %Req.Response{status: 200, body: body} =
   Transport.IRVE.Fetcher.get!(sample_url, compressed: false, decode_body: false)
 
-df = Explorer.DataFrame.load_csv!(body)
+schema_file = "apps/shared/meta/schema-irve-statique.json"
+
+# temporary mapper
+type_mapper = fn
+  # TODO: extract individual coordinates
+  :geopoint -> :string
+  # works for this specific case
+  :number -> {:u, 16}
+  type -> type
+end
+
+dtypes =
+  schema_file
+  |> File.read!()
+  |> Jason.decode!()
+  |> Map.fetch!("fields")
+  |> Enum.map(fn %{"name" => name, "type" => type} ->
+    {String.to_atom(name), String.to_atom(type) |> type_mapper.()}
+  end)
+
+IO.inspect(dtypes, IEx.inspect_opts())
+
+df =
+  Explorer.DataFrame.load_csv!(body,
+    dtypes: dtypes
+  )
 
 IO.inspect(df, IEx.inspect_opts())
-
-# Explorer.DataFrame.new()
