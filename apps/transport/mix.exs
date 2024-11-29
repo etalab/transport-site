705366510a26f526fdc27f38ecc83ff69e3779bf
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-11-29 07:46:32 +0100

    mix deps.update ecto_interval ecto_sql ecto postgrex ecto_psql_extras

--- apps/transport/mix.exs
+++ apps/transport/mix.exs
@@ -115,8 +115,8 @@ defmodule Transport.Mixfile do
       {:nimble_csv, "~> 1.2.0"},
       {:kino, "~> 0.6", only: :dev},
       # db
-      {:ecto, "~> 3.7"},
-      {:ecto_sql, "~> 3.7"},
+      {:ecto, "~> 3.12"},
+      {:ecto_sql, "~> 3.12"},
       {:postgrex, ">= 0.0.0"},
       {:geo_postgis, "~> 3.4"},
       {:ecto_interval, "~> 0.2.5"},
