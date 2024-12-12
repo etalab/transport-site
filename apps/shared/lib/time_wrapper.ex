44bb6296d6151d32511ddc547a6892d42269fd9a
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-12-12 20:55:47 +0100

    Add wrapper for "{YYYY}{0M}{0D}"

--- apps/shared/lib/time_wrapper.ex
+++ apps/shared/lib/time_wrapper.ex
@@ -2,4 +2,8 @@ defmodule TimeWrapper do
   def parse!(date_as_string, param = "{ISO:Extended}") do
     Timex.parse!(date_as_string, param)
   end
+
+  def parse!(date_as_string, param = "{YYYY}{0M}{0D}") do
+    Timex.parse!(date_as_string, param)
+  end
 end
