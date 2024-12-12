bbcd6478e8289fbee8cb79da15ba7a7fce06f0d9
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-12-12 20:57:51 +0100

    Move test calls

--- apps/shared/lib/time_wrapper.ex
+++ apps/shared/lib/time_wrapper.ex
@@ -6,4 +6,12 @@ defmodule TimeWrapper do
   def parse!(date_as_string, param = "{YYYY}{0M}{0D}") do
     Timex.parse!(date_as_string, param)
   end
+
+  def parse!(datetime_as_string, param = "{WDshort}, {D} {Mshort} {YYYY} {h24}:{m}:{s} GMT") do
+    Timex.parse!(datetime_as_string, param)
+  end
+
+  def diff(first, second, param = :hours) do
+    Timex.diff(first, second, param)
+  end
 end
