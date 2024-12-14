86016c3edab588fe842ebcbc0459012483298bdb
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-12-14 11:20:12 +0100

    Fix credo warnings

--- apps/shared/lib/time_wrapper.ex
+++ apps/shared/lib/time_wrapper.ex
@@ -1,22 +1,22 @@
 defmodule TimeWrapper do
-  def parse!(date_as_string, param = "{ISO:Extended}") do
+  def parse!(date_as_string, "{ISO:Extended}" = param) do
     Timex.parse!(date_as_string, param)
   end
 
-  def parse!(date_as_string, param = "{YYYY}{0M}{0D}") do
+  def parse!(date_as_string, "{YYYY}{0M}{0D}" = param) do
     Timex.parse!(date_as_string, param)
   end
 
   # TODO: remove completely. This is rfc2822 ; Plug encodes it, but there is no built-in decoder.
-  def parse!(datetime_as_string, param = "{WDshort}, {D} {Mshort} {YYYY} {h24}:{m}:{s} GMT") do
+  def parse!(datetime_as_string, "{WDshort}, {D} {Mshort} {YYYY} {h24}:{m}:{s} GMT" = param) do
     Timex.parse!(datetime_as_string, param)
   end
 
-  def diff(first, second, param = :hours) do
+  def diff(first, second, :hours = param) do
     Timex.diff(first, second, param)
   end
 
-  def now() do
+  def now do
     Timex.now()
   end
 
