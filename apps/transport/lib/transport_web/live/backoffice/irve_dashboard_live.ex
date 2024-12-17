5f442d10656df3994150a7e00e34dc1685fcbc2f
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-12-17 11:23:12 +0100

    Fix exception, field is nil for resources in 404

--- apps/transport/lib/transport_web/live/backoffice/irve_dashboard_live.ex
+++ apps/transport/lib/transport_web/live/backoffice/irve_dashboard_live.ex
@@ -3,7 +3,7 @@ defmodule TransportWeb.Backoffice.IRVEDashboardLive do
   use Phoenix.HTML
   import TransportWeb.Backoffice.JobsLive, only: [ensure_admin_auth_or_redirect: 3]
   import TransportWeb.Router.Helpers
-  import Helpers, only: [format_number: 1]
+  import Helpers, only: [format_number_maybe_nil: 2]
   import Ecto.Query, only: [last: 2]
 
   @impl true
