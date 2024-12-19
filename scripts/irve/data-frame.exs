d8103195b0504b4bc18c0c040b37d0b9b4915f26
Thibaut Barrère <thibaut.barrere@gmail.com>
2024-12-19 01:18:30 +0100

    Map most fields

--- scripts/irve/data-frame.exs
+++ scripts/irve/data-frame.exs
@@ -59,9 +59,69 @@ defmodule Demo do
       end
 
       df =
-        Transport.IRVE.DataFrame.dataframe_from_csv_body!(body, Transport.IRVE.StaticIRVESchema.schema_content(), false)
+        # TODO: preprocess/conform booleans (`0` -> `FALSE`) so that we can use `strict = true`
+        # TODO: be smooth about `cable_t2_attache` - only added in v2.1.0 (https://github.com/etalab/schema-irve/releases/tag/v2.1.0)
+        # and often not provided
+        Transport.IRVE.DataFrame.dataframe_from_csv_body!(body, Transport.IRVE.StaticIRVESchema.schema_content(), _strict = false)
         |> Transport.IRVE.DataFrame.preprocess_data()
-        |> Explorer.DataFrame.select(["id_pdc_itinerance", "x", "y"])
+        |> Transport.IRVE.DataFrame.preprocess_boolean("prise_type_ef")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("prise_type_2")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("prise_type_combo_ccs")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("prise_type_chademo")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("prise_type_autre")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("gratuit")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("paiement_acte")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("paiement_cb")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("paiement_autre")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("reservation")
+        |> Transport.IRVE.DataFrame.preprocess_boolean("station_deux_roues")
+        |> Explorer.DataFrame.select(
+          [
+            "nom_amenageur",
+            "siren_amenageur",
+            "contact_amenageur",
+            "nom_operateur",
+            "contact_operateur",
+            "telephone_operateur",
+            "nom_enseigne",
+            "id_station_itinerance",
+            "id_station_local",
+            "nom_station",
+            "implantation_station",
+            "adresse_station",
+            "code_insee_commune",
+            # "coordonneesXY",
+            "nbre_pdc",
+            "id_pdc_itinerance",
+            "id_pdc_local",
+            "puissance_nominale",
+            "prise_type_ef",
+            "prise_type_2",
+            "prise_type_combo_ccs",
+            "prise_type_chademo",
+            "prise_type_autre",
+            "gratuit",
+            "paiement_acte",
+            "paiement_cb",
+            "paiement_autre",
+            "tarification",
+            "condition_acces",
+            "reservation",
+            "horaires",
+            "accessibilite_pmr",
+            "restriction_gabarit",
+            "station_deux_roues",
+            "raccordement",
+            "num_pdl",
+            "date_mise_en_service",
+            "observations",
+            "date_maj",
+            # "cable_t2_attache",
+            # extracted
+            "x",
+            "y"
+          ]
+        )
 
       nil_counts = Explorer.DataFrame.nil_count(df)
       nil_counts = {
