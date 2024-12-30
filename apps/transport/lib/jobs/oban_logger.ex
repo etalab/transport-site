10afef72745fef08cda56adee7903bd6cc3c2f6d
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2024-12-30 19:24:34 +0100

    Oban : logs des composants importants

--- apps/transport/lib/jobs/oban_logger.ex
+++ apps/transport/lib/jobs/oban_logger.ex
@@ -1,8 +1,14 @@
 defmodule Transport.Jobs.ObanLogger do
   @moduledoc """
-  Logs the Oban job exceptions as warnings
+  Setup telemetry/logging for Oban.
+
+  We:
+  - log job exceptions as warnings
+  - log Oban events related to the orchestration (notifier, queues, plugins etc.)
+  - we send an email when a job failed after its maximum attempt for jobs with a specific tag
   """
   require Logger
+
   @tag_email_on_failure "email_on_failure"
 
   @doc """
@@ -35,5 +41,22 @@ defmodule Transport.Jobs.ObanLogger do
     )
   end
 
-  def setup, do: :telemetry.attach("oban-logger", [:oban, :job, :exception], &handle_event/4, nil)
+  def setup do
+    :telemetry.attach("oban-logger", [:oban, :job, :exception], &handle_event/4, nil)
+
+    # Log recommended events for production.
+    # We leave out `job` events because job start/end can be quite noisy.
+    # https://hexdocs.pm/oban/preparing_for_production.html#logging
+    # https://hexdocs.pm/oban/Oban.Telemetry.html
+    # We may simplify this when
+    # https://github.com/oban-bg/oban/commit/13eabe3f8019e350ef979369a26f186bdf7be63e
+    # will be released.
+    events = [
+      [:oban, :notifier, :switch],
+      [:oban, :queue, :shutdown],
+      [:oban, :stager, :switch]
+    ]
+
+    :telemetry.attach_many("oban-default-logger", events, &Oban.Telemetry.handle_event/4, encode: true, level: :info)
+  end
 end
