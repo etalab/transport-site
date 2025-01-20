4979628300ca66c62f201c6802bb363d275f557f
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-01-20 22:25:51 +0100

    Tell what is happening

--- scripts/irve/data-frame.exs
+++ scripts/irve/data-frame.exs
@@ -194,15 +194,21 @@ defmodule Demo do
         }
       end)
 
+    consolidation_filename = "consolidation.csv"
+    Logger.info "Generating #{consolidation_filename}"
+
     output.df
-    |> Explorer.DataFrame.to_csv!("consolidation.csv")
+    |> Explorer.DataFrame.to_csv!(consolidation_filename)
+
+    report_filename = "report.csv"
+    Logger.info "Generating #{report_filename}"
 
     output.report
     |> Enum.reverse()
     |> Enum.map(&Map.from_struct/1)
     |> Enum.map(fn x -> Map.put(x, :error, x.error |> inspect) end)
     |> Explorer.DataFrame.new()
-    |> Explorer.DataFrame.to_csv!("report.csv")
+    |> Explorer.DataFrame.to_csv!(report_filename)
   end
 end
 
