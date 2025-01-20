b09a5bfc305efcc236326aa699f1200aa42c22bf
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-01-20 21:48:03 +0100

    Mix format

--- scripts/irve/data-frame.exs
+++ scripts/irve/data-frame.exs
@@ -58,11 +58,15 @@ defmodule Demo do
         raise("content has no id_pdc_itinerance in first line")
       end
 
+      # TODO: preprocess/conform booleans (`0` -> `FALSE`) so that we can use `strict = true`
+      # TODO: be smooth about `cable_t2_attache` - only added in v2.1.0 (https://github.com/etalab/schema-irve/releases/tag/v2.1.0)
+      # and often not provided
       df =
-        # TODO: preprocess/conform booleans (`0` -> `FALSE`) so that we can use `strict = true`
-        # TODO: be smooth about `cable_t2_attache` - only added in v2.1.0 (https://github.com/etalab/schema-irve/releases/tag/v2.1.0)
-        # and often not provided
-        Transport.IRVE.DataFrame.dataframe_from_csv_body!(body, Transport.IRVE.StaticIRVESchema.schema_content(), _strict = false)
+        Transport.IRVE.DataFrame.dataframe_from_csv_body!(
+          body,
+          Transport.IRVE.StaticIRVESchema.schema_content(),
+          _strict = false
+        )
         # TODO: rename accordingly
         |> Transport.IRVE.DataFrame.preprocess_data()
         # TODO: loop programmatically
@@ -77,60 +81,60 @@ defmodule Demo do
         |> Transport.IRVE.DataFrame.preprocess_boolean("paiement_autre")
         |> Transport.IRVE.DataFrame.preprocess_boolean("reservation")
         |> Transport.IRVE.DataFrame.preprocess_boolean("station_deux_roues")
-        |> Explorer.DataFrame.select(
-          [
-            "nom_amenageur",
-            "siren_amenageur",
-            "contact_amenageur",
-            "nom_operateur",
-            "contact_operateur",
-            "telephone_operateur",
-            "nom_enseigne",
-            "id_station_itinerance",
-            "id_station_local",
-            "nom_station",
-            "implantation_station",
-            "adresse_station",
-            "code_insee_commune",
-            # "coordonneesXY",
-            "nbre_pdc",
-            "id_pdc_itinerance",
-            "id_pdc_local",
-            "puissance_nominale",
-            "prise_type_ef",
-            "prise_type_2",
-            "prise_type_combo_ccs",
-            "prise_type_chademo",
-            "prise_type_autre",
-            "gratuit",
-            "paiement_acte",
-            "paiement_cb",
-            "paiement_autre",
-            "tarification",
-            "condition_acces",
-            "reservation",
-            "horaires",
-            "accessibilite_pmr",
-            "restriction_gabarit",
-            "station_deux_roues",
-            "raccordement",
-            "num_pdl",
-            "date_mise_en_service",
-            "observations",
-            "date_maj",
-            # "cable_t2_attache",
-            # extracted
-            "x",
-            "y"
-          ]
-        )
+        |> Explorer.DataFrame.select([
+          "nom_amenageur",
+          "siren_amenageur",
+          "contact_amenageur",
+          "nom_operateur",
+          "contact_operateur",
+          "telephone_operateur",
+          "nom_enseigne",
+          "id_station_itinerance",
+          "id_station_local",
+          "nom_station",
+          "implantation_station",
+          "adresse_station",
+          "code_insee_commune",
+          # "coordonneesXY",
+          "nbre_pdc",
+          "id_pdc_itinerance",
+          "id_pdc_local",
+          "puissance_nominale",
+          "prise_type_ef",
+          "prise_type_2",
+          "prise_type_combo_ccs",
+          "prise_type_chademo",
+          "prise_type_autre",
+          "gratuit",
+          "paiement_acte",
+          "paiement_cb",
+          "paiement_autre",
+          "tarification",
+          "condition_acces",
+          "reservation",
+          "horaires",
+          "accessibilite_pmr",
+          "restriction_gabarit",
+          "station_deux_roues",
+          "raccordement",
+          "num_pdl",
+          "date_mise_en_service",
+          "observations",
+          "date_maj",
+          # "cable_t2_attache",
+          # extracted
+          "x",
+          "y"
+        ])
 
       nil_counts = Explorer.DataFrame.nil_count(df)
+
       nil_counts = {
         Explorer.Series.at(nil_counts[:id_pdc_itinerance], 0),
         Explorer.Series.at(nil_counts[:x], 0),
         Explorer.Series.at(nil_counts[:y], 0)
       }
+
       unless nil_counts == {0, 0, 0} do
         IO.puts(row.url)
         IO.inspect(nil_counts, IEx.inspect_opts())
@@ -140,8 +144,9 @@ defmodule Demo do
     rescue
       error ->
         if String.contains?(error |> inspect, "KeyError") do
-          IO.inspect %{error: error, row: row}, IEx.inspect_opts
+          IO.inspect(%{error: error, row: row}, IEx.inspect_opts())
         end
+
         {:error, error}
     end
   end
@@ -155,32 +160,33 @@ defmodule Demo do
   end
 
   def show_more() do
-    output = Transport.IRVE.Extractor.datagouv_resources()
-    # exclude data gouv generated consolidation
-    |> Enum.reject(fn r -> r.dataset_organisation_id == "646b7187b50b2a93b1ae3d45" end)
-    |> Enum.sort_by(fn r -> [r.dataset_id, r.resource_id] end)
-    # |> Stream.drop(1001)
-    # |> Stream.take(10)
-    # |> Enum.filter(&(&1.resource_id == "cbd64933-26df-4ab5-b9e8-104f9af9a16c"))
-    |> Enum.reduce(%{df: nil, report: []}, fn row, %{df: main_df, report: report} ->
-      {main_df, error} =
-        case process_one(row) do
-          {:ok, df} -> {concat_rows(main_df, df), nil}
-          {:error, error} -> {main_df, error}
-        end
+    output =
+      Transport.IRVE.Extractor.datagouv_resources()
+      # exclude data gouv generated consolidation
+      |> Enum.reject(fn r -> r.dataset_organisation_id == "646b7187b50b2a93b1ae3d45" end)
+      |> Enum.sort_by(fn r -> [r.dataset_id, r.resource_id] end)
+      # |> Stream.drop(1001)
+      # |> Stream.take(10)
+      # |> Enum.filter(&(&1.resource_id == "cbd64933-26df-4ab5-b9e8-104f9af9a16c"))
+      |> Enum.reduce(%{df: nil, report: []}, fn row, %{df: main_df, report: report} ->
+        {main_df, error} =
+          case process_one(row) do
+            {:ok, df} -> {concat_rows(main_df, df), nil}
+            {:error, error} -> {main_df, error}
+          end
 
-      description = %ReportItem{
-        dataset_id: row.dataset_id,
-        resource_id: row.resource_id,
-        resource_url: row.url,
-        error: error
-      }
+        description = %ReportItem{
+          dataset_id: row.dataset_id,
+          resource_id: row.resource_id,
+          resource_url: row.url,
+          error: error
+        }
 
-      %{
-        df: main_df,
-        report: [description | report]
-      }
-    end)
+        %{
+          df: main_df,
+          report: [description | report]
+        }
+      end)
 
     output.df
     |> Explorer.DataFrame.to_csv!("consolidation.csv")
@@ -188,7 +194,7 @@ defmodule Demo do
     output.report
     |> Enum.reverse()
     |> Enum.map(&Map.from_struct/1)
-    |> Enum.map(fn(x) -> Map.put(x, :error, x.error |> inspect) end)
+    |> Enum.map(fn x -> Map.put(x, :error, x.error |> inspect) end)
     |> Explorer.DataFrame.new()
     |> Explorer.DataFrame.to_csv!("report.csv")
   end
