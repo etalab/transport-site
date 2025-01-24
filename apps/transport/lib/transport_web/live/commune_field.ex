5942eeba39af61c51d97b8b0d817b78d6478b0d8
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-01-24 17:40:44 +0100

    Move commune field to live component

--- apps/transport/lib/transport_web/live/commune_field.ex
+++ apps/transport/lib/transport_web/live/commune_field.ex
@@ -2,36 +2,33 @@ defmodule TransportWeb.Live.CommuneField do
   @moduledoc """
   Field with autocomplete to find INSEE code
   """
-  use Phoenix.LiveView
+  use Phoenix.LiveComponent
   alias Transport.SearchCommunes
   alias TransportWeb.InputHelpers
 
   def render(assigns) do
     ~H"""
-    <%= InputHelpers.text_input(@form, :insee,
-      placeholder: "Commune faisant partie de l'AOM (code INSEE ou nom)",
-      value: @insee,
-      phx_keyup: "suggest",
-      list: "matches",
-      autocomplete: "off",
-      id: "communes_q"
-    ) %>
-    <datalist id="matches">
-      <%= for match <- @matches do %>
-        <option value={match.insee}><%= "#{match.nom} #{match.insee}" %></option>
-      <% end %>
-    </datalist>
+    <div>
+      <%= InputHelpers.text_input(@form, :insee,
+        placeholder: "Commune faisant partie de l'AOM (code INSEE ou nom)",
+        value: @insee,
+        phx_keyup: "suggest",
+        list: "matches",
+        autocomplete: "off",
+        id: "communes_q",
+        phx_target: @myself
+      ) %>
+      <datalist id="matches">
+        <%= for match <- @matches do %>
+          <option value={match.insee}><%= "#{match.nom} #{match.insee}" %></option>
+        <% end %>
+      </datalist>
+    </div>
     """
   end
 
-  def mount(_params, session, socket) do
-    assigns =
-      socket
-      |> assign(matches: [])
-      |> assign(insee: session["insee"])
-      |> assign(form: session["form"])
-
-    {:ok, assigns}
+  def mount(socket) do
+    {:ok, socket |> assign(matches: [])}
   end
 
   def handle_event("suggest", %{"value" => query}, socket) when byte_size(query) <= 100 do
