ce03a9618ea96e5fdc7d43c35ebe56998900725a
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-03-18 16:23:27 +0100

    Remove dead code

--- scripts/irve/data-frame.exs
+++ scripts/irve/data-frame.exs
@@ -1,19 +1,4 @@
 defmodule Demo do
-  # https://www.data.gouv.fr/fr/datasets/623ca46c13130c3228abd018/ - Electra dataset (mid-sized)
-  # https://www.data.gouv.fr/fr/datasets/623ca46c13130c3228abd018/#/resources/e9bb3424-77cd-40ba-8bbd-5a19362d0365
-
-  @sample_url "https://www.data.gouv.fr/fr/datasets/r/e9bb3424-77cd-40ba-8bbd-5a19362d0365"
-
-  def show_one() do
-    # Note: cached in development if you set `irve_consolidation_caching: true` in `dev.secret.exs`
-    %Req.Response{status: 200, body: body} =
-      Transport.IRVE.Fetcher.get!(@sample_url, compressed: false, decode_body: false)
-
-    Transport.IRVE.DataFrame.dataframe_from_csv_body!(body)
-    |> Explorer.DataFrame.select("id_pdc_itinerance")
-    |> IO.inspect(IEx.inspect_opts())
-  end
-
   def first_line(body) do
     body
     |> String.split("\n", parts: 2)
@@ -246,10 +231,6 @@ defmodule Demo do
     |> Explorer.DataFrame.to_csv!(report_filename)
   end
 end
-
-# IO.puts("========== just one sample ==========")
-
-# Demo.show_one()
 
 # IO.puts("========== go further ==========")
 
