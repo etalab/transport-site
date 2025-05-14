c6ae401d3d4ca4d4ab896e99f52b5132c3e4dded
Frédéric Menou <frederic.menou@transport.data.gouv.fr>
2025-05-14 13:57:31 +0200

    Validation NeTEx: mise à jour du rule_set de base
    Le rule_set utilisé date de 2024, avant la publication de l'API de définition de
    règles.
    
    Il est désormais possible d'en définir de personnelles mais avant de s'engager
    sur ce chemin long et difficile il est possible d'utiliser celui basique fourni
    par enRoute, nommé [enroute:starter-kit].
    
    Puisqu'il existe désormais des règles utiles :
    - les erreurs autres que XSD ne sont plus converties en avertissements
    - la validation XSD n'est plus demandée
    
    De ces premières règles, une d'entre elle est ignorée car jugée peu pertinente
    en l'état : `public-code-length` `"A Public Code have 12 or less than characters"`.
    
    [enroute:starter-kit]: https://enroute.atlassian.net/wiki/spaces/PUBLIC/pages/2761687047/Sprint+123#Provide-a-NeTEx-Starter-Kit-ruleset

--- apps/transport/test/transport/jobs/netex_poller_job_test.exs
+++ apps/transport/test/transport/jobs/netex_poller_job_test.exs
@@ -93,14 +93,14 @@ defmodule Transport.Jobs.NeTExPollerJobTest do
                %{
                  "code" => "uic-operating-period",
                  "message" => "Resource 23504000009 hasn't expected class but Netex::OperatingPeriod",
-                 "criticity" => "warning"
+                 "criticity" => "error"
                }
              ],
              "valid-day-bits" => [
                %{
                  "code" => "valid-day-bits",
                  "message" => "Mandatory attribute valid_day_bits not found",
-                 "criticity" => "warning"
+                 "criticity" => "error"
                }
              ],
              "frame-arret-resources" => [
@@ -113,7 +113,7 @@ defmodule Transport.Jobs.NeTExPollerJobTest do
              "unknown-code" => [
                %{
                  "message" => "Reference MOBIITI:Quay:104325 doesn't match any existing Resource",
-                 "criticity" => "warning"
+                 "criticity" => "error"
                }
              ]
            }
--- apps/transport/test/transport/validators/enroute_chouette_valid_client_test.exs
+++ apps/transport/test/transport/validators/enroute_chouette_valid_client_test.exs
@@ -15,9 +15,9 @@ defmodule Transport.EnRouteChouetteValidClientTest do
       """
       {
         "id": "d8e2b6c2-b1e5-4890-84d4-9b761a445882",
-        "rule_set": "french",
+        "rule_set": "enroute:starter-kit",
         "user_status": "pending",
-        "include_schema": true,
+        "include_schema": false,
         "created_at": "2024-07-05T14:41:19.933Z",
         "updated_at": "2024-07-05T14:41:19.933Z"
       }
@@ -31,8 +31,8 @@ defmodule Transport.EnRouteChouetteValidClientTest do
       assert @expected_headers == headers
 
       assert [
-               {"validation[rule_set]", "french"},
-               {"validation[include_schema]", "true"},
+               {"validation[rule_set]", "enroute:starter-kit"},
+               {"validation[include_schema]", "false"},
                {:file, tmp_file, {"form-data", [{:name, "validation[file]"}, {:filename, Path.basename(tmp_file)}]}, []}
              ] == parts
 
@@ -50,9 +50,9 @@ defmodule Transport.EnRouteChouetteValidClientTest do
         """
         {
           "id": "#{validation_id}",
-          "rule_set": "french",
+          "rule_set": "enroute:starter-kit",
           "user_status": "pending",
-          "include_schema": true,
+          "include_schema": false,
           "started_at": "2024-07-05T14:41:20.680Z",
           "created_at": "2024-07-05T14:41:19.933Z",
           "updated_at": "2024-07-05T14:41:20.933Z"
@@ -76,9 +76,9 @@ defmodule Transport.EnRouteChouetteValidClientTest do
         """
         {
           "id": "#{validation_id}",
-          "rule_set": "french",
+          "rule_set": "enroute:starter-kit",
           "user_status": "successful",
-          "include_schema": true,
+          "include_schema": false,
           "started_at": "2024-07-05T14:41:20.680Z",
           "ended_at": "2024-07-05T14:41:25.685Z",
           "created_at": "2024-07-05T14:41:19.933Z",
@@ -103,9 +103,9 @@ defmodule Transport.EnRouteChouetteValidClientTest do
         """
         {
           "id": "#{validation_id}",
-          "rule_set": "french",
+          "rule_set": "enroute:starter-kit",
           "user_status": "warning",
-          "include_schema": true,
+          "include_schema": false,
           "started_at": "2024-07-05T14:41:20.680Z",
           "ended_at": "2024-07-05T14:41:24.685Z",
           "created_at": "2024-07-05T14:41:19.933Z",
@@ -130,9 +130,9 @@ defmodule Transport.EnRouteChouetteValidClientTest do
         """
         {
           "id": "#{validation_id}",
-          "rule_set": "french",
+          "rule_set": "enroute:starter-kit",
           "user_status": "failed",
-          "include_schema": true,
+          "include_schema": false,
           "started_at": "2024-07-05T14:41:20.680Z",
           "ended_at": "2024-07-05T14:41:28.685Z",
           "created_at": "2024-07-05T14:41:19.933Z",
