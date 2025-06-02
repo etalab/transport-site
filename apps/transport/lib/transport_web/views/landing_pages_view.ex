0f70eeed594cce69d9e3b86e4225a28deacd4e4d
Frédéric Menou <frederic.menou@transport.data.gouv.fr>
2025-06-02 16:25:57 +0200

    English version

--- apps/transport/lib/transport_web/views/landing_pages_view.ex
+++ apps/transport/lib/transport_web/views/landing_pages_view.ex
@@ -1,3 +1,24 @@
 defmodule TransportWeb.LandingPagesView do
   use TransportWeb, :view
+
+  def gbfs_abbreviation(text) do
+    text |> abbreviation("GBFS", "Standard General Bikeshare Feed Specification")
+  end
+
+  def aoms_abbreviation(text) do
+    aoms = dgettext("landing-vls", "Autorités Organisatrices de la Mobilité: Mobility Organizing Authorities")
+    text |> abbreviation("AOMs", aoms)
+  end
+
+  defp abbreviation(text, abbreviation, explanation) do
+    String.replace(text, abbreviation, "<abbr class=\"inline-help\" title=\"#{explanation}\">#{abbreviation}</abbr>")
+  end
+
+  def homepage_link(text) do
+    String.replace(
+      text,
+      "transport.data.gouv.fr",
+      "<a href=\"https://transport.data.gouv.fr\">transport.data.gouv.fr</a>"
+    )
+  end
 end
