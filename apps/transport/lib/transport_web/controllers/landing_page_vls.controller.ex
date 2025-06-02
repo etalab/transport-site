b41b8f6afc2c930ec5384352733f5a82b284217a
Frédéric Menou <frederic.menou@transport.data.gouv.fr>
2025-06-02 17:50:39 +0200

    Use contact email from configuration

--- apps/transport/lib/transport_web/controllers/landing_page_vls.controller.ex
+++ apps/transport/lib/transport_web/controllers/landing_page_vls.controller.ex
@@ -4,6 +4,7 @@ defmodule TransportWeb.LandingPagesController do
   def vls(conn, _params) do
     conn
     |> assign(:seo_page, "vls")
+    |> assign(:contact_email, Application.fetch_env!(:transport, :contact_email))
     |> render("vls.html")
   end
 end
