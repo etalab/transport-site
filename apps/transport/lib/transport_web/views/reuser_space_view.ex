a4bbc076c892a79a76c61fd62ab519aa9bd60504
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2025-06-02 09:25:22 -0400

    Remove blank line

--- apps/transport/lib/transport_web/views/reuser_space_view.ex
+++ apps/transport/lib/transport_web/views/reuser_space_view.ex
@@ -2,7 +2,6 @@ defmodule TransportWeb.ReuserSpaceView do
   use TransportWeb, :view
   import TransportWeb.BreadCrumbs, only: [breadcrumbs: 1]
 
-
   def default_for_contact?(%DB.Token{default_for_contact_id: default_for_contact_id}, %DB.Contact{id: contact_id}) do
     default_for_contact_id == contact_id
   end
