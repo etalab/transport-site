41f12729264bd24b21072faa09cf36c0b7b7a06d
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-06-03 16:16:30 +0200

    Document script sampling mode

--- scripts/irve/data-frame.exs
+++ scripts/irve/data-frame.exs
@@ -1,3 +1,7 @@
+# mix run scripts/irve/data-frame.exs
+#
+# LIMIT=5 mix run scripts/irve/data-frame.exs
+
 limit = System.get_env("LIMIT") |> then(&if &1, do: String.to_integer(&1))
 
 Transport.Jobs.IRVEConsolidationJob.perform(%Oban.Job{args: %{limit: limit}})
