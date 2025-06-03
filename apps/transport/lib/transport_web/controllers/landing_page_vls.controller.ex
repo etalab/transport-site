d8d211df285c8663c3d9ee96717b8e1bedeab755
Frédéric Menou <frederic.menou@transport.data.gouv.fr>
2025-06-03 12:17:17 +0200

    Décompte de ressources GBFS plutôt

--- apps/transport/lib/transport_web/controllers/landing_page_vls.controller.ex
+++ apps/transport/lib/transport_web/controllers/landing_page_vls.controller.ex
@@ -5,6 +5,24 @@ defmodule TransportWeb.LandingPagesController do
     conn
     |> assign(:seo_page, "vls")
     |> assign(:contact_email, Application.fetch_env!(:transport, :contact_email))
-    |> render("vls.html")
+    |> render("vls.html", statistics())
+  end
+
+  defp statistics do
+    Transport.Cache.fetch(
+      "landing-vls-stats",
+      fn -> compute_statistics() end,
+      :timer.hours(12)
+    )
+  end
+
+  defp compute_statistics do
+    # For now it's computed by hand
+    national_coverage = 70
+
+    datasets = DB.Dataset.count_by_type("bike-scooter-sharing") + DB.Dataset.count_by_type("car-motorbike-sharing")
+    resources = DB.Resource.count_by_format("gbfs")
+
+    %{datasets: datasets, resources: resources, national_coverage: national_coverage}
   end
 end
--- apps/transport/test/transport_web/controllers/landing_page_vls_controller_test.exs
+++ apps/transport/test/transport_web/controllers/landing_page_vls_controller_test.exs
@@ -1,6 +1,10 @@
 defmodule TransportWeb.LandingPageVLSControllerTest do
   use TransportWeb.ConnCase, async: true
 
+  setup do
+    Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+  end
+
   test "GET /landing-vls", %{conn: conn} do
     conn = conn |> get(~p"/landing-vls")
 
