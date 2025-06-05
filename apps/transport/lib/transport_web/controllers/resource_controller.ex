755905850d43e637faf2a15919ad900c4a5f604c
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2025-06-05 12:51:56 +0200

    Téléchargement avec URLs authentifiées pour ressources PAN (#4622)

--- apps/transport/lib/transport_web/controllers/resource_controller.ex
+++ apps/transport/lib/transport_web/controllers/resource_controller.ex
@@ -218,47 +218,91 @@ defmodule TransportWeb.ResourceController do
   they are referenced on an HTTPS page.
   """
   def download(%Plug.Conn{assigns: %{original_method: "HEAD"}} = conn, %{"id" => id}) do
-    resource = Resource |> Repo.get!(id)
+    resource = DB.Resource |> DB.Repo.get!(id) |> DB.Repo.preload(:dataset)
 
-    if Resource.can_direct_download?(resource) do
-      # should not happen
-      # if direct download is possible, we don't expect the function `download` to be called
-      conn |> Plug.Conn.send_resp(:not_found, "")
-    else
-      case Transport.Shared.Wrapper.HTTPoison.impl().head(resource.url, []) do
-        {:ok, %HTTPoison.Response{status_code: status_code, headers: headers}} ->
-          send_head_response(conn, status_code, headers)
+    cond do
+      DB.Resource.pan_resource?(resource) ->
+        conn |> Plug.Conn.send_resp(:ok, "")
 
-        _ ->
-          conn |> Plug.Conn.send_resp(:bad_gateway, "")
-      end
+      DB.Resource.can_direct_download?(resource) ->
+        conn |> Plug.Conn.send_resp(:not_found, "")
+
+      true ->
+        case Transport.Shared.Wrapper.HTTPoison.impl().head(resource.url, []) do
+          {:ok, %HTTPoison.Response{status_code: status_code, headers: headers}} ->
+            send_head_response(conn, status_code, headers)
+
+          _ ->
+            conn |> Plug.Conn.send_resp(:bad_gateway, "")
+        end
     end
   end
 
-  def download(conn, %{"id" => id}) do
-    resource = Resource |> Repo.get!(id)
+  def download(%Plug.Conn{method: "GET"} = conn, %{"id" => id}) do
+    resource = DB.Resource |> DB.Repo.get!(id) |> DB.Repo.preload(:dataset)
 
-    if Resource.can_direct_download?(resource) do
-      redirect(conn, external: resource.url)
-    else
-      case Transport.Shared.Wrapper.HTTPoison.impl().get(resource.url, [], hackney: [follow_redirect: true]) do
-        {:ok, %HTTPoison.Response{status_code: 200} = response} ->
-          headers = Enum.into(response.headers, %{}, &downcase_header(&1))
-          %{"content-type" => content_type} = headers
+    cond do
+      DB.Resource.pan_resource?(resource) ->
+        download_pan_resource(conn, resource)
 
-          send_download(conn, {:binary, response.body},
-            content_type: content_type,
-            disposition: :attachment,
-            filename: Transport.FileDownloads.guess_filename(headers, resource.url)
-          )
+      DB.Resource.can_direct_download?(resource) ->
+        redirect(conn, external: resource.url)
 
-        _ ->
-          conn
-          |> put_flash(:error, dgettext("resource", "Resource is not available on remote server"))
-          |> put_status(:not_found)
-          |> put_view(ErrorView)
-          |> render("404.html")
+      true ->
+        case Transport.Shared.Wrapper.HTTPoison.impl().get(resource.url, [], hackney: [follow_redirect: true]) do
+          {:ok, %HTTPoison.Response{status_code: 200} = response} ->
+            headers = Enum.into(response.headers, %{}, &downcase_header(&1))
+            %{"content-type" => content_type} = headers
+
+            send_download(conn, {:binary, response.body},
+              content_type: content_type,
+              disposition: :attachment,
+              filename: Transport.FileDownloads.guess_filename(headers, resource.url)
+            )
+
+          _ ->
+            conn
+            |> put_flash(:error, dgettext("resource", "Resource is not available on remote server"))
+            |> put_status(:not_found)
+            |> put_view(ErrorView)
+            |> render("404.html")
+        end
+    end
+  end
+
+  defp download_pan_resource(%Plug.Conn{} = conn, %DB.Resource{} = resource) do
+    case find_token(Map.get(conn.query_params, "token")) do
+      {:ok, token} ->
+        log_download_request(resource, token)
+        redirect(conn, external: resource.latest_url)
+
+      :error ->
+        conn |> Plug.Conn.send_resp(:unauthorized, "You must set a valid Authorization header")
+    end
+  end
+
+  defp log_download_request(%DB.Resource{id: resource_id}, token) do
+    token_id =
+      case token do
+        %DB.Token{id: token_id} -> token_id
+        nil -> nil
       end
+
+    %DB.ResourceDownload{}
+    |> Ecto.Changeset.change(%{
+      time: DateTime.utc_now(),
+      resource_id: resource_id,
+      token_id: token_id
+    })
+    |> DB.Repo.insert!()
+  end
+
+  defp find_token(nil), do: {:ok, nil}
+
+  defp find_token(secret_hash) do
+    case DB.Repo.get_by(DB.Token, secret_hash: secret_hash) do
+      %DB.Token{} = token -> {:ok, token}
+      nil -> :error
     end
   end
 
