6a582227f9387fdc50b9e3a319c79e14c00eca61
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2025-06-18 15:25:15 +0200

    Log feature usage

---
+++ apps/transport/lib/db/feature_usage.ex
@@ -0,0 +1,36 @@
+defmodule DB.FeatureUsage do
+  @moduledoc """
+  Logs when a feature has been used by a contact with metadata.
+  """
+  use Ecto.Schema
+  use TypedEctoSchema
+
+  @primary_key false
+
+  typed_schema "feature_usage" do
+    field(:time, :utc_datetime_usec)
+
+    field(:feature, Ecto.Enum,
+      values: [
+        :on_demand_validation,
+        :download_resource_history,
+        :post_discussion,
+        :post_comment
+      ]
+    )
+
+    field(:metadata, :map)
+    belongs_to(:contact, DB.Contact)
+  end
+
+  def insert!(feature, contact_id, metadata) do
+    %__MODULE__{}
+    |> Ecto.Changeset.change(%{
+      feature: feature,
+      contact_id: contact_id,
+      metadata: metadata,
+      time: DateTime.utc_now()
+    })
+    |> DB.Repo.insert!()
+  end
+end
--- apps/transport/lib/transport_web/controllers/discussion_controller.ex
+++ apps/transport/lib/transport_web/controllers/discussion_controller.ex
@@ -3,6 +3,9 @@ defmodule TransportWeb.DiscussionController do
   alias Datagouvfr.Client.Discussions.Wrapper, as: Discussions
   require Logger
 
+  plug(:assign_dataset)
+  plug(:assign_current_contact)
+
   @spec post_discussion(Plug.Conn.t(), map) :: Plug.Conn.t()
   def post_discussion(%Plug.Conn{} = conn, %{
         "comment" => comment,
@@ -10,6 +13,12 @@ defmodule TransportWeb.DiscussionController do
         "title" => title,
         "dataset_slug" => dataset_slug
       }) do
+    DB.FeatureUsage.insert!(
+      :post_discussion,
+      get_in(conn.assigns.current_contact.id),
+      %{dataset_id: conn.assigns.dataset.id}
+    )
+
     conn
     |> Discussions.post(dataset_datagouv_id, title, comment)
     |> case do
@@ -28,6 +37,12 @@ defmodule TransportWeb.DiscussionController do
         %Plug.Conn{} = conn,
         %{"discussion_id" => discussion_id, "comment" => comment, "dataset_slug" => dataset_slug} = params
       ) do
+    DB.FeatureUsage.insert!(
+      :post_comment,
+      get_in(conn.assigns.current_contact.id),
+      %{dataset_id: conn.assigns.dataset.id, close: Map.has_key?(params, "answer_and_close")}
+    )
+
     conn
     |> Discussions.post(discussion_id, comment, close: Map.has_key?(params, "answer_and_close"))
     |> case do
@@ -42,5 +57,26 @@ defmodule TransportWeb.DiscussionController do
         |> put_flash(:error, dgettext("page-dataset-details", "Unable to publish the answer"))
     end
     |> redirect(to: dataset_path(conn, :details, dataset_slug))
+  end
+
+  defp assign_dataset(%Plug.Conn{params: %{"dataset_slug" => dataset_slug}} = conn, _options) do
+    assign(
+      conn,
+      :dataset,
+      DB.Repo.get_by(DB.Dataset, slug: dataset_slug)
+    )
+  end
+
+  defp assign_current_contact(%Plug.Conn{assigns: %{current_user: current_user}} = conn, _options) do
+    current_contact =
+      if is_nil(current_user) do
+        nil
+      else
+        DB.Contact
+        |> DB.Repo.get_by!(datagouv_user_id: Map.fetch!(current_user, "id"))
+        |> DB.Repo.preload(:default_tokens)
+      end
+
+    assign(conn, :current_contact, current_contact)
   end
 end
