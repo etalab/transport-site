68e0b5e2ab78af0e85b4dd5103f1cb3efd5cda09
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-07-16 11:29:01 +0200

    moduledoc improvement
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>

--- apps/transport/lib/db/administrative_division.ex
+++ apps/transport/lib/db/administrative_division.ex
@@ -1,14 +1,21 @@
 defmodule DB.AdministrativeDivision do
   @moduledoc """
-  AdministrativeDivision schema, in French "Collectivité Territoriale".
-  This schema is used to represent the territorial divisions in France: communes, EPCI, departments and regions.
-  The schema for now doesn’t show relationships between territorial divisions, such as the relationship between a commune and its EPCI or department.
-  (But this relationship is available in the Commune schema, for example.)
-  It relies on a single table in the database, administrative_division.
-  The data is replicated (denormalized) from the dedicated commune, epci, departement and region tables.
+  AdministrativeDivision schema.
+  
+  This concept is used to represent various levels of divisions of the French territory:
+  - Communes
+  - Departments
+  - Regions
+  - [EPCIs](https://fr.wikipedia.org/wiki/Établissement_public_de_coopération_intercommunale)
+  - The whole country itself
+  - (more could actually be added here)
+  
+  Unlike pre-existing concepts (`DB.Commune`/`DB.Region`/`DB.EPCI`), `DB.AdministrativeDivision` does not currently include relationships between the various entities.
+
+  At the moment, data is replicated & denormalised from the `commune`, `epci`, `departement` and `region` tables. 
   This is done to simplify the queries and the data model for some use cases: search, link to dataset, etc.
-  Other approaches were considered, such as using a materialized view, the existing separate tables,
-  but they all had drawbacks. For example, a materialized view would not allow us to use foreign keys, which is important for data integrity.
+
+    Other approaches were considered (such as using a materialized view, leveraging the existing separate tables). For example, a materialized view would not allow us to use foreign keys, which is important for data integrity.
   """
   use Ecto.Schema
   use TypedEctoSchema
