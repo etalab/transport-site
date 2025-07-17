a7162a0139ec02d1a3c4b02a1420ca6287a8d612
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-07-17 11:52:58 +0200

    start the declarative spatial area form

---
+++ apps/transport/lib/transport_web/live/backoffice/declarative_spatial_areas_live.ex
@@ -0,0 +1,12 @@
+defmodule TransportWeb.DeclarativeSpatialAreasLive do
+  use Phoenix.LiveComponent
+  alias TransportWeb.InputHelpers
+
+  def render(assigns) do
+    ~H"""
+    <div class="pt-24">
+    </div>
+    """
+
+  end
+end
