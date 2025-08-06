4362d81471759e8b34498db91425014ac8351a52
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2025-08-06 12:26:24 +0200

    CreateTokensJob : création d'un token pour contacts sans org (#4749)
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>

--- config/runtime.exs
+++ config/runtime.exs
@@ -151,7 +151,8 @@ oban_prod_crontab = [
   {"45 2 * * *", Transport.Jobs.RemoveHistoryJob, args: %{schema_name: "etalab/schema-irve-dynamique", days_limit: 7}},
   {"0 16 * * *", Transport.Jobs.DatasetQualityScoreDispatcher},
   {"40 3 * * *", Transport.Jobs.UpdateContactsJob},
-  {"50 3 * * *", Transport.Jobs.CreateTokensJob},
+  {"50 3 * * *", Transport.Jobs.CreateTokensJob, args: %{action: "create_tokens_for_organizations"}},
+  {"30 4 * * *", Transport.Jobs.CreateTokensJob, args: %{action: "create_tokens_for_contacts_without_org"}},
   {"10 5 * * *", Transport.Jobs.NotificationSubscriptionProducerJob},
   # "At 08:15 on Monday in March, June, and November.""
   # The job will make sure that it's executed only on the first Monday of these months
