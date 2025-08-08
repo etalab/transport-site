0d8795d0d13f64d977ec28e47223a9558cfca744
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-08-08 14:43:09 +0200

    IRVE statique : ajout automatique de certains champs optionnels quand ils sont manquants (#4756)

--- apps/transport/lib/irve/processing.ex
+++ apps/transport/lib/irve/processing.ex
@@ -10,6 +10,7 @@ defmodule Transport.IRVE.Processing do
   def read_as_data_frame(body) do
     body
     |> convert_to_dataframe!()
+    |> add_missing_optional_columns()
     |> preprocess_fields()
     |> select_fields()
   end
@@ -40,6 +41,40 @@ defmodule Transport.IRVE.Processing do
     |> Transport.IRVE.DataFrame.preprocess_boolean("paiement_autre")
     |> Transport.IRVE.DataFrame.preprocess_boolean("reservation")
     |> Transport.IRVE.DataFrame.preprocess_boolean("station_deux_roues")
+  end
+
+  @doc """
+  Add empty columns for optional schema fields that are sometimes missing in CSV files.
+
+  Manually cherry-picked fields which are all "non-required" in the schema at time of writing.
+
+  A later version will likely automatically use all fields, but manual exceptions are likely
+  to still be useful.
+  """
+  def add_missing_optional_columns(dataframe) do
+    dataframe
+    # as seen in dataset 62ea8cd6af9f2e745fa84023
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("id_pdc_local")
+    # as seen in dataset 62ea8cd6af9f2e745fa84023
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("tarification")
+    # dataset 650866fc526f1050c8e4e252
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("paiement_cb")
+    # dataset 61606900558502c87d0c9522
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("id_station_local")
+    # dataset 661e3f4f8ee5dff6c8286fd2, 648758ebd41d68c851fa15c4
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("paiement_autre")
+    # dataset 661e3f4f8ee5dff6c8286fd2
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("raccordement")
+    # dataset 661e3f4f8ee5dff6c8286fd2
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("num_pdl")
+    # dataset 661e3f4f8ee5dff6c8286fd2
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("observations")
+    # dataset 650866fc526f1050c8e4e252
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("date_mise_en_service")
+    # 623ca46c13130c3228abd018
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("telephone_operateur")
+    # 623ca46c13130c3228abd018
+    |> Transport.IRVE.DataFrame.add_empty_column_if_missing("code_insee_commune")
   end
 
   def select_fields(dataframe) do
