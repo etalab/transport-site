6bc1d53613e5ce2170b6eda2070d09397286dbde
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-08-27 10:53:47 +0200

    Extract field names from real schema

--- apps/transport/lib/irve/static_irve_schema.ex
+++ apps/transport/lib/irve/static_irve_schema.ex
@@ -16,4 +16,25 @@ defmodule Transport.IRVE.StaticIRVESchema do
     |> File.read!()
     |> Jason.decode!()
   end
+
+  @doc """
+  Field names list from the json schema
+  iex> field_names_list()
+  ["nom_amenageur", "siren_amenageur", "contact_amenageur", "nom_operateur",
+  "contact_operateur", "telephone_operateur", "nom_enseigne",
+  "id_station_itinerance", "id_station_local", "nom_station",
+  "implantation_station", "adresse_station", "code_insee_commune",
+  "coordonneesXY", "nbre_pdc", "id_pdc_itinerance", "id_pdc_local",
+  "puissance_nominale", "prise_type_ef", "prise_type_2", "prise_type_combo_ccs",
+  "prise_type_chademo", "prise_type_autre", "gratuit", "paiement_acte",
+  "paiement_cb", "paiement_autre", "tarification", "condition_acces",
+  "reservation", "horaires", "accessibilite_pmr", "restriction_gabarit",
+  "station_deux_roues", "raccordement", "num_pdl", "date_mise_en_service",
+  "observations", "date_maj", "cable_t2_attache"]
+  """
+  def field_names_list do
+    schema_content()
+    |> Map.fetch!("fields")
+    |> Enum.map(&Map.fetch!(&1, "name"))
+  end
 end
