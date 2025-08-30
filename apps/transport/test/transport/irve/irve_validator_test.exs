250525263b32ee4ba956f919d33c7b6fe1f27880
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-08-30 07:59:53 +0200

    Use new method

--- apps/transport/test/transport/irve/irve_validator_test.exs
+++ apps/transport/test/transport/irve/irve_validator_test.exs
@@ -27,10 +27,7 @@ defmodule Transport.IRVE.ValidationTests do
     df = Explorer.DataFrame.from_csv!(file, dtypes: dtypes)
     |> Explorer.DataFrame.select([:id_pdc_itinerance, :coordonneesXY])
 
-    pattern = schema["fields"]
-    |> Enum.find(& &1["name"] == "id_pdc_itinerance")
-    |> get_in(["constraints", "pattern"])
-
+    id_pdc_itinerance_pattern = get_field_by_name(schema, "id_pdc_itinerance") |> get_in(["constraints", "pattern"])
     geopoint_array_pattern = ~S'^\[\-?\d+(\.\d+)?,\s?\-?\d+(\.\d+)?\]$'
 
     df
