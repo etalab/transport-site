d1a94ecba51ca10f8c3f7915cbb1471f781c5930
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-09-05 14:14:01 +0200

    IRVE schemas and migrations

---
+++ apps/transport/lib/db/irve_valid_file.ex
@@ -0,0 +1,16 @@
+defmodule DB.IrveValidFile do
+  @moduledoc """
+  IRVE file that has been validated and stored. This file refers to a datagouv resource and dataset
+  that is not imported on transport.data.gouv, so no reference to the dataset/resource tables.
+  """
+  use Ecto.Schema
+  use TypedEctoSchema
+
+  typed_schema "irve_valid_file" do
+    field(:dataset_datagouv_id, :string)
+    field(:resource_datagouv_id, :string)
+    field(:checksum, :string)
+    has_many(:irve_valid_pdcs, DB.IrveValidPdc, foreign_key: :irve_valid_file_id)
+    timestamps(type: :utc_datetime_usec)
+  end
+end
