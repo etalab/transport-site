d6f9adf8df0143ab0f301db76274cd8d9a28b782
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-09-17 11:12:47 +0200

    Remove direct import of data without dataframe tests and code

--- apps/transport/lib/db/irve_valid_pdc.ex
+++ apps/transport/lib/db/irve_valid_pdc.ex
@@ -2,6 +2,11 @@ defmodule DB.IRVEValidPDC do
   @moduledoc """
   IRVE Point de Charge (PDC) record from a validated IRVE file.
   This schema reflects the IRVE static schema, plus a reference to the IRVE valid file it belongs to.
+  Generating data from raw CSV rows ready to be inserted in the database needs some preprocessing.
+  Use first Transport.IRVE.Processing for work on coordinates.
+  Then use `raw_data_to_schema/1` to filter out only valid fields and convert keys to atoms.
+  Finally use `insert_timestamps/1` to add inserted_at and updated_at timestamps before inserting in the database.
+  See Transport.IRVE.DatabaseImporter for a module that orchestrates the whole import process.
   """
   use Ecto.Schema
   use TypedEctoSchema
@@ -59,9 +64,6 @@ defmodule DB.IRVEValidPDC do
     raw_data
     |> Enum.filter(fn {k, _v} -> k in valid_fields() end)
     |> Enum.into(%{}, fn {k, v} -> {String.to_existing_atom(k), v} end)
-    |> split_coordinates()
-    |> Map.update(:date_mise_en_service, nil, &parse_date/1)
-    |> Map.update(:date_maj, nil, &parse_date/1)
   end
 
   def insert_timestamps(data) do
@@ -71,35 +73,6 @@ defmodule DB.IRVEValidPDC do
     |> Map.put(:inserted_at, now)
     |> Map.put(:updated_at, now)
   end
-
-  defp split_coordinates(%{longitude: _longitude, latitude: _latitude} = map), do: map
-
-  defp split_coordinates(%{coordonneesXY: coords} = map) do
-    [longitude, latitude] = process_coordinates(coords)
-
-    map
-    |> Map.delete(:coordonneesXY)
-    |> Map.put(:longitude, longitude)
-    |> Map.put(:latitude, latitude)
-  end
-
-  @doc """
-  iex> process_coordinates("[7.48710500,48.345345]")
-  [7.48710500, 48.345345]
-  """
-  def process_coordinates(coords) do
-    coords
-    |> String.trim_leading("[")
-    |> String.trim_trailing("]")
-    |> String.split(",")
-    |> Enum.map(&String.to_float/1)
-  end
-
-  defp parse_date(date_string) when is_binary(date_string) do
-    Date.from_iso8601!(date_string)
-  end
-
-  defp parse_date(%Date{} = date), do: date
 
   defp valid_fields,
     do: Transport.IRVE.StaticIRVESchema.field_names_list() ++ ["id", "irve_valid_file_id", "longitude", "latitude"]
