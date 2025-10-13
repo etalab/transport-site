7761f87de45fb0328a98ff71b3eed20a526e72e4
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-10-13 17:28:52 +0200

    Add placeholder

---
+++ apps/transport/lib/irve/validator.ex
@@ -0,0 +1,12 @@
+defmodule Transport.IRVE.Validator do
+  @moduledoc """
+  This modules implements a validator for the static IRVE file format (see `schema-irve-statique.json`).
+
+  It aims to comply with:
+  - a subset of the `TableSchema` specification https://specs.frictionlessdata.io/table-schema/
+  - and for only the static IRVE schema at this point
+  """
+
+  def validate(file_path) do
+  end
+end
