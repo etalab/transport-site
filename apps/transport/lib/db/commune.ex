054ae3f4895a496bb4bce0944b1812ac8ea51a7e
Antoine Augusti <antoine.augusti@gmail.com>
2025-10-27 17:07:39 +0100

    Mise à jour 2025 pour Commune et EPCI

--- apps/transport/lib/db/commune.ex
+++ apps/transport/lib/db/commune.ex
@@ -17,7 +17,7 @@ defmodule DB.Commune do
     field(:arrondissement_insee, :string)
 
     # In theory a commune has only one AOM, the reference is done through the composition_res_id attribute
-    belongs_to(:aom_res, AOM, references: :composition_res_id)
+    # belongs_to(:aom_res, AOM, references: :composition_res_id)
     belongs_to(:region, Region)
     belongs_to(:departement, Departement, foreign_key: :departement_insee, references: :insee, type: :string)
     belongs_to(:epci, EPCI, foreign_key: :epci_insee, references: :insee, type: :string)
