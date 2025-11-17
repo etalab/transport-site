ff6bbf7890d84c2a7907e8145686f11ca933da68
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-11-17 11:22:52 +0100

    Remove double logs

--- apps/transport/lib/transport_web/endpoint.ex
+++ apps/transport/lib/transport_web/endpoint.ex
@@ -41,7 +41,6 @@ defmodule TransportWeb.Endpoint do
   plug(Plug.RequestId)
   plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
   plug(RemoteIp, headers: ["x-forwarded-for"])
-  plug(Plug.Logger)
 
   plug(Plug.Parsers,
     parsers: [
