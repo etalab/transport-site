36dd00e0123d6b6e349b672e51e0b89c88d7eacf
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-11-25 17:59:40 +0100

    Add roadmap

--- apps/transport/lib/irve/validator/validator.ex
+++ apps/transport/lib/irve/validator/validator.ex
@@ -10,4 +10,22 @@ defmodule Transport.IRVE.Validator do
     |> Transport.IRVE.Validator.DataFrameValidation.setup_computed_field_validation_columns(schema)
     |> Transport.IRVE.Validator.DataFrameValidation.setup_computed_row_validation_column()
   end
+
+  def validate_file(file_path) do
+    # prendre le fichier
+    # probe: si c'est un zip, lever une erreur et arrêter
+    # probe: si c'est un encoding latin1, convertir et noter
+    # probe: voir les colonnes. si V1, lever une erreur spécifique
+    # probe: voir si on a id_pdc_itinerance. lever une erreur sinon (trop différent)
+    # probe: deviner le séparateur. Si ;, corriger en , et noter. Sinon, lever une erreur et arrêter
+    # probe: vérifier les colonnes. Plein de possibilités. Colonnes qui manquent?  Colonnes en trop? Doublons?
+    # (note: faire un profiling global de toutes les ressources dispos sur ce sujet, voir ce qui est utile à implémenter)
+    # (on peut commencer strict : il faut "au moins exactement toutes les colonnes", et on droppe le reste avec un warning)
+    # processing: supprimer les whitespaces (idéalement noter quand c'est fait avec un bitmask) en heading/trailing
+    # processing: détecter les lignes cassées (pas le bon nombre de champs sur la ligne elle même)
+    # avaler la data résultante via DataFrame, et calculer colonnes dérivées
+    # persister: un JSON avec checksum du fichier, résultat général valid: true/false, valid row count, chemin vers dataframe
+    #            et le dataframe réduit (bits) dans le format le plus compact possible. Pourquoi pas un zip avec les deux.
+    raise "ToBeImplemented"
+  end
 end
