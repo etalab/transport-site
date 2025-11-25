aebef1555b44fd17ee4a9e82559a91de5dbb673a
Antoine Augusti <antoine.augusti@gmail.com>
2025-11-25 10:21:07 +0100

    PopulateAdministrativeDivisions : ajout colonne population

--- apps/transport/lib/mix/tasks/transport/populate_administrative_divisions.ex
+++ apps/transport/lib/mix/tasks/transport/populate_administrative_divisions.ex
@@ -38,67 +38,93 @@ defmodule Mix.Tasks.Transport.PopulateAdministrativeDivisions do
 
   defp insert_communes do
     DB.Repo.query!("""
-      INSERT INTO administrative_division (type_insee, insee, type, nom, geom)
+      INSERT INTO administrative_division (type_insee, insee, type, nom, geom, population)
       SELECT
         CONCAT('commune_', insee) AS type_insee,
         insee,
         'commune' AS type,
         nom,
-        geom
+        geom,
+        population
       FROM commune
     """)
   end
 
   defp insert_epcis do
     DB.Repo.query!("""
-      INSERT INTO administrative_division (type_insee, insee, type, nom, geom)
+      INSERT INTO administrative_division (type_insee, insee, type, nom, geom, population)
       SELECT
-        CONCAT('epci_', insee) AS type_insee,
-        insee,
+        CONCAT('epci_', epci.insee) AS type_insee,
+        epci.insee,
         'epci' AS type,
         nom,
-        geom
+        geom,
+        t.population
       FROM epci
+      JOIN (
+        SELECT epci_insee insee, sum(population) population
+        FROM commune
+        WHERE epci_insee is not null
+        group by 1
+      ) t on t.insee = epci.insee
     """)
   end
 
   defp insert_departements do
     DB.Repo.query!("""
-      INSERT INTO administrative_division (type_insee, insee, type, nom, geom)
+      INSERT INTO administrative_division (type_insee, insee, type, nom, geom, population)
       SELECT
-        CONCAT('departement_', insee) AS type_insee,
-        insee,
+        CONCAT('departement_', departement.insee) AS type_insee,
+        departement.insee,
         'departement' AS type,
         nom,
-        geom
+        geom,
+        t.population
       FROM departement
+      JOIN (
+        SELECT departement_insee insee, sum(population) population
+        FROM commune
+        group by 1
+      ) t on t.insee = departement.insee
     """)
   end
 
   defp insert_regions do
     DB.Repo.query!("""
-      INSERT INTO administrative_division (type_insee, insee, type, nom, geom)
+      INSERT INTO administrative_division (type_insee, insee, type, nom, geom, population)
       SELECT
-        CONCAT('region_', insee) AS type_insee,
-        insee,
+        CONCAT('region_', region.insee) AS type_insee,
+        region.insee,
         'region' AS type,
         nom,
-        geom
+        geom,
+        t.population
       FROM region
+      JOIN (
+        SELECT r.insee insee, sum(population) population
+        FROM commune c
+        join region r on r.id = c.region_id
+        group by 1
+      ) t on t.insee = region.insee
       WHERE NOT nom = 'National'
     """)
   end
 
   defp insert_france do
     DB.Repo.query!("""
-      INSERT INTO administrative_division (type_insee, insee, type, nom, geom)
+      INSERT INTO administrative_division (type_insee, insee, type, nom, geom, population)
       SELECT
         'pays_FR' AS type_insee,
         'FR' AS insee,
         'pays' AS type,
         'France' AS nom,
-        ST_Union(geom) AS geom
+        ST_Union(geom) AS geom,
+        min(t.population)
       FROM region
+      JOIN (
+        select sum(population) population
+        from commune
+      ) t on true
     """)
   end
 end
