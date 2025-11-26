7cb4348fd41e03ff32fcabbfcd4628d7dffdf0b5
Antoine Augusti <antoine.augusti@gmail.com>
2025-11-26 18:30:05 +0100

    Write multiple perform clauses

--- apps/transport/lib/jobs/clean_multi_validation_job.ex
+++ apps/transport/lib/jobs/clean_multi_validation_job.ex
@@ -2,43 +2,88 @@ defmodule Transport.Jobs.CleanMultiValidationJob do
   @moduledoc """
   This job is in charge of archiving `DB.MultiValidation` records.
 
-  It keeps the 3 most recent records per resource ID, other records
-  have their `result` and `data_vis` fields set to `null`.
+  - For static resources, we keep the 3 most recent records per resource ID.
+  - For real-time resources, we archive rows older than 30 days.
+
+  Archived records have their `result` and `data_vis` fields set to `null`.
   """
   use Oban.Worker, max_attempts: 3
   import Ecto.Query
 
   @max_records 1_000
   @next_job_dispatch {5, :second}
+  # For static resources
   @records_to_keep_per_resource 3
+  # For real-time resources
+  @days_to_keep_realtime_rows 30
 
+  # Dispatch jobs for both cases
   @impl Oban.Worker
-  def perform(%Oban.Job{}) do
+  def perform(%Oban.Job{args: args}) when args == %{} do
+    new(%{"static" => true}) |> Oban.insert!()
+    new(%{"real_time" => true}) |> Oban.insert!()
+    :ok
+  end
+
+  # Logic for static resources
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: %{"static" => true} = job_args}) do
     max_records = max_records()
 
-    ids =
-      subquery(
-        DB.MultiValidation.base_query()
-        |> where([mv], not is_nil(mv.resource_id))
-        |> select([mv], %{
+    inner_query =
+      from(mv in DB.MultiValidation,
+        join: rh in DB.ResourceHistory,
+        on: rh.id == mv.resource_history_id,
+        where: not is_nil(mv.result),
+        select: %{
           id: mv.id,
-          row_number: row_number() |> over(partition_by: mv.resource_id, order_by: {:desc, mv.inserted_at})
-        })
+          row_number: row_number() |> over(partition_by: rh.resource_id, order_by: {:desc, mv.inserted_at})
+        }
       )
-      |> select([t], t.id)
-      |> where([t], t.row_number > @records_to_keep_per_resource)
-      |> order_by([t], {:asc, t.id})
-      |> limit(^max_records)
+
+    ids =
+      from(q in subquery(inner_query),
+        where: q.row_number >= @records_to_keep_per_resource,
+        select: q.id,
+        order_by: {:asc, q.id},
+        limit: ^max_records
+      )
       |> DB.Repo.all()
 
-    DB.MultiValidation.base_query()
-    |> where([mv], mv.id in ^ids)
-    |> update([mv], set: [result: nil, data_vis: nil])
-    |> DB.Repo.update_all([])
+    archive_records(ids)
 
     # We handled `max_records`. We've got more rows to handle, schedule another job
     if Enum.count(ids) == max_records do
-      new(%{}, schedule_in: @next_job_dispatch) |> Oban.insert!()
+      new(job_args, schedule_in: @next_job_dispatch) |> Oban.insert!()
+    end
+
+    :ok
+  end
+
+  # Logic for real-time resources
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: %{"real_time" => true} = job_args}) do
+    max_records = max_records()
+
+    ids =
+      from(mv in DB.MultiValidation,
+        join: mv2 in DB.MultiValidation,
+        on:
+          mv2.resource_id == mv.resource_id and
+            mv2.inserted_at <= datetime_add(mv.inserted_at, -@days_to_keep_realtime_rows, "day"),
+        where: not is_nil(mv2.result),
+        order_by: {:asc, mv2.id},
+        select: mv2.id,
+        distinct: true,
+        limit: ^max_records
+      )
+      |> DB.Repo.all()
+
+    archive_records(ids)
+
+    # We handled `max_records`. We've got more rows to handle, schedule another job
+    if Enum.count(ids) == max_records do
+      new(job_args, schedule_in: @next_job_dispatch) |> Oban.insert!()
     end
 
     :ok
@@ -50,5 +95,12 @@ defmodule Transport.Jobs.CleanMultiValidationJob do
     else
       @max_records
     end
+  end
+
+  defp archive_records(ids) do
+    DB.MultiValidation.base_query()
+    |> where([mv], mv.id in ^ids)
+    |> update([mv], set: [result: nil, data_vis: nil])
+    |> DB.Repo.update_all([])
   end
 end
--- apps/transport/test/transport/jobs/clean_multi_validation_job_test.exs
+++ apps/transport/test/transport/jobs/clean_multi_validation_job_test.exs
@@ -13,20 +13,23 @@ defmodule Transport.Test.Transport.Jobs.CleanMultiValidationJobTest do
     assert CleanMultiValidationJob.max_records() == 1
   end
 
-  test "perform" do
+  test "perform for real-time resources" do
+    now = DateTime.utc_now()
+    month_ago = now |> DateTime.add(-30, :day)
+    two_month_ago = now |> DateTime.add(-60, :day)
     %DB.Resource{id: r1_id} = r1 = insert(:resource)
     %DB.Resource{id: r2_id} = r2 = insert(:resource)
 
-    insert(:multi_validation, result: %{"value" => 4}, resource: r1)
-    insert(:multi_validation, result: %{"value" => 3}, resource: r1)
-    insert(:multi_validation, result: %{"value" => 2}, resource: r1)
-    insert(:multi_validation, result: %{"value" => 1}, resource: r1)
+    insert(:multi_validation, result: %{"value" => 4}, resource: r1, inserted_at: two_month_ago)
+    insert(:multi_validation, result: %{"value" => 3}, resource: r1, inserted_at: month_ago)
+    insert(:multi_validation, result: %{"value" => 2}, resource: r1, inserted_at: month_ago)
+    insert(:multi_validation, result: %{"value" => 1}, resource: r1, inserted_at: now)
 
-    insert(:multi_validation, result: %{"value" => 3}, resource: r2)
-    insert(:multi_validation, result: %{"value" => 2}, resource: r2)
-    insert(:multi_validation, result: %{"value" => 1}, resource: r2)
+    insert(:multi_validation, result: %{"value" => 3}, resource: r2, inserted_at: month_ago)
+    insert(:multi_validation, result: %{"value" => 2}, resource: r2, inserted_at: month_ago)
+    insert(:multi_validation, result: %{"value" => 1}, resource: r2, inserted_at: now)
 
-    assert :ok == perform_job(CleanMultiValidationJob, %{})
+    assert :ok == perform_job(CleanMultiValidationJob, %{"real_time" => true})
 
     # oldest record has been nullified
     assert [
@@ -44,13 +47,82 @@ defmodule Transport.Test.Transport.Jobs.CleanMultiValidationJobTest do
            ] == results_for_resource_id(r2_id)
 
     assert [
-             %Oban.Job{worker: "Transport.Jobs.CleanMultiValidationJob", state: "scheduled"}
+             %Oban.Job{
+               worker: "Transport.Jobs.CleanMultiValidationJob",
+               state: "scheduled",
+               args: %{"real_time" => true}
+             }
+           ] = all_enqueued()
+  end
+
+  test "perform for static resources" do
+    %DB.Resource{id: r1_id} = r1 = insert(:resource)
+    %DB.Resource{id: r2_id} = r2 = insert(:resource)
+
+    insert(:multi_validation, result: %{"value" => 4}, resource_history: insert(:resource_history, resource: r1))
+    insert(:multi_validation, result: %{"value" => 3}, resource_history: insert(:resource_history, resource: r1))
+    insert(:multi_validation, result: %{"value" => 2}, resource_history: insert(:resource_history, resource: r1))
+    insert(:multi_validation, result: %{"value" => 1}, resource_history: insert(:resource_history, resource: r1))
+
+    insert(:multi_validation, result: %{"value" => 3}, resource_history: insert(:resource_history, resource: r2))
+    insert(:multi_validation, result: %{"value" => 2}, resource_history: insert(:resource_history, resource: r2))
+    insert(:multi_validation, result: %{"value" => 1}, resource_history: insert(:resource_history, resource: r2))
+
+    assert :ok == perform_job(CleanMultiValidationJob, %{"static" => true})
+
+    # oldest record has been nullified
+    assert [
+             nil,
+             %{"value" => 3},
+             %{"value" => 2},
+             %{"value" => 1}
+           ] == results_for_resource_history(r1_id)
+
+    # records have not been touched
+    assert [
+             %{"value" => 3},
+             %{"value" => 2},
+             %{"value" => 1}
+           ] == results_for_resource_history(r2_id)
+
+    assert [
+             %Oban.Job{
+               worker: "Transport.Jobs.CleanMultiValidationJob",
+               state: "scheduled",
+               args: %{"static" => true}
+             }
+           ] = all_enqueued()
+  end
+
+  test "perform dispatches both jobs" do
+    assert :ok == perform_job(CleanMultiValidationJob, %{})
+
+    assert [
+             %Oban.Job{
+               worker: "Transport.Jobs.CleanMultiValidationJob",
+               state: "available",
+               args: %{"real_time" => true}
+             },
+             %Oban.Job{
+               worker: "Transport.Jobs.CleanMultiValidationJob",
+               state: "available",
+               args: %{"static" => true}
+             }
            ] = all_enqueued()
   end
 
   defp results_for_resource_id(r_id) do
     DB.MultiValidation.with_result()
     |> where([mv], mv.resource_id == ^r_id)
+    |> order_by([mv], {:asc, mv.id})
+    |> DB.Repo.all()
+    |> Enum.map(& &1.result)
+  end
+
+  def results_for_resource_history(r_id) do
+    DB.MultiValidation.with_result()
+    |> join(:inner, [mv], rh in DB.ResourceHistory, on: rh.id == mv.resource_history_id)
+    |> where([_mv, rh], rh.resource_id == ^r_id)
     |> order_by([mv], {:asc, mv.id})
     |> DB.Repo.all()
     |> Enum.map(& &1.result)
