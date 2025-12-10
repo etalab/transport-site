be16d8238ba18511b569852d5210f03863e9385b
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-12-10 13:42:25 +0100

    Create helper to tell if full file valid

--- apps/transport/lib/irve/validator/validator.ex
+++ apps/transport/lib/irve/validator/validator.ex
@@ -10,4 +10,13 @@ defmodule Transport.IRVE.Validator do
     |> Transport.IRVE.Validator.DataFrameValidation.setup_computed_field_validation_columns(schema)
     |> Transport.IRVE.Validator.DataFrameValidation.setup_computed_row_validation_column()
   end
+
+  @doc """
+  Says from the dataframe output of compute_validation/1 if all rows are valid.
+  """
+
+  def full_file_valid?(%Explorer.DataFrame{} = df) do
+    df["check_row_valid"]
+    |> Explorer.Series.all?()
+  end
 end
