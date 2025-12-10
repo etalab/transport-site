ecc57ee8a92f7abe46d632dcc4b95a3c8bacc511
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-12-10 22:50:27 +0100

    Report on 4 types of situations
    We can now track:
    - the file wasn't imported because it doesn't follow the schema ("not valid")
    - the exact same file was already inserted before
    - the file is valid and has been imported
    - a blocking error occurred

--- apps/transport/lib/irve/simple_consolidation.ex
+++ apps/transport/lib/irve/simple_consolidation.ex
@@ -6,24 +6,51 @@ defmodule Transport.IRVE.SimpleConsolidation do
   """
 
   def process do
-    # Get list of datagouv resources
-    resource_list()
-    # Then for each resource, launch a kind of job that will dl the resource, etc.
-    # |> Enum.drop(2)
-    # |> Enum.take(10)
-    |> Task.async_stream(
-      fn resource ->
-        process_or_rescue(resource)
-      end,
-      on_timeout: :kill_task,
-      timeout: :timer.seconds(60),
-      max_concurrency: 5
-    )
-    |> Stream.map(fn {:ok, result} -> result end)
-    |> Enum.into([])
+    df =
+      resource_list()
+      |> Task.async_stream(
+        &process_or_rescue/1,
+        ordered: true,
+        on_timeout: :kill_task,
+        timeout: :timer.seconds(60),
+        max_concurrency: 10
+      )
+      |> Stream.map(fn {:ok, result} -> result end)
+      |> Stream.map(&prepare_report_item/1)
+      |> Enum.into([])
+      |> Explorer.DataFrame.new()
 
-    # |> Enum.frequencies()
-    # |> IO.inspect()
+    report_file = "irve-processed-resources.csv"
+    Logger.info("Saving report to #{report_file}...")
+    Explorer.DataFrame.to_csv!(df, report_file)
+
+    # NOTE: to be removed ; but nicely displays what happened
+    df["status"]
+    |> Explorer.Series.frequencies()
+    |> IO.inspect(IEx.inspect_opts())
+  end
+
+  def base_report_item(resource) do
+    resource
+    |> Map.take([:dataset_id, :resource_id, :url, :dataset_title])
+    |> Map.put_new(:error_message, nil)
+    |> Map.put_new(:error_type, nil)
+  end
+
+  def prepare_report_item({:error_occurred, error, resource}) do
+    base_report_item(resource)
+    |> Map.put(:status, :error_occurred)
+    |> Map.merge(%{
+      error_message: Exception.message(error),
+      error_type: error.__struct__ |> inspect()
+    })
+    |> Map.update!(:status, &to_string/1)
+  end
+
+  def prepare_report_item({status, resource}) do
+    base_report_item(resource)
+    |> Map.merge(%{status: status})
+    |> Map.update!(:status, &to_string/1)
   end
 
   def resource_list do
@@ -38,7 +65,7 @@ defmodule Transport.IRVE.SimpleConsolidation do
   rescue
     error ->
       # Logger.error("Error processing resource #{resource.resource_id} : #{inspect(error)}")
-      {:error, error}
+      {:error_occurred, error, resource}
   end
 
   def process_resource(resource) do
@@ -52,14 +79,32 @@ defmodule Transport.IRVE.SimpleConsolidation do
       validation_result = df |> Transport.IRVE.Validator.compute_validation()
       file_valid? = validation_result |> Transport.IRVE.Validator.full_file_valid?()
 
+      if file_valid? do
+        try_import_into_db(resource, path)
+      else
+        {:not_compliant_with_schema, resource}
+      end
+    end)
+  end
+
+  def try_import_into_db(resource, path) do
+    try do
       Transport.IRVE.DatabaseImporter.write_to_db(
         path,
         resource.dataset_id,
         resource.resource_id
       )
 
-      {:ok, file_valid?}
-    end)
+      {:import_successful, resource}
+    rescue
+      x in [Ecto.ConstraintError] ->
+        # TODO: encapsulate that logic inside `write_to_db`, returning just an atom there
+        if x.type == :unique && x.constraint == "irve_valid_file_resource_datagouv_id_checksum_index" do
+          {:already_in_db, resource}
+        else
+          reraise(x, __STACKTRACE__)
+        end
+    end
   end
 
   def storage_path(resource_id) do
