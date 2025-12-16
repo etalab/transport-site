4203b846295ba977f1d04798763f112de43537c2
Frédéric Menou <frederic.menou@gmail.com>
2025-12-16 11:40:46 +0100

    NeTEx: résultats lus du format binaire (#5054)

--- apps/transport/lib/validators/netex/results_adapters/v0_1_0.ex
+++ apps/transport/lib/validators/netex/results_adapters/v0_1_0.ex
@@ -5,6 +5,7 @@ defmodule Transport.Validators.NeTEx.ResultsAdapters.V0_1_0 do
 
   use Gettext, backend: TransportWeb.Gettext
 
+  require Explorer.DataFrame, as: DF
   alias Transport.Validators.NeTEx.ResultsAdapters.Commons
 
   @behaviour Transport.Validators.NeTEx.ResultsAdapter
@@ -159,45 +160,43 @@ defmodule Transport.Validators.NeTEx.ResultsAdapters.V0_1_0 do
 
   @doc """
   Get issues from validation results. For a specific issue type if specified, or the most severe.
-
-  iex> validation_result = %{"uic-operating-period" => [%{"code" => "uic-operating-period", "message" => "Resource 23504000009 hasn't expected class but Netex::OperatingPeriod", "criticity" => "error"}], "valid-day-bits" => [%{"code" => "valid-day-bits", "message" => "Mandatory attribute valid_day_bits not found", "criticity" => "error"}], "frame-arret-resources" => [%{"code" => "frame-arret-resources", "message" => "Tag frame_id doesn't match ''", "criticity" => "warning"}]}
-  iex> get_issues(validation_result, %{"issue_type" => "uic-operating-period"})
-  [%{"code" => "uic-operating-period", "message" => "Resource 23504000009 hasn't expected class but Netex::OperatingPeriod", "criticity" => "error"}]
-  iex> get_issues(validation_result, %{"issue_type" => "broken-file"})
-  []
-  iex> get_issues(validation_result, nil)
-  [%{"code" => "uic-operating-period", "message" => "Resource 23504000009 hasn't expected class but Netex::OperatingPeriod", "criticity" => "error"}]
-  iex> get_issues(%{}, nil)
-  []
-  iex> get_issues([], nil)
-  []
   """
   @impl Transport.Validators.NeTEx.ResultsAdapter
-  def get_issues(%{} = validation_result, %{"issue_type" => issue_type}) do
-    validation_result
-    |> Map.get(issue_type, [])
-    |> order_issues_by_location()
+  def get_issues(binary, %{} = filter, %Scrivener.Config{} = pagination_config) when is_binary(binary) do
+    binary
+    |> Commons.from_binary()
+    |> get_issues(filter, pagination_config)
   end
 
-  def get_issues(%{} = validation_result, _) do
-    validation_result
-    |> Map.values()
-    |> Enum.sort_by(fn [%{"criticity" => severity} | _] -> severity_level(severity) end)
-    |> List.first([])
-    |> order_issues_by_location()
+  def get_issues(
+        %Explorer.DataFrame{} = df,
+        %{"issue_type" => issue_type} = filter,
+        %Scrivener.Config{} = pagination_config
+      ) do
+    {filter,
+     df
+     |> DF.filter(code == ^issue_type)
+     |> order_issues_by_location()
+     |> Commons.count_and_slice(pagination_config)}
   end
 
-  def get_issues(_, _), do: []
+  def get_issues(%Explorer.DataFrame{} = df, %{}, %Scrivener.Config{} = pagination_config) do
+    filter = %{"issue_type" => pick_default_issue_type(df)}
 
-  def order_issues_by_location(issues) do
-    issues
-    |> Enum.sort_by(fn issue ->
-      message = Map.get(issue, "message", "")
-      resource = Map.get(issue, "resource", %{})
-      filename = Map.get(resource, "filename", "")
-      line = Map.get(resource, "line", "")
-      {filename, line, message}
-    end)
+    get_issues(df, filter, pagination_config)
+  end
+
+  def get_issues(_, _, _), do: {%{"issue_type" => ""}, {0, []}}
+
+  def pick_default_issue_type(%Explorer.DataFrame{} = df) do
+    get_codes(df) |> List.first()
+  end
+
+  def get_codes(%Explorer.DataFrame{} = df), do: Commons.get_values(df, "code")
+
+  def order_issues_by_location(%Explorer.DataFrame{} = df) do
+    df
+    |> DF.sort_by(&[&1["resource.filename"], &1["resource.line"], &1["message"]])
   end
 
   @impl Transport.Validators.NeTEx.ResultsAdapter
