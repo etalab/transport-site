4b0d0eba09cd6d45be977b9f73ab85e0e97aa37a
Vincent Degove <vincent.degove@beta.gouv.fr>
2025-12-17 11:35:46 +0100

    Move report items modules for consolidation

--- apps/transport/lib/irve/simple_consolidation.ex
+++ apps/transport/lib/irve/simple_consolidation.ex
@@ -30,7 +30,7 @@ defmodule Transport.IRVE.SimpleConsolidation do
       # If a task times out, we get {:exit, :timeout} instead of {:ok, result} and the following line will crash.
       # This is intentional, we want to be aware of such timeouts.
       |> Stream.map(fn {:ok, result} -> result end)
-      |> Stream.map(&ReportRow.from_result/1)
+      |> Stream.map(&Transport.IRVE.SimpleReportItem.from_result/1)
       |> Enum.into([])
 
     generate_report(report_rows, destination: destination)
@@ -70,7 +70,7 @@ defmodule Transport.IRVE.SimpleConsolidation do
   def generate_report(report_rows, destination: destination) do
     report_df =
       report_rows
-      |> Enum.map(&ReportRow.to_map/1)
+      |> Enum.map(&Transport.IRVE.SimpleReportItem.to_map/1)
       |> Explorer.DataFrame.new()
 
     case destination do
@@ -122,48 +122,5 @@ defmodule Transport.IRVE.SimpleConsolidation do
     unless status == 200 do
       raise "Error processing resource (#{resource_id}) (http_status=#{status})"
     end
-  end
-
-  defmodule ReportRow do
-    @moduledoc """
-    Quick & dirty structure to massage the outcome into a report with all the same keys
-    (as expected by the `DataFrame` that we use to create the CSV file).
-
-    It's all quite tied to the other module here.
-    """
-    @enforce_keys [:dataset_id, :resource_id, :url, :dataset_title, :status]
-    defstruct [:dataset_id, :resource_id, :url, :dataset_title, :status, :error_message, :error_type]
-
-    def from_result({:error_occurred, error, resource}) do
-      new(resource, :error_occurred, error)
-    end
-
-    def from_result({status, resource}) do
-      new(resource, status, nil)
-    end
-
-    def to_map(%__MODULE__{} = report_row) do
-      report_row
-      |> Map.from_struct()
-      |> Map.update!(:status, &to_string/1)
-    end
-
-    defp new(resource, status, error) do
-      %__MODULE__{
-        dataset_id: resource.dataset_id,
-        resource_id: resource.resource_id,
-        url: resource.url,
-        dataset_title: resource.dataset_title,
-        status: status,
-        error_message: maybe_error_message(error),
-        error_type: maybe_error_type(error)
-      }
-    end
-
-    defp maybe_error_message(nil), do: nil
-    defp maybe_error_message(error), do: Exception.message(error)
-
-    defp maybe_error_type(nil), do: nil
-    defp maybe_error_type(error), do: error.__struct__ |> inspect()
   end
 end
