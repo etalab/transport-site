7941c4d9107196ef22c92351135c215b2b682484
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2025-12-17 17:31:51 +0100

    ProxyConfigLive : permet de filtrer la configuration (#5095)

--- apps/transport/test/transport_web/live_views/proxy_config_live_test.exs
+++ apps/transport/test/transport_web/live_views/proxy_config_live_test.exs
@@ -5,6 +5,8 @@ defmodule TransportWeb.Backoffice.ProxyConfigLiveTest do
   import Mox
   import Phoenix.LiveViewTest
 
+  doctest TransportWeb.Backoffice.ProxyConfigLive, import: true
+
   @endpoint TransportWeb.Endpoint
   @url "/backoffice/proxy-config"
 
@@ -119,5 +121,61 @@ defmodule TransportWeb.Backoffice.ProxyConfigLiveTest do
              _siri_item,
              _s3_item
            ] = extract_data_from_html(render(view))
+  end
+
+  test "can search feeds", %{conn: conn} do
+    item_id = "gtfs-rt-slug"
+    siri_item_id = "siri-slug"
+    aggregate_item_id = "aggregate-slug"
+    s3_item_id = "s3-slug"
+    setup_proxy_config(item_id, siri_item_id, aggregate_item_id, s3_item_id)
+
+    {:ok, view, _html} = conn |> setup_admin_in_session() |> get(@url) |> live()
+
+    assert [
+             {"select", [{"id", "backoffice_search_container_type"}, {"name", "type"}],
+              [
+                {"option", [{"selected", "selected"}, {"value", ""}], ["Tout"]},
+                {"option", [{"value", "Aggregate"}], ["Aggregate"]},
+                {"option", [{"value", "HTTP"}], ["HTTP"]},
+                {"option", [{"value", "S3"}], ["S3"]},
+                {"option", [{"value", "SIRI"}], ["SIRI"]}
+              ]}
+           ] == view |> element("form select") |> render() |> Floki.parse_document!()
+
+    assert ["aggregate-slug", "gtfs-rt-slug", "s3-slug", "siri-slug"] == slugs(view)
+
+    form = view |> element("form")
+
+    assert ["s3-slug"] == form |> search_by_type("S3") |> slugs()
+
+    # Reset the form
+    form |> search_by_type("")
+
+    assert ["siri-slug"] == form |> search_by_value("siri") |> slugs()
+
+    form |> search_by_type("SIRI")
+    assert ["siri-slug"] == form |> search_by_value("siri") |> slugs()
+
+    assert [] == form |> search_by_value("other") |> slugs()
+    form |> search_by_type("")
+    assert ["aggregate-slug", "gtfs-rt-slug", "s3-slug", "siri-slug"] == form |> search_by_value("slug") |> slugs()
+  end
+
+  defp search_by_value(%Phoenix.LiveViewTest.Element{} = el, value) do
+    render_change(el, %{_target: ["search"], search: value})
+  end
+
+  defp search_by_type(%Phoenix.LiveViewTest.Element{} = el, value) do
+    render_change(el, %{_target: ["type"], type: value})
+  end
+
+  defp slugs(%Phoenix.LiveViewTest.View{} = view), do: view |> render() |> slugs()
+
+  defp slugs(content) when is_binary(content) do
+    content
+    |> Floki.parse_document!()
+    |> Floki.find(~s|[data-name="unique-slug"]|)
+    |> Enum.map(&(&1 |> Floki.text() |> String.trim()))
   end
 end
