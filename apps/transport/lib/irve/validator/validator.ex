07581cba55b4937f84506b797362695f65eb0964
Thibaut Barrère <thibaut.barrere@gmail.com>
2025-12-21 07:38:42 +0100

    Fix warnings

--- apps/transport/lib/irve/validator/validator.ex
+++ apps/transport/lib/irve/validator/validator.ex
@@ -12,20 +12,27 @@ defmodule Transport.IRVE.Validator do
   end
 
   def validate(path) do
-    # prendre le fichier
+    # NOTES rapportées de ma branche de travail
+    #
+    # prendre le fichier:
     # probe: si c'est un zip, lever une erreur et arrêter
     # probe: si c'est un encoding latin1, convertir et noter
     # probe: voir les colonnes. si V1, lever une erreur spécifique
     # probe: voir si on a id_pdc_itinerance. lever une erreur sinon (trop différent)
-    # probe: deviner le séparateur. Si ;, corriger en , et noter. Sinon, lever une erreur et arrêter
-    # probe: vérifier les colonnes. Plein de possibilités. Colonnes qui manquent?  Colonnes en trop? Doublons?
-    # (note: faire un profiling global de toutes les ressources dispos sur ce sujet, voir ce qui est utile à implémenter)
-    # (on peut commencer strict : il faut "au moins exactement toutes les colonnes", et on droppe le reste avec un warning)
-    # processing: supprimer les whitespaces (idéalement noter quand c'est fait avec un bitmask) en heading/trailing
+    # probe: deviner le séparateur. Si ;, corriger en , et noter. Sinon, lever une erreur
+    #        et arrêter
+    # probe: vérifier les colonnes. Plein de possibilités. Colonnes qui manquent?  Colonnes
+    #        en trop? Doublons?
+    # (note: faire un profiling global de toutes les ressources dispos sur ce sujet, voir ce
+    #  qui est utile à implémenter)
+    # (on peut commencer strict : il faut "au moins exactement toutes les colonnes",
+    #  et on droppe le reste avec un warning)
+    # processing: supprimer les whitespaces (idéalement noter quand c'est fait avec
+    #  un bitmask) en heading/trailing
     # processing: détecter les lignes cassées (pas le bon nombre de champs sur la ligne elle même)
-    # avaler la data résultante via DataFrame, et calculer colonnes dérivées
-    # persister: un JSON avec checksum du fichier, résultat général valid: true/false, valid row count, chemin vers dataframe
-    #            et le dataframe réduit (bits) dans le format le plus compact possible. Pourquoi pas un zip avec les deux.
+    # à terme: persister: un JSON avec checksum du fichier, résultat général valid: true/false,
+    #  valid row count, chemin vers dataframe et le dataframe réduit (bits) dans le format le plus
+    #  compact possible. Pourquoi pas un zip avec les deux.
 
     path
     |> load_file_as_dataframe()
