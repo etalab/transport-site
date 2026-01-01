1e3e6e3e5a4ffaed1bb74994019a384fa86a9fca
Antoine Augusti <antoine.augusti@transport.data.gouv.fr>
2026-01-01 21:51:24 +0100

    Espace réutilisateur : lien vers discussion sans réponse (#5203)

--- apps/transport/lib/transport_web/views/espace_producteur_view.ex
+++ apps/transport/lib/transport_web/views/espace_producteur_view.ex
@@ -119,7 +119,7 @@ defmodule TransportWeb.EspaceProducteurView do
     """
   end
 
-  defp issue_link(%{mode: :reuser} = assigns) do
+  defp issue_link(%{mode: :reuser, issue: %DB.Resource{}} = assigns) do
     ~H"""
     <td>
       <a
@@ -130,6 +130,21 @@ defmodule TransportWeb.EspaceProducteurView do
         data-tracking-action="urgent_issues_see_resource_button"
       >
         <%= dgettext("reuser-space", "See the resource") %>
+      </a>
+    </td>
+    """
+  end
+
+  defp issue_link(%{mode: :reuser, check_name: :unanswered_discussions} = assigns) do
+    ~H"""
+    <td>
+      <a
+        href={dataset_path(TransportWeb.Endpoint, :details, @dataset.slug) <> ~s|#discussion-#{@issue["id"]}|}
+        class="button-outline primary small-padding"
+        data-tracking-category="espace_producteur"
+        data-tracking-action="urgent_issues_see_discussion_button"
+      >
+        <i class="icon fas fa-comments"></i><%= dgettext("espace-producteurs", "See the discussion") %>
       </a>
     </td>
     """
