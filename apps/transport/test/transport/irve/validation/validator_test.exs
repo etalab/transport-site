54a01b2451c4cbc3c90704199403aaa89e909930
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-01-07 17:28:22 +0100

    Improve test & extract reusable tmp file abstraction

--- apps/transport/test/transport/irve/validation/validator_test.exs
+++ apps/transport/test/transport/irve/validation/validator_test.exs
@@ -7,6 +7,7 @@ defmodule Transport.IRVE.ValidatorTest do
   """
 
   use ExUnit.Case, async: true
+  import Transport.TmpFile
 
   test "validate/1 returns a dataframe with validation columns" do
     csv_content = [DB.Factory.IRVE.generate_row()] |> DB.Factory.IRVE.to_csv_body()
@@ -26,25 +27,10 @@ defmodule Transport.IRVE.ValidatorTest do
   end
 
   test "a ZIP file should raise an error" do
-    zip_content = "PK\x03\x04" <> "some content"
-
-    temp_path = System.tmp_dir!() |> Path.join("irve_validator_test_#{Ecto.UUID.generate()}.csv")
-
-    try do
-      File.write!(temp_path, zip_content)
-      Transport.IRVE.Validator.validate(temp_path)
-    rescue
-      e in RuntimeError ->
-        assert e.message == "the content is likely to be a zip file, not uncompressed CSV data"
-    after
-      File.rm(temp_path)
-    end
-
-    # Alternative version without try/rescue
-    # File.write!(temp_path, zip_content)
-    # assert_raise RuntimeError, "the content is likely to be a zip file, not uncompressed CSV data", fn ->
-    #   Transport.IRVE.Validator.validate(temp_path)
-    # end
-    # File.rm(temp_path)
+    with_tmp_file(_zip_content = "PK\x03\x04" <> "some content", fn path ->
+      assert_raise RuntimeError, ~r/content is likely to be a zip file/, fn ->
+        Transport.IRVE.Validator.validate(path)
+      end
+    end)
   end
 end
