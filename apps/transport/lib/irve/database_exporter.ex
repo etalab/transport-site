22de89c4b8693913fc8ace456ead830b89a8dfb9
Vincent Degove <vincent.degove@beta.gouv.fr>
2026-01-13 22:10:53 +0100

    add file fields

--- apps/transport/lib/irve/database_exporter.ex
+++ apps/transport/lib/irve/database_exporter.ex
@@ -17,12 +17,19 @@ defmodule Transport.IRVE.DatabaseExporter do
       |> Enum.concat(["longitude", "latitude"])
       |> Enum.map(&String.to_atom/1)
 
-    stream = DB.IRVEValidPDC |> select([p], map(p, ^fields)) |> DB.Repo.stream()
+    additionnal_file_fields = [:dataset_datagouv_id, :resource_datagouv_id]
+
+    stream =
+      DB.IRVEValidPDC
+      |> join(:inner, [p], f in DB.IRVEValidFile, on: p.irve_valid_file_id == f.id)
+      |> select([p, f], {map(p, ^fields), map(f, ^additionnal_file_fields)})
+      |> DB.Repo.stream()
 
     {:ok, df} =
       DB.Repo.transact(fn ->
         result =
           stream
+          |> Stream.map(&Map.merge(elem(&1, 0), elem(&1, 1)))
           |> Enum.into([])
           |> Explorer.DataFrame.new()
 
