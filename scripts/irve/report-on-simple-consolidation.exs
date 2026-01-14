bd4344332086a43a74d2dddc5ac23931a3b73c0f
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-01-14 21:51:21 +0100

    Add script to provide actionable report

---
+++ scripts/irve/report-on-simple-consolidation.exs
@@ -0,0 +1,11 @@
+require Explorer.DataFrame, as: DF
+
+Path.join(__DIR__, "../../irve_static_consolidation_v2_report.csv")
+|> DF.from_csv!()
+|> DF.mutate(error_message_trunc: re_replace(error_message, "\n.*", ""))
+|> DF.mutate(error_message_trunc: substring(error_message_trunc, 0, 70))
+|> DF.mutate(error_type_trunc: substring(error_type, 0, 18))
+|> DF.group_by(["status", "error_type_trunc", "error_message_trunc"])
+|> DF.summarise(est_pdc_count: sum(estimated_pdc_count))
+|> DF.sort_by(desc: est_pdc_count)
+|> DF.print(limit: :infinity)
