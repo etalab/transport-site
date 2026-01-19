2b76784377d2dc1a17ebfdfff60ecab81c8caba9
Frédéric Menou <frederic.menou@gmail.com>
2026-01-19 17:02:22 +0100

    NeTEx: Générateur de règles pour Chouette Valid (#4844)

--- apps/transport/lib/enroute/chouette_valid_rulesets_client.ex
+++ apps/transport/lib/enroute/chouette_valid_rulesets_client.ex
@@ -3,22 +3,44 @@ defmodule Transport.EnRoute.ChouetteValidRulesetsClient.Wrapper do
   API client for Chouette Valid rulesets management.
   Documentation: https://enroute.atlassian.net/wiki/spaces/PUBLIC/pages/2761687047/Sprint+123#Manage-rulesets-by-API
   """
+  alias Transport.EnRoute.ChouetteValidRulesetsClient.Slugs
+
   @type ruleset_id :: binary
-  @type slug :: binary
 
   @callback list_rulesets() :: list()
 
-  @callback get_ruleset(slug :: slug()) :: {:ok, map()} | {:error, binary()}
+  @callback get_ruleset(slug :: Slugs.slug()) :: {:ok, map()} | {:error, binary()}
 
-  @callback create_ruleset(definition :: binary(), name :: binary(), slug :: slug()) ::
+  @callback create_ruleset(definition :: binary(), name :: binary(), slug :: Slugs.slug()) ::
               {:ok, ruleset_id()}
               | {:error, binary()}
 
-  @callback update_ruleset(definition :: binary(), name :: binary(), slug :: slug()) ::
+  @callback update_ruleset(definition :: binary(), name :: binary(), slug :: Slugs.slug()) ::
               {:ok, ruleset_id()}
               | {:error, binary()}
 
   @callback delete_ruleset(ruleset_id :: ruleset_id()) :: :ok | {:error, binary()}
+
+  @spec list_versions(slug :: Slugs.slug()) :: list()
+  def list_versions(slug) do
+    reference_slug =
+      if Slugs.versioned_slug?(slug) do
+        slug
+      else
+        "#{slug}:1"
+      end
+
+    impl().list_rulesets()
+    |> Enum.filter(fn %{"slug" => version_slug} -> Slugs.sibling_slugs?(reference_slug, version_slug) end)
+  end
+
+  @spec find_ruleset_id(slug :: Slugs.slug()) :: ruleset_id() | nil
+  def find_ruleset_id(expected_slug) do
+    case impl().get_ruleset(expected_slug) do
+      {:ok, ruleset} -> ruleset["id"]
+      {:error, _} -> nil
+    end
+  end
 
   def impl, do: Application.get_env(:transport, :enroute_rulesets_client)
 end
@@ -101,4 +123,62 @@ defmodule Transport.EnRoute.ChouetteValidRulesetsClient do
   end
 
   defp http_client, do: Transport.Req.impl()
+end
+
+defmodule Transport.EnRoute.ChouetteValidRulesetsClient.Slugs do
+  @moduledoc """
+  Helpers to manipulate rulesets slugs.
+  """
+
+  @type slug :: binary
+
+  @spec check_slug!(slug :: binary()) :: slug()
+  def check_slug!(slug) do
+    if versioned_slug?(slug) do
+      slug
+    else
+      raise "Invalid slug #{slug}"
+    end
+  end
+
+  @spec versioned_slug?(slug :: binary()) :: boolean()
+  @doc """
+  iex> versioned_slug?("pan:profile:1")
+  true
+  iex> versioned_slug?("pan:profile")
+  false
+  """
+  def versioned_slug?(slug) do
+    case parse_slug(slug) do
+      {:ok, _} -> true
+      _ -> false
+    end
+  end
+
+  @spec sibling_slugs?(slug_a :: binary(), slug_b :: binary()) :: boolean()
+  @doc """
+  iex> sibling_slugs?("pan:profile:1", "pan:profile:2")
+  true
+  iex> sibling_slugs?("pan:profile", "pan:profile:2")
+  false
+  iex> sibling_slugs?("pan:profile:1", "pan:exceptions:2")
+  false
+  """
+  def sibling_slugs?(slug_a, slug_b) do
+    case {parse_slug(slug_a), parse_slug(slug_b)} do
+      {{:ok, {basename_a, _}}, {:ok, {basename_b, _}}} -> basename_a == basename_b
+      _ -> false
+    end
+  end
+
+  defp parse_slug(slug) do
+    case Regex.named_captures(~r/(?<basename>.*):(?<version>[0-9]+)$/, slug) do
+      %{"basename" => basename, "version" => version} ->
+        version = String.to_integer(version)
+        {:ok, {basename, version}}
+
+      _ ->
+        :error
+    end
+  end
 end
