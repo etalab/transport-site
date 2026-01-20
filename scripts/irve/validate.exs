9564f352dd7943abe43b6d68587c3b1eb13b8feb
Vincent Degove <vincent.degove@beta.gouv.fr>
2026-01-20 14:38:28 +0100

    Amélioration du script de test de validation (#5267)

--- scripts/irve/validate.exs
+++ scripts/irve/validate.exs
@@ -1,8 +1,10 @@
 test_resources = [
   # https://www.data.gouv.fr/datasets/infrastructures-de-recharge-pour-vehicules-electriques-donnees-ouvertes/
-  %{url: "https://proxy.transport.data.gouv.fr/resource/qualicharge-irve-statique"},
+  # %{url: "https://proxy.transport.data.gouv.fr/resource/qualicharge-irve-statique"},
   # https://www.data.gouv.fr/datasets/base-nationale-des-irve-infrastructures-de-recharge-pour-vehicules-electriques/
-  %{url: "https://www.data.gouv.fr/api/1/datasets/r/eb76d20a-8501-400e-b336-d85724de5435"}
+  # %{url: "https://www.data.gouv.fr/api/1/datasets/r/eb76d20a-8501-400e-b336-d85724de5435"},
+  # https://www.data.gouv.fr/datasets/fichier-irve-gireve
+  %{url: "https://www.data.gouv.fr/api/1/datasets/r/61387a4e-22f7-4662-b241-d5cac4dd91fd"}
 ]
 
 cache_dir = Path.join(__DIR__, "../../cache-dir")
@@ -19,33 +21,32 @@ test_resources
     IO.puts("#{path} already there")
   end
 
-  Transport.LogTimeTaken.log_time_taken("Validating downloaded copy of #{url} (#{path})", fn ->
-    df = Explorer.DataFrame.from_csv!(path, infer_schema_length: 0)
+  Transport.LogTimeTaken.log_time_taken("Validating #{path}", fn ->
+    IO.puts("Starting validating downloaded copy of #{url} (#{path})…")
+    df = Transport.IRVE.Validator.validate(path)
 
-    schema = Transport.IRVE.StaticIRVESchema.schema_content()
+    IO.puts("Is the full file valid? #{df |> Transport.IRVE.Validator.full_file_valid?()}")
 
-    # example of pre-processing to ensure Qualicharge resource follows the
-    # exact requirement (`true` or `false`, no other strings)
-    df =
-      Explorer.DataFrame.mutate_with(df, fn df ->
-        schema
-        |> Map.fetch!("fields")
-        |> Enum.filter(fn %{"type" => type} -> type == "boolean" end)
-        |> Enum.map(fn %{"name" => name} ->
-          {
-            name,
-            df[name]
-            |> Explorer.Series.re_replace(~S/\ATrue\z/, "true")
-            |> Explorer.Series.re_replace(~S/\AFalse\z/, "false")
-          }
-        end)
-        |> Enum.into(%{})
-      end)
-
-    df = Transport.IRVE.Validator.compute_validation(df)
+    IO.puts("Validation summary (how many rows are valid or invalid):")
 
     df["check_row_valid"]
     |> Explorer.Series.frequencies()
     |> IO.inspect(IEx.inspect_opts())
+
+    report_columns = Transport.IRVE.StaticIRVESchema.field_names_list() |> Enum.map(&"check_column_#{&1}_valid")
+
+    columns_with_false =
+      report_columns
+      |> Enum.reject(fn col ->
+        df[col]
+        |> Explorer.Series.all?()
+      end)
+
+    IO.puts("Columns with at least one invalid value: #{inspect(columns_with_false)}")
+
+    report_path = Path.rootname(path) <> "-validation-report.csv"
+    IO.puts("Writing validation report to #{report_path}")
+
+    df |> Explorer.DataFrame.to_csv!(report_path)
   end)
 end)
