3c38db9551ba9328f613155124be9030cc663152
Vincent Degove <vincent.degove@beta.gouv.fr>
2026-01-20 11:58:05 +0100

    Focus on Gireve file

--- scripts/irve/validate.exs
+++ scripts/irve/validate.exs
@@ -1,8 +1,10 @@
 test_resources = [
   # https://www.data.gouv.fr/datasets/infrastructures-de-recharge-pour-vehicules-electriques-donnees-ouvertes/
-  %{url: "https://proxy.transport.data.gouv.fr/resource/qualicharge-irve-statique"},
+  # %{url: "https://proxy.transport.data.gouv.fr/resource/qualicharge-irve-statique"},
   # https://www.data.gouv.fr/datasets/base-nationale-des-irve-infrastructures-de-recharge-pour-vehicules-electriques/
-  %{url: "https://www.data.gouv.fr/api/1/datasets/r/eb76d20a-8501-400e-b336-d85724de5435"}
+  # %{url: "https://www.data.gouv.fr/api/1/datasets/r/eb76d20a-8501-400e-b336-d85724de5435"},
+  # https://www.data.gouv.fr/datasets/fichier-irve-gireve
+  %{url: "https://www.data.gouv.fr/api/1/datasets/r/61387a4e-22f7-4662-b241-d5cac4dd91fd"}
 ]
 
 cache_dir = Path.join(__DIR__, "../../cache-dir")
@@ -25,5 +27,11 @@ test_resources
     df["check_row_valid"]
     |> Explorer.Series.frequencies()
     |> IO.inspect(IEx.inspect_opts())
+
+    if Transport.IRVE.Validator.full_file_valid?(df) do
+      IO.puts("All rows are valid!")
+    else
+      IO.puts("Some rows are invalid.")
+    end
   end)
 end)
