d94fbb2a57db1d6dc6b8946a39f511e31139de4e
Vincent Degove <vincent.degove@beta.gouv.fr>
2026-01-26 11:02:43 +0100

    Log start of simple consolidation job

--- apps/transport/lib/jobs/irve_simple_consolidation_job.ex
+++ apps/transport/lib/jobs/irve_simple_consolidation_job.ex
@@ -4,8 +4,12 @@ defmodule Transport.Jobs.IRVESimpleConsolidationJob do
   """
   use Oban.Worker, unique: [period: {1, :days}], tags: ["irve"], max_attempts: 3
 
+  require Logger
+
   @impl Oban.Worker
   def perform(%Oban.Job{}) do
+    Logger.info("Starting IRVE Simple Consolidation Job")
+
     Transport.IRVE.SimpleConsolidation.process()
   end
 end
