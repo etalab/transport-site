b45b8fa233366fb016b5e91a3646fb4e2d902b42
Antoine Augusti <antoine.augusti@gmail.com>
2026-01-28 14:02:19 +0100

    Recherche : pouvoir chercher par sous-type de données

--- apps/transport/lib/db/dataset_subtype.ex
+++ apps/transport/lib/db/dataset_subtype.ex
@@ -6,6 +6,7 @@ defmodule DB.DatasetSubtype do
   use Ecto.Schema
   use TypedEctoSchema
   import Ecto.Changeset
+  use Gettext, backend: TransportWeb.Gettext
 
   typed_schema "dataset_subtype" do
     field(:parent_type, :string)
@@ -22,4 +23,30 @@ defmodule DB.DatasetSubtype do
     |> validate_required([:parent_type, :slug])
     |> unique_constraint([:parent_type, :slug])
   end
+
+  @doc """
+  Converts a subtype slug to a human-readable string.
+
+  ## Examples
+
+      iex> DB.DatasetSubtype.slug_to_str("urban")
+      "Urbain"
+  """
+  @spec slug_to_str(binary()) :: binary() | nil
+  def slug_to_str(slug) when is_binary(slug) do
+    case slug do
+      "urban" -> dgettext("page-shortlist", "Urban")
+      "intercity" -> dgettext("page-shortlist", "Intercity")
+      "school" -> dgettext("page-shortlist", "School transport")
+      "seasonal" -> dgettext("page-shortlist", "Seasonal")
+      "zonal_drt" -> dgettext("page-shortlist", "Demand responsive transport")
+      "bicycle" -> dgettext("page-shortlist", "Bicycle")
+      "scooter" -> dgettext("page-shortlist", "Scooter")
+      "carsharing" -> dgettext("page-shortlist", "Carsharing")
+      "moped" -> dgettext("page-shortlist", "Moped")
+      "freefloating" -> dgettext("page-shortlist", "Free-floating")
+    end
+  end
+
+  def slug_to_str(_), do: nil
 end
