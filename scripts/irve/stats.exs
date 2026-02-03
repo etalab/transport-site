81258478229459e9745baec9481b0569c1809683
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-02-03 13:58:57 +0100

    Mix format

--- scripts/irve/stats.exs
+++ scripts/irve/stats.exs
@@ -8,8 +8,8 @@ sources = [
    "consolidation-transport-avec-doublons-irve-statique.csv", :resource_datagouv_id},
   {"https://proxy.transport.data.gouv.fr/resource/consolidation-nationale-irve-statique-brute-v1",
    "consolidation-nationale-irve-statique-brute-v1.csv", :original_resource_id},
-  {"https://www.data.gouv.fr/fr/datasets/r/eb76d20a-8501-400e-b336-d85724de5435",
-   "consolidation-data-gouv.csv", :datagouv_resource_id},
+  {"https://www.data.gouv.fr/fr/datasets/r/eb76d20a-8501-400e-b336-d85724de5435", "consolidation-data-gouv.csv",
+   :datagouv_resource_id}
   # # generate with `mix run dump-simple-consolidation.exs`
   # {:on_disk, "simple-consolidation.csv", :datagouv_resource_id}
 ]
@@ -62,6 +62,7 @@ defmodule Stats do
   def compute_stats({url_or_atom, identifier, _resource_id_column}) do
     local_path = local_path(identifier)
     maybe_download(url_or_atom, local_path)
+
     Stats.compute(local_path)
     |> Map.put(:path, local_path |> Path.basename())
   end
