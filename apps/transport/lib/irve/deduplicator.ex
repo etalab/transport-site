3a039d7e236adfc694107823f5ae85fd6e289dd1
Vincent Degove <vincent.degove@beta.gouv.fr>
2026-02-17 14:41:30 +0100

    Add MAJ rule

--- apps/transport/lib/irve/deduplicator.ex
+++ apps/transport/lib/irve/deduplicator.ex
@@ -9,30 +9,73 @@ defmodule Transport.IRVE.Deduplicator do
   @doc """
   Takes in entry a dataframe of IRVE valid PDCs,
   and returns a dataframe with an additional column "deduplication_status" with information about the duplicates.
+  Deduplication is done on the "id_pdc_itinerance" column.
+  There are multiple filters applied, and how a filter works is tricky (and depends of the previous filter):
+  - Unique: if a pdc is unique, it’s written in the deduplication_status column.
+  - Date_maj: for the non uniques one, we look at the max date_maj for each group of duplicates.
+    - If there is only one most recent date_maj, we keep it and mark others as duplicates.
+  - Datagouv_last_modified: if we cannot decide, then for dups that have the max date_maj,
+    we mark as kept the one with the most recent datagouv_last_modified, and the others as duplicates.
+
   Values of the additional column:
   - unique
+  - kept_because_date_maj_more_recent
+  - removed_because_date_maj_not_more_recent
   - kept_because_resource_more_recent
   - removed_because_resource_not_more_recent
   """
   def add_duplicates_column(df) do
     # TODO at one point: deal with non_concerné and such.
     Explorer.DataFrame.group_by(df, "id_pdc_itinerance")
-    # writes "unique" if unique, nil otherwise
-    |> unique_filter()
-    # last chance filter based on datagouv_last_modified, only for the non-unique ones
-    |> datagouv_last_modified_filter()
-    |> Explorer.DataFrame.discard("similar_pdc_count")
-    |> Explorer.DataFrame.discard("max_datagouv_last_modified")
+    |> unique_rule()
+    |> date_maj_rule()
+    |> datagouv_last_modified_rule()
     |> Explorer.DataFrame.ungroup()
   end
 
-  def unique_filter(df) do
+  def discard_duplicates(df) do
+    df
+    |> Explorer.DataFrame.filter(
+      deduplication_status in ["unique", "kept_because_date_maj_more_recent", "kept_because_resource_more_recent"]
+    )
+    |> Explorer.DataFrame.discard("deduplication_status")
+  end
+
+  def unique_rule(df) do
     df
     |> Explorer.DataFrame.mutate(similar_pdc_count: count(datagouv_resource_id))
     |> Explorer.DataFrame.mutate(deduplication_status: if(similar_pdc_count == 1, do: "unique"))
+    |> Explorer.DataFrame.discard("similar_pdc_count")
   end
 
-  def datagouv_last_modified_filter(df) do
+  def date_maj_rule(df) do
+    df
+    |> Explorer.DataFrame.mutate(max_date_maj: max(date_maj))
+    |> Explorer.DataFrame.mutate(is_max_date_maj: date_maj == max_date_maj)
+    |> Explorer.DataFrame.group_by(["id_pdc_itinerance", "is_max_date_maj"])
+    |> Explorer.DataFrame.mutate(count_is_max_date_maj: count(datagouv_resource_id))
+    |> Explorer.DataFrame.mutate(
+      deduplication_status:
+        cond do
+          is_not_nil(deduplication_status) ->
+            deduplication_status
+
+          count_is_max_date_maj > 1 ->
+            nil
+
+          is_max_date_maj and count_is_max_date_maj == 1 ->
+            "kept_because_date_maj_more_recent"
+
+          not is_max_date_maj and count_is_max_date_maj == 1 ->
+            "removed_because_date_maj_not_more_recent"
+        end
+    )
+    |> Explorer.DataFrame.discard("max_date_maj")
+    |> Explorer.DataFrame.discard("count_is_max_date_maj")
+    |> Explorer.DataFrame.group_by("id_pdc_itinerance")
+  end
+
+  def datagouv_last_modified_rule(df) do
     df
     |> Explorer.DataFrame.mutate(max_datagouv_last_modified: max(datagouv_last_modified))
     |> Explorer.DataFrame.mutate(
@@ -48,11 +91,6 @@ defmodule Transport.IRVE.Deduplicator do
             "removed_because_resource_not_more_recent"
         end
     )
-  end
-
-  def discard_duplicates(df) do
-    df
-    |> Explorer.DataFrame.filter(deduplication_status in ["unique", "kept_because_more_recent"])
-    |> Explorer.DataFrame.discard("deduplication_status")
+    |> Explorer.DataFrame.discard("max_datagouv_last_modified")
   end
 end
--- apps/transport/test/transport/irve/deduplicator_test.exs
+++ apps/transport/test/transport/irve/deduplicator_test.exs
@@ -26,8 +26,7 @@ defmodule Transport.IRVE.DeduplicatorTest do
              },
              %{
                "datagouv_resource_id" => "resource-1",
-               #               "deduplication_status" => "kept_because_date_maj_more_recent",
-               "deduplication_status" => "removed_because_resource_not_more_recent",
+               "deduplication_status" => "kept_because_date_maj_more_recent",
                "id_pdc_itinerance" => "FRS31DUPLICATE2"
              },
              %{
@@ -37,8 +36,7 @@ defmodule Transport.IRVE.DeduplicatorTest do
              },
              %{
                "datagouv_resource_id" => "resource-2",
-               #              "deduplication_status" => "removed_because_date_maj_not_more_recent",
-               "deduplication_status" => "kept_because_resource_more_recent",
+               "deduplication_status" => "removed_because_date_maj_not_more_recent",
                "id_pdc_itinerance" => "FRS31DUPLICATE2"
              },
              %{
@@ -58,7 +56,7 @@ defmodule Transport.IRVE.DeduplicatorTest do
     first_resource = %{
       "datagouv_dataset_id" => "dataset-1",
       "datagouv_resource_id" => "resource-1",
-      "datagouv_last_modified" => DateTime.new!(~D[2026-02-01], ~T[10:30:00.000], "Etc/UTC")
+      "datagouv_last_modified" => DateTime.new!(~D[2026-02-15], ~T[10:30:00.000], "Etc/UTC")
     }
 
     first_resource_content =
@@ -84,35 +82,36 @@ defmodule Transport.IRVE.DeduplicatorTest do
     second_resource = %{
       "datagouv_dataset_id" => "dataset-2",
       "datagouv_resource_id" => "resource-2",
-      "datagouv_last_modified" => DateTime.new!(~D[2026-02-02], ~T[14:15:00.000], "Etc/UTC")
+      "datagouv_last_modified" => DateTime.new!(~D[2026-02-17], ~T[14:15:00.000], "Etc/UTC")
     }
 
     second_resource_content =
       [
         %{
           "id_pdc_itinerance" => "FRS31DUPLICATE1",
-          "date_maj" => ~D[2026-02-02]
+          "date_maj" => ~D[2026-02-01]
         },
         %{
           "id_pdc_itinerance" => "FRS31DUPLICATE2",
-          "date_maj" => ~D[2025-02-02]
+          "date_maj" => ~D[2026-01-01]
         }
       ]
       |> Enum.map(&Map.merge(&1, second_resource))
 
-    # Third resource - 1 entry, older duplicate
+    # Third resource - 1 entry, same date_maj than first resource, but has an older datagouv_last_modified.
+    # Should be removed.
 
     third_resource = %{
       "datagouv_dataset_id" => "dataset-3",
       "datagouv_resource_id" => "resource-3",
-      "datagouv_last_modified" => DateTime.new!(~D[2026-01-01], ~T[09:00:00.000], "Etc/UTC")
+      "datagouv_last_modified" => DateTime.new!(~D[2026-02-02], ~T[09:00:00.000], "Etc/UTC")
     }
 
     third_resource_content =
       [
         %{
           "id_pdc_itinerance" => "FRS31DUPLICATE1",
-          "date_maj" => ~D[2026-01-01]
+          "date_maj" => ~D[2026-02-01]
         }
       ]
       |> Enum.map(&Map.merge(&1, third_resource))
