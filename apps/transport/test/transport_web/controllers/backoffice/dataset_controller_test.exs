1e4632d5c9527ea72dbe657124df32e8db0258cb
Antoine Augusti <antoine.augusti@gmail.com>
2026-03-16 11:17:21 +0100

    Backoffice dataset : refresh de l'index de recherche en mémoire

--- apps/transport/test/transport_web/controllers/backoffice/dataset_controller_test.exs
+++ apps/transport/test/transport_web/controllers/backoffice/dataset_controller_test.exs
@@ -9,6 +9,7 @@ defmodule TransportWeb.Backoffice.DatasetControllerTest do
 
   setup do
     Ecto.Adapters.SQL.Sandbox.checkout(DB.Repo)
+    Ecto.Adapters.SQL.Sandbox.allow(DB.Repo, self(), Process.whereis(Transport.DatasetIndex))
     # Using the real implementation for the moment, then it falls back on `HTTPoison.Mock`
     Mox.stub_with(Datagouvfr.Client.Datasets.Mock, Datagouvfr.Client.Datasets.External)
     :ok
