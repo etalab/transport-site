d7a2832525464a4e4758393d16db60e537be7807
Frédéric Menou <frederic.menou@gmail.com>
2026-03-25 13:22:57 +0100

    Update apps/transport/lib/netex/french_profile/v1.ex
    Co-authored-by: Thibaut Barrère <thibaut.barrere@gmail.com>

--- apps/transport/lib/netex/french_profile/v1.ex
+++ apps/transport/lib/netex/french_profile/v1.ex
@@ -2,7 +2,7 @@ defmodule Transport.NeTEx.FrenchProfile.V1 do
   @moduledoc """
   Definition of the validation rules for the French NeTEx profile.
 
-  This definition is still work in progress. It's versionned to track progress
+  This definition is still work in progress. It's versioned to track progress
   and adapt any relevant user interface.
 
   According to <https://bitbucket.org/enroute-mobi/netex/src/8099b6ebb4327f32cbc1f266099d966fcafbb761/lib/netex/source.rb#lines-219>,
