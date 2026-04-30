ba577eb3090be12173d719dbac0a6ab334fc5742
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-04-30 10:11:26 +0200

    Mise à jour majeure stack javascript (tooling & build) (#5493)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- generate_deps_changelogs.exs
+++ generate_deps_changelogs.exs
@@ -1,18 +1,21 @@
 # Run with:
 # elixir generate_deps_changelogs.exs
 #
-# This is a quick script to generate changelogs on screen, comparing `mix.lock` on current branch vs `master`.
-# It makes it easier to review changes provided by a given PR. Yes, dependabot exists,
-# but does not support grouped updates (https://github.com/dependabot/dependabot-core/issues/1190)
-# and does not work well on an existing PR.
+# This is a quick script to generate changelogs on screen, comparing `mix.lock` and `yarn.lock`
+# on current branch vs `master`. It makes it easier to review changes provided by a given PR.
+# Yes, dependabot exists, but does not support grouped updates
+# (https://github.com/dependabot/dependabot-core/issues/1190) and does not work well on an existing PR.
 #
 # Already used in the past, and if useful on the long run, we'll create a GitHub action workflow to run
 # and automatically edit the PR description with this.
 #
 
+Mix.install([:jason, :req])
+
 defmodule Scanner do
   def scan(mix_lock_content) do
-    {lock_deps, []} = mix_lock_content |> Code.eval_string()
+    {{lock_deps, []}, _diagnostics} =
+      Code.with_diagnostics(fn -> Code.eval_string(mix_lock_content) end)
 
     lock_deps
     |> Enum.map(fn {entry, data} ->
@@ -32,15 +35,14 @@ end
 master = Scanner.scan(master_mix_lock)
 current = Scanner.scan(File.read!("./mix.lock"))
 
-IO.puts "### Changelogs\n"
 IO.puts "(initially generated with `elixir generate_deps_changelogs.exs`)\n"
+IO.puts "### Elixir dependencies\n"
 
 master
 |> Enum.each(fn {dep, old} ->
   new = Keyword.get(current, dep)
 
-  diff =
-    case {old, new} do
+  case {old, new} do
       {{:hex, v1}, {:hex, v2}} ->
         if v1 != v2 do
           IO.puts("* https://diff.hex.pm/diff/#{dep}/#{v1}..#{v2}")
@@ -57,11 +59,74 @@ master
             IO.puts "* From #{repo}/commit/#{sha1} to #{repo_next}/commit/#{sha2}"
           end
         end
-      {{:hex, v1}, {:git, repo, sha2}} ->
+      {{:hex, _v1}, {:git, repo, sha2}} ->
         IO.puts("* Now using git-version #{repo} @ #{sha2}")
-      {{:git, repo, sha1}, {:hex, v2}} ->
+      {{:git, repo, sha1}, {:hex, _v2}} ->
         IO.puts("* Now using https://hex.pm/packages/#{dep} instead of #{repo} @ #{sha1}")
       {{:hex, v1}, nil} ->
         IO.puts("* REMOVED: #{dep}@#{v1}")
     end
+end)
+
+defmodule YarnScanner do
+  @path "apps/transport/client"
+
+  def at(:local),
+    do: parse(File.read!("#{@path}/yarn.lock"), File.read!("#{@path}/package.json"))
+
+  def at(ref) do
+    {pj, 0} = System.cmd("git", ["show", "#{ref}:#{@path}/package.json"])
+    {yl, 0} = System.cmd("git", ["show", "#{ref}:#{@path}/yarn.lock"])
+    parse(yl, pj)
+  end
+
+  def diff_url(name, v1, v2) do
+    with {:ok, %{status: 200, body: body}} <- Req.get("https://registry.npmjs.org/#{name}"),
+         url <- get_in(body, ["repository", "url"]) || "",
+         [_, owner, repo] <- Regex.run(~r{github\.com[/:]([^/]+)/([^/.]+)}, url) do
+      "https://github.com/#{owner}/#{repo}/compare/v#{v1}...v#{v2}"
+    else
+      _ -> "https://www.npmjs.com/package/#{name}?activeTab=versions"
+    end
+  end
+
+  defp parse(lock, json) do
+    direct =
+      json
+      |> Jason.decode!()
+      |> Map.take(~w(dependencies devDependencies resolutions))
+      |> Map.values()
+      |> Enum.flat_map(&Map.keys/1)
+      |> MapSet.new()
+
+    lock
+    |> String.split("\n\n")
+    |> Enum.reduce(%{}, fn block, acc ->
+      with [_, name] <- Regex.run(~r/^"?((?:@[^\/"]+\/)?[^@"\s]+)@/m, block),
+           true <- MapSet.member?(direct, name),
+           [_, ver] <- Regex.run(~r/version "([^"]+)"/, block) do
+        Map.put(acc, name, ver)
+      else
+        _ -> acc
+      end
+    end)
+  end
+end
+
+IO.puts "\n### JS dependencies (top-level only)"
+IO.puts "(may include duplicates, since we bundle multiple versions at times)\n"
+yarn_master = YarnScanner.at("master")
+yarn_current = YarnScanner.at(:local)
+
+(Map.keys(yarn_master) ++ Map.keys(yarn_current))
+|> Enum.uniq()
+|> Enum.sort()
+|> Enum.each(fn name ->
+  case {yarn_master[name], yarn_current[name]} do
+    {v1, v2} when not is_nil(v1) and not is_nil(v2) and v1 != v2 ->
+      IO.puts "* #{YarnScanner.diff_url(name, v1, v2)}"
+    {nil, v2} when not is_nil(v2) -> IO.puts "* ADDED: #{name}@#{v2}"
+    {v1, nil} when not is_nil(v1) -> IO.puts "* REMOVED: #{name}@#{v1}"
+    _ -> :ok
+  end
 end)
