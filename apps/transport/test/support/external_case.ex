bb1e9a437d6caa70e317cee0dc13e5b0cd6aebb7
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-07-12 14:12:08 +0200

    Remove references to exvcr in ExternalCase

--- apps/transport/test/support/external_case.ex
+++ apps/transport/test/support/external_case.ex
@@ -1,14 +1,12 @@
 defmodule TransportWeb.ExternalCase do
   @moduledoc """
-  This module defines the test case to be used by
-  test that require to mock external API calls.
+  Test case for tests that exercise external API calls (now mocked via Mox).
   """
 
   use ExUnit.CaseTemplate
 
   using do
     quote do
-      use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney, options: [clear_mock: true]
       import Plug.Test
     end
   end
