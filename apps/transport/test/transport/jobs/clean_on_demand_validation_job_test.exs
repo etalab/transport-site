51e3a06d401779bf7b23976fb0272802636803ff
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-07-28 20:52:41 +0200

    DRY tests before adding 2 more

--- apps/transport/test/transport/jobs/clean_on_demand_validation_job_test.exs
+++ apps/transport/test/transport/jobs/clean_on_demand_validation_job_test.exs
@@ -17,41 +17,18 @@ defmodule Transport.Test.Transport.Jobs.CleanOnDemandValidationJobTest do
     two_month_ago = today |> DateTime.add(-60, :day)
     three_months_ago = today |> DateTime.add(-90, :day)
 
-    insert(:multi_validation,
-      result: %{"valid" => true},
-      binary_result: <<1>>,
-      oban_args: %{"foo" => "bar"},
-      inserted_at: last_week
-    )
-
-    insert(:multi_validation,
-      result: %{"valid" => true},
-      binary_result: <<1>>,
-      oban_args: %{"foo" => "bar"},
-      inserted_at: last_month
-    )
-
-    insert(:multi_validation,
-      result: %{"valid" => true},
-      binary_result: <<1>>,
-      oban_args: %{"foo" => "bar"},
-      inserted_at: last_month
-    )
-
-    insert(:multi_validation,
-      result: %{"valid" => true},
-      binary_result: <<1>>,
-      oban_args: %{"foo" => "bar"},
-      inserted_at: three_months_ago
-    )
+    insert_on_demand_validation(inserted_at: last_week)
+    insert_on_demand_validation(inserted_at: last_month)
+    insert_on_demand_validation(inserted_at: last_month)
+    insert_on_demand_validation(inserted_at: three_months_ago)
 
     # inserted more than 30 days ago, but already cleaned => should not be included
-    insert(:multi_validation, result: nil, binary_result: nil, oban_args: %{"foo" => "bar"}, inserted_at: two_month_ago)
+    insert_on_demand_validation(result: nil, binary_result: nil, inserted_at: two_month_ago)
 
     assert :ok == perform_job(CleanOnDemandValidationJob, %{})
 
-    last_month_date = last_month |> DateTime.to_date() |> Date.to_iso8601()
-    three_months_ago_date = three_months_ago |> DateTime.to_date() |> Date.to_iso8601()
+    last_month_date = last_month |> to_iso_date()
+    three_months_ago_date = three_months_ago |> to_iso_date()
 
     assert [
              %Oban.Job{
@@ -71,56 +48,49 @@ defmodule Transport.Test.Transport.Jobs.CleanOnDemandValidationJobTest do
     test_datetime = DateTime.utc_now() |> DateTime.add(-31, :day)
     other_datetime = test_datetime |> DateTime.add(-1, :day)
 
-    mv =
-      insert(:multi_validation,
-        oban_args: %{"foo" => "bar"},
-        result: %{"result" => 42},
-        binary_result: <<42>>,
-        data_vis: %{"data_vis" => 42},
-        inserted_at: test_datetime
-      )
+    %{id: mv_id} = insert_on_demand_validation(inserted_at: test_datetime)
 
     # multi_validation created on another day: should be left untouched
-    %{id: mv2_id} =
-      insert(:multi_validation,
-        oban_args: %{"foo" => "bar"},
-        result: %{"result" => 42},
-        binary_result: <<42>>,
-        data_vis: %{"data_vis" => 42},
-        inserted_at: other_datetime
-      )
+    %{id: mv2_id} = insert_on_demand_validation(inserted_at: other_datetime)
 
     # multi_validation with `oban_args = nil`: should be left untouched
-    %{id: mv3_id} =
-      insert(:multi_validation,
-        oban_args: nil,
-        result: %{"result" => 42},
-        binary_result: <<42>>,
-        data_vis: %{"data_vis" => 42},
-        inserted_at: test_datetime
-      )
+    %{id: mv3_id} = insert_on_demand_validation(oban_args: nil, inserted_at: test_datetime)
 
-    assert :ok ==
-             perform_job(CleanOnDemandValidationJob, %{
-               "date" => test_datetime |> DateTime.to_date() |> Date.to_iso8601()
-             })
+    assert :ok == archive_day(test_datetime)
 
     # `result`, `binary_result` and `data_vis` have been cleaned
-    %DB.MultiValidation{result: nil, binary_result: nil, data_vis: nil} =
-      load_multivalidation(mv.id)
+    assert %DB.MultiValidation{result: nil, binary_result: nil, data_vis: nil} = load_multivalidation(mv_id)
 
     # Other records have not changed
-    %DB.MultiValidation{result: %{"result" => 42}, binary_result: <<42>>, data_vis: %{"data_vis" => 42}} =
-      load_multivalidation(mv2_id)
+    assert %DB.MultiValidation{result: %{"result" => 42}, binary_result: <<42>>, data_vis: %{"data_vis" => 42}} =
+             load_multivalidation(mv2_id)
 
-    %DB.MultiValidation{result: %{"result" => 42}, binary_result: <<42>>, data_vis: %{"data_vis" => 42}} =
-      load_multivalidation(mv3_id)
+    assert %DB.MultiValidation{result: %{"result" => 42}, binary_result: <<42>>, data_vis: %{"data_vis" => 42}} =
+             load_multivalidation(mv3_id)
   end
 
   test "does not try to archive if not past the retention period" do
     assert {:cancel, "Cannot archive rows if they are not older than 30 days"} =
              perform_job(CleanOnDemandValidationJob, %{"date" => Date.utc_today() |> Date.to_iso8601()})
   end
+
+  # An on-demand validation is attached to neither a resource nor a resource history
+  defp insert_on_demand_validation(attrs) do
+    defaults = [
+      oban_args: %{"foo" => "bar"},
+      result: %{"result" => 42},
+      binary_result: <<42>>,
+      data_vis: %{"data_vis" => 42}
+    ]
+
+    insert(:multi_validation, Keyword.merge(defaults, attrs))
+  end
+
+  defp archive_day(datetime) do
+    perform_job(CleanOnDemandValidationJob, %{"date" => to_iso_date(datetime)})
+  end
+
+  defp to_iso_date(datetime), do: datetime |> DateTime.to_date() |> Date.to_iso8601()
 
   defp load_multivalidation(mv_id) do
     DB.MultiValidation.base_query(include_result: true, include_binary_result: true)
