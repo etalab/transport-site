b20b24426825d35bfd73c8018024be0da4421131
Thibaut Barrère <thibaut.barrere@gmail.com>
2026-09-03 15:10:56 +0200

    Fix keyword order used in assertions

--- apps/transport/test/transport_web/plugs/rate_limiter_test.exs
+++ apps/transport/test/transport_web/plugs/rate_limiter_test.exs
@@ -26,6 +26,7 @@ defmodule TransportWeb.Plugs.RateLimiterTest do
                  block_user_agent_keywords: "",
                  allow_user_agents: ""
                )
+               |> Enum.sort()
 
       assert [
                allow_user_agents: ["bar", "baz"],
@@ -37,6 +38,7 @@ defmodule TransportWeb.Plugs.RateLimiterTest do
                  block_user_agent_keywords: "foo|bar",
                  allow_user_agents: "bar|baz"
                )
+               |> Enum.sort()
     end
 
     test "with keywords" do
